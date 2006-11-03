5ac8feb0a5d8ede6f4f3b28cd0fc4e28955e81f1
almgren <almgren>
2006-11-03 19:00:18 +0000

    Replace make_rhohalf.f90 by make_at_halftime.f90 so that we can call it for S as well.

---
+++ Source/make_at_halftime.f90
@@ -0,0 +1,94 @@
+module phihalf_module
+
+  use bl_types
+  use bl_constants_module
+  use bc_module
+  use variables
+  use multifab_module
+
+  implicit none
+
+contains
+
+
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+   subroutine make_at_halftime (phihalf,sold,snew,in_comp,out_comp)
+
+      type(multifab) , intent(inout) :: phihalf
+      type(multifab) , intent(in   ) :: sold
+      type(multifab) , intent(in   ) :: snew
+      integer        , intent(in   ) :: in_comp,out_comp
+
+      real(kind=dp_t), pointer:: rhp(:,:,:,:)
+      real(kind=dp_t), pointer:: rop(:,:,:,:)
+      real(kind=dp_t), pointer:: rnp(:,:,:,:)
+      integer :: lo(phihalf%dim),hi(phihalf%dim),ng_h,ng_o,dm
+      integer :: i
+
+      dm = phihalf%dim
+      ng_h = phihalf%ng
+      ng_o = sold%ng
+
+      do i = 1, phihalf%nboxes
+         if ( multifab_remote(phihalf, i) ) cycle
+         rhp => dataptr(phihalf, i)
+         rop => dataptr(sold, i)
+         rnp => dataptr(snew, i)
+         lo =  lwb(get_box(phihalf, i))
+         hi =  upb(get_box(phihalf, i))
+         select case (dm)
+            case (2)
+              call make_at_halftime_2d(rhp(:,:,1,out_comp),rop(:,:,1,in_comp),rnp(:,:,1,in_comp),&
+                                   lo,hi,ng_h,ng_o)
+            case (3)
+              call make_at_halftime_3d(rhp(:,:,:,out_comp),rop(:,:,:,in_comp),rnp(:,:,:,in_comp),&
+                                   lo,hi,ng_h,ng_o)
+         end select
+      end do
+
+   end subroutine make_at_halftime
+
+   subroutine make_at_halftime_2d (phihalf,phiold,phinew,lo,hi,ng_half,ng_old)
+
+      implicit none
+
+      integer         , intent(in   ) :: lo(:), hi(:), ng_half, ng_old
+      real (kind=dp_t), intent(  out) :: phihalf(lo(1)-ng_half:,lo(2)-ng_half:)
+      real (kind=dp_t), intent(in   ) ::  phiold(lo(1)-ng_old :,lo(2)-ng_old :)
+      real (kind=dp_t), intent(in   ) ::  phinew(lo(1)-ng_old :,lo(2)-ng_old :)
+
+!     Local variables
+      integer          :: i, j
+
+      do j = lo(2),hi(2)
+      do i = lo(1),hi(1)
+        phihalf(i,j) = HALF * (phiold(i,j) + phinew(i,j))
+      end do
+      end do
+ 
+   end subroutine make_at_halftime_2d
+
+   subroutine make_at_halftime_3d (phihalf,phiold,phinew,lo,hi,ng_half,ng_old)
+
+      implicit none
+
+      integer         , intent(in   ) :: lo(:), hi(:), ng_half, ng_old
+      real (kind=dp_t), intent(  out) :: phihalf(lo(1)-ng_half:,lo(2)-ng_half:,lo(3)-ng_half:)
+      real (kind=dp_t), intent(in   ) ::  phiold(lo(1)-ng_old:,lo(2)-ng_old:,lo(3)-ng_old:)
+      real (kind=dp_t), intent(in   ) ::  phinew(lo(1)-ng_old:,lo(2)-ng_old:,lo(3)-ng_old:)
+
+!     Local variables
+      integer          :: i, j, k
+
+      do k = lo(3),hi(3)
+      do j = lo(2),hi(2)
+      do i = lo(1),hi(1)
+        phihalf(i,j,k) = HALF * (phiold(i,j,k) + phinew(i,j,k))
+      end do
+      end do
+      end do
+ 
+   end subroutine make_at_halftime_3d
+
+end module phihalf_module
