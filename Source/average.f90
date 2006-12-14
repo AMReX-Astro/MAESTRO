ef86544b2c8eb3ac6cc77760cf06ac74b18194b9
almgren <almgren>
2006-12-14 02:41:51 +0000

    In theory this should make it work for multiple grids, but it doesn't work for multiple grids yet.

--- Source/average.f90
+++ Source/average.f90
@@ -15,15 +15,30 @@ contains
    subroutine average (phi,phibar,dx)
 
       type(multifab) , intent(inout) :: phi
-      real(kind=dp_t), intent(  out) :: phibar(:,:)
+      real(kind=dp_t), intent(  out) :: phibar(0:,:)
       real(kind=dp_t), intent(in   ) :: dx(:)
 
       real(kind=dp_t), pointer:: pp(:,:,:,:)
-      integer :: lo(phi%dim),hi(phi%dim),ng,dm
-      integer :: i
+      integer :: lo(phi%dim),hi(phi%dim),ng,dm,nr
+      integer :: i,k
+      integer        , allocatable :: npts_grid(:), npts_proc(:), npts_tot(:)
+      real(kind=dp_t), allocatable :: vol_grid(:), vol_proc(:), vol_tot(:)
 
       dm = phi%dim
       ng = phi%ng
+      nr = size(phibar,dim=1)
+
+      if (spherical .eq. 1) then
+        allocate(vol_grid(0:nr-1),vol_proc(0:nr-1),vol_tot(0:nr-1))
+        vol_proc(:) = ZERO
+        vol_tot(:)  = ZERO
+      else
+        allocate(npts_grid(0:nr-1),npts_proc(0:nr-1),npts_tot(0:nr-1))
+        npts_proc(:) = 0
+        npts_tot(:)  = 0
+      end if
+
+      phibar(:,:) = ZERO
 
       do i = 1, phi%nboxes
          if ( multifab_remote(phi, i) ) cycle
@@ -32,99 +47,109 @@ contains
          hi =  upb(get_box(phi, i))
          select case (dm)
             case (2)
-              call average_2d(pp(:,:,1,:),phibar,lo,hi,ng)
+              npts_grid(:) = 0
+              call average_2d(pp(:,:,1,:),phibar(lo(2):,:),lo,hi,ng,npts_grid(lo(2):))
+              npts_proc(lo(2):hi(2)) = npts_proc(lo(2):hi(2)) + npts_grid(lo(2):hi(2))
             case (3)
               if (spherical .eq. 1) then
-                call average_3d_sphr(pp(:,:,:,:),phibar,ng,dx)
+                vol_grid(:) = ZERO
+                call average_3d_sphr(pp(:,:,:,:),phibar,lo,hi,ng,dx,vol_grid)
+                vol_proc = vol_proc + vol_grid
               else
-                call average_3d(pp(:,:,:,:),phibar,lo,hi,ng)
+                npts_grid(:) = 0
+                call average_3d(pp(:,:,:,:),phibar(lo(3):,:),lo,hi,ng,npts_grid(lo(3):))
+                npts_proc(lo(3):hi(3)) = npts_proc(lo(3):hi(3)) + npts_grid(lo(3):hi(3))
               end if
          end select
       end do
 
+      if (dm .eq. 2 .or. (dm.eq.3.and.spherical.eq.0)) then
+        call parallel_reduce(npts_tot,npts_proc,MPI_SUM)
+        do k = 0,nr-1
+          phibar(k,:) = phibar(k,:) / dble(npts_tot(k))
+        end do
+        deallocate(npts_grid,npts_proc,npts_tot)
+      else
+        do k = 0,nr-1
+          call parallel_reduce(vol_tot(k),vol_proc(k),MPI_SUM)
+          if (vol_tot(k) .gt. ZERO) then
+            phibar(k,:) = phibar(k,:) / vol_tot(k)
+          else
+            phibar(k,:) = ZERO
+          end if
+        end do
+        deallocate(vol_grid,vol_proc,vol_tot)
+      end if
+
    end subroutine average
 
-   subroutine average_2d (phi,phibar,lo,hi,ng)
+   subroutine average_2d (phi,phibar,lo,hi,ng,npts)
 
       integer         , intent(in   ) :: lo(:), hi(:), ng
       real (kind=dp_t), intent(in   ) :: phi(lo(1)-ng:,lo(2)-ng:,:)
       real (kind=dp_t), intent(  out) :: phibar(lo(2):,:)
+      integer         , intent(  out) :: npts(lo(2):)
 
 !     Local variables
       integer          :: i, j, n
-      real (kind=dp_t) :: denom
 
-      denom = dble(hi(1)-lo(1)+1)
 
       do n = 1,size(phibar,dim=2)
       do j = lo(2),hi(2)
-        phibar(j,n) = zero
+        npts(j) = hi(1)-lo(1)+1
         do i = lo(1),hi(1)
           phibar(j,n) = phibar(j,n) + phi(i,j,n)
         end do
-        phibar(j,n) = phibar(j,n) / denom
       end do
       end do
  
    end subroutine average_2d
 
-   subroutine average_3d (phi,phibar,lo,hi,ng)
+   subroutine average_3d (phi,phibar,lo,hi,ng,npts)
 
       integer         , intent(in   ) :: lo(:), hi(:), ng
       real (kind=dp_t), intent(in   ) :: phi(lo(1)-ng:,lo(2)-ng:,lo(3)-ng:,:)
       real (kind=dp_t), intent(  out) :: phibar(lo(3):,:)
+      integer         , intent(  out) :: npts(lo(3):)
 
 !     Local variables
       integer          :: i, j, k, n
-      real (kind=dp_t) :: denom
-
-      denom = dble((hi(1)-lo(1)+1)*(hi(2)-lo(2)+1))
 
       do n = 1,size(phibar,dim=2)
       do k = lo(3),hi(3)
-        phibar(k,n) = zero
+        npts(k) = (hi(1)-lo(1)+1)*(hi(2)-lo(2)+1)
         do j = lo(2),hi(3)
         do i = lo(1),hi(1)
           phibar(k,n) = phibar(k,n) + phi(i,j,k,n)
         end do
         end do
-        phibar(k,n) = phibar(k,n) / denom
       end do
       end do
  
    end subroutine average_3d
 
-   subroutine average_3d_sphr (phi,phibar,ng,dx)
+   subroutine average_3d_sphr (phi,phibar,lo,hi,ng,dx,sum)
 
-      integer         , intent(in   ) :: ng
-      real (kind=dp_t), intent(in   ) :: phi(1-ng:,1-ng:,1-ng:,:)
-      real (kind=dp_t), intent(  out) :: phibar(:,:)
+      integer         , intent(in   ) :: lo(:),hi(:),ng
+      real (kind=dp_t), intent(in   ) :: phi(lo(1)-ng:,lo(2)-ng:,lo(3)-ng:,:)
+      real (kind=dp_t), intent(  out) :: phibar(0:,:)
       real (kind=dp_t), intent(in   ) :: dx(:)
+      real (kind=dp_t), intent(  out) :: sum(:)
 
 !     Local variables
       integer                       :: i, j, k, n, index
-      integer                       :: nx, ny, nz, nr, nc
+      integer                       :: nr, nc
       real (kind=dp_t)              :: x,y,z,radius,vol
-      real (kind=dp_t), allocatable :: sum(:)
       real (kind=dp_t), parameter   :: fourthirdspi = 4.18879020478639098400_dp_t
-
-      nx = size(phi,dim=1)-2*ng
-      ny = size(phi,dim=2)-2*ng
-      nz = size(phi,dim=3)-2*ng
 
       nr = size(phibar,dim=1)
       nc = size(phibar,dim=2)
 
-      allocate(sum(nr))
-
-      phibar = ZERO
-      sum    = ZERO
-
-      do k = 1,nz
+      do k = lo(3),hi(3)
         z = (dble(k)-HALF)*dx(3) - center(3)
-        do j = 1,nz
+        do j = lo(2),hi(2)
           y = (dble(j)-HALF)*dx(2) - center(2)
-          do i = 1,nz
+          do i = lo(3),hi(3)
             x = (dble(i)-HALF)*dx(1) - center(1)
 
             radius = sqrt(x**2 + y**2 + z**2)
@@ -150,18 +175,6 @@ contains
           end do
         end do
       end do
-
-      do i = 1,nr-1
-        if (sum(i) .gt. ZERO) then
-          do n = 1,nc
-            phibar(i,n) = phibar(i,n) / sum(i)
-          end do
-        else
-          phibar(i,1:nc) = ZERO
-        end if
-      end do
-
-      deallocate(sum)
  
    end subroutine average_3d_sphr
 
--- Source/make_hgrhs.f90
+++ Source/make_hgrhs.f90
@@ -18,50 +18,71 @@ contains
       type(multifab) , intent(inout) :: hgrhs
       type(multifab) , intent(in   ) :: Source
       type(multifab) , intent(in   ) :: gamma1_term
-      real(kind=dp_t), intent(in   ) :: Sbar(:)
-      real(kind=dp_t), intent(in   ) :: div_coeff(:)
+      real(kind=dp_t), intent(in   ) :: Sbar(0:)
+      real(kind=dp_t), intent(in   ) :: div_coeff(0:)
       real(kind=dp_t), intent(in   ) :: dx(:)
       type(box      ), intent(in   ) :: domain
 
-      real(kind=dp_t), pointer:: hp(:,:,:,:),sp(:,:,:,:),gp(:,:,:,:)
-      integer :: lo(Source%dim),hi(Source%dim),dm
-      integer :: i
+      type(multifab)  :: rhs_cc
+      real(kind=dp_t), pointer:: hp(:,:,:,:),sp(:,:,:,:),gp(:,:,:,:),rp(:,:,:,:)
+      integer :: lo(Source%dim),hi(Source%dim)
+      integer :: i,dm,lo_z,hi_z
 
       dm = Source%dim
+      lo_z = domain%lo(dm)
+      hi_z = domain%hi(dm)
+
+      call multifab_build(rhs_cc,Source%la,1,1)
+      call setval(rhs_cc,ZERO,all=.true.)
 
       do i = 1, Source%nboxes
          if ( multifab_remote(Source, i) ) cycle
-         hp => dataptr(hgrhs, i)
+         rp => dataptr(rhs_cc, i)
          sp => dataptr(Source, i)
          gp => dataptr(gamma1_term, i)
          lo =  lwb(get_box(Source, i))
          hi =  upb(get_box(Source, i))
          select case (dm)
             case (2)
-              call make_hgrhs_2d(lo,hi,hp(:,:,1,1),sp(:,:,1,1),gp(:,:,1,1),Sbar,div_coeff)
+              call make_rhscc_2d(lo,hi,rp(:,:,1,1),sp(:,:,1,1),gp(:,:,1,1),Sbar,div_coeff)
             case (3)
-              call make_hgrhs_3d(lo,hi,hp(:,:,:,1),sp(:,:,:,1),gp(:,:,:,1),Sbar,div_coeff,dx)
+              call make_rhscc_3d(lo,hi,rp(:,:,:,1),sp(:,:,:,1),gp(:,:,:,1),Sbar,div_coeff,dx)
+         end select
+      end do
+      call multifab_fill_boundary(rhs_cc)
+
+      call setval(hgrhs,ZERO,all=.true.)
+      do i = 1, Source%nboxes
+         if ( multifab_remote(Source, i) ) cycle
+         hp => dataptr(hgrhs, i)
+         rp => dataptr(rhs_cc, i)
+         lo =  lwb(get_box(Source, i))
+         hi =  upb(get_box(Source, i))
+         select case (dm)
+            case (2)
+              call make_hgrhs_2d(lo,hi,lo_z,hi_z,hp(:,:,1,1),rp(:,:,1,1))
+            case (3)
+              call make_hgrhs_3d(lo,hi,lo_z,hi_z,hp(:,:,:,1),rp(:,:,:,1))
          end select
       end do
 
+      call multifab_destroy(rhs_cc)
+
    end subroutine make_hgrhs
 
-   subroutine make_hgrhs_2d(lo,hi,rhs,Source,gamma1_term,Sbar,div_coeff)
+   subroutine make_rhscc_2d(lo,hi,rhs_cc,Source,gamma1_term,Sbar,div_coeff)
 
       implicit none
 
       integer         , intent(in   ) :: lo(:), hi(:)
-      real (kind=dp_t), intent(  out) :: rhs(lo(1):,lo(2):)  
+      real (kind=dp_t), intent(  out) :: rhs_cc(lo(1)-1:,lo(2)-1:)
       real (kind=dp_t), intent(in   ) :: Source(lo(1):,lo(2):)
       real (kind=dp_t), intent(in   ) :: gamma1_term(lo(1):,lo(2):)  
-      real (kind=dp_t), intent(in   ) :: Sbar(lo(2):)
-      real (kind=dp_t), intent(in   ) :: div_coeff(lo(2):)
+      real (kind=dp_t), intent(in   ) ::      Sbar(0:)
+      real (kind=dp_t), intent(in   ) :: div_coeff(0:)
 
 !     Local variables
       integer :: i, j
-      real (kind=dp_t), allocatable :: rhs_cc(:,:)
-
-      allocate(rhs_cc(lo(1):hi(1),lo(2):hi(2)))
 
       do j = lo(2),hi(2)
       do i = lo(1),hi(1)
@@ -69,41 +90,49 @@ contains
       end do
       end do
 
-!     HACK : THIS ASSUMES PERIODIC AT SIDES AND NO HEATING NEAR TOP OR BOTTOM!!!
-      rhs(:,lo(2)  ) = ZERO
-      rhs(:,hi(2)  ) = ZERO
-      rhs(:,hi(2)+1) = ZERO
-      do j = lo(2)+1, hi(2)-1
-        rhs(lo(1)  ,j) = FOURTH * ( rhs_cc(lo(1),j) + rhs_cc(lo(1),j-1) + &
-                                    rhs_cc(hi(1),j) + rhs_cc(hi(1),j-1) )
-        rhs(hi(1)+1,j) = rhs(lo(1),j)
-        do i = lo(1)+1, hi(1)
-          rhs(i,j) = FOURTH * ( rhs_cc(i,j  ) + rhs_cc(i-1,j  ) &
-                               +rhs_cc(i,j-1) + rhs_cc(i-1,j-1) )
-        enddo
-      enddo
+   end subroutine make_rhscc_2d
 
-      deallocate(rhs_cc)
+   subroutine make_hgrhs_2d(lo,hi,lo_z,hi_z,rhs,rhs_cc)
+
+      implicit none
+
+      integer         , intent(in   ) :: lo(:), hi(:), lo_z, hi_z
+      real (kind=dp_t), intent(  out) :: rhs(lo(1):,lo(2):)  
+      real (kind=dp_t), intent(in   ) :: rhs_cc(lo(1)-1:,lo(2)-1:)
+
+!     Local variables
+      integer :: i, j
+
+      print *,'LO(2) LO_Z ',lo(2),lo_z
+      print *,'HI(2) HI_Z ',hi(2),hi_z
+
+      do j = lo(2),hi(2)+1
+        if (j.ne.lo_z .and. j.ne.(hi_z+1)) then
+          do i = lo(1), hi(1)+1
+            rhs(i,j) = FOURTH * ( rhs_cc(i,j  ) + rhs_cc(i-1,j  ) &
+                                 +rhs_cc(i,j-1) + rhs_cc(i-1,j-1) )
+            if (abs(rhs(i,j)).gt.1540.) print *,'RHS ',i,j,rhs(i,j)
+          enddo
+        end if
+      enddo
 
    end subroutine make_hgrhs_2d
 
-   subroutine make_hgrhs_3d(lo,hi,rhs,Source,gamma1_term,Sbar,div_coeff,dx)
+   subroutine make_rhscc_3d(lo,hi,rhs_cc,Source,gamma1_term,Sbar,div_coeff,dx)
 
       implicit none
 
       integer         , intent(in   ) :: lo(:), hi(:)
-      real (kind=dp_t), intent(  out) :: rhs(lo(1):,lo(2):,lo(3):)  
+      real (kind=dp_t), intent(  out) :: rhs_cc(lo(1)-1:,lo(2)-1:,lo(3)-1:)  
       real (kind=dp_t), intent(in   ) :: Source(lo(1):,lo(2):,lo(3):)  
       real (kind=dp_t), intent(in   ) :: gamma1_term(lo(1):,lo(2):,lo(3):)  
-      real (kind=dp_t), intent(in   ) :: Sbar(lo(3):)
-      real (kind=dp_t), intent(in   ) :: div_coeff(lo(3):)
+      real (kind=dp_t), intent(in   ) :: Sbar(0:)
+      real (kind=dp_t), intent(in   ) :: div_coeff(0:)
       real (kind=dp_t), intent(in   ) :: dx(:)
 
 !     Local variables
       integer :: i, j,k
-      real (kind=dp_t), allocatable :: rhs_cc(:,:,:),Sbar_cart(:,:,:),div_cart(:,:,:)
-
-      allocate(rhs_cc(lo(1):hi(1),lo(2):hi(2),lo(3):hi(3)))
+      real (kind=dp_t), allocatable :: Sbar_cart(:,:,:),div_cart(:,:,:)
 
      if (spherical .eq. 1) then
  
@@ -122,19 +151,6 @@ contains
         end do
        
         deallocate(Sbar_cart,div_cart) 
-
-        ! HACK : THIS ASSUMES OUTFLOW AT ALL SIDES AND NO HEATING NEAR ANY SIDES
-        rhs = ZERO
-        do k = lo(3)+1, hi(3)
-        do j = lo(2)+1, hi(2)
-        do i = lo(1)+1, hi(1)
-          rhs(i,j,k) = EIGHTH * ( rhs_cc(i,j  ,k-1) + rhs_cc(i-1,j  ,k-1) &
-                                 +rhs_cc(i,j-1,k-1) + rhs_cc(i-1,j-1,k-1) &
-                                 +rhs_cc(i,j-1,k  ) + rhs_cc(i-1,j-1,k  ) &
-                                 +rhs_cc(i,j-1,k  ) + rhs_cc(i-1,j-1,k  ) )
-        enddo
-        enddo
-        enddo
        
       else  
        
@@ -146,44 +162,33 @@ contains
         end do 
         end do 
 
-        ! HACK : THIS ASSUMES PERIODIC AT SIDES AND NO HEATING NEAR TOP OR BOTTOM!!!
-        rhs(:,:,lo(3)  ) = ZERO
-        rhs(:,:,hi(3)  ) = ZERO
-        rhs(:,:,hi(3)+1) = ZERO
-        do k = lo(3)+1, hi(3)-1
-        do j = lo(2)+1, hi(2)
-          rhs(lo(1)  ,j,k) = EIGHTH * ( rhs_cc(lo(1),j,k-1) + rhs_cc(lo(1),j-1,k-1) &
-                                       +rhs_cc(lo(1),j,k  ) + rhs_cc(lo(1),j-1,k  ) &
-                                       +rhs_cc(hi(1),j,k-1) + rhs_cc(hi(1),j-1,k-1) &
-                                       +rhs_cc(hi(1),j,k  ) + rhs_cc(hi(1),j-1,k  ) )
-          rhs(hi(1)+1,j,k) = rhs(lo(1),j,k)
-                            
-        enddo
-        do i = lo(1)+1, hi(1)
-          rhs(i,lo(2)  ,k) = EIGHTH * ( rhs_cc(i,lo(2),k-1) + rhs_cc(i-1,lo(2),k-1) &
-                                       +rhs_cc(i,lo(2),k  ) + rhs_cc(i-1,lo(2),k  ) &
-                                       +rhs_cc(i,hi(2),k-1) + rhs_cc(i-1,hi(2),k-1) &
-                                       +rhs_cc(i,hi(2),k  ) + rhs_cc(i-1,hi(2),k  ) )
-          rhs(i,hi(2)+1,k) =  rhs(i,lo(2),k)
-                            
-        enddo
-        rhs(lo(1)  ,lo(2)  ,k) = EIGHTH * ( rhs_cc(lo(1),lo(2),k-1) + rhs_cc(lo(1),lo(2),k) &
-                                           +rhs_cc(hi(1),lo(2),k-1) + rhs_cc(hi(1),lo(2),k) &
-                                           +rhs_cc(lo(1),hi(2),k-1) + rhs_cc(lo(1),hi(2),k) &
-                                           +rhs_cc(hi(1),hi(2),k-1) + rhs_cc(hi(1),hi(2),k) )
-        rhs(hi(1)+1,lo(2)  ,k) = rhs(lo(1),lo(2),k)
-        rhs(lo(1)  ,hi(2)+1,k) = rhs(lo(1),lo(2),k)
-        rhs(hi(1)+1,hi(2)+1,k) = rhs(lo(1),lo(2),k)
-        do j = lo(2)+1, hi(2)
-        do i = lo(1)+1, hi(1)
-          rhs(i,j,k) = EIGHTH * ( rhs_cc(i,j  ,k-1) + rhs_cc(i-1,j  ,k-1) &
-                                 +rhs_cc(i,j-1,k-1) + rhs_cc(i-1,j-1,k-1) &
-                                 +rhs_cc(i,j-1,k  ) + rhs_cc(i-1,j-1,k  ) &
-                                 +rhs_cc(i,j-1,k  ) + rhs_cc(i-1,j-1,k  ) )
-        enddo
-        enddo
-        enddo
       end if
+
+   end subroutine make_rhscc_3d
+
+   subroutine make_hgrhs_3d(lo,hi,lo_z,hi_z,rhs,rhs_cc)
+
+      implicit none
+
+      integer         , intent(in   ) :: lo(:), hi(:), lo_z, hi_z
+      real (kind=dp_t), intent(  out) ::    rhs(lo(1)  :,lo(2)  :,lo(3)  :)  
+      real (kind=dp_t), intent(in   ) :: rhs_cc(lo(1)-1:,lo(2)-1:,lo(3)-1:)  
+
+!     Local variables
+      integer :: i, j,k
+ 
+        do k = lo(3), hi(3)+1
+          if (k.ne.lo_z .and. k.ne.hi_z+1) then
+            do j = lo(2), hi(2)+1
+            do i = lo(1), hi(1)+1
+              rhs(i,j,k) = EIGHTH * ( rhs_cc(i,j  ,k-1) + rhs_cc(i-1,j  ,k-1) &
+                                     +rhs_cc(i,j-1,k-1) + rhs_cc(i-1,j-1,k-1) &
+                                     +rhs_cc(i,j-1,k  ) + rhs_cc(i-1,j-1,k  ) &
+                                     +rhs_cc(i,j-1,k  ) + rhs_cc(i-1,j-1,k  ) )
+            enddo
+            enddo
+          end if
+        enddo
 
    end subroutine make_hgrhs_3d
 
