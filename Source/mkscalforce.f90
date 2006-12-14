416c2156847aa0f04acc3afee734fb30aca3ae97
mzingale <mzingale>
2006-12-14 21:55:31 +0000

    fix up the indexing in the 3d

--- Source/mkscalforce.f90
+++ Source/mkscalforce.f90
@@ -129,44 +129,42 @@ contains
 
     ! compute the source terms for the non-reactive part of the enthalpy equation { w dp0/dr }
 
-    real(kind=dp_t), intent(  out) ::  force(0:,0:,0:)
-    real(kind=dp_t), intent(in   ) ::   wmac(0:,0:,0:)
     integer,         intent(in   ) :: lo(:), hi(:), ng
-    real(kind=dp_t), intent(in   ) :: s(lo(1)-ng:,lo(2)-ng:,lo(3)-ng:,:)
+    real(kind=dp_t), intent(  out) ::  force(lo(1)- 1:,lo(2)- 1:,lo(3)- 1:)
+    real(kind=dp_t), intent(in   ) ::   wmac(lo(1)- 1:,lo(2)- 1:,lo(3)- 1:)
+    real(kind=dp_t), intent(in   ) ::      s(lo(1)-ng:,lo(2)-ng:,lo(3)-ng:,:)
     real(kind=dp_t), intent(in   ) :: dx(:)
     real(kind=dp_t), intent(in   ) :: time
-    real(kind=dp_t), intent(in   ) :: p0_old(:)
-    real(kind=dp_t), intent(in   ) :: p0_new(:)
-    real(kind=dp_t), intent(in   ) :: s0_old(:,:)
-    real(kind=dp_t), intent(in   ) :: s0_new(:,:)
-    real(kind=dp_t), intent(in   ) :: temp0(:)
+    real(kind=dp_t), intent(in   ) :: p0_old(lo(2):)
+    real(kind=dp_t), intent(in   ) :: p0_new(lo(2):)
+    real(kind=dp_t), intent(in   ) :: s0_old(lo(2):,:)
+    real(kind=dp_t), intent(in   ) :: s0_new(lo(2):,:)
+    real(kind=dp_t), intent(in   ) :: temp0(lo(2):)
     real(kind=dp_t), intent(in   ) ::     dr
 
     real(kind=dp_t) :: gradp0,wadv
-    integer :: i,j,k,nx,ny,nz
-
-    nx = size(force,dim=1) - 2
-    ny = size(force,dim=2) - 2
-    nz = size(force,dim=3) - 2
+    integer :: i,j,k
  
     force = ZERO
 
-    do k = 1,nz
-       do j = 1,ny
-       do i = 1,nx
-          wadv = HALF*(wmac(i,j,k)+wmac(i,j,k+1))
-          if (k.eq.1) then
-             gradp0 = HALF * ( p0_old(k+1) + p0_new(k+1) &
-                              -p0_old(k  ) - p0_new(k  ) ) / dr
-          else if (k.eq.nz) then
-             gradp0 = HALF * ( p0_old(k  ) + p0_new(k  ) &
-                              -p0_old(k-1) - p0_new(k-1) ) / dr
-          else
-             gradp0 = FOURTH * ( p0_old(k+1) + p0_new(k+1) &
-                                -p0_old(k-1) - p0_new(k-1) ) / dr
-          end if
-          force(i,j,k) = wadv * gradp0 
-       end do
+    do k = lo(3),hi(3)
+       do j = lo(2),hi(2)
+          do i = lo(1),hi(1)
+
+             wadv = HALF*(wmac(i,j,k)+wmac(i,j,k+1))
+
+             if (k.eq.lo(3)) then
+                gradp0 = HALF * ( p0_old(k+1) + p0_new(k+1) &
+                                 -p0_old(k  ) - p0_new(k  ) ) / dr
+             else if (k.eq.hi(3)) then
+                gradp0 = HALF * ( p0_old(k  ) + p0_new(k  ) &
+                                 -p0_old(k-1) - p0_new(k-1) ) / dr
+             else
+                gradp0 = FOURTH * ( p0_old(k+1) + p0_new(k+1) &
+                                   -p0_old(k-1) - p0_new(k-1) ) / dr
+             end if
+             force(i,j,k) = wadv * gradp0 
+          end do
        end do
     end do
 
@@ -178,61 +176,59 @@ contains
 
     ! compute the source terms for the non-reactive part of the enthalpy equation { w dp0/dr }
 
-    real(kind=dp_t), intent(  out) ::  force(0:,0:,0:)
-    real(kind=dp_t), intent(in   ) ::   umac(0:,0:,0:)
-    real(kind=dp_t), intent(in   ) ::   vmac(0:,0:,0:)
-    real(kind=dp_t), intent(in   ) ::   wmac(0:,0:,0:)
-    real(kind=dp_t), intent(in   ) :: normal(0:,0:,0:,:)
     integer,         intent(in   ) :: lo(:), hi(:), ng
-    real(kind=dp_t), intent(in   ) :: s(lo(1)-ng:,lo(2)-ng:,lo(3)-ng:,:)
+    real(kind=dp_t), intent(  out) ::  force(lo(1)- 1:,lo(2)- 1:,lo(3)-1:)
+    real(kind=dp_t), intent(in   ) ::   umac(lo(1)- 1:,lo(2)- 1:,lo(3)-1:)
+    real(kind=dp_t), intent(in   ) ::   vmac(lo(1)- 1:,lo(2)- 1:,lo(3)-1:)
+    real(kind=dp_t), intent(in   ) ::   wmac(lo(1)- 1:,lo(2)- 1:,lo(3)-1:)
+    real(kind=dp_t), intent(in   ) :: normal(lo(1)- 1:,lo(2)- 1:,lo(3)-1:,:)
+    real(kind=dp_t), intent(in   ) ::      s(lo(1)-ng:,lo(2)-ng:,lo(3)-ng:,:)
     real(kind=dp_t), intent(in   ) :: dx(:)
     real(kind=dp_t), intent(in   ) :: time
-    real(kind=dp_t), intent(in   ) :: p0_old(:)
-    real(kind=dp_t), intent(in   ) :: p0_new(:)
-    real(kind=dp_t), intent(in   ) :: s0_old(:,:)
-    real(kind=dp_t), intent(in   ) :: s0_new(:,:)
-    real(kind=dp_t), intent(in   ) :: temp0(:)
+    real(kind=dp_t), intent(in   ) :: p0_old(0:)
+    real(kind=dp_t), intent(in   ) :: p0_new(0:)
+    real(kind=dp_t), intent(in   ) :: s0_old(0:,:)
+    real(kind=dp_t), intent(in   ) :: s0_new(0:,:)
+    real(kind=dp_t), intent(in   ) :: temp0(0:)
 
     real(kind=dp_t) :: uadv,vadv,wadv,normal_vel
     real(kind=dp_t), allocatable :: gradp_rad(:)
     real(kind=dp_t), allocatable :: gradp_cart(:,:,:)
-    integer :: i,j,k,nx,ny,nz,nr
-
-    nx = size(force,dim=1) - 2
-    ny = size(force,dim=2) - 2
-    nz = size(force,dim=3) - 2
+    integer :: i,j,k,nr
 
     nr = size(p0_old,dim=1)
 
-    allocate(gradp_rad(nr))
-    allocate(gradp_cart(nx,ny,nz))
+    allocate(gradp_rad(0:nr-1))
+    allocate(gradp_cart(lo(1):hi(1),lo(2):hi(2),lo(3):hi(3)))
  
     force = ZERO
 
-    do k = 1,nr
-      if (k.eq.1) then
-         gradp_rad(k) = HALF * ( p0_old(k+1) + p0_new(k+1) &
-                                -p0_old(k  ) - p0_new(k  ) ) / dr
-      else if (k.eq.nr) then
-         gradp_rad(k) = HALF * ( p0_old(k  ) + p0_new(k  ) &
-                                -p0_old(k-1) - p0_new(k-1) ) / dr
-      else
-         gradp_rad(k) = FOURTH * ( p0_old(k+1) + p0_new(k+1) &
-                                  -p0_old(k-1) - p0_new(k-1) ) / dr
-      end if
+    do k = 0, nr-1
+       
+       if (k.eq.0) then
+          gradp_rad(k) = HALF * ( p0_old(k+1) + p0_new(k+1) &
+                                 -p0_old(k  ) - p0_new(k  ) ) / dr
+       else if (k.eq.nr-1) then 
+          gradp_rad(k) = HALF * ( p0_old(k  ) + p0_new(k  ) &
+                                 -p0_old(k-1) - p0_new(k-1) ) / dr
+       else
+          gradp_rad(k) = FOURTH * ( p0_old(k+1) + p0_new(k+1) &
+                                   -p0_old(k-1) - p0_new(k-1) ) / dr
+       end if
     end do
 
     call fill_3d_data(gradp_cart,gradp_rad,dx,0)
 
-    do k = 1,nz
-       do j = 1,ny
-       do i = 1,nx
-          uadv = HALF*(umac(i,j,k)+umac(i+1,j,k))
-          vadv = HALF*(vmac(i,j,k)+vmac(i,j+1,k))
-          wadv = HALF*(wmac(i,j,k)+wmac(i,j,k+1))
-          normal_vel = uadv*normal(i,j,k,1)+vadv*normal(i,j,k,2)+wadv*normal(i,j,k,3)
-          force(i,j,k) = gradp_cart(i,j,k) * normal_vel
-       end do
+    do k = lo(3),hi(3)
+       do j = lo(2),hi(2)
+          do i = lo(1),hi(1)
+
+             uadv = HALF*(umac(i,j,k)+umac(i+1,j,k))
+             vadv = HALF*(vmac(i,j,k)+vmac(i,j+1,k))
+             wadv = HALF*(wmac(i,j,k)+wmac(i,j,k+1))
+             normal_vel = uadv*normal(i,j,k,1)+vadv*normal(i,j,k,2)+wadv*normal(i,j,k,3)
+             force(i,j,k) = gradp_cart(i,j,k) * normal_vel
+          end do
        end do
     end do
 
