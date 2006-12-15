5ebc909bcefa27418963d150c01583bd083ed7ad
almgren <almgren>
2006-12-15 18:52:50 +0000

    Added (tfromH-tfromrho) as a new plot variable : deltaT.

--- Source/make_plot_variables.f90
+++ Source/make_plot_variables.f90
@@ -787,5 +787,110 @@ contains
      enddo
      
    end subroutine makeomega_3d
+
+  subroutine make_deltaT (plotdata,comp,state,p0,temp0, dx)
+
+    integer        , intent(in   ) :: comp
+    type(multifab) , intent(inout) :: plotdata
+    type(multifab) , intent(in   ) :: state
+    real(kind=dp_t), intent(in   ) ::    p0(:)
+    real(kind=dp_t), intent(in   ) :: temp0(:)
+    real(kind=dp_t), intent(in   ) :: dx(:)
+
+    real(kind=dp_t), pointer:: sp(:,:,:,:)
+    real(kind=dp_t), pointer:: tp(:,:,:,:)
+    integer :: lo(state%dim),hi(state%dim),ng,dm
+    integer :: i
+
+    ng = state%ng
+    dm = state%dim
+
+    do i = 1, state%nboxes
+       if ( multifab_remote(state, i) ) cycle
+       sp => dataptr(state, i)
+       tp => dataptr(plotdata, i)
+       lo =  lwb(get_box(state, i))
+       hi =  upb(get_box(state, i))
+       select case (dm)
+       case (2)
+          call makedeltaT_2d(tp(:,:,1,comp),sp(:,:,1,:), lo, hi, ng, p0, temp0)
+       case (3)
+       end select
+    end do
+
+  end subroutine make_deltaT
+
+  subroutine makedeltaT_2d (dT,state,lo,hi,ng,p0,temp0)
+
+    implicit none
+    integer, intent(in) :: lo(:), hi(:), ng
+    real (kind = dp_t), intent(  out) ::     dT(lo(1)   :,lo(2):)  
+    real (kind = dp_t), intent(in   ) ::  state(lo(1)-ng:,lo(2)-ng:,:)
+    real (kind = dp_t), intent(in   ) ::     p0(lo(2):)
+    real (kind = dp_t), intent(in   ) ::  temp0(lo(2):)
+
+    !     Local variables
+    integer :: i, j
+
+    integer :: nx
+
+    do_diag = .false.
+
+    do j = lo(2), hi(2)
+       do i = lo(1), hi(1)
+
+          ! (rho, H) --> T, p
+            
+          den_row(1)  = state(i,j,rho_comp)
+          h_row(1)    = state(i,j,rhoh_comp) / state(i,j,rho_comp)
+          p_row(1)    = p0(j)
+          temp_row(1) = temp0(j)
+          xn_zone(:) = state(i,j,spec_comp:spec_comp+nspec-1)/den_row(1)
+
+          ! (rho,H) --> T,p
+          input_flag = 2
+  
+          call eos(input_flag, den_row, temp_row, &
+                   npts, nspec, &
+                   xn_zone, aion, zion, &
+                   p_row, h_row, e_row, &
+                   cv_row, cp_row, xne_row, eta_row, pele_row, &
+                   dpdt_row, dpdr_row, dedt_row, dedr_row, &
+                   dpdX_row, dhdX_row, &
+                   gam1_row, cs_row, s_row, &
+                   do_diag)
+          
+          dT(i,j) = temp_row(1)
+          
+       enddo
+    enddo
+
+    do j = lo(2), hi(2)
+       do i = lo(1), hi(1)
+
+          den_row(1) = state(i,j,rho_comp)
+          temp_row(1) = temp0(j)
+          p_row(1) = p0(j)
+          xn_zone(:) = state(i,j,spec_comp:spec_comp+nspec-1)/den_row(1)
+
+          ! (rho,P) --> T,h
+          input_flag = 4
+
+          call eos(input_flag, den_row, temp_row, &
+                   npts, nspec, &
+                   xn_zone, aion, zion, &
+                   p_row, h_row, e_row, &
+                   cv_row, cp_row, xne_row, eta_row, pele_row, &
+                   dpdt_row, dpdr_row, dedt_row, dedr_row, &
+                   dpdX_row, dhdX_row, &
+                   gam1_row, cs_row, s_row, &
+                   do_diag)
+
+          dT(i,j) = dT(i,j) - temp_row(1)
+
+       enddo
+    enddo
+
+  end subroutine makedeltaT_2d
    
  end module plot_variables_module
