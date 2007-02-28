2d1e6d5f86b53dcf8b06417fe379d704d0e2fe63
mzingale <mzingale>
2007-02-28 22:32:37 +0000

    make the burner routine a module so we do bounds checking

--- Microphysics/networks/null/burner.f90
+++ Microphysics/networks/null/burner.f90
@@ -1,4 +1,4 @@
-subroutine burner(dens, temp, Xin, hin, dt, Xout, hout, rho_omegadot)
+module burner_module
 
   use bl_types
   use bl_constants_module
@@ -6,25 +6,32 @@ subroutine burner(dens, temp, Xin, hin, dt, Xout, hout, rho_omegadot)
   use eos_module
   use network
 
-  implicit none
+contains
 
-  real(kind=dp_t), intent(in) :: dens, temp, Xin(nspec), hin, dt
-  real(kind=dp_t), intent(out) :: Xout(nspec), hout, rho_omegadot(nspec)
+  subroutine burner(dens, temp, Xin, hin, dt, Xout, hout, rho_omegadot)
+
+
+    implicit none
+    
+    real(kind=dp_t), intent(in) :: dens, temp, Xin(nspec), hin, dt
+    real(kind=dp_t), intent(out) :: Xout(nspec), hout, rho_omegadot(nspec)
+    
+    integer :: n
+    real(kind=dp_t) :: enuc, dX
+    
+    Xout(:) = Xin(:)
+    
+    enuc = 0.0_dp_t
+    do n = 1, nspec
+       dX = Xout(n)-Xin(n) 
+       
+       enuc = enuc - ebin(n) * dX
+       
+       rho_omegadot(n) = dens * dX / dt
+    enddo
   
-  integer :: n
-  real(kind=dp_t) :: enuc, dX
-
-  Xout(:) = Xin(:)
-
-  enuc = 0.0_dp_t
-  do n = 1, nspec
-     dX = Xout(n)-Xin(n) 
-     
-     enuc = enuc - ebin(n) * dX
-     
-     rho_omegadot(n) = dens * dX / dt
-  enddo
+    hout = hin + enuc
   
-  hout = hin + enuc
+  end subroutine burner
 
-end subroutine burner
+end module burner_module
