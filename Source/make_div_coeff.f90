daf4cbe1e6c2f562090c5e1a8f2b5f9b688a6c56
almgren <almgren>
2007-06-11 16:01:46 +0000

    New version with limited slopes and protection against divide by zero.

--- Source/make_div_coeff.f90
+++ Source/make_div_coeff.f90
@@ -24,6 +24,7 @@ contains
 
       real(kind=dp_t) :: lambda, mu, nu
       real(kind=dp_t) :: denom, coeff1, coeff2
+      real(kind=dp_t) :: del,dpls,dmin,slim,sflag
 
       ny = size(div_coeff,dim=1)
       j_anel = ny
@@ -41,28 +42,54 @@ contains
             mu = ZERO
             nu = ZERO
          else
-            lambda = HALF*(rho0(j+1) - rho0(j-1))/dr
-            mu     = HALF*(gam1(j+1) - gam1(j-1))/dr
-            nu     = HALF*(  p0(j+1)   - p0(j-1))/dr
+
+            del    = HALF* (rho0(j+1) - rho0(j-1))/dr
+            dpls   = TWO * (rho0(j+1) - rho0(j  ))/dr
+            dmin   = TWO * (rho0(j  ) - rho0(j-1))/dr
+            slim   = min(abs(dpls), abs(dmin))
+            slim   = merge(slim, zero, dpls*dmin.gt.ZERO)
+            sflag  = sign(ONE,del)
+            lambda = sflag*min(slim,abs(del))
+
+            del   = HALF* (gam1(j+1) - gam1(j-1))/dr
+            dpls  = TWO * (gam1(j+1) - gam1(j  ))/dr
+            dmin  = TWO * (gam1(j  ) - gam1(j-1))/dr
+            slim  = min(abs(dpls), abs(dmin))
+            slim  = merge(slim, zero, dpls*dmin.gt.ZERO)
+            sflag = sign(ONE,del)
+            mu    = sflag*min(slim,abs(del))
+
+            del   = HALF* (  p0(j+1) -   p0(j-1))/dr
+            dpls  = TWO * (  p0(j+1) -   p0(j  ))/dr
+            dmin  = TWO * (  p0(j  ) -   p0(j-1))/dr
+            slim  = min(abs(dpls), abs(dmin))
+            slim  = merge(slim, zero, dpls*dmin.gt.ZERO)
+            sflag = sign(ONE,del)
+            nu    = sflag*min(slim,abs(del))
+
          endif
 
          if (j == 1 .or. j == ny) then
 
             integral = abs(grav_center(j))*rho0(j)*dr/(p0(j)*gam1(j))
 
-         else
+         else if (nu .eq. ZERO .or. mu .eq. ZERO) then
+
+            integral = abs(grav_center(j))*rho0(j)*dr/(p0(j)*gam1(j))
+
+         else 
+
             denom = nu*gam1(j) - mu*p0(j)
-            
+
             coeff1 = lambda*gam1(j)/mu - rho0(j)
             coeff2 = lambda*p0(j)/nu - rho0(j)
-
+ 
             integral = (abs(grav_center(j))/denom)* &
                  (coeff1*log( (gam1(j) + HALF*mu*dr)/ &
                               (gam1(j) - HALF*mu*dr)) - &
                   coeff2*log( (p0(j) + HALF*nu*dr)/ &
                               (p0(j) - HALF*nu*dr)) )
          endif
-
 
          beta0_edge_hi = beta0_edge_lo * exp(-integral)
 
@@ -79,8 +106,7 @@ contains
 
       do j = j_anel,ny
         div_coeff(j) = div_coeff(j-1) * (rho0(j)/rho0(j-1))
-      end do
-
+      end do 
    end subroutine make_div_coeff
 
    subroutine put_1d_beta_on_edges (div_coeff_cell,div_coeff_edge)
