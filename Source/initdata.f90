f5d648c254df99d7a23a504255fc29485a979dc4
mzingale <mzingale>
2007-07-12 18:18:28 +0000

    store w0 for restart.  Also store the base state stuff in the chkXXXX directory now

--- Source/initdata.f90
+++ Source/initdata.f90
@@ -723,55 +723,91 @@ contains
   end function interpolate
 
 
-  subroutine write_base_state(sd_name,s0,temp0,p0,div_coeff)
+  subroutine write_base_state(state_name,w0_name,chk_name,s0,temp0,p0,w0,div_coeff)
 
-    character(len=9), intent(in) :: sd_name
-    real(kind=dp_t) , intent(in) :: s0(:,:),p0(:),temp0(:),div_coeff(:)
-    real(kind=dp_t) , allocatable   :: base_r(:)
+    character(len=10), intent(in) :: state_name
+    character(len=7), intent(in) :: w0_name
+    character(len=7), intent(in) :: chk_name
+    real(kind=dp_t) , intent(in) :: s0(:,:),p0(:),temp0(:),div_coeff(:), w0(:)
+    real(kind=dp_t) :: base_r
 
+    character(len=18) :: out_name
     integer :: i, n, nr
 
     nr = size(s0,dim=1)
-    allocate(base_r(nr))
+
 
     if (parallel_IOProcessor()) then
-       write(6,*) 'Writing base state to ',sd_name
 
-       open(unit=99,file=sd_name,form = "formatted", access = "sequential",action="write")
+       ! write out the base state quantities
+       out_name = chk_name // "/" // state_name
+       write(6,*) 'Writing base state to ',out_name
+
+       open(unit=99,file=out_name,form = "formatted", access = "sequential",action="write")
        do i = 1, nr
-          base_r(i) = (dble(i)-HALF) * dr
-          write(99,1000)  base_r(i),s0(i,rho_comp), p0(i), s0(i,rhoh_comp), &
+          base_r = (dble(i)-HALF) * dr
+          write(99,1000)  base_r,s0(i,rho_comp), p0(i), s0(i,rhoh_comp), &
                (s0(i,n), n=spec_comp,spec_comp+nspec-1), temp0(i), div_coeff(i)
        end do
        close(99)
+
+       ! write out w0 (it is nodal, so it gets a separate file)
+       out_name = chk_name // "/" // w0_name
+       write(6,*) 'Writing w0 state to ',out_name
+
+       open(unit=99,file=out_name,form = "formatted", access = "sequential",action="write")
+       do i = 1, nr+1
+          base_r = (dble(i)-1) * dr
+          write(99,1000)  base_r,w0(i)
+       end do
+       close(99)
+
     endif
 
-    deallocate(base_r)
 
 1000 format(16(e30.20,1x))
 
   end subroutine write_base_state
 
 
-  subroutine read_base_state(sd_name,s0,temp0,p0,div_coeff)
+  subroutine read_base_state(state_name,w0_name,chk_name,s0,temp0,p0,w0,div_coeff)
     
-    character(len=9), intent(in   ) :: sd_name
-    real(kind=dp_t) , intent(inout) :: s0(:,:),p0(:),temp0(:),div_coeff(:)
+    character(len=10), intent(in   ) :: state_name
+    character(len=7) , intent(in   ) :: w0_name
+    character(len=7) , intent(in   ) :: chk_name    
+    real(kind=dp_t) , intent(inout) :: s0(:,:),p0(:),temp0(:),div_coeff(:),w0(:)
     real(kind=dp_t) , allocatable   :: base_r(:)
 
+    real(kind=dp_t) :: r_dummy
+    character(len=18) :: out_name
     integer :: i, n, nr
 
     nr = size(s0,dim=1)
     allocate(base_r(nr))
 
+    ! read in the state variables
+    out_name = chk_name // "/" // state_name
     if (parallel_IOProcessor()) then
-      print *,'Reading base state from ',sd_name
+      print *,'Reading base state from ',out_name
     end if
 
-    open(unit=99,file=sd_name)
+    open(unit=99,file=out_name)
     do i = 1, size(s0,dim=1)
        read(99,*)  base_r(i),s0(i,rho_comp), p0(i), s0(i,rhoh_comp), &
                    (s0(i,n), n=spec_comp,spec_comp+nspec-1), temp0(i), div_coeff(i)
+    end do
+    close(99)
+
+
+    ! read in w0
+    out_name = chk_name // "/" // w0_name
+    if (parallel_IOProcessor()) then
+      print *,'Reading w0 state from ',out_name
+    end if
+
+    open(unit=99,file=out_name)
+    do i = 1, size(w0,dim=1)
+       read(99,*)  r_dummy, w0(i)
     end do
     close(99)
 
