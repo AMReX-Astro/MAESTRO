43217207d466a2ca9b60e55487d68c8366e58962
mzingale <mzingale>
2007-07-17 17:36:55 +0000

    new runtime option, evolve_base_state which determines whether we are evolving the base state or not.  The default is .true.  This parameter is carried by the average module, and is simply used to bypass the averaging if we are not evolving the base state.

--- Source/average.f90
+++ Source/average.f90
@@ -7,7 +7,31 @@ module average_module
 
   implicit none
 
+  ! if we set phibar to 0, then the evolution of the base state is effectively 
+  ! turned off.  Here we set a parameter that allows us to do that.  We will 
+  ! use the routines enable_base_evolution() and disable_base_evolution() to 
+  ! control this.  By default, we have base state evolution enabled.
+  logical, private, save :: evolve_base = .true.
+
 contains
+
+  subroutine enable_base_evolution()
+    
+    implicit none
+
+    evolve_base = .true.
+
+  end subroutine enable_base_evolution
+  
+
+  subroutine disable_base_evolution()
+
+    implicit none
+
+    evolve_base = .false.
+
+  end subroutine disable_base_evolution
+
 
 
 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
@@ -29,65 +53,77 @@ contains
       nr = size(phibar,dim=1)
       nlevs = size(dx,dim=1)
 
-      if (spherical .eq. 1) then
-        allocate(vol_grid(0:nr-1))
-      end if
-      allocate(vol_proc(0:nr-1),vol_tot(0:nr-1))
-      allocate(phibar_proc(0:nr-1,ncomp))
-
-      vol_proc(:) = ZERO
-      vol_tot(:)  = ZERO
-
       phibar(:,:) = ZERO
-      phibar_proc(:,:) = ZERO
 
-      do n = 1, nlevs
-       do i = 1, phi(n)%nboxes
-         if ( multifab_remote(phi(n), i) ) cycle
-         pp => dataptr(phi(n), i)
-         lo =  lwb(get_box(phi(n), i))
-         hi =  upb(get_box(phi(n), i))
-         select case (dm)
-            case (2)
-              call average_2d(pp(:,:,1,:),phibar_proc,lo,hi,ng,comp,ncomp,dx(n,:))
-              vol_proc(lo(2):hi(2)) = vol_proc(lo(2):hi(2)) + (hi(1)-lo(1)+1)*dx(n,1)*dx(n,2)
-            case (3)
-              if (spherical .eq. 1) then
-                vol_grid(:) = ZERO
-                call average_3d_sphr(pp(:,:,:,:),phibar_proc,lo,hi,ng,dx(n,:),vol_grid,comp,ncomp)
-                vol_proc = vol_proc + vol_grid
-              else
-                call average_3d(pp(:,:,:,:),phibar_proc,lo,hi,ng,comp,ncomp,dx(n,:))
-                vol_proc(lo(3):hi(3)) = vol_proc(lo(3):hi(3)) + (hi(1)-lo(1)+1)*(hi(2)-lo(2)+1)*dx(n,1)*dx(n,2)*dx(n,3)
-              end if
-         end select
-       end do
-      end do
+      if (evolve_base) then
 
-      if (dm .eq. 2 .or. (dm.eq.3.and.spherical.eq.0)) then
-        do k = 0,nr-1
-           call parallel_reduce(vol_tot(k),  vol_proc(k),      MPI_SUM)
-           call parallel_reduce(phibar(k,:), phibar_proc(k,:), MPI_SUM)
+         if (spherical .eq. 1) then
+            allocate(vol_grid(0:nr-1))
+         end if
 
-           phibar(k,:) = phibar(k,:) / vol_tot(k)
-        end do
+         allocate(vol_proc(0:nr-1),vol_tot(0:nr-1))
+         allocate(phibar_proc(0:nr-1,ncomp))
 
-      else
-        do k = 0,nr-1
-          call parallel_reduce(vol_tot(k),  vol_proc(k),      MPI_SUM)
-          call parallel_reduce(phibar(k,:), phibar_proc(k,:), MPI_SUM)
+         vol_proc(:) = ZERO
+         vol_tot(:)  = ZERO
 
-          if (vol_tot(k) .gt. ZERO) then
-            phibar(k,:) = phibar(k,:) / vol_tot(k)
-          else
-            phibar(k,:) = ZERO
-          end if
+         phibar_proc(:,:) = ZERO
 
-        end do
-        deallocate(vol_grid)
+         do n = 1, nlevs
+            do i = 1, phi(n)%nboxes
+               if ( multifab_remote(phi(n), i) ) cycle
 
-      end if
-      deallocate(vol_proc,vol_tot,phibar_proc)
+               pp => dataptr(phi(n), i)
+               lo =  lwb(get_box(phi(n), i))
+               hi =  upb(get_box(phi(n), i))
+
+               select case (dm)
+               case (2)
+                  call average_2d(pp(:,:,1,:),phibar_proc,lo,hi,ng,comp,ncomp,dx(n,:))
+                  vol_proc(lo(2):hi(2)) = vol_proc(lo(2):hi(2)) + &
+                       (hi(1)-lo(1)+1)*dx(n,1)*dx(n,2)
+
+               case (3)
+                  if (spherical .eq. 1) then
+                     vol_grid(:) = ZERO
+                     call average_3d_sphr(pp(:,:,:,:),phibar_proc,lo,hi,ng,dx(n,:),vol_grid,comp,ncomp)
+                     vol_proc = vol_proc + vol_grid
+                  else
+                     call average_3d(pp(:,:,:,:),phibar_proc,lo,hi,ng,comp,ncomp,dx(n,:))
+                     vol_proc(lo(3):hi(3)) = vol_proc(lo(3):hi(3)) + &
+                          (hi(1)-lo(1)+1)*(hi(2)-lo(2)+1)*dx(n,1)*dx(n,2)*dx(n,3)
+                  end if
+
+               end select
+            end do
+         end do
+
+         if (dm .eq. 2 .or. (dm.eq.3.and.spherical.eq.0)) then
+            do k = 0,nr-1
+               call parallel_reduce(vol_tot(k),  vol_proc(k),      MPI_SUM)
+               call parallel_reduce(phibar(k,:), phibar_proc(k,:), MPI_SUM)
+
+               phibar(k,:) = phibar(k,:) / vol_tot(k)
+            end do
+
+         else
+            do k = 0,nr-1
+               call parallel_reduce(vol_tot(k),  vol_proc(k),      MPI_SUM)
+               call parallel_reduce(phibar(k,:), phibar_proc(k,:), MPI_SUM)
+
+               if (vol_tot(k) .gt. ZERO) then
+                  phibar(k,:) = phibar(k,:) / vol_tot(k)
+               else
+                  phibar(k,:) = ZERO
+               end if
+
+            end do
+            deallocate(vol_grid)
+
+         end if
+         deallocate(vol_proc,vol_tot,phibar_proc)
+
+      endif
 
    end subroutine average
 
