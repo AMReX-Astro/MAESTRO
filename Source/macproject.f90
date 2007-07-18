a4a2a0efbe15692ff034f3aa0601503fe19b34f4
ajnonaka <ajnonaka>
2007-07-18 17:54:03 +0000

    Working on creating a new subroutine, "mac_applyop", which will live in macproject.f90, and simply returns the residual "-(alpha-del dot beta grad)phi".  The subroutine "mac_applyop" is based on subroutine "mac_multigrid", but calls a new subroutine, "ml_cc_applyop" rather than "ml_cc".  Subroutine "ml_cc_applyop" is based on subroutine "ml_cc", but stops after the residual is computed.

--- Source/macproject.f90
+++ Source/macproject.f90
@@ -1226,7 +1226,8 @@ subroutine macproject(mla,umac,phi,rho,dx,the_bc_tower,verbose,mg_verbose,cg_ver
 end subroutine macproject
 
 subroutine mac_multigrid(mla,rh,phi,fine_flx,alpha,beta,dx,&
-                         the_bc_tower,bc_comp,stencil_order,ref_ratio,mg_verbose,cg_verbose,umac_norm)
+                         the_bc_tower,bc_comp,stencil_order,ref_ratio, &
+                         mg_verbose,cg_verbose,umac_norm)
 
   use f2kcli
   use stencil_module
@@ -1344,26 +1345,26 @@ subroutine mac_multigrid(mla,rh,phi,fine_flx,alpha,beta,dx,&
      pd = layout_get_pd(mla%la(n))
 
      call mg_tower_build(mgt(n), mla%la(n), pd, &
-                         the_bc_tower%bc_tower_array(n)%ell_bc_level_array(0,:,:,bc_comp), &
-          dh = dx(n,:), &
-          ns = ns, &
-          smoother = smoother, &
-          nu1 = nu1, &
-          nu2 = nu2, &
-          gamma = gamma, &
-          cycle = cycle, &
-          omega = omega, &
-          bottom_solver = bottom_solver, &
-          bottom_max_iter = bottom_max_iter, &
-          bottom_solver_eps = bottom_solver_eps, &
-          max_iter = max_iter, &
-          max_nlevel = max_nlevel_in, &
-          min_width = min_width, &
-          eps = rel_eps, &
-          abs_eps = abs_eps, &
-          verbose = mg_verbose, &
-          cg_verbose = cg_verbose, &
-          nodal = rh(nlevs)%nodal)
+            the_bc_tower%bc_tower_array(n)%ell_bc_level_array(0,:,:,bc_comp), &
+            dh = dx(n,:), &
+            ns = ns, &
+            smoother = smoother, &
+            nu1 = nu1, &
+            nu2 = nu2, &
+            gamma = gamma, &
+            cycle = cycle, &
+            omega = omega, &
+            bottom_solver = bottom_solver, &
+            bottom_max_iter = bottom_max_iter, &
+            bottom_solver_eps = bottom_solver_eps, &
+            max_iter = max_iter, &
+            max_nlevel = max_nlevel_in, &
+            min_width = min_width, &
+            eps = rel_eps, &
+            abs_eps = abs_eps, &
+            verbose = mg_verbose, &
+            cg_verbose = cg_verbose, &
+            nodal = rh(nlevs)%nodal)
 
   end do
 
@@ -1439,5 +1440,245 @@ subroutine mac_multigrid(mla,rh,phi,fine_flx,alpha,beta,dx,&
   deallocate(mgt)
 
 end subroutine mac_multigrid
+
+subroutine mac_applyop(mla,rh,phi,fine_flx,alpha,beta,dx,&
+                       the_bc_tower,bc_comp,stencil_order,ref_ratio, &
+                       mg_verbose,cg_verbose,umac_norm)
+
+  use f2kcli
+  use stencil_module
+  use coeffs_module
+  use mg_module
+  use list_box_module
+  use itsol_module
+  use sparse_solve_module
+  use ml_solve_module
+  use bl_mem_stat_module
+  use box_util_module
+  use bl_IO_module
+
+  type(ml_layout),intent(inout) :: mla
+  integer        ,intent(in   ) :: stencil_order
+  integer        ,intent(in   ) :: ref_ratio(:,:)
+  integer        ,intent(in   ) :: mg_verbose, cg_verbose
+
+  real(dp_t), intent(in) :: dx(:,:)
+  type(bc_tower), intent(in) :: the_bc_tower
+  integer     ,intent(in   ) :: bc_comp
+  real(dp_t), intent(in), optional :: umac_norm(:)
+
+  type(layout  ) :: la
+  type(boxarray) :: pdv
+  type(box     ) :: pd
+
+  type(multifab), allocatable :: coeffs(:)
+
+  type(multifab) , intent(in   ) :: alpha(:), beta(:)
+  type(multifab) , intent(inout) ::    rh(:),  phi(:)
+  type(bndry_reg), intent(inout) :: fine_flx(2:)
+
+  type( multifab) :: ss
+  type(imultifab) :: mm
+  type(sparse) :: sparse_object
+  type(mg_tower), allocatable :: mgt(:)
+  integer        :: i, dm, ns, nlevs
+  integer        :: test
+
+  ! MG solver defaults
+  integer :: bottom_solver, bottom_max_iter
+  integer    :: max_iter
+  integer    :: min_width
+  integer    :: max_nlevel
+  integer    :: n, nu1, nu2, gamma, cycle, smoother
+  integer    :: max_nlevel_in,do_diagnostics
+  real(dp_t) :: rel_eps,abs_eps,omega,bottom_solver_eps
+  real(dp_t) ::  xa(mla%dim),  xb(mla%dim)
+  real(dp_t) :: pxa(mla%dim), pxb(mla%dim)
+
+  type(lmultifab), pointer     :: fine_mask(:) => Null()
+  type(boxarray) :: bac
+
+  !! Defaults:
+
+  nlevs = mla%nlevel
+  dm    = mla%dim
+
+  allocate(mgt(nlevs))
+
+  test           = 0
+
+  max_nlevel        = mgt(nlevs)%max_nlevel
+  max_iter          = mgt(nlevs)%max_iter
+  rel_eps           = mgt(nlevs)%eps
+  abs_eps           = mgt(nlevs)%abs_eps
+  smoother          = mgt(nlevs)%smoother
+  nu1               = mgt(nlevs)%nu1
+  nu2               = mgt(nlevs)%nu2
+  gamma             = mgt(nlevs)%gamma
+  omega             = mgt(nlevs)%omega
+  cycle             = mgt(nlevs)%cycle
+  bottom_solver     = mgt(nlevs)%bottom_solver
+  bottom_solver_eps = mgt(nlevs)%bottom_solver_eps
+  bottom_max_iter   = mgt(nlevs)%bottom_max_iter
+  min_width         = mgt(nlevs)%min_width
+
+! Note: put this here to minimize asymmetries - ASA
+  if (nlevs .eq. 1) then
+     rel_eps = 1.d-12
+  else if (nlevs .eq. 2) then
+     rel_eps = 1.d-11
+  else
+     rel_eps = 1.d-10
+  endif
+
+  abs_eps = -1.0_dp_t
+  if (present(umac_norm)) then
+    do n = 1,nlevs
+      abs_eps = max(abs_eps, umac_norm(n) / dx(n,1))
+    end do
+    abs_eps = rel_eps * abs_eps
+  end if
+
+  bottom_solver = 2
+  bottom_solver_eps = 1.d-3
+
+  if ( test /= 0 .AND. max_iter == mgt(nlevs)%max_iter ) &
+     max_iter = 1000
+
+  ns = 1 + dm*3
+
+  do n = nlevs, 1, -1
+
+     if (n == 1) then
+        max_nlevel_in = max_nlevel
+     else
+        if ( all(ref_ratio(n-1,:) == 2) ) then
+           max_nlevel_in = 1
+        else if ( all(ref_ratio(n-1,:) == 4) ) then
+           max_nlevel_in = 2
+        else
+           call bl_error("MAC_MULTIGRID: confused about ref_ratio")
+        end if
+     end if
+
+     pd = layout_get_pd(mla%la(n))
+
+     call mg_tower_build(mgt(n), mla%la(n), pd, &
+            the_bc_tower%bc_tower_array(n)%ell_bc_level_array(0,:,:,bc_comp), &
+            dh = dx(n,:), &
+            ns = ns, &
+            smoother = smoother, &
+            nu1 = nu1, &
+            nu2 = nu2, &
+            gamma = gamma, &
+            cycle = cycle, &
+            omega = omega, &
+            bottom_solver = bottom_solver, &
+            bottom_max_iter = bottom_max_iter, &
+            bottom_solver_eps = bottom_solver_eps, &
+            max_iter = max_iter, &
+            max_nlevel = max_nlevel_in, &
+            min_width = min_width, &
+            eps = rel_eps, &
+            abs_eps = abs_eps, &
+            verbose = mg_verbose, &
+            cg_verbose = cg_verbose, &
+            nodal = rh(nlevs)%nodal)
+
+  end do
+
+  !! Fill coefficient array
+
+  do n = nlevs,1,-1
+
+     allocate(coeffs(mgt(n)%nlevels))
+
+     la = mla%la(n)
+     pd = layout_get_pd(la)
+
+     call multifab_build(coeffs(mgt(n)%nlevels), la, 1+dm, 1)
+     call multifab_copy_c(coeffs(mgt(n)%nlevels),1,alpha(n),1, 1,all=.true.)
+     call multifab_copy_c(coeffs(mgt(n)%nlevels),2, beta(n),1,dm,all=.true.)
+
+     do i = mgt(n)%nlevels-1, 1, -1
+        call multifab_build(coeffs(i), mgt(n)%ss(i)%la, 1+dm, 1)
+        call setval(coeffs(i), ZERO, 1, dm+1, all=.true.)
+        call coarsen_coeffs(coeffs(i+1),coeffs(i))
+     end do
+
+     if (n > 1) then
+        xa = HALF*ref_ratio(n-1,:)*mgt(n)%dh(:,mgt(n)%nlevels)
+        xb = HALF*ref_ratio(n-1,:)*mgt(n)%dh(:,mgt(n)%nlevels)
+     else
+        xa = ZERO
+        xb = ZERO
+     end if
+
+     pxa = ZERO
+     pxb = ZERO
+     do i = mgt(n)%nlevels, 1, -1
+        pdv = layout_boxarray(mgt(n)%ss(i)%la)
+        call stencil_fill_cc(mgt(n)%ss(i), coeffs(i), mgt(n)%dh(:,i), &
+             pdv, mgt(n)%mm(i), xa, xb, pxa, pxb, pd, stencil_order, &
+             the_bc_tower%bc_tower_array(n)%ell_bc_level_array(0,:,:,bc_comp))
+     end do
+
+     if ( n == 1 .and. bottom_solver == 3 ) then
+        call sparse_build(mgt(n)%sparse_object, mgt(n)%ss(1), &
+             mgt(n)%mm(1), mgt(n)%ss(1)%la, stencil_order, mgt(nlevs)%verbose)
+     end if
+     do i = mgt(n)%nlevels, 1, -1
+        call multifab_destroy(coeffs(i))
+     end do
+     deallocate(coeffs)
+
+  end do
+
+  if (mg_verbose >= 3) then
+    do_diagnostics = 1
+  else
+    do_diagnostics = 0
+  end if
+
+  ! Copied in from ml_solve.f90:54 to create fine_mask
+  allocate(fine_mask(nlevs))
+  do n = nlevs, 1, -1
+     call lmultifab_build(fine_mask(n), mla%la(n), 1, 0)
+     call setval(fine_mask(n), val = .true., all = .true.)
+  end do
+  do n = nlevs-1, 1, -1
+     call copy(bac, get_boxarray(mla%la(n+1)))
+     call boxarray_coarsen(bac, ref_ratio(n,:))
+     call setval(fine_mask(n), .false., bac)
+     call destroy(bac)
+  end do
+
+  call ml_cc_applyop(mla, mgt, rh, phi, fine_mask, ref_ratio, do_diagnostics)
+
+  ! Copied in from ml_solve.f90:85 to destroy fine_mask
+  do n = 1,nlevs
+     call lmultifab_destroy(fine_mask(n))
+  end do
+  deallocate(fine_mask)
+
+
+  do n = 1,nlevs
+     call multifab_fill_boundary(phi(n))
+  end do
+
+  if ( test == 3 ) then
+     call sparse_destroy(sparse_object)
+  end if
+  if ( test > 0 ) then
+     call destroy(ss)
+     call destroy(mm)
+  end if
+
+  do n = 1, nlevs
+     call mg_tower_destroy(mgt(n))
+  end do
+  deallocate(mgt)
+
+end subroutine mac_applyop
 
 end module macproject_module
