307bcc11864bb4b111f3f061d8010524f9366522
ajnonaka <ajnonaka>
2007-08-17 16:28:34 +0000

    the enthalpy boundary conditions are properly set for use_big_h = T

--- TEST_PROBLEMS/test_smallscale/inlet_bc.f90
+++ TEST_PROBLEMS/test_smallscale/inlet_bc.f90
@@ -7,6 +7,7 @@ module inlet_bc
    real(kind=dp_t), parameter :: INLET_VT      =  0.0d0
    real(kind=dp_t), parameter :: INLET_RHO     =  4.0e7
    real(kind=dp_t), parameter :: INLET_RHOH    =  1.8954332646e25
+   real(kind=dp_t), parameter :: INLET_RHOBIGH =  3.2107986796e26
    real(kind=dp_t), parameter :: INLET_RHOC12  =  2.0e7
    real(kind=dp_t), parameter :: INLET_RHOO16  =  2.0e7
    real(kind=dp_t), parameter :: INLET_RHOMG24 =  0.0d0
