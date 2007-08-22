b2947798c28bfd7ec239aad9f9535d2c2e8f09de
almgren <almgren>
2007-08-22 22:22:17 +0000

    Need more precision in inlet rho value in order to get zero source at the first interior cell on the bottom.
    Note alternate value for RhoH is for the case where we replace ebin by qs.

--- TEST_PROBLEMS/test_smallscale/inlet_bc.f90
+++ TEST_PROBLEMS/test_smallscale/inlet_bc.f90
@@ -6,10 +6,11 @@ module inlet_bc
    real(kind=dp_t), parameter :: INLET_VN      =  0.0d0
    real(kind=dp_t), parameter :: INLET_VT      =  0.0d0
    real(kind=dp_t), parameter :: INLET_RHO     =  4.0e7
-   real(kind=dp_t), parameter :: INLET_RHOH    =  1.8954332646e25
-   real(kind=dp_t), parameter :: INLET_RHOBIGH = -2.8317120266e26
-   real(kind=dp_t), parameter :: INLET_RHOC12  =  2.0e7
-   real(kind=dp_t), parameter :: INLET_RHOO16  =  2.0e7
+   real(kind=dp_t), parameter :: INLET_RHOH    =  1.8954332646d25
+   real(kind=dp_t), parameter :: INLET_RHOBIGH = -2.8317120266d26
+!  real(kind=dp_t), parameter :: INLET_RHOBIGH = 3.009433254708944d25
+   real(kind=dp_t), parameter :: INLET_RHOC12  =  19999999.8217626d0
+   real(kind=dp_t), parameter :: INLET_RHOO16  =  19999999.8217626d0
    real(kind=dp_t), parameter :: INLET_RHOMG24 =  0.0d0
    real(kind=dp_t), parameter :: INLET_TRA     =  0.0d0
 
