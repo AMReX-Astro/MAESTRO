459c254546ded2f46a17c623b2ff1b2a1bf7a4be
ajnonaka <ajnonaka>
2007-08-29 17:22:32 +0000

    added more precision

--- TEST_PROBLEMS/test_smallscale/inlet_bc.f90
+++ TEST_PROBLEMS/test_smallscale/inlet_bc.f90
@@ -5,12 +5,14 @@ module inlet_bc
 
    real(kind=dp_t), parameter :: INLET_VN      =  0.0d0
    real(kind=dp_t), parameter :: INLET_VT      =  0.0d0
-   real(kind=dp_t), parameter :: INLET_RHO     =  4.0e7
-   real(kind=dp_t), parameter :: INLET_RHOH    =  1.8954332646d25
-   real(kind=dp_t), parameter :: INLET_RHOBIGH = -2.8317120266d26
-!  real(kind=dp_t), parameter :: INLET_RHOBIGH = 3.009433254708944d25
-   real(kind=dp_t), parameter :: INLET_RHOC12  =  19999999.8217626d0
-   real(kind=dp_t), parameter :: INLET_RHOO16  =  19999999.8217626d0
+   real(kind=dp_t), parameter :: INLET_RHO     =  3.99999996435252d7
+   real(kind=dp_t), parameter :: INLET_RHOH    =  1.89543326464019d25
+!  This is for the ebin formulation
+   real(kind=dp_t), parameter :: INLET_RHOBIGH = -2.83171202661189d26
+!  This is for the qs formulation
+!   real(kind=dp_t), parameter :: INLET_RHOBIGH =  3.00943325470894d25
+   real(kind=dp_t), parameter :: INLET_RHOC12  =  1.99999998217626d7
+   real(kind=dp_t), parameter :: INLET_RHOO16  =  1.99999998217626d7
    real(kind=dp_t), parameter :: INLET_RHOMG24 =  0.0d0
    real(kind=dp_t), parameter :: INLET_TRA     =  0.0d0
 
