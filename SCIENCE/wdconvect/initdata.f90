0022dd41a93bf7ea8b8b5d315e155e0b4750f9ff
ajnonaka <ajnonaka>
2007-09-07 22:15:52 +0000

    first pass at random perturbation

--- SCIENCE/wdconvect/initdata.f90
+++ SCIENCE/wdconvect/initdata.f90
@@ -265,21 +265,155 @@ contains
     implicit none
 
     integer, intent(in) :: lo(:), hi(:), ng
-    real (kind = dp_t), intent(out) :: u(lo(1)-ng:,lo(2)-ng:,lo(3)-ng:,:)  
+    real (kind = dp_t), intent(out) :: u(lo(1)-ng:,lo(2)-ng:,lo(3)-ng:,:)
     real (kind = dp_t), intent(in ) :: dx(:)
     real (kind = dp_t), intent(in ) :: prob_lo(:)
     real (kind = dp_t), intent(in ) :: prob_hi(:)
     real(kind=dp_t), intent(in   ) ::    s0(0:,:)
     real(kind=dp_t), intent(in   ) ::    p0(0:)
 
-    !     Local variables
-    integer :: i, j, k, n
-    real(kind=dp_t) :: x,y,z,r,r0,r1,r2,temp
-    real(kind=dp_t) :: dens_pert, rhoh_pert, rhoX_pert(nspec), trac_pert(ntrac)
+    ! Local variables
+    real(kind=dp_t) :: pi = 3.141592653589793238462643383279502884197d0
+    integer :: i, j, k
+    integer :: iloc, jloc, kloc
+    ! L2 norm of k
+    real(kind=dp_t) :: normk(3,3,3)
+    ! random numbers (-1,1]
+    real(kind=dp_t) :: alpha(3,3,3), beta(3,3,3), gamma(3,3,3)
+    ! random numbers (0,2*pi)
+    real(kind=dp_t) :: phix(3,3,3), phiy(3,3,3), phiz(3,3,3)
+    ! cos and sin (k_i*x_i + phi_i)
+    real(kind=dp_t) :: cx(3,3,3), cy(3,3,3), cz(3,3,3), sx(3,3,3), sy(3,3,3), sz(3,3,3)
+    ! location of center of star
+    real(kind=dp_t) :: xc(3)
+    ! radius, or distance, to center of star
+    real(kind=dp_t) :: rloc
+    ! the point we're at
+    real(kind=dp_t) :: xloc(3)
+    ! amplitude, cutoff radius, scaling, and cutoff steepness
+    real(kind=dp_t) :: A, R, L, W
+    ! perturbational velocity to add
+    real(kind=dp_t) :: upert(3)
+    ! random number
+    real(kind=dp_t) :: rand
 
-    ! initial the velocity
+    ! begin by initializing the velocity to zero everywhere
     u = ZERO
-    
+
+    ! set A, R, L, and W
+    A = 1.0d0
+    R = 0.5d8
+    L = 0.6d8
+    W = 1.0d0
+
+    ! load in random numbers alpha, beta, gamma, phix, phiy, and phiz
+    do i=1,3
+       do j=1,3
+          do k=1,3
+             call random_number(rand)
+             rand = 2.0d0*rand - 1.0d0
+             alpha(i,j,k) = rand
+             call random_number(rand)
+             rand = 2.0d0*rand - 1.0d0
+             beta(i,j,k) = rand
+             call random_number(rand)
+             rand = 2.0d0*rand - 1.0d0
+             gamma(i,j,k) = rand
+             call random_number(rand)
+             rand = 2.0d0*pi*rand
+             phix(i,j,k) = rand
+             call random_number(rand)
+             rand = 2.0d0*pi*rand
+             phiy(i,j,k) = rand
+             call random_number(rand)
+             rand = 2.0d0*pi*rand
+             phiz(i,j,k) = rand
+          enddo
+       enddo
+    enddo
+
+    ! compute the norm of k
+    do i=1,3
+       do j=1,3
+          do k=1,3
+             normk(i,j,k) = sqrt(dble(i)**2+dble(j)**2+dble(k)**2)
+          enddo
+       enddo
+    enddo
+
+    ! define where center of star is
+    ! for now I am assuming at the center of the domain
+    do i=1,3
+       xc(i) = 0.5d0*(prob_lo(i)+prob_hi(i))
+    enddo
+
+    ! now do the big loop over all points
+    do iloc = lo(1),hi(1)
+       do jloc = lo(2),hi(2)
+          do kloc = lo(3),hi(3)
+
+             ! set perturbational velocity to zero
+             upert = ZERO
+
+             ! compute where we are
+             xloc(1) = prob_lo(1) + (dble(iloc)+0.5d0)*dx(1)
+             xloc(2) = prob_lo(2) + (dble(jloc)+0.5d0)*dx(2)
+             xloc(3) = prob_lo(3) + (dble(kloc)+0.5d0)*dx(3)
+
+             ! compute distance to the center of the star
+             rloc = ZERO
+             do i=1,3
+                rloc = rloc + (xloc(i) - xc(i))**2
+             enddo
+             rloc = sqrt(rloc)
+
+             ! loop over the 27 combinations of fourier components
+             do i=1,3
+                do j=1,3
+                   do k=1,3
+                      ! compute cosines and sines
+                      cx(i,j,k) = cos(2.0d0*pi*dble(i)*xloc(1)/L + phix(i,j,k))
+                      cy(i,j,k) = cos(2.0d0*pi*dble(j)*xloc(2)/L + phiy(i,j,k))
+                      cz(i,j,k) = cos(2.0d0*pi*dble(k)*xloc(3)/L + phiz(i,j,k))
+                      sx(i,j,k) = sin(2.0d0*pi*dble(i)*xloc(1)/L + phix(i,j,k))
+                      sy(i,j,k) = sin(2.0d0*pi*dble(j)*xloc(2)/L + phiy(i,j,k))
+                      sz(i,j,k) = sin(2.0d0*pi*dble(k)*xloc(3)/L + phiz(i,j,k))
+                   enddo
+                enddo
+             enddo
+
+             ! loop over the 27 combinations of fourier components
+             do i=1,3
+                do j=1,3
+                   do k=1,3
+                      ! compute additions to perturbational velocity
+                      upert(1) = upert(1) + &
+                           (-gamma(i,j,k)*dble(j)*cx(i,j,k)*cz(i,j,k)*sy(i,j,k) &
+                            +beta(i,j,k)*dble(k)*cx(i,j,k)*cy(i,j,k)*sz(i,j,k)) / normk(i,j,k)
+
+                      upert(2) = upert(2) + &
+                           (gamma(i,j,k)*dble(i)*cy(i,j,k)*cz(i,j,k)*sx(i,j,k) &
+                            -alpha(i,j,k)*dble(k)*cx(i,j,k)*cy(i,j,k)*sz(i,j,k)) / normk(i,j,k)
+
+                      upert(3) = upert(3) + &
+                           (-beta(i,j,k)*dble(i)*cy(i,j,k)*cz(i,j,k)*sx(i,j,k) &
+                            +alpha(i,j,k)*dble(j)*cx(i,j,k)*cz(i,j,k)*sy(i,j,k)) / normk(i,j,k)
+                   enddo
+                enddo
+             enddo
+
+             ! apply the cutoff function to the perturbational velocity
+             upert = upert*(0.5d0+0.5d0*tanh((R-rloc)/W))
+
+             ! add perturbational velocity to background velocity
+             do i=1,3
+                u(iloc,jloc,kloc,i) = u(iloc,jloc,kloc,i) + upert(i)
+             enddo
+
+          enddo
+       enddo
+    enddo
+      
   end subroutine initveldata_3d
 
 
