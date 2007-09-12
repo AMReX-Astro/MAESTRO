40cb7ef56347686ed39dc6f329e50c96683649da
ajnonaka <ajnonaka>
2007-09-12 18:09:51 +0000

    even more precision added to inflow.  note that for nova, the inflow bc is not equal to the first valid cell value.  they actually read in a value that is outside the domain, then call some eos's to make sure the state is consistent.  for now, this code will just hard code in their values

--- TEST_PROBLEMS/test_smallscale/inlet_bc.f90
+++ TEST_PROBLEMS/test_smallscale/inlet_bc.f90
@@ -5,14 +5,14 @@ module inlet_bc
 
    real(kind=dp_t), parameter :: INLET_VN      =  0.0d0
    real(kind=dp_t), parameter :: INLET_VT      =  0.0d0
-   real(kind=dp_t), parameter :: INLET_RHO     =  3.99999996435252d7
+   real(kind=dp_t), parameter :: INLET_RHO     =  3.9999999999798551d7
    real(kind=dp_t), parameter :: INLET_RHOH    =  1.89543326464019d25
 !  This is for the ebin formulation
    real(kind=dp_t), parameter :: INLET_RHOBIGH = -2.83171202661189d26
 !  This is for the qs formulation
 !   real(kind=dp_t), parameter :: INLET_RHOBIGH =  3.00943325470894d25
-   real(kind=dp_t), parameter :: INLET_RHOC12  =  1.99999998217626d7
-   real(kind=dp_t), parameter :: INLET_RHOO16  =  1.99999998217626d7
+   real(kind=dp_t), parameter :: INLET_RHOC12  =  0.5d0*3.9999999999798551d7
+   real(kind=dp_t), parameter :: INLET_RHOO16  =  0.5d0*3.9999999999798551d7
    real(kind=dp_t), parameter :: INLET_RHOMG24 =  0.0d0
    real(kind=dp_t), parameter :: INLET_TEMP    =  1.00000000047000d8
    real(kind=dp_t), parameter :: INLET_TRA     =  0.0d0
