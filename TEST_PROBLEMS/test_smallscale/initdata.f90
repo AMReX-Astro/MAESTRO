18d83de029905b7ffa400b89d12252f60c4d51f7
ajnonaka <ajnonaka>
2007-09-15 00:41:16 +0000

    major cleanup.  re-arranged the boundary conditions to call to multifab_fill_boundary is before setbc.  removed the perturbation stuff since the bubble perturbations dont' apply to this test problem.  set the base state density in 2d to be constant

--- TEST_PROBLEMS/test_smallscale/initdata.f90
+++ TEST_PROBLEMS/test_smallscale/initdata.f90
@@ -42,29 +42,37 @@ contains
        sop => dataptr(s, i)
        lo =  lwb(get_box(s, i))
        hi =  upb(get_box(s, i))
-
        select case (dm)
        case (2)
           call initscalardata_2d(sop(:,:,1,:), lo, hi, ng, dx, perturb_model, &
                                  prob_lo, prob_hi, s0, p0)
+       case (3)
+          call initscalardata_3d(sop(:,:,:,:), lo, hi, ng, dx, perturb_model, &
+                                 prob_lo, prob_hi, s0, p0)
+       end select
+    end do
 
+    call multifab_fill_boundary(s)
+
+    do i = 1, s%nboxes
+       if ( multifab_remote(s, i) ) cycle
+       sop => dataptr(s, i)
+       lo =  lwb(get_box(s, i))
+       hi =  upb(get_box(s, i))
+       select case (dm)
+       case (2)
           do n = 1,nscal
              call setbc_2d(sop(:,:,1,n), lo, ng, &
                            bc%adv_bc_level_array(i,:,:,dm+n),dx,dm+n)
           end do
 
        case (3)
-          call initscalardata_3d(sop(:,:,:,:), lo, hi, ng, dx, perturb_model, &
-                                 prob_lo, prob_hi, s0, p0)
-
-          do n = 1, nscal
+          do n = 1,nscal
              call setbc_3d(sop(:,:,:,n), lo, ng, &
                            bc%adv_bc_level_array(i,:,:,dm+n),dx,dm+n)
           end do
        end select
     end do
-
-    call multifab_fill_boundary(s)
 
   end subroutine initscalardata
 
@@ -90,7 +98,7 @@ contains
     s = ZERO
 
     ! initialize the scalars
-    do n = rho_comp,temp_comp
+    do n = rho_comp,rho_comp+nscal-1
        do j = lo(2), hi(2)
           do i = lo(1), hi(1)
              s(i,j,n) = s0(j,n)
@@ -98,25 +106,10 @@ contains
        enddo
     enddo
     
-    ! add an optional perturbation
-    if (perturb_model) then
-       do j = lo(2), hi(2)
-          y = prob_lo(2) + (dble(j)+HALF) * dx(2)
-       
-          do i = lo(1), hi(1)
-             x = prob_lo(1) + (dble(i)+HALF) * dx(1)
-          
-             call perturb_2d(x, y, p0(j), s0(j,:), &
-                             dens_pert, rhoh_pert, rhoX_pert, &
-                             temp_pert, trac_pert)
-
-             s(i,j,rho_comp) = dens_pert
-             s(i,j,rhoh_comp) = rhoh_pert
-             s(i,j,spec_comp:spec_comp+nspec-1) = rhoX_pert(1:)
-             s(i,j,trac_comp:trac_comp+ntrac-1) = trac_pert(:)
-          enddo
-       enddo
-    endif
+    ! set density in base state to the constant, "bottom domain" value
+    do j=lo(2),hi(2)
+       s0(j,rho_comp) = s0(j,rho_comp)
+    enddo
     
   end subroutine initscalardata_2d
 
@@ -144,15 +137,12 @@ contains
     s = ZERO
   
     if (spherical .eq. 1) then
-
-       do n = rho_comp, spec_comp+nspec-1
-          call fill_3d_data (s(:,:,:,n),s0(:,n),lo,hi,dx,ng)
+       do n = rho_comp,rho_comp+nscal-1
+          call fill_3d_data(s(:,:,:,n),s0(:,n),lo,hi,dx,ng)
        end do
-
     else 
- 
-       ! initialize the scalars
-       do n = rho_comp,spec_comp+nspec-1
+        ! initialize the scalars
+       do n = rho_comp,rho_comp+nscal-1
           do k = lo(3), hi(3)
              do j = lo(2), hi(2)
                 do i = lo(1), hi(1)
@@ -166,32 +156,6 @@ contains
        do k=lo(3),hi(3)
           s0(k,rho_comp) = s0(k,rho_comp)
        enddo
-       
-       if (perturb_model) then
-
-          ! add an optional perturbation
-          do k = lo(3), hi(3)
-             z = prob_lo(3) + (dble(k)+HALF) * dx(3)
-             
-             do j = lo(2), hi(2)
-                y = prob_lo(2) + (dble(j)+HALF) * dx(2)
-                
-                do i = lo(1), hi(1)
-                   x = prob_lo(1) + (dble(i)+HALF) * dx(1)
-                   
-                   call perturb_3d(x, y, z, p0(k), s0(k,:), &
-                                   dens_pert, rhoh_pert, rhoX_pert, &
-                                   temp_pert, trac_pert)
-
-                   s(i,j,k,rho_comp) = dens_pert
-                   s(i,j,k,rhoh_comp) = rhoh_pert
-                   s(i,j,k,spec_comp:spec_comp+nspec-1) = rhoX_pert(:)
-                   s(i,j,k,trac_comp:trac_comp+ntrac-1) = trac_pert(:)
-                enddo
-             enddo
-          enddo
-       endif
-
     end if
     
   end subroutine initscalardata_3d
@@ -218,30 +182,36 @@ contains
        uop => dataptr(u, i)
        lo =  lwb(get_box(u, i))
        hi =  upb(get_box(u, i))
-
        select case (dm)
        case (2)
           call initveldata_2d(uop(:,:,1,:), lo, hi, ng, dx, &
                               prob_lo, prob_hi, s0, p0)
-   
-          do n = 1,dm
-             call setbc_2d(uop(:,:,1,n), lo, ng, &
-                           bc%adv_bc_level_array(i,:,:,   n),dx,   n)
-          end do
-
        case (3)
           call initveldata_3d(uop(:,:,:,:), lo, hi, ng, dx, &
                               prob_lo, prob_hi, s0, p0)
-
-          do n = 1, dm
-             call setbc_3d(uop(:,:,:,n), lo, ng, &
-                           bc%adv_bc_level_array(i,:,:,   n),dx,   n)
-          end do
-
        end select
     end do
 
     call multifab_fill_boundary(u)
+
+    do i = 1, u%nboxes
+       if ( multifab_remote(u, i) ) cycle
+       uop => dataptr(u, i)
+       lo =  lwb(get_box(u, i))
+       hi =  upb(get_box(u, i))
+       select case (dm)
+       case (2)
+          do n = 1,dm
+             call setbc_2d(uop(:,:,1,n), lo, ng, &
+                           bc%adv_bc_level_array(i,:,:,   n),dx,   n)
+          end do
+       case (3)
+          do n = 1, dm
+             call setbc_3d(uop(:,:,:,n), lo, ng, &
+                           bc%adv_bc_level_array(i,:,:,   n),dx,   n)
+          end do
+       end select
+    end do
 
   end subroutine initveldata
 
@@ -323,164 +293,6 @@ contains
 
   end subroutine initveldata_3d
 
-  subroutine perturb_2d(x, y, p0, s0, dens_pert, rhoh_pert, rhoX_pert, temp_pert, trac_pert)
-
-    ! apply an optional perturbation to the initial temperature field
-    ! to see some bubbles
-
-    real(kind=dp_t), intent(in ) :: x, y
-    real(kind=dp_t), intent(in ) :: p0, s0(:)
-    real(kind=dp_t), intent(out) :: dens_pert, rhoh_pert, temp_pert
-    real(kind=dp_t), intent(out) :: rhoX_pert(:)
-    real(kind=dp_t), intent(out) :: trac_pert(:)
-
-    real(kind=dp_t) :: temp,t0
-    real(kind=dp_t) :: x0, y0, x1, y1, x2, y2
-    integer :: i, j
-    real(kind=dp_t) :: r0, r1, r2
-
-    t0 = s0(temp_comp)
-
-    x0 = 5.0d7
-    y0 = 6.5d7
-    
-    x1 = 1.2d8
-    y1 = 8.5d7
-    
-    x2 = 2.0d8
-    y2 = 7.5d7
-
-    ! Tanh bubbles
-    r0 = sqrt( (x-x0)**2 +(y-y0)**2 ) / 2.5e6
-    r1 = sqrt( (x-x1)**2 +(y-y1)**2 ) / 0.05
-    r2 = sqrt( (x-x2)**2 +(y-y2)**2 ) / 2.5e6
-    
-    ! This case works
-    temp = t0 * (ONE + TWO * ( &
-         .15_dp_t * 0.5_dp_t * (1.0_dp_t + tanh((2.0-r0))) + &
-         .3_dp_t * 0.5_dp_t * (1.0_dp_t + tanh((2.0-r1))) + &
-         .225_dp_t * 0.5_dp_t * (1.0_dp_t + tanh((2.0-r2)))  ) )
-
-    ! This case breaks
-!   temp = t0 * (ONE + FOUR * ( &
-!        .15_dp_t * 0.5_dp_t * (1.0_dp_t + tanh((2.0-r0))) + &
-!        .3_dp_t * 0.5_dp_t * (1.0_dp_t + tanh((2.0-r1))) + &
-!        .225_dp_t * 0.5_dp_t * (1.0_dp_t + tanh((2.0-r2)))  ) )
-          
-    ! Use the EOS to make this temperature perturbation occur at constant 
-    ! pressure
-    temp_row(1) = temp
-    p_row(1) = p0
-    den_row(1) = s0(rho_comp)
-    xn_zone(:) = s0(spec_comp:)/s0(rho_comp)
-
-    input_flag = 3      ! (t, p) -> (rho, h)
-
-    call eos(input_flag, den_row, temp_row, &
-             npts, nspec, &
-             xn_zone, aion, zion, &
-             p_row, h_row, e_row, &
-             cv_row, cp_row, xne_row, eta_row, pele_row, &
-             dpdt_row, dpdr_row, dedt_row, dedr_row, &
-             dpdX_row, dhdX_row, &
-             gam1_row, cs_row, s_row, &
-             dsdt_row, dsdr_row, &
-             do_diag)
-
-    dens_pert = den_row(1)
-    rhoh_pert = den_row(1)*h_row(1)
-    rhoX_pert(:) = dens_pert*xn_zone(:)
-
-    temp_pert = temp
-    
-!   if ( (r0 .lt. 2.0) .or. (r1 .lt. 2.0) .or. (r2 .lt. 2.0) ) then
-!     trac_pert(:) = ONE
-!   else
-      trac_pert(:) = ZERO
-!   end if
-
-  end subroutine perturb_2d
-
-  subroutine perturb_3d(x, y, z, p0, s0, dens_pert, rhoh_pert, rhoX_pert, temp_pert, trac_pert)
-
-    ! apply an optional perturbation to the initial temperature field
-    ! to see some bubbles
-
-    real(kind=dp_t), intent(in ) :: x, y, z
-    real(kind=dp_t), intent(in ) :: p0, s0(:)
-    real(kind=dp_t), intent(out) :: dens_pert, rhoh_pert, temp_pert
-    real(kind=dp_t), intent(out) :: rhoX_pert(:)
-    real(kind=dp_t), intent(out) :: trac_pert(:)
-
-    real(kind=dp_t) :: temp,t0
-    real(kind=dp_t) :: x0, y0, z0, x1, y1, z1, x2, y2, z2
-    integer :: i, j, k
-    real(kind=dp_t) :: r0, r1, r2
-
-    x0 = 5.0d7
-    y0 = 5.0d7
-    z0 = 6.5d7
-    
-    x1 = 1.2d8
-    y1 = 1.2d8
-    z1 = 8.5d7
-    
-    x2 = 2.0d8
-    y2 = 2.0d8
-    z2 = 7.5d7
-
-    t0 = s0(temp_comp)
-
-!   temp = t0 * (ONE + TWO * ( &
-!        .0625_dp_t * 0.5_dp_t * (1.0_dp_t + tanh((2.0-r0))) + &
-!        .1875_dp_t * 0.5_dp_t * (1.0_dp_t + tanh((2.0-r1))) + &
-!        .1250_dp_t * 0.5_dp_t * (1.0_dp_t + tanh((2.0-r2)))  ) )
-
-    ! Tanh bubbles from perturb_2d
-    r0 = sqrt( (y-y0)**2 +(z-z0)**2 ) / 2.5e6
-    r1 = sqrt( (y-y1)**2 +(z-z1)**2 ) / 2.5e6
-    r2 = sqrt( (y-y2)**2 +(z-z2)**2 ) / 2.5e6
-    
-    ! This case works
-    temp = t0 * (ONE + TWO * ( &
-         .150_dp_t * 0.5_dp_t * (1.0_dp_t + tanh((2.0-r0))) + &
-         .300_dp_t * 0.5_dp_t * (1.0_dp_t + tanh((2.0-r1))) + &
-         .225_dp_t * 0.5_dp_t * (1.0_dp_t + tanh((2.0-r2)))  ) )
-
-    ! Use the EOS to make this temperature perturbation occur at constant 
-    ! pressure
-    temp_row(1) = temp
-    p_row(1) = p0
-    den_row(1) = s0(rho_comp)
-    xn_zone(:) = s0(spec_comp:)/s0(rho_comp)
-
-    input_flag = 3      ! (t, p) -> (rho, h)
-
-    call eos(input_flag, den_row, temp_row, &
-             npts, nspec, &
-             xn_zone, aion, zion, &
-             p_row, h_row, e_row, &
-             cv_row, cp_row, xne_row, eta_row, pele_row, &
-             dpdt_row, dpdr_row, dedt_row, dedr_row, &
-             dpdX_row, dhdX_row, &
-             gam1_row, cs_row, s_row, &
-             dsdt_row, dsdr_row, &
-             do_diag)
-
-    dens_pert = den_row(1)
-    rhoh_pert = den_row(1)*h_row(1)
-    rhoX_pert(:) = dens_pert*xn_zone(:)
-
-    temp_pert = temp
-    
-!   if (r1 .lt. 2.0) then
-!     trac_pert(:) = ONE
-!   else
-      trac_pert(:) = ZERO
-!   end if
-
-  end subroutine perturb_3d
-
   subroutine init_base_state (model_file,n_base,s0,p0,gam1,dx,prob_lo,prob_hi)
 
     character (len=256), intent(in) :: model_file ! I'm not using this anymore
@@ -511,9 +323,10 @@ contains
     ! first set the inflow boundary condition
     call asin1d(lamsolfile, -.00125d0, 0.d0, state1d, ndum, .false.)
 
+    Pamb = state1d(18)
+    p_row(1) = Pamb
+
     den_row(1) = state1d(3)
-    p_row(1) = state1d(18)
-    Pamb = p_row(1)
     temp_row(1) = state1d(9)
     do j=1,nspec
        if(spec_names(j) .eq. "carbon-12") then
@@ -587,8 +400,8 @@ contains
 
        call asin1d(lamsolfile, loloc, hiloc, state1d, ndum, .false.)
 
-       den_row(1) = state1d(3)
        p_row(1) = Pamb
+       den_row(1) = state1d(3)
        temp_row(1) = state1d(9)
        do j=1,nspec
           if(spec_names(j) .eq. "carbon-12") then
