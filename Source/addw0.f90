636949974f319d792b89f24d413eeb6975633611
ajnonaka <ajnonaka>
2007-12-11 20:14:52 +0000

    the loop over nlevs is now inside of addw0.f90

--- Source/addw0.f90
+++ Source/addw0.f90
@@ -13,122 +13,125 @@ module addw0_module
 
 contains
 
-      subroutine addw0(umac,w0,w0_cart,dx,mult)
+  subroutine addw0(nlevs,umac,w0,w0_cart,dx,mult)
 
-      type(multifab) , intent(inout) :: umac(:)
-      real(kind=dp_t), intent(in   ) :: w0(0:)
-      type(multifab) , intent(in   ) :: w0_cart
-      real(kind=dp_t), intent(in   ) :: dx(:),mult
+    integer        , intent(in   ) :: nlevs
+    type(multifab) , intent(inout) :: umac(:,:)
+    real(kind=dp_t), intent(in   ) :: w0(:,0:)
+    type(multifab) , intent(in   ) :: w0_cart(:)
+    real(kind=dp_t), intent(in   ) :: dx(:,:),mult
 
-      ! Local variables
-      integer :: i,lo(umac(1)%dim),hi(umac(1)%dim),dm
-      real(kind=dp_t), pointer :: ump(:,:,:,:)
-      real(kind=dp_t), pointer :: vmp(:,:,:,:)
-      real(kind=dp_t), pointer :: wmp(:,:,:,:)
-      real(kind=dp_t), pointer :: w0p(:,:,:,:)
+    ! Local variables
+    integer :: i,lo(umac(1,1)%dim),hi(umac(1,1)%dim),dm,n
+    real(kind=dp_t), pointer :: ump(:,:,:,:)
+    real(kind=dp_t), pointer :: vmp(:,:,:,:)
+    real(kind=dp_t), pointer :: wmp(:,:,:,:)
+    real(kind=dp_t), pointer :: w0p(:,:,:,:)
 
-      dm = umac(1)%dim
+    dm = umac(1,1)%dim
 
-      do i = 1, umac(dm)%nboxes
-         if ( multifab_remote(umac(dm), i) ) cycle
-         wmp  => dataptr(umac(dm), i)
-         lo =  lwb(get_box(umac(dm), i))
-         hi =  upb(get_box(umac(dm), i))
-         select case(dm)
-         case(2)
-           call addw0_2d(wmp(:,:,1,1),w0,lo,hi,dx,mult)
-         case(3)
-           if (spherical .eq. 0) then
-             call addw0_3d(wmp(:,:,:,1),w0,lo,hi,dx,mult)
-           else
-             ump  => dataptr(umac(1), i)
-             vmp  => dataptr(umac(2), i)
-             wmp  => dataptr(umac(3), i)
-             w0p  => dataptr(w0_cart, i)
-               lo =  lwb(get_box(w0_cart, i))
-               hi =  upb(get_box(w0_cart, i))
-             call addw0_3d_sphr(ump(:,:,:,1),vmp(:,:,:,1),wmp(:,:,:,1),w0p(:,:,:,:), &
-                                lo,hi,mult)
-           end if
-         end select
-      end do
+    do n = 1, nlevs
+       do i = 1, umac(n,dm)%nboxes
+          if ( multifab_remote(umac(n,dm), i) ) cycle
+          wmp  => dataptr(umac(n,dm), i)
+          lo =  lwb(get_box(umac(n,dm), i))
+          hi =  upb(get_box(umac(n,dm), i))
+          select case(dm)
+          case(2)
+             call addw0_2d(wmp(:,:,1,1),w0(n,:),lo,hi,dx(n,:),mult)
+          case(3)
+             if (spherical .eq. 0) then
+                call addw0_3d(wmp(:,:,:,1),w0(n,:),lo,hi,dx(n,:),mult)
+             else
+                ump  => dataptr(umac(n,1), i)
+                vmp  => dataptr(umac(n,2), i)
+                wmp  => dataptr(umac(n,3), i)
+                w0p  => dataptr(w0_cart(n), i)
+                lo =  lwb(get_box(w0_cart(n), i))
+                hi =  upb(get_box(w0_cart(n), i))
+                call addw0_3d_sphr(ump(:,:,:,1),vmp(:,:,:,1),wmp(:,:,:,1),w0p(:,:,:,:), &
+                                   lo,hi,mult)
+             end if
+          end select
+       end do
+    end do
 
-      end subroutine addw0
+  end subroutine addw0
 
-      subroutine addw0_2d(vmac,w0,lo,hi,dx,mult)
+  subroutine addw0_2d(vmac,w0,lo,hi,dx,mult)
 
-      integer        , intent(in   ) :: lo(:),hi(:)
-      real(kind=dp_t), intent(inout) :: vmac(lo(1)- 1:,lo(2)- 1:)
-      real(kind=dp_t), intent(in   ) ::   w0(0:)
-      real(kind=dp_t), intent(in   ) :: mult
-      real(kind=dp_t), intent(in   ) :: dx(:)
+    integer        , intent(in   ) :: lo(:),hi(:)
+    real(kind=dp_t), intent(inout) :: vmac(lo(1)- 1:,lo(2)- 1:)
+    real(kind=dp_t), intent(in   ) ::   w0(0:)
+    real(kind=dp_t), intent(in   ) :: mult
+    real(kind=dp_t), intent(in   ) :: dx(:)
 
-      integer :: i,j
+    integer :: i,j
 
-      do j = lo(2)  ,hi(2)
-         do i = lo(1)-1,hi(1)+1
-            vmac(i,j) = vmac(i,j) + mult * w0(j)
-         end do
-      end do
+    do j = lo(2)  ,hi(2)
+       do i = lo(1)-1,hi(1)+1
+          vmac(i,j) = vmac(i,j) + mult * w0(j)
+       end do
+    end do
 
-      end subroutine addw0_2d
+  end subroutine addw0_2d
 
-      subroutine addw0_3d(wmac,w0,lo,hi,dx,mult)
+  subroutine addw0_3d(wmac,w0,lo,hi,dx,mult)
 
-      integer        , intent(in   ) :: lo(:),hi(:)
-      real(kind=dp_t), intent(inout) :: wmac(lo(1)-1:,lo(2)-1:,lo(3)-1:)
-      real(kind=dp_t), intent(in   ) ::   w0(0:)
-      real(kind=dp_t), intent(in   ) :: mult
-      real(kind=dp_t), intent(in   ) :: dx(:)
+    integer        , intent(in   ) :: lo(:),hi(:)
+    real(kind=dp_t), intent(inout) :: wmac(lo(1)-1:,lo(2)-1:,lo(3)-1:)
+    real(kind=dp_t), intent(in   ) ::   w0(0:)
+    real(kind=dp_t), intent(in   ) :: mult
+    real(kind=dp_t), intent(in   ) :: dx(:)
 
-      integer :: i,j,k
+    integer :: i,j,k
 
-      do k = lo(3),hi(3)
-         do j = lo(2)-1,hi(2)+1
-            do i = lo(1)-1,hi(1)+1
-               wmac(i,j,k) = wmac(i,j,k) + mult * w0(k)
-            end do
-         end do
-      end do
+    do k = lo(3),hi(3)
+       do j = lo(2)-1,hi(2)+1
+          do i = lo(1)-1,hi(1)+1
+             wmac(i,j,k) = wmac(i,j,k) + mult * w0(k)
+          end do
+       end do
+    end do
 
-      end subroutine addw0_3d
+  end subroutine addw0_3d
 
-      subroutine addw0_3d_sphr(umac,vmac,wmac,w0_cart,lo,hi,mult)
+  subroutine addw0_3d_sphr(umac,vmac,wmac,w0_cart,lo,hi,mult)
 
-      integer        , intent(in   ) :: lo(:),hi(:)
-      real(kind=dp_t), intent(inout) ::   umac(lo(1)-1:,lo(2)-1:,lo(3)-1:)
-      real(kind=dp_t), intent(inout) ::   vmac(lo(1)-1:,lo(2)-1:,lo(3)-1:)
-      real(kind=dp_t), intent(inout) ::   wmac(lo(1)-1:,lo(2)-1:,lo(3)-1:)
-      real(kind=dp_t), intent(in   ) :: w0_cart(lo(1)-1:,lo(2)-1:,lo(3)-1:,:)
-      real(kind=dp_t), intent(in   ) :: mult
+    integer        , intent(in   ) :: lo(:),hi(:)
+    real(kind=dp_t), intent(inout) ::   umac(lo(1)-1:,lo(2)-1:,lo(3)-1:)
+    real(kind=dp_t), intent(inout) ::   vmac(lo(1)-1:,lo(2)-1:,lo(3)-1:)
+    real(kind=dp_t), intent(inout) ::   wmac(lo(1)-1:,lo(2)-1:,lo(3)-1:)
+    real(kind=dp_t), intent(in   ) :: w0_cart(lo(1)-1:,lo(2)-1:,lo(3)-1:,:)
+    real(kind=dp_t), intent(in   ) :: mult
 
-      integer :: i,j,k
+    integer :: i,j,k
 
-      do k = lo(3),hi(3)
-      do j = lo(2),hi(2)
-      do i = lo(1),hi(1)+1
-         umac(i,j,k) = umac(i,j,k) + mult * &
-                       HALF * ( w0_cart(i  ,j,k,1) +w0_cart(i-1,j,k,1) )
-      end do
-      end do
-      end do
-      do k = lo(3),hi(3)
-      do j = lo(2),hi(2)+1
-      do i = lo(1),hi(1)
-         vmac(i,j,k) = vmac(i,j,k) + mult * &
-                       HALF * ( w0_cart(i,j  ,k,2) + w0_cart(i,j-1,k,2) )
-      end do
-      end do
-      end do
-      do k = lo(3),hi(3)+1
-      do j = lo(2),hi(2)
-      do i = lo(1),hi(1)
-         wmac(i,j,k) = wmac(i,j,k) + mult * &
-                       HALF * ( w0_cart(i,j,k  ,3) + w0_cart(i,j,k-1,3) )
-      end do
-      end do
-      end do
+    do k = lo(3),hi(3)
+       do j = lo(2),hi(2)
+          do i = lo(1),hi(1)+1
+             umac(i,j,k) = umac(i,j,k) + mult * &
+                  HALF * ( w0_cart(i  ,j,k,1) +w0_cart(i-1,j,k,1) )
+          end do
+       end do
+    end do
+    do k = lo(3),hi(3)
+       do j = lo(2),hi(2)+1
+          do i = lo(1),hi(1)
+             vmac(i,j,k) = vmac(i,j,k) + mult * &
+                  HALF * ( w0_cart(i,j  ,k,2) + w0_cart(i,j-1,k,2) )
+          end do
+       end do
+    end do
+    do k = lo(3),hi(3)+1
+       do j = lo(2),hi(2)
+          do i = lo(1),hi(1)
+             wmac(i,j,k) = wmac(i,j,k) + mult * &
+                  HALF * ( w0_cart(i,j,k  ,3) + w0_cart(i,j,k-1,3) )
+          end do
+       end do
+    end do
 
-      end subroutine addw0_3d_sphr
+  end subroutine addw0_3d_sphr
 
 end module addw0_module
--- scalar_advance.f90
+++ scalar_advance.f90
@@ -32,7 +32,7 @@ contains
   subroutine scalar_advance(nlevs,mla,which_step,uold,sold,snew,thermal,umac,w0, &
                             w0_cart_vec,eta,sedge,utrans,ext_scal_force,normal, &
                             s0_old,s0_new,p0_old,p0_new,dx,dt,the_bc_level,verbose)
- 
+
     integer        , intent(in   ) :: nlevs
     type(ml_layout), intent(inout) :: mla
     integer        , intent(in   ) :: which_step
@@ -60,11 +60,10 @@ contains
     type(multifab), allocatable :: scal_force(:)
     type(multifab), allocatable :: s0_old_cart(:)
     type(multifab), allocatable :: s0_new_cart(:)
-    
+
     real(kind=dp_t), allocatable :: s0_edge_old(:,:)
     real(kind=dp_t), allocatable :: s0_edge_new(:,:)
 
-    real(dp_t) :: mult
     real(dp_t) :: smin,smax
 
     type(box) :: domain
@@ -73,14 +72,14 @@ contains
     integer :: velpred,comp,n,dm,ng_cell
 
     logical :: is_vel
-    
+
     allocate(scal_force(nlevs))
     allocate(s0_old_cart(nlevs))
     allocate(s0_new_cart(nlevs))
 
     allocate(s0_edge_old(0:nr(nlevs),nscal))
     allocate(s0_edge_new(0:nr(nlevs),nscal))
-    
+
     velpred = 0    
     is_vel = .false.
     ng_cell = sold(1)%ng
@@ -99,15 +98,15 @@ contains
        call setval(s0_old_cart(n),ZERO,all=.true.)
        call setval(s0_new_cart(n),ZERO,all=.true.)
     enddo
-    
-!**************************************************************************
-!     Create scalar source term at time n for (rho X)_i and (rho H).  
-!     The source term for (rho X) is zero.
-!     The source term for (rho h) has only the w dp0/dr term.
-!
-!     The call to modify_scal_force is used to add those advective terms 
-!     that appear as forces when we write it in convective/perturbational form.
-!**************************************************************************
+
+    !**************************************************************************
+    !     Create scalar source term at time n for (rho X)_i and (rho H).  
+    !     The source term for (rho X) is zero.
+    !     The source term for (rho h) has only the w dp0/dr term.
+    !
+    !     The call to modify_scal_force is used to add those advective terms 
+    !     that appear as forces when we write it in convective/perturbational form.
+    !**************************************************************************
 
     ! Define s0_old_cart and s0_new_cart
     if (spherical .eq. 1) then
@@ -122,9 +121,9 @@ contains
           call fill_3d_data_wrapper(nlevs,s0_new_cart,s0_new(:,:,comp),dx,comp)
        enddo
     end if
-    
+
     do n = 1, nlevs
-   
+
        domain = layout_get_pd(uold(n)%la)
        domlo = lwb(domain)
        domhi = upb(domain)
@@ -133,7 +132,7 @@ contains
        ! call makeTfromRhoH(sold(n),s0_old(n,:,temp_comp))
        ! call multifab_fill_boundary_c(sold(n),temp_comp,1)
        ! call multifab_physbc(sold(n),temp_comp,dm+temp_comp,1,dx(n,:),the_bc_level(n))
-       
+
        ! make force for species
        call modify_scal_force(n,scal_force(n),sold(n),umac(n,:),s0_old(n,:,:),s0_edge_old, &
                               w0(n,:),dx(n,:),domlo,domhi,s0_old_cart(n),spec_comp,nspec)
@@ -143,7 +142,7 @@ contains
        do comp = spec_comp, spec_comp+nspec-1
           call multifab_physbc(scal_force(n),comp,foextrap_comp,1,dx(n,:),the_bc_level(n))
        enddo
-       
+
        if(use_temp_in_mkflux) then
 
           ! make force for temperature
@@ -162,9 +161,9 @@ contains
           call modify_scal_force(n,scal_force(n),sold(n),umac(n,:),s0_old(n,:,:), &
                                  s0_edge_old,w0(n,:),dx(n,:),domlo,domhi,s0_old_cart(n), &
                                  rhoh_comp,1)
-
+          
           if(use_thermal_diffusion) then
-            call multifab_plus_plus_c(scal_force(n),rhoh_comp,thermal(n),1,1)
+             call multifab_plus_plus_c(scal_force(n),rhoh_comp,thermal(n),1,1)
           endif
 
           call multifab_fill_boundary_c(scal_force(n),rhoh_comp,1)
@@ -172,23 +171,26 @@ contains
           call multifab_physbc(scal_force(n),rhoh_comp,foextrap_comp,1,dx(n,:), &
                                the_bc_level(n))
        endif
-       
-!**************************************************************************
-!     Add w0 to MAC velocities (trans velocities already have w0).
-!**************************************************************************
 
-       mult = ONE
-       call addw0(umac(n,:),w0(n,:),w0_cart_vec(n),dx(n,:),mult)
+    end do
 
-!**************************************************************************
-!     Create the edge states of (rho h)' and (rho X)_i.
-!**************************************************************************
+    !**************************************************************************
+    !     Add w0 to MAC velocities (trans velocities already have w0).
+    !**************************************************************************
+
+    call addw0(nlevs,umac,w0,w0_cart_vec,dx,mult=ONE)
+
+    do n = 1, nlevs
+
+       !**************************************************************************
+       !     Create the edge states of (rho h)' and (rho X)_i.
+       !**************************************************************************
 
        if (.not. use_temp_in_mkflux) &
             call put_in_pert_form(n,sold(n),s0_old(n,:,:),dx(n,:),rhoh_comp,1,.true.)
-       
+
        call put_in_pert_form(n,sold(n),s0_old(n,:,:),dx(n,:),spec_comp,nspec,.true.)
-       
+
        if (use_temp_in_mkflux) then
           comp = temp_comp
        else
@@ -196,90 +198,89 @@ contains
        end if
 
     end do ! end loop over levels
-       
+
     ! create temperature or enthalpy edge states
     call mkflux(nlevs,sold,uold,sedge,umac,utrans,scal_force,w0,w0_cart_vec,dx,dt,is_vel, &
                 the_bc_level,velpred,comp,dm+comp,1)
-       
+
     ! create species edge states
     call mkflux(nlevs,sold,uold,sedge,umac,utrans,scal_force,w0,w0_cart_vec,dx,dt,is_vel, &
                 the_bc_level,velpred,spec_comp,dm+spec_comp,nspec)
-    
+
     do n=1,nlevs
 
        if(use_temp_in_mkflux) then
           call makeRhoHfromT(uold(n),sedge(n,:),s0_old(n,:,:),s0_edge_old,s0_new(n,:,:), &
                              s0_edge_new)
        endif
-       
+
        if (.not. use_temp_in_mkflux) &
             call put_in_pert_form(n,sold(n),s0_old(n,:,:),dx(n,:),rhoh_comp,1,.false.)
-       
+
        call put_in_pert_form(n,sold(n),s0_old(n,:,:),dx(n,:),spec_comp,nspec,.false.)
 
     end do
-       
-!**************************************************************************
-!     Create the edge states of tracers.
-!**************************************************************************
+
+    !**************************************************************************
+    !     Create the edge states of tracers.
+    !**************************************************************************
 
     if (ntrac .ge. 1) then
        call mkflux(nlevs,sold,uold,sedge,umac,utrans,scal_force,w0,w0_cart_vec,dx,dt, &
                    is_vel,the_bc_level,velpred,trac_comp,dm+trac_comp,ntrac)
     end if
-    
+
+    !**************************************************************************
+    !     Subtract w0 from MAC velocities.
+    !**************************************************************************
+
+    call addw0(nlevs,umac,w0,w0_cart_vec,dx,mult=-ONE)
+
     do n=1,nlevs
-    
-!**************************************************************************
-!     Subtract w0 from MAC velocities.
-!**************************************************************************
 
-       mult = -ONE
-       call addw0(umac(n,:),w0(n,:),w0_cart_vec(n),dx(n,:),mult)
-
-!**************************************************************************
-!     1) Set force for (rho X)_i at time n+1/2 = 0.
-!     2) Update (rho X)'_i with conservative differencing.
-!     3) Define density as the sum of the (rho X)_i
-!**************************************************************************
+       !**************************************************************************
+       !     1) Set force for (rho X)_i at time n+1/2 = 0.
+       !     2) Update (rho X)'_i with conservative differencing.
+       !     3) Define density as the sum of the (rho X)_i
+       !**************************************************************************
 
        call setval(scal_force(n),ZERO)
-       
+
        call update_scal(which_step,spec_comp,spec_comp+nspec-1,sold(n),snew(n),umac(n,:), &
                         w0(n,:),w0_cart_vec(n),eta(n,:,:),sedge(n,:),scal_force(n), &
                         s0_old(n,:,:),s0_edge_old,s0_new(n,:,:),s0_edge_new,s0_old_cart(n), &
                         s0_new_cart(n),domlo,domhi,dx(n,:),dt,evolve_base_state)
-       
+
        if (verbose .ge. 1) then
           do comp = spec_comp,spec_comp+nspec-1
              call multifab_div_div_c(snew(n),comp,snew(n),rho_comp,1)
-             
+
              smin = multifab_min_c(snew(n),comp) 
              smax = multifab_max_c(snew(n),comp)
-             
+
              if (parallel_IOProcessor()) &
                   write(6,2002) spec_names(comp-spec_comp+1), smin,smax
              call multifab_mult_mult_c(snew(n),comp,snew(n),rho_comp,1)
           end do
-          
+
           smin = multifab_min_c(snew(n),rho_comp) 
           smax = multifab_max_c(snew(n),rho_comp)
-          
+
           if (parallel_IOProcessor()) &
                write(6,2000) smin,smax
        end if
-       
-!**************************************************************************
-!     2) Update tracers with convective differencing.
-!**************************************************************************
-      
+
+       !**************************************************************************
+       !     2) Update tracers with convective differencing.
+       !**************************************************************************
+
        if (ntrac .ge. 1) then
           call update_scal(which_step,trac_comp,trac_comp+ntrac-1,sold(n),snew(n), &
                            umac(n,:),w0(n,:),w0_cart_vec(n),eta(n,:,:),sedge(n,:), &
                            scal_force(n),s0_old(n,:,:),s0_edge_old,s0_new(n,:,:), &
                            s0_edge_new,s0_old_cart(n),s0_new_cart(n),domlo,domhi,dx(n,:), &
                            dt,evolve_base_state)
-          
+
           if (verbose .eq. 1) then
              smin = multifab_min_c(snew(n),trac_comp) 
              smax = multifab_max_c(snew(n),trac_comp)
@@ -287,28 +288,28 @@ contains
                   write(6,2003) smin,smax
           end if
        end if
-       
-!**************************************************************************
-!     1) Create (rhoh)' force at time n+1/2.
-!          (NOTE: we don't worry about filling ghost cells of the scal_force
-!                 because we only need them in valid regions...)     
-!     2) Update (rho h)' with conservative differencing.
-!**************************************************************************
 
-       
+       !**************************************************************************
+       !     1) Create (rhoh)' force at time n+1/2.
+       !          (NOTE: we don't worry about filling ghost cells of the scal_force
+       !                 because we only need them in valid regions...)     
+       !     2) Update (rho h)' with conservative differencing.
+       !**************************************************************************
+
+
        call mkrhohforce(n,scal_force(n),rhoh_comp,umac(n,:),p0_old(n,:),p0_new(n,:), &
                         normal(n),dx(n,:))
-       
+
        call update_scal(which_step,rhoh_comp,rhoh_comp,sold(n),snew(n), &
                         umac(n,:),w0(n,:),w0_cart_vec(n),eta(n,:,:),sedge(n,:), &
                         scal_force(n),s0_old(n,:,:),s0_edge_old,s0_new(n,:,:),s0_edge_new, &
                         s0_old_cart(n),s0_new_cart(n),domlo,domhi,dx(n,:),dt, &
                         evolve_base_state)
-       
+
        if(.not. use_thermal_diffusion) then
           call makeTfromRhoH(snew(n),s0_new(n,:,temp_comp))
        endif
-       
+
        if (verbose .eq. 1) then
           smin = multifab_min_c(snew(n),rhoh_comp) 
           smax = multifab_max_c(snew(n),rhoh_comp)
@@ -317,11 +318,11 @@ contains
              write(6,2004) 
           end if
        end if
-      
-!**************************************************************************
-!     Call fill_boundary for all components of snew
-!**************************************************************************
-       
+
+       !**************************************************************************
+       !     Call fill_boundary for all components of snew
+       !**************************************************************************
+
        call multifab_fill_boundary(snew(n))
        call multifab_physbc(snew(n),rho_comp,dm+rho_comp,nscal,dx(n,:),the_bc_level(n))
 
@@ -329,27 +330,27 @@ contains
 
     do n = nlevs, 2, -1
        call ml_cc_restriction(snew(n-1),snew(n),mla%mba%rr(n-1,:))
-       
+
        call multifab_fill_ghost_cells(snew(n),snew(n-1), &
                                       ng_cell,mla%mba%rr(n-1,:), &
                                       the_bc_level(n-1), the_bc_level(n), &
                                       1,dm+rho_comp,nscal)
     end do
-    
+
     do n = 1,nlevs
        call multifab_destroy(scal_force(n))
        call multifab_destroy(s0_old_cart(n))
        call multifab_destroy(s0_new_cart(n))
     enddo
-    
+
     deallocate(s0_edge_old,s0_edge_new)
-    
+
 2000 format('... new min/max : density           ',e17.10,2x,e17.10)
 2001 format('... new min/max : rho * H           ',e17.10,2x,e17.10)
 2002 format('... new min/max : ',a16,2x,e17.10,2x,e17.10)
 2003 format('... new min/max : tracer            ',e17.10,2x,e17.10)
 2004 format(' ')
-    
+
   end subroutine scalar_advance
 
 end module scalar_advance_module
