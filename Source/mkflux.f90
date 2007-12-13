4afef88f76d063a2831570e5fd04d17622fba99f
ajnonaka <ajnonaka>
2007-12-13 00:39:02 +0000

    mkflux for spherical case written; not implemented yet in update_scal though

--- Source/mkflux.f90
+++ Source/mkflux.f90
@@ -11,6 +11,7 @@ module mkflux_module
   use ml_restriction_module
   use variables
   use network, ONLY: nspec
+  use addw0_module
 
   implicit none
 
@@ -25,7 +26,8 @@ contains
 
     integer        , intent(in   ) :: nlevs
     type(multifab) , intent(inout) :: sflux(:,:)
-    type(multifab) , intent(in   ) :: sold(:),sedge(:,:),umac(:,:)
+    type(multifab) , intent(in   ) :: sold(:),sedge(:,:)
+    type(multifab) , intent(inout) :: umac(:,:)
     real(kind=dp_t), intent(in   ) :: w0(:,0:)
     type(multifab) , intent(in   ) :: w0_cart_vec(:)
     real(kind=dp_t), intent(in   ) :: s0_old(:,0:,:),s0_edge_old(:,0:,:)
@@ -36,7 +38,10 @@ contains
     real(kind=dp_t), intent(in   ) :: dx(:,:)
     type(ml_layout), intent(inout) :: mla
 
-    ! local
+    ! local    
+    type(box) :: domain
+
+    integer :: domlo(sold(1)%dim),domhi(sold(1)%dim)
     integer :: i,dm,n,comp
     integer :: lo(sold(1)%dim),hi(sold(1)%dim)
 
@@ -50,12 +55,16 @@ contains
     real(kind=dp_t), pointer :: vmp(:,:,:,:)
     real(kind=dp_t), pointer :: wmp(:,:,:,:)
     real(kind=dp_t), pointer :: w0p(:,:,:,:)
-    real(kind=dp_t), pointer :: w0op(:,:,:,:)
-    real(kind=dp_t), pointer :: w0np(:,:,:,:)
+    real(kind=dp_t), pointer :: s0op(:,:,:,:)
+    real(kind=dp_t), pointer :: s0np(:,:,:,:)
 
     dm = sold(1)%dim
     
     do n=1,nlevs
+
+       domain = layout_get_pd(sold(n)%la)
+       domlo = lwb(domain)
+       domhi = upb(domain)
 
        do i=1, sold(n)%nboxes
           if ( multifab_remote(sold(n),i) ) cycle
@@ -90,13 +99,16 @@ contains
                                     startcomp,endcomp,which_step,lo,hi)
 
              else
+                s0op => dataptr(s0_old_cart(n), i)
+                s0np => dataptr(s0_new_cart(n), i)
+                w0p => dataptr(w0_cart_vec(n),i)
                 call mkflux_3d_sphr(sfxp(:,:,:,:), sfyp(:,:,:,:), sfzp(:,:,:,:), &
                                     sexp(:,:,:,:), seyp(:,:,:,:), sezp(:,:,:,:), &
                                     ump(:,:,:,1), vmp(:,:,:,1), wmp(:,:,:,1), &
-                                    s0_old(n,:,:), s0_edge_old(n,:,:), &
-                                    s0_new(n,:,:), s0_edge_new(n,:,:), &
-                                    w0(n,:), &
-                                    startcomp,endcomp,which_step,lo,hi)
+                                    s0_old(n,:,:), s0_edge_old(n,:,:), s0op(:,:,:,:), &
+                                    s0_new(n,:,:), s0_edge_new(n,:,:), s0np(:,:,:,:), &
+                                    w0(n,:), w0p(:,:,:,:), &
+                                    startcomp,endcomp,which_step,lo,hi,domlo,domhi)
              endif
           end select
        end do
@@ -250,28 +262,178 @@ contains
   end subroutine mkflux_3d_cart
 
   subroutine mkflux_3d_sphr(sfluxx,sfluxy,sfluxz,sedgex,sedgey,sedgez,umac,vmac,wmac, &
-                            s0_old,s0_edge_old,s0_new,s0_edge_new,w0,startcomp,endcomp, &
-                            which_step,lo,hi)
+                            s0_old,s0_edge_old,s0_old_cart,s0_new,s0_edge_new,s0_new_cart, &
+                            w0,w0_cart,startcomp,endcomp,which_step,lo,hi,domlo,domhi)
 
-    integer        , intent(in   ) :: lo(:),hi(:)
+    integer        , intent(in   ) :: lo(:),hi(:),domlo(:),domhi(:)
     real(kind=dp_t), intent(inout) :: sfluxx(lo(1)  :,lo(2)  :,lo(3)  :,:)
     real(kind=dp_t), intent(inout) :: sfluxy(lo(1)  :,lo(2)  :,lo(3)  :,:)
     real(kind=dp_t), intent(inout) :: sfluxz(lo(1)  :,lo(2)  :,lo(3)  :,:)
     real(kind=dp_t), intent(in   ) :: sedgex(lo(1)  :,lo(2)  :,lo(3)  :,:)
     real(kind=dp_t), intent(in   ) :: sedgey(lo(1)  :,lo(2)  :,lo(3)  :,:)
     real(kind=dp_t), intent(in   ) :: sedgez(lo(1)  :,lo(2)  :,lo(3)  :,:)
-    real(kind=dp_t), intent(in   ) ::   umac(lo(1)-1:,lo(2)-1:,lo(3)-1:)
-    real(kind=dp_t), intent(in   ) ::   vmac(lo(1)-1:,lo(2)-1:,lo(3)-1:)
-    real(kind=dp_t), intent(in   ) ::   wmac(lo(1)-1:,lo(2)-1:,lo(3)-1:)
-    real(kind=dp_t), intent(in   ) :: s0_old(0:,:), s0_edge_old(0:,:)
-    real(kind=dp_t), intent(in   ) :: s0_new(0:,:), s0_edge_new(0:,:)
-    real(kind=dp_t), intent(in   ) :: w0(0:)
+    real(kind=dp_t), intent(inout) ::   umac(lo(1)-1:,lo(2)-1:,lo(3)-1:)
+    real(kind=dp_t), intent(inout) ::   vmac(lo(1)-1:,lo(2)-1:,lo(3)-1:)
+    real(kind=dp_t), intent(inout) ::   wmac(lo(1)-1:,lo(2)-1:,lo(3)-1:)
+    real(kind=dp_t), intent(in   ) ::      s0_old(0:,:)
+    real(kind=dp_t), intent(in   ) :: s0_edge_old(0:,:)
+    real(kind=dp_t), intent(in   ) :: s0_old_cart(lo(1)-1:,lo(2)-1:,lo(3)-1:,:)
+    real(kind=dp_t), intent(in   ) ::      s0_new(0:,:)
+    real(kind=dp_t), intent(in   ) :: s0_edge_new(0:,:)
+    real(kind=dp_t), intent(in   ) :: s0_new_cart(lo(1)-1:,lo(2)-1:,lo(3)-1:,:)
+    real(kind=dp_t), intent(in   ) ::          w0(0:)
+    real(kind=dp_t), intent(in   ) ::     w0_cart(lo(1)-1:,lo(2)-1:,lo(3)-1:,:)
     integer        , intent(in   ) :: startcomp,endcomp,which_step
 
+    ! local
+    integer         :: i,j,k,comp
+    real(kind=dp_t) :: mult
+    real(kind=dp_t) :: bc_lox,bc_loy,bc_loz
+    real(kind=dp_t) :: bc_hix,bc_hiy,bc_hiz
 
+    ! Note the umac here does NOT have w0 in it
 
+    do comp = startcomp, endcomp
 
+       ! loop for x-fluxes
+       do k = lo(3), hi(3)
+          do j = lo(2), hi(2)
+             do i = lo(1), hi(1)+1
 
+                if (which_step .eq. 1) then
+
+                   bc_lox = (s0_old_cart(i,j,k,comp)+s0_old_cart(i-1,j,k,comp)) * HALF
+                   bc_hix = (s0_old_cart(i,j,k,comp)+s0_old_cart(i+1,j,k,comp)) * HALF
+                   
+                   if (i.eq.domlo(1)) bc_lox = s0_old_cart(i,j,k,comp)
+                   if (i.eq.domhi(1)+1) bc_hix = s0_old_cart(i,j,k,comp)
+
+                else if (which_step .eq. 2) then
+
+                   bc_lox = (s0_old_cart(i,j,k,comp)+s0_old_cart(i-1,j,k,comp) &
+                        +s0_new_cart(i,j,k,comp)+s0_new_cart(i-1,j,k,comp) ) * FOURTH
+                   bc_hix = (s0_old_cart(i,j,k,comp)+s0_old_cart(i+1,j,k,comp) &
+                        +s0_new_cart(i,j,k,comp)+s0_new_cart(i+1,j,k,comp) ) * FOURTH
+
+                   if (i.eq.domlo(1)) bc_lox = &
+                        HALF * (s0_old_cart(i,j,k,comp)+s0_new_cart(i,j,k,comp))
+                   if (i.eq.domhi(1)+1) bc_hix = &
+                        HALF * (s0_old_cart(i,j,k,comp)+s0_new_cart(i,j,k,comp))
+
+                end if
+
+                sfluxx(i,j,k,comp) = bc_hix*umac(i+1,j,k) - bc_lox*umac(i,j,k)
+                
+             end do
+          end do
+       end do
+
+       ! loop for y-fluxes
+       do k = lo(3), hi(3)
+          do j = lo(2), hi(2)+1
+             do i = lo(1), hi(1)
+
+                if (which_step .eq. 1) then
+
+                   bc_loy = (s0_old_cart(i,j,k,comp)+s0_old_cart(i,j-1,k,comp)) * HALF
+                   bc_hiy = (s0_old_cart(i,j,k,comp)+s0_old_cart(i,j+1,k,comp)) * HALF
+                   
+                   if (j.eq.domlo(2)) bc_loy = s0_old_cart(i,j,k,comp)
+                   if (j.eq.domhi(2)+1) bc_hiy = s0_old_cart(i,j,k,comp)
+
+                else if (which_step .eq. 2) then
+
+                   bc_loy = (s0_old_cart(i,j,k,comp)+s0_old_cart(i,j-1,k,comp) &
+                        +s0_new_cart(i,j,k,comp)+s0_new_cart(i,j-1,k,comp) ) * FOURTH
+                   bc_hiy = (s0_old_cart(i,j,k,comp)+s0_old_cart(i,j+1,k,comp) &
+                        +s0_new_cart(i,j,k,comp)+s0_new_cart(i,j+1,k,comp) ) * FOURTH
+
+                   if (j.eq.domlo(2)) bc_loy = &
+                        HALF * (s0_old_cart(i,j,k,comp)+s0_new_cart(i,j,k,comp))
+                   if (j.eq.domhi(2)+1) bc_hiy = &
+                        HALF * (s0_old_cart(i,j,k,comp)+s0_new_cart(i,j,k,comp))
+
+                end if
+
+                sfluxy(i,j,k,comp) = bc_hiy*vmac(i,j+1,k) - bc_loy*vmac(i,j,k)
+
+             end do
+          end do
+       end do
+
+       ! loop for z-fluxes
+       do k = lo(3), hi(3)+1
+          do j = lo(2), hi(2)
+             do i = lo(1), hi(1)
+
+                if (which_step .eq. 1) then
+
+                   bc_loz = (s0_old_cart(i,j,k,comp)+s0_old_cart(i,j,k-1,comp)) * HALF
+                   bc_hiz = (s0_old_cart(i,j,k,comp)+s0_old_cart(i,j,k+1,comp)) * HALF
+                   
+                   if (k.eq.domlo(3)) bc_loz = s0_old_cart(i,j,k,comp)
+                   if (k.eq.domhi(3)+1) bc_hiz = s0_old_cart(i,j,k,comp)
+
+                else if (which_step .eq. 2) then
+
+                   bc_loz = (s0_old_cart(i,j,k,comp)+s0_old_cart(i,j,k-1,comp) &
+                        +s0_new_cart(i,j,k,comp)+s0_new_cart(i,j,k-1,comp) ) * FOURTH
+                   bc_hiz = (s0_old_cart(i,j,k,comp)+s0_old_cart(i,j,k+1,comp) &
+                        +s0_new_cart(i,j,k,comp)+s0_new_cart(i,j,k+1,comp) ) * FOURTH
+
+                   if (k.eq.domlo(3)) bc_loz = &
+                        HALF * (s0_old_cart(i,j,k,comp)+s0_new_cart(i,j,k,comp))
+                   if (k.eq.domhi(3)+1) bc_hiz = &
+                        HALF * (s0_old_cart(i,j,k,comp)+s0_new_cart(i,j,k,comp))
+
+                end if
+
+                sfluxz(i,j,k,comp) = bc_hiz*wmac(i,j,k+1) - bc_loz*wmac(i,j,k)
+
+             end do
+          end do
+       end do
+
+    end do ! end loop over components
+
+    mult = ONE
+    call addw0_3d_sphr(umac,vmac,wmac,w0_cart,lo,hi,mult)
+
+    ! Note the umac here DOES have w0 in it
+
+    do comp = startcomp, endcomp
+
+       ! loop for x-fluxes
+       do k = lo(3), hi(3)
+          do j = lo(2), hi(2)
+             do i = lo(1), hi(1)+1
+                sfluxx(i,j,k,comp) = sfluxx(i,j,k,comp) + umac(i,j,k)*sedgex(i,j,k,comp)
+             end do
+          end do
+       end do
+
+       ! loop for y-fluxes
+       do k = lo(3), hi(3)
+          do j = lo(2), hi(2)+1
+             do i = lo(1), hi(1)
+                sfluxy(i,j,k,comp) = sfluxy(i,j,k,comp) + vmac(i,j,k)*sedgey(i,j,k,comp)
+             end do
+          end do
+       end do
+
+       ! loop for z-fluxes
+       do k = lo(3), hi(3)+1
+          do j = lo(2), hi(2)
+             do i = lo(1), hi(1)
+                sfluxz(i,j,k,comp) = sfluxz(i,j,k,comp) + wmac(i,j,k)*sedgez(i,j,k,comp)
+             end do
+          end do
+       end do
+
+    end do ! end loop over components
+
+    mult = -ONE
+    call addw0_3d_sphr(umac,vmac,wmac,w0_cart,lo,hi,mult)
      
   end subroutine mkflux_3d_sphr
    
