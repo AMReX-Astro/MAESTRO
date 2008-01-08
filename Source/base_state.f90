eebcccfcd2016bcc50d8efa9ac5d3bce47793e0e
ajnonaka <ajnonaka>
2008-01-08 20:51:01 +0000

    interpolation coordinates now properly respect prob_lo

--- Source/base_state.f90
+++ Source/base_state.f90
@@ -19,7 +19,7 @@ contains
     use define_bc_module
     use bl_constants_module
     use eos_module
-    use probin_module, ONLY: base_cutoff_density, anelastic_cutoff, prob_lo_z
+    use probin_module, ONLY: base_cutoff_density, anelastic_cutoff, prob_lo_y, prob_lo_z
     use variables, only: rho_comp, rhoh_comp, temp_comp, spec_comp, trac_comp, ntrac
     use geometry, only: dr, nr, spherical
 
@@ -200,11 +200,11 @@ contains
        print *,'DR , RMAX OF BASE ARRAY',dr(n), dble(nr(n)) * dr(n)
     end if
 
-    if (spherical .eq. 0) then
+    if (dm .eq. 2) then
+       starting_rad = prob_lo_y
+    else if(dm .eq. 3) then
        starting_rad = prob_lo_z
-    else
-       starting_rad = ZERO
-    end if
+    endif
 
     j_cutoff = nr(n)
     do j = 0,nr(n)-1
