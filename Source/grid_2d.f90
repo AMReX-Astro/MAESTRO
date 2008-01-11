2c69eb22a45df0a8257dde7c219f658687060102
ajnonaka <ajnonaka>
2008-01-11 22:26:36 +0000

    handles domains with 1000 or more cells per direction now

--- Source/grid_2d.f90
+++ Source/grid_2d.f90
@@ -2,17 +2,17 @@ program grid_2d
 
   integer :: i, j
 
-  integer, parameter :: nx = 16
-  integer, parameter :: ny = 16
-  integer, parameter :: nzonesx = 768
-  integer, parameter :: nzonesy = 768
+  integer, parameter :: nx = 10
+  integer, parameter :: ny = 10
+  integer, parameter :: nzonesx = 1280
+  integer, parameter :: nzonesy = 1280
 
   integer :: ix, iy
   integer :: nlevs, ngrids
 
  99 format(i1)
-100 format('         ((',i3,',',i3,') (',i3,',',i3,') ('i3,',',i3,'))')
-101 format('   ((',i3,',',i3,') (',i3,',',i3,') ('i3,',',i3,'))', i4)
+100 format('         ((',i4,',',i4,') (',i4,',',i4,') ('i4,',',i4,'))')
+101 format('   ((',i4,',',i4,') (',i4,',',i4,') ('i4,',',i4,'))', i5)
 
   ix = 0
   nlevs = 1
