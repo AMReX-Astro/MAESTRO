08aed76c3f537dc3f51d5734cb5a37eb1c308b12
ajnonaka <ajnonaka>
2008-01-22 06:29:37 +0000

    passing in nlevs as an argument to checkpoint_read and write.  using the nr(:) from the geometry module.  this is all in preparation for writing a multilevel version or restart that handles the base state properly

--- Source/base_io.f90
+++ Source/base_io.f90
@@ -10,30 +10,30 @@ module base_io_module
 
 contains
 
-  subroutine write_base_state(state_name,w0_name,chk_name,s0,p0,gam1,w0,div_coeff,problo)
+  subroutine write_base_state(nlevs,state_name,w0_name,chk_name,s0,p0,gam1,w0, &
+                              div_coeff,problo)
     
     use parallel
     use bl_prof_module
-    use geometry, only : dr
+    use geometry, only : dr, nr
     use network, only: nspec
     use variables, only: rho_comp, spec_comp, temp_comp, rhoh_comp
     use bl_constants_module
     use probin_module, only: prob_lo_y, prob_lo_z
 
+    integer          , intent(in) :: nlevs
     character(len=10), intent(in) :: state_name
-    character(len=7), intent(in) :: w0_name
-    character(len=7), intent(in) :: chk_name
-    real(kind=dp_t) , intent(in) :: s0(:,:),p0(:),gam1(:),div_coeff(:), w0(:)
-    real(kind=dp_t) :: base_r, problo
+    character(len=7) , intent(in) :: w0_name
+    character(len=7) , intent(in) :: chk_name
+    real(kind=dp_t)  , intent(in) :: s0(:,:),p0(:),gam1(:),div_coeff(:), w0(:)
 
+    real(kind=dp_t) :: base_r, problo
     character(len=18) :: out_name
-    integer :: i, n, nr
+    integer :: i, n
 
     type(bl_prof_timer), save :: bpt
 
     call build(bpt, "write_base_state")
-
-    nr = size(s0,dim=1)
 
     if (parallel_IOProcessor()) then
 
@@ -42,7 +42,7 @@ contains
        write(6,*) 'Writing base state to ',out_name
 
        open(unit=99,file=out_name,form = "formatted", access = "sequential",action="write")
-       do i = 1, nr
+       do i = 1, nr(1)
           base_r = problo + (dble(i)-HALF) * dr(1)
           write(99,1000)  base_r,s0(i,rho_comp), p0(i), gam1(i), s0(i,rhoh_comp), &
                (s0(i,n), n=spec_comp,spec_comp+nspec-1), s0(i,temp_comp), div_coeff(i)
@@ -55,7 +55,7 @@ contains
        write(6,*) ''
 
        open(unit=99,file=out_name,form = "formatted", access = "sequential",action="write")
-       do i = 1, nr+1
+       do i = 1, nr(1)+1
           base_r = problo + (dble(i)-1) * dr(1)
           write(99,1000)  base_r,w0(i)
        end do
@@ -70,32 +70,32 @@ contains
   end subroutine write_base_state
 
 
-  subroutine read_base_state(state_name,w0_name,chk_name,s0,p0,gam1,w0,div_coeff)
+  subroutine read_base_state(nlevs,state_name,w0_name,chk_name,s0,p0,gam1,w0,div_coeff)
 
 
     use parallel
     use bl_prof_module
     use variables, only: rho_comp, rhoh_comp, spec_comp, temp_comp
     use network, only: nspec
-    use geometry, only : dr
+    use geometry, only : dr, nr
     use bl_constants_module
     
+    integer          , intent(in   ) :: nlevs
     character(len=10), intent(in   ) :: state_name
     character(len=7) , intent(in   ) :: w0_name
     character(len=7) , intent(in   ) :: chk_name    
-    real(kind=dp_t) , intent(inout) :: s0(:,:),p0(:),gam1(:),div_coeff(:),w0(:)
-    real(kind=dp_t) , allocatable   :: base_r(:)
+    real(kind=dp_t)  , intent(inout) :: s0(:,:),p0(:),gam1(:),div_coeff(:),w0(:)
+    real(kind=dp_t)  , allocatable   :: base_r(:)
 
     real(kind=dp_t) :: r_dummy
     character(len=18) :: out_name
-    integer :: i, n, nr
+    integer :: i, n
 
     type(bl_prof_timer), save :: bpt
 
     call build(bpt, "read_base_state")
 
-    nr = size(s0,dim=1)
-    allocate(base_r(nr))
+    allocate(base_r(nr(1)))
 
     ! read in the state variables
     out_name = chk_name // "/" // state_name
--- TEST_PROBLEMS/test_smallscale/base_io.f90
+++ TEST_PROBLEMS/test_smallscale/base_io.f90
@@ -10,29 +10,29 @@ module base_io_module
 
 contains
 
-  subroutine write_base_state(state_name,w0_name,chk_name,s0,p0,gam1,w0,div_coeff,problo)
+  subroutine write_base_state(nlevs,state_name,w0_name,chk_name,s0,p0,gam1,w0, &
+                              div_coeff,problo)
     
     use parallel
     use bl_prof_module
-    use geometry, only : dr
+    use geometry, only : dr, nr
     use network, only: nspec
     use variables, only: rho_comp, spec_comp, temp_comp, rhoh_comp
     use bl_constants_module
 
+    integer          , intent(in) :: nlevs
     character(len=10), intent(in) :: state_name
-    character(len=7), intent(in) :: w0_name
-    character(len=7), intent(in) :: chk_name
-    real(kind=dp_t) , intent(in) :: s0(:,:),p0(:),gam1(:),div_coeff(:), w0(:)
-    real(kind=dp_t) :: base_r, problo
+    character(len=7) , intent(in) :: w0_name
+    character(len=7) , intent(in) :: chk_name
+    real(kind=dp_t)  , intent(in) :: s0(:,:),p0(:),gam1(:),div_coeff(:), w0(:)
 
+    real(kind=dp_t) :: base_r, problo
     character(len=18) :: out_name
-    integer :: i, n, nr
+    integer :: i, n
 
     type(bl_prof_timer), save :: bpt
 
     call build(bpt, "write_base_state")
-
-    nr = size(s0,dim=1)
 
     if (parallel_IOProcessor()) then
 
@@ -41,7 +41,7 @@ contains
        write(6,*) 'Writing base state to ',out_name
 
        open(unit=99,file=out_name,form = "formatted", access = "sequential",action="write")
-       do i = 1, nr
+       do i = 1, nr(1)
           base_r = problo + (dble(i)-HALF) * dr(1)
           write(99,1000)  base_r,s0(i,rho_comp), p0(i), gam1(i), s0(i,rhoh_comp), &
                (s0(i,n), n=spec_comp,spec_comp+nspec-1), s0(i,temp_comp), div_coeff(i)
@@ -54,7 +54,7 @@ contains
        write(6,*) ''
 
        open(unit=99,file=out_name,form = "formatted", access = "sequential",action="write")
-       do i = 1, nr+1
+       do i = 1, nr(1)+1
           base_r = problo + (dble(i)-1) * dr(1)
           write(99,1000)  base_r,w0(i)
        end do
@@ -69,18 +69,19 @@ contains
   end subroutine write_base_state
 
 
-  subroutine read_base_state(state_name,w0_name,chk_name,s0,p0,gam1,w0,div_coeff)
+  subroutine read_base_state(nlevs,state_name,w0_name,chk_name,s0,p0,gam1,w0,div_coeff)
 
     use parallel
     use bl_prof_module
     use variables, only: rho_comp, rhoh_comp, spec_comp, temp_comp
     use network, only: nspec
-    use geometry, only : dr
+    use geometry, only : dr, nr
     use bl_constants_module
     use eos_module
     use inlet_bc_module
     use probin_module, ONLY: use_big_h
     
+    integer          , intent(in   ) :: nlevs
     character(len=10), intent(in   ) :: state_name
     character(len=7) , intent(in   ) :: w0_name
     character(len=7) , intent(in   ) :: chk_name    
@@ -89,7 +90,7 @@ contains
 
     real(kind=dp_t) :: r_dummy
     character(len=18) :: out_name
-    integer :: i, n, nr
+    integer :: i, n
 
     type(bl_prof_timer), save :: bpt
 
@@ -100,8 +101,7 @@ contains
 
     call build(bpt, "read_base_state")
 
-    nr = size(s0,dim=1)
-    allocate(base_r(nr))
+    allocate(base_r(nr(1)))
 
     ! read in the state variables
     out_name = chk_name // "/" // state_name
