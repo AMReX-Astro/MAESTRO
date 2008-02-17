619a55af02ca80eee640e1ff576e7bc770f5055e
mzingale <mzingale>
2008-02-17 16:22:31 +0000

    start added the (U.e_r) dw_0/dr e_r term to the velocity update (and the predictor) for spherical -- it was not there.  Right now, all the interface changes are done (mostly passing normal through where needed).  update_vel.f90 now computes the term, but the update is commented out at the moment.  The prediction in make_edge_states.f90 still needs it added.

--- Source/update_vel.f90
+++ Source/update_vel.f90
@@ -12,7 +12,7 @@ module update_vel_module
 
 contains
 
-  subroutine update_velocity(nlevs,uold,unew,umac,uedge,force,w0,w0_cart,w0_force, &
+  subroutine update_velocity(nlevs,uold,unew,umac,uedge,force,normal,w0,w0_cart,w0_force, &
                              w0_force_cart,dx,dt,sponge,mla,the_bc_level)
 
     use bl_prof_module
@@ -27,6 +27,7 @@ contains
     type(multifab)    , intent(in   ) :: umac(:,:)
     type(multifab)    , intent(in   ) :: uedge(:,:)
     type(multifab)    , intent(in   ) :: force(:)
+    type(multifab)    , intent(in   ) :: normal(:)
     real (kind = dp_t), intent(in   ) :: w0(:,0:)
     type(multifab)    , intent(in   ) :: w0_cart(:)
     real (kind = dp_t), intent(in   ) :: w0_force(:,0:)
@@ -51,6 +52,7 @@ contains
     real(kind=dp_t), pointer:: uepz(:,:,:,:)
     real(kind=dp_t), pointer:: spp(:,:,:,:)
     real(kind=dp_t), pointer:: fp(:,:,:,:)
+    real(kind=dp_t), pointer:: np(:,:,:,:)
     real(kind=dp_t), pointer:: w0p(:,:,:,:)
     real(kind=dp_t), pointer:: w0fp(:,:,:,:)
 
@@ -73,6 +75,7 @@ contains
           uepy => dataptr(uedge(n,2),i)
           spp  => dataptr(sponge(n),i)
           fp   =>  dataptr(force(n),i)
+          np   =>  dataptr(normal(n),i)
           lo = lwb(get_box(uold(n),i))
           hi = upb(get_box(uold(n),i))
           select case (dm)
@@ -87,11 +90,13 @@ contains
              uepz  => dataptr(uedge(n,3),i)
              w0p   => dataptr(w0_cart(n),i)
              w0fp  => dataptr(w0_force_cart(n),i)
-             call update_velocity_3d(uop(:,:,:,:), unp(:,:,:,:), &
+             call update_velocity_3d(n, &
+                                     uop(:,:,:,:), unp(:,:,:,:), &
                                      ump(:,:,:,1), vmp(:,:,:,1), &
                                      wmp(:,:,:,1), uepx(:,:,:,:), &
                                      uepy(:,:,:,:), uepz(:,:,:,:), &
-                                     fp(:,:,:,:), w0(n,:), w0p(:,:,:,:), &
+                                     fp(:,:,:,:), np(:,:,:,:), &
+                                     w0(n,:), w0p(:,:,:,:), &
                                      w0_force(n,:), w0fp(:,:,:,:), &
                                      lo, hi, ng, dx(n,:), dt, spp(:,:,:,1))
           end select
@@ -171,15 +176,16 @@ contains
 
   end subroutine update_velocity_2d
 
-  subroutine update_velocity_3d(uold,unew,umac,vmac,wmac,uedgex,uedgey,uedgez, &
-                                force,w0,w0_cart,w0_force,w0_force_cart,lo,hi,ng,dx,dt, &
+  subroutine update_velocity_3d(n,uold,unew,umac,vmac,wmac,uedgex,uedgey,uedgez,force, &
+                                normal,w0,w0_cart,w0_force,w0_force_cart,lo,hi,ng,dx,dt, &
                                 sponge)
 
-    use geometry, only: spherical
+    use fill_3d_module
+    use geometry, only: spherical, nr, dr
     use bl_constants_module
     use probin_module, only: do_sponge
 
-    integer, intent(in) :: lo(:), hi(:), ng
+    integer, intent(in) :: n, lo(:), hi(:), ng
     real (kind = dp_t), intent(in   ) ::     uold(lo(1)-ng:,lo(2)-ng:,lo(3)-ng:,:)
     real (kind = dp_t), intent(  out) ::     unew(lo(1)-ng:,lo(2)-ng:,lo(3)-ng:,:)
     real (kind = dp_t), intent(in   ) ::     umac(lo(1)- 1:,lo(2)- 1:,lo(3)- 1:  )
@@ -189,6 +195,7 @@ contains
     real (kind = dp_t), intent(in   ) ::   uedgey(lo(1)   :,lo(2)   :,lo(3)   :,:)
     real (kind = dp_t), intent(in   ) ::   uedgez(lo(1)   :,lo(2)   :,lo(3)   :,:)
     real (kind = dp_t), intent(in   ) ::    force(lo(1)- 1:,lo(2)- 1:,lo(3)- 1:,:)
+    real (kind = dp_t), intent(in   ) ::   normal(lo(1)- 1:,lo(2)- 1:,lo(3)- 1:,:)
     real (kind = dp_t), intent(in   ) ::   sponge(lo(1)   :,lo(2)   :,lo(3)   :  ) 
     real (kind = dp_t), intent(in   ) ::       w0(0:)
     real (kind = dp_t), intent(in   ) ::  w0_cart(lo(1)- 1:,lo(2)- 1:,lo(3)- 1:,:)
@@ -197,7 +204,7 @@ contains
     real (kind = dp_t), intent(in   ) :: dx(:)
     real (kind = dp_t), intent(in   ) :: dt
 
-    integer :: i, j, k
+    integer :: i, j, k, r
     real (kind = dp_t) ubar,vbar,wbar
     real (kind = dp_t) ugradu,ugradv,ugradw
     real (kind = dp_t) :: gradux,graduy,graduz
@@ -205,6 +212,9 @@ contains
     real (kind = dp_t) :: gradwx,gradwy,gradwz
     real (kind = dp_t) :: w0_gradur,w0_gradvr,w0_gradwr
     real (kind = dp_t) :: gradw0
+    real (kind = dp_t) :: U_dot_er
+    real (kind = dp_t), allocatable :: gradw0_rad(:)
+    real (kind = dp_t), allocatable :: gradw0_cart(:,:,:)
 
     ! 1) Subtract (Utilde dot grad) Utilde term from old Utilde
     ! 2) Add forcing term to new Utilde
@@ -236,11 +246,12 @@ contains
        enddo
     enddo
 
-    ! A) Subtract (Utilde dot er) dot grad w0 term from new Utilde.
-    ! B) Subtract w0 dot grad U term from new Utilde
+
     if (spherical .eq. 0) then
 
        do k = lo(3), hi(3)
+
+          ! A) Subtract (Utilde dot er) grad w0 er term from new Utilde.
 
           gradw0 = (w0(k+1) - w0(k)) /dx(3)
           do j = lo(2), hi(2)
@@ -249,6 +260,9 @@ contains
                 unew(i,j,k,3) = unew(i,j,k,3) - dt * wbar * gradw0
              enddo
           enddo
+
+
+          ! B) Subtract w0 dot grad U term from new Utilde
 
           wbar = HALF*(w0(k) + w0(k+1))
           do j = lo(2), hi(2)
@@ -270,37 +284,69 @@ contains
 
     else
 
-       ! A) Subtract (Utilde dot er) dot grad w0 term from new Utilde.
+       allocate(gradw0_rad(0:nr(n)-1))
+       allocate(gradw0_cart(lo(1):hi(1),lo(2):hi(2),lo(3):hi(3)))
 
-       ! B) Subtract (w0 dot grad) U term from new Utilde
+       do r = 0, nr(n)-1
+          gradw0_rad(r) = (w0(r+1) - w0(r)) / dr(n)
+       enddo
+
+       call fill_3d_data(n,gradw0_cart,gradw0_rad,lo,hi,dx,0)
 
        do k = lo(3), hi(3)
           do j = lo(2), hi(2)
              do i = lo(1), hi(1)
 
+                ! A) Subtract (Utilde dot er) grad w0 er term from new Utilde.
+                U_dot_er = HALF*(umac(i,j,k) + umac(i+1,j,k)) * normal(i,j,k,1) + &
+                           HALF*(vmac(i,j,k) + vmac(i,j+1,k)) * normal(i,j,k,2) + &
+                           HALF*(wmac(i,j,k) + wmac(i,j,k+1)) * normal(i,j,k,3)
+
+
+                !unew(i,j,k,1) = unew(i,j,k,1) - &
+                !     dt * U_dot_er * gradw0_cart(i,j,k) * normal(i,j,k,1)
+
+                !unew(i,j,k,2) = unew(i,j,k,2) - &
+                !     dt * U_dot_er * gradw0_cart(i,j,k) * normal(i,j,k,2)
+
+                !unew(i,j,k,3) = unew(i,j,k,3) - &
+                !     dt * U_dot_er * gradw0_cart(i,j,k) * normal(i,j,k,3)
+
+
+                ! B) Subtract (w0 dot grad) U term from new Utilde
+
                 gradux = (uedgex(i+1,j,k,1) - uedgex(i,j,k,1))/dx(1)
                 gradvx = (uedgex(i+1,j,k,2) - uedgex(i,j,k,2))/dx(1)
                 gradwx = (uedgex(i+1,j,k,3) - uedgex(i,j,k,3))/dx(1)
+
                 graduy = (uedgey(i,j+1,k,1) - uedgey(i,j,k,1))/dx(2)
                 gradvy = (uedgey(i,j+1,k,2) - uedgey(i,j,k,2))/dx(2)
                 gradwy = (uedgey(i,j+1,k,3) - uedgey(i,j,k,3))/dx(2)
+
                 graduz = (uedgez(i,j,k+1,1) - uedgez(i,j,k,1))/dx(3)
                 gradvz = (uedgez(i,j,k+1,2) - uedgez(i,j,k,2))/dx(3)
                 gradwz = (uedgez(i,j,k+1,3) - uedgez(i,j,k,3))/dx(3)
 
                 w0_gradur = gradux * w0_cart(i,j,k,1) &
-                     + graduy * w0_cart(i,j,k,2) + graduz * w0_cart(i,j,k,3)
+                          + graduy * w0_cart(i,j,k,2) &
+                          + graduz * w0_cart(i,j,k,3)
+
                 w0_gradvr = gradvx * w0_cart(i,j,k,1) &
-                     + gradvy * w0_cart(i,j,k,2) + gradvz * w0_cart(i,j,k,3)
+                          + gradvy * w0_cart(i,j,k,2) &
+                          + gradvz * w0_cart(i,j,k,3)
+
                 w0_gradwr = gradwx * w0_cart(i,j,k,1) &
-                     + gradwy * w0_cart(i,j,k,2) + gradwz * w0_cart(i,j,k,3)
+                          + gradwy * w0_cart(i,j,k,2) &
+                          + gradwz * w0_cart(i,j,k,3)
 
                 unew(i,j,k,1) = unew(i,j,k,1) - dt * w0_gradur
                 unew(i,j,k,2) = unew(i,j,k,2) - dt * w0_gradvr
                 unew(i,j,k,3) = unew(i,j,k,3) - dt * w0_gradwr
 
+
                 ! Add in the pi0 term.
                 unew(i,j,k,:) = unew(i,j,k,:) - dt * w0_force_cart(i,j,k,:)
+
 
                 ! Add the sponge
                 if (do_sponge) unew(i,j,k,:) = unew(i,j,k,:) * sponge(i,j,k)
@@ -308,6 +354,8 @@ contains
              enddo
           enddo
        enddo
+
+       deallocate (gradw0_cart, gradw0_rad)
 
     end if
 
