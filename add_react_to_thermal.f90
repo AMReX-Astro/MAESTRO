761f94647776bbcbde3770444d57e228483ce2bf
ajnonaka <ajnonaka>
2008-02-28 23:25:58 +0000

    cleaned up the way the ghost cell fillers are called.  Eliminated a lot of the double-calls to multifab_fill_boundary and multifab_physbc.  I'm going to use the code and comments I wrote here as a template as I fix the ghost cell fillers in the entire MAESTRO directory

--- add_react_to_thermal.f90
+++ add_react_to_thermal.f90
@@ -61,25 +61,35 @@ contains
              call add_react_to_thermal_3d(lo,hi,tp(:,:,:,1),rwp(:,:,:,:),sp(:,:,:,:))
           end select
        end do
+    enddo
+
+    if (nlevs .eq. 1) then
 
        ! fill ghost cells for two adjacent grids at the same level
-       ! this includes periodic domain boundary conditions
-       call multifab_fill_boundary(thermal(n))
-       
-       call multifab_physbc(thermal(n),1,foextrap_comp,1,the_bc_level(n))
-    enddo
+       ! this includes periodic domain boundary ghost cells
+       call multifab_fill_boundary(thermal(nlevs))
 
-    do n=nlevs,2,-1
-       ! make sure that coarse cells are the average of the fine cells covering it.
+       ! fill non-periodic domain boundary ghost cells
+       call multifab_physbc(thermal(nlevs),1,foextrap_comp,1,the_bc_level(nlevs))
+
+    else
+
        ! the loop over nlevs must count backwards to make sure the finer grids are done first
-       call ml_cc_restriction(thermal(n-1),thermal(n),mla%mba%rr(n-1,:))
+       do n=nlevs,2,-1
 
-       ! fill fine ghost cells using interpolation from the underlying coarse data
-       call multifab_fill_ghost_cells(thermal(n),thermal(n-1), &
-                                      1,mla%mba%rr(n-1,:), &
-                                      the_bc_level(n-1), the_bc_level(n), &
-                                      1,foextrap_comp,1)
-    enddo
+          ! set level n-1 data to be the average of the level n data covering it
+          call ml_cc_restriction(thermal(n-1),thermal(n),mla%mba%rr(n-1,:))
+          
+          ! fill level n ghost cells using interpolation from level n-1 data
+          ! note that multifab_fill_boundary and multifab_physbc are called for
+          ! both levels n-1 and n
+          call multifab_fill_ghost_cells(thermal(n),thermal(n-1), &
+                                         1,mla%mba%rr(n-1,:), &
+                                         the_bc_level(n-1), the_bc_level(n), &
+                                         1,foextrap_comp,1)
+       enddo
+
+    end if
 
     call destroy(bpt)
        
