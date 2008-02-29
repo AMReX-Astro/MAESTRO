8ad6eeebb19061c76a309f4a90368f9ee80edceb
ajnonaka <ajnonaka>
2008-02-29 01:57:58 +0000

    clean up ghost cell stuff; added call to ml_cc_restriction

--- Source/make_at_halftime.f90
+++ Source/make_at_halftime.f90
@@ -60,7 +60,7 @@ contains
           end select
        end do
 
-    enddo
+    end do
 
     call destroy(bpt)
 
@@ -93,7 +93,6 @@ contains
     ng_o = sold(1)%ng
 
     do n = 1, nlevs
-
        do i = 1, phihalf(n)%nboxes
           if ( multifab_remote(phihalf(n), i) ) cycle
           rhp => dataptr(phihalf(n), i)
@@ -110,24 +109,35 @@ contains
                                       rnp(:,:,:,in_comp),lo,hi,ng_h,ng_o)
           end select
        end do
+    end do
 
-       if(ng_h .gt. 0) then
-          call multifab_fill_boundary(phihalf(n))
-          call multifab_physbc(phihalf(n),out_comp,dm+in_comp,1,the_bc_level(n))
-       endif
+    if (nlevs .eq. 1) then
+       
+       ! fill ghost cells for two adjacent grids at the same level
+       ! this includes periodic domain boundary ghost cells
+       call multifab_fill_boundary(phihalf(nlevs))
 
-    enddo ! end loop over nlevs
+       ! fill non-periodic domain boundary ghost cells
+       call multifab_physbc(phihalf(nlevs),out_comp,dm+in_comp,1,the_bc_level(nlevs))
 
-    if(ng_h .gt. 0) then
-       do n = nlevs, 2, -1
+    else
+
+       ! the loop over nlevs must count backwards to make sure the finer grids are done first
+       do n=nlevs,2,-1
+
+          ! set level n-1 data to be the average of the level n data covering it
           call ml_cc_restriction(phihalf(n-1),phihalf(n),mla%mba%rr(n-1,:))
 
+          ! fill level n ghost cells using interpolation from level n-1 data
+          ! note that multifab_fill_boundary and multifab_physbc are called for
+          ! both levels n-1 and n
           call multifab_fill_ghost_cells(phihalf(n),phihalf(n-1), &
                                          ng_h,mla%mba%rr(n-1,:), &
                                          the_bc_level(n-1), the_bc_level(n  ), &
                                          1,dm+in_comp,1)
-       enddo
-    endif
+       end do
+
+    end if
 
   end subroutine make_at_halftime
 
