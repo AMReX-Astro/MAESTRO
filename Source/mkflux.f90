b48f892475741c53bcc8197b60430ccd00547d44
ajnonaka <ajnonaka>
2008-03-12 19:13:33 +0000

    bugfix -
    if we are computing fluxes for enthalpy, we want the "full" flux (ie, test=T) if either predict_h_at_edges=T OR (predict_temp_at_edges=T AND predict_X_at_edges=T).
    
    Before we set test=T if either predict_h_at_edges=T OR predict_temp_at_edges=T.
    
    This is wrong because for the case where predict_temp_at_edges=T and predict_X_at_edges=F, sedgex contains (rho h)'.
    
    For the case where predict_temp_at_edges=T and predict_X_at_edges=T, sedgex contains h

--- Source/mkflux.f90
+++ Source/mkflux.f90
@@ -172,7 +172,8 @@ contains
     do comp = startcomp, endcomp
 
        test = ( (comp.ge.spec_comp).and.(comp.le.spec_comp+nspec-1).and.predict_X_at_edges) &
-         .or. ( (comp.eq.rhoh_comp).and.(predict_h_at_edges.or.predict_temp_at_edges) )
+         .or. ( (comp.eq.rhoh_comp).and. &
+                     (predict_h_at_edges.or.(predict_temp_at_edges.and.predict_X_at_edges)) )
        
        ! create x-fluxes
        if (test) then
