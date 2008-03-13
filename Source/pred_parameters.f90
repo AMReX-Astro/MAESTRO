2ad11500660664ab3601ce404db60495d31767c5
jbb <jbb>
2008-03-13 18:20:03 +0000

    Add a pred_parameters file to define the four enthalpy_pred options as integer parameters.

---
+++ Source/pred_parameters.f90
@@ -0,0 +1,10 @@
+module pred_parameters
+
+   implicit none
+
+   integer, parameter :: predict_rhohprime        = 1
+   integer, parameter :: predict_h                = 2
+   integer, parameter :: predict_T_into_rhohprime = 3
+   integer, parameter :: predict_T_into_h         = 4
+
+end module pred_parameters
