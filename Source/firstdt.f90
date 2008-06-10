5702689ea1d6f783131e79486a064ddf144fac3e
ajnonaka <ajnonaka>
2008-06-10 21:24:30 +0000

    replaced nr with r_end_coord

--- Source/firstdt.f90
+++ Source/firstdt.f90
@@ -68,7 +68,7 @@ contains
 
     use eos_module
     use variables, only: rho_comp, temp_comp, spec_comp
-    use geometry,  only: nr
+    use geometry,  only: r_end_coord
     use bl_constants_module
     
     integer, intent(in)             :: n, lo(:), hi(:), ng
@@ -153,7 +153,7 @@ contains
     do j = lo(2), hi(2)
        if (j .eq. 0) then
           gradp0 = (p0(j+1) - p0(j))/dx(2)
-       else if (j .eq. nr(n)-1) then
+       else if (j .eq. r_end_coord(n)) then
           gradp0 = (p0(j) - p0(j-1))/dx(2)
        else
           gradp0 = HALF*(p0(j+1) - p0(j-1))/dx(2)
@@ -171,7 +171,7 @@ contains
   
   subroutine firstdt_3d(n,u,s,force,divU,p0,gamma1bar,lo,hi,ng,dx,dt,cfl)
 
-    use geometry,  only: spherical, nr
+    use geometry,  only: spherical, r_end_coord
     use variables, only: rho_comp, temp_comp, spec_comp
     use eos_module
     use fill_3d_module
@@ -271,7 +271,7 @@ contains
     do k = lo(3), hi(3)
        if (k .eq. 0) then
           gradp0 = (p0(k+1) - p0(k))/dx(3)
-       else if (k .eq. nr(n)-1) then
+       else if (k .eq. r_end_coord(n)) then
           gradp0 = (p0(k) - p0(k-1))/dx(3)
        else
           gradp0 = HALF*(p0(k+1) - p0(k-1))/dx(3)
