aa5245830f1b84ef49e395000c85b79fa04bd21b
ajnonaka <ajnonaka>
2008-06-12 17:57:34 +0000

    after calling init_base_state, I call restrict_base on rho, rhoh, rhoX, and tracers.  This makes the coarse value equal to the average of the fine values covering it.  These are chosen because they are "conserved" variables.
    Then, I call a new subroutine  "adjust_base_state", which computes p0 and T at
    the non-finest level(s) to make the state thermodynamically consistent.

--- Source/base_state.f90
+++ Source/base_state.f90
@@ -13,7 +13,7 @@ module base_state_module
 
   private
 
-  public :: init_base_state
+  public :: init_base_state, adjust_base_state
 
 contains
 
@@ -416,6 +416,52 @@ contains
     call destroy(bpt)
 
   end subroutine init_base_state
+
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+  subroutine adjust_base_state(nlevs,s0,p0)
+
+    use eos_module
+    use geometry, only: r_start_coord, r_end_coord
+    use variables, only: temp_comp, rho_comp, rhoh_comp, spec_comp
+    use network, only: nspec
+
+    integer           , intent(in   ) :: nlevs
+    real(kind=dp_t)   , intent(inout) :: s0(:,0:,:)
+    real(kind=dp_t)   , intent(inout) :: p0(:,0:)
+
+    ! local
+    integer :: n,r
+
+    do n=1,nlevs-1
+       do r=r_start_coord(n),r_end_coord(n)
+          ! use the EOS to make the state consistent
+          temp_eos(1) = s0(n,r,temp_comp)
+          den_eos(1)  = s0(n,r,rho_comp)
+          h_eos(1)    = s0(n,r,rhoh_comp)/den_eos(1)
+          p_eos(1)    = p0(n,r)
+          xn_eos(1,:) = s0(n,r,spec_comp:spec_comp+nspec-1)/den_eos(1)
+
+          ! (rho,h) --> p,T
+          call eos(eos_input_rh, den_eos, temp_eos, &
+                   npts, nspec, &
+                   xn_eos, &
+                   p_eos, h_eos, e_eos, &
+                   cv_eos, cp_eos, xne_eos, eta_eos, pele_eos, &
+                   dpdt_eos, dpdr_eos, dedt_eos, dedr_eos, &
+                   dpdX_eos, dhdX_eos, &
+                   gam1_eos, cs_eos, s_eos, &
+                   dsdt_eos, dsdr_eos, &
+                   do_diag)
+
+          p0(n,r) = p_eos(1)
+          s0(n,r,temp_comp) = temp_eos(1)
+       end do
+    end do
+
+  end subroutine adjust_base_state
+
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
   function interpolate(r, npts, model_r, model_var)
 
