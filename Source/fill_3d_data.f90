1d7fb492404b9ce3b297a7c84a0ace50aa829425
ajnonaka <ajnonaka>
2008-08-12 18:26:46 +0000

    starting work on new edge centered w0

--- Source/fill_3d_data.f90
+++ Source/fill_3d_data.f90
@@ -7,8 +7,8 @@ module fill_3d_module
 
   private
   
-  public :: put_1d_array_on_cart, put_1d_array_on_cart_3d_sphr
-  public :: put_1d_array_on_edges_3d_sphr
+  public :: put_1d_array_on_cart,  put_1d_array_on_cart_3d_sphr
+  public :: put_w0_on_edges, put_w0_on_edges_3d_sphr
   public :: make_3d_normal
   
 contains  
@@ -380,27 +380,122 @@ contains
 
   end subroutine put_1d_array_on_cart_3d_sphr
 
-  subroutine put_1d_array_on_edges_3d_sphr(n,s0,s0_cartx,s0_carty,s0_cartz,lo,hi,dx,ng)
+  subroutine put_w0_on_edges(nlevs,w0,w0mac,dx,normal)
+
+    use geometry, only: spherical
+
+    integer        , intent(in   )           :: nlevs
+    real(kind=dp_t), intent(in   )           :: w0(:,0:)
+    type(multifab) , intent(inout)           :: w0mac(:)
+    real(kind=dp_t), intent(in   )           :: dx(:,:)
+    type(multifab) , intent(in   ), optional :: normal(:)
+
+    integer :: lo(w0mac(1)%dim)
+    integer :: hi(w0mac(1)%dim)
+    integer :: i,n,dm,ng_w0,ng_n
+    real(kind=dp_t), pointer :: w0mp(:,:,:,:)
+    real(kind=dp_t), pointer :: np(:,:,:,:)
+    
+    type(bl_prof_timer), save :: bpt
+
+    call build(bpt, "put_w0_on_edges")
+
+    if (spherical .eq. 1 .and. (.not. present(normal)) ) then
+       call bl_error('Error: Calling put_w0_on_edges for spherical without normal')
+    end if
+
+    dm = w0mac(1)%dim
+    ng_w0 = w0mac(1)%ng
+    ng_n = normal(1)%ng
+
+    do n=1,nlevs
+       do i=1,w0mac(n)%nboxes
+          if ( multifab_remote(w0mac(n), i) ) cycle
+          w0mp => dataptr(w0mac(n), i)
+          np   => dataptr(normal(n), i)
+          lo = lwb(get_box(w0mac(n), i))
+          hi = upb(get_box(w0mac(n), i))
+          select case (dm)
+          case (2)
+             call bl_error('Error: Should not have to call put_w0_on_edges in 2d')
+          case (3)
+             if (spherical .eq. 0) then
+                call put_w0_on_edges_3d_sphr(n,w0(n,:),w0mp(:,:,:,:),ng_w0,np(:,:,:,:), &
+                                             ng_n,lo,hi,dx(n,:))
+             else
+                call bl_error('Error: Should not have to call put_w0_on_edges in 3d cart')
+             end if
+          end select
+       end do
+    end do
+
+  end subroutine put_w0_on_edges
+  
+  subroutine put_w0_on_edges_3d_sphr(n,w0,w0mac,ng_w0,normal,ng_n,lo,hi,dx)
 
     use bl_constants_module
-    use geometry, only: dr, center, r_cc_loc, nr_fine
-    use probin_module, only: interp_type_radial_bin_to_cart
+    use geometry, only: dr, center
 
-    integer        , intent(in   ) :: n
-    integer        , intent(in   ) :: lo(:),hi(:),ng
-    real(kind=dp_t), intent(in   ) :: s0(0:)
-    real(kind=dp_t), intent(inout) :: s0_cartx(lo(1)-ng:,lo(2)-ng:,lo(3)-ng:)
-    real(kind=dp_t), intent(inout) :: s0_carty(lo(1)-ng:,lo(2)-ng:,lo(3)-ng:)
-    real(kind=dp_t), intent(inout) :: s0_cartz(lo(1)-ng:,lo(2)-ng:,lo(3)-ng:)
+    integer        , intent(in   ) :: n,lo(:),hi(:),ng_w0,ng_n
+    real(kind=dp_t), intent(in   ) :: w0(0:)
+    real(kind=dp_t), intent(inout) ::  w0mac(lo(1)-ng_w0:,lo(2)-ng_w0:,lo(3)-ng_w0:,:)
+    real(kind=dp_t), intent(inout) :: normal(lo(1)-ng_n :,lo(2)-ng_n :,lo(3)-ng_n :,:)    
     real(kind=dp_t), intent(in   ) :: dx(:)
 
-    integer         :: i,j,k,index
-    real(kind=dp_t) :: x,y,z
-    real(kind=dp_t) :: radius,s0_cart_val
+    integer         :: i,j,k,index,w0mac_interp_type
+    real(kind=dp_t) :: x,y,z,nx,ny,nz
+    real(kind=dp_t) :: radius,w0_cart_val,rfac
+    real(kind=dp_t), allocatable :: w0_cc(:,:,:,:)
 
-    ! interpolate from radial bin centered values onto x-edges
+    ! we currently have three different ideas for computing w0mac
+    ! 1.  Interpolate w0 to cell centers, then average to edges
+    ! 2.  Interpolate w0 to edges directly
+    ! 3.  Interpolate w0 to nodes, then average to edges
 
-    do k = lo(3),hi(3)
+    w0mac_interp_type = 1
+
+    if (w0mac_interp_type .eq. 1) then
+
+       allocate(w0_cc(lo(1)-2:hi(1)+2,lo(2)-2:hi(2)+2,lo(3)-2:hi(3)+2,3))
+
+       do k = lo(3)-2,hi(3)+2
+          z = (dble(k)+HALF)*dx(3) - center(3)
+          do j = lo(2)-2,hi(2)+2
+             y = (dble(j)+HALF)*dx(2) - center(2)
+             do i = lo(1)-2,hi(1)+2
+                x = (dble(i)+HALF)*dx(1) - center(1)
+
+                radius = sqrt(x**2 + y**2 + z**2)
+                index  = int(radius / dr(n))
+                
+                rfac = (radius - dble(index)*dr(n)) / dr(n)
+                w0_cart_val = rfac * w0(index) + (ONE-rfac) * w0(index+1)
+
+                w0_cc(i,j,k,1) = w0_cart_val * normal(i,j,k,1)
+                w0_cc(i,j,k,2) = w0_cart_val * normal(i,j,k,2)
+                w0_cc(i,j,k,3) = w0_cart_val * normal(i,j,k,3)
+
+             end do
+          end do
+       end do
+
+       do k=lo(3)-1,hi(3)+1
+          do j=lo(2)-1,hi(2)+1
+             do i=lo(1)-1,hi(1)+2
+           
+!                w0mac(i,j,k,1) = 
+
+             end do
+          end do
+       end do
+
+
+
+
+    else if (w0mac_interp_type .eq. 2) then
+
+       ! NOTE NEED TO ADD MORE GHOST CELLS
+       do k = lo(3),hi(3)
           z = (dble(k)+HALF)*dx(3) - center(3)
           do j = lo(2),hi(2)
              y = (dble(j)+HALF)*dx(2) - center(2)
@@ -408,39 +503,15 @@ contains
                 x = (dble(i)     )*dx(1) - center(1)
                 radius = sqrt(x**2 + y**2 + z**2)
                 index  = int(radius / dr(n))
-                
-                if (interp_type_radial_bin_to_cart .eq. 1) then
 
-                   s0_cart_val = s0(index)
 
-                else if (interp_type_radial_bin_to_cart .eq. 2) then
 
-                   if (radius .ge. r_cc_loc(n,index)) then
-                      if (index .eq. nr_fine-1) then
-                         s0_cart_val = s0(index)
-                      else
-                         s0_cart_val = s0(index+1)*(radius-r_cc_loc(n,index)  )/dr(n) &
-                                     + s0(index  )*(r_cc_loc(n,index+1)-radius)/dr(n)
-                      endif
-                   else
-                      if (index .eq. 0) then
-                         s0_cart_val = s0(index)
-                      else
-                         s0_cart_val = s0(index  )*(radius-r_cc_loc(n,index-1))/dr(n) &
-                                     + s0(index-1)*(r_cc_loc(n,index)-radius)  /dr(n)
-                      end if
-                   end if
 
-                else
-                   call bl_error('Error: interp_type_radial_bin_to_cart not defined')
-                end if
-
-                s0_cartx(i,j,k) = s0_cart_val
              end do
           end do
-    end do
+       end do
 
-    do k = lo(3),hi(3)
+       do k = lo(3),hi(3)
           z = (dble(k)+HALF)*dx(3) - center(3)
           do j = lo(2),hi(2)+1
              y = (dble(j)     )*dx(2) - center(2)
@@ -448,39 +519,15 @@ contains
                 x = (dble(i)+HALF)*dx(1) - center(1)
                 radius = sqrt(x**2 + y**2 + z**2)
                 index  = int(radius / dr(n))
-                
-                if (interp_type_radial_bin_to_cart .eq. 1) then
 
-                   s0_cart_val = s0(index)
 
-                else if (interp_type_radial_bin_to_cart .eq. 2) then
 
-                   if (radius .ge. r_cc_loc(n,index)) then
-                      if (index .eq. nr_fine-1) then
-                         s0_cart_val = s0(index)
-                      else
-                         s0_cart_val = s0(index+1)*(radius-r_cc_loc(n,index)  )/dr(n) &
-                                     + s0(index  )*(r_cc_loc(n,index+1)-radius)/dr(n)
-                      endif
-                   else
-                      if (index .eq. 0) then
-                         s0_cart_val = s0(index)
-                      else
-                         s0_cart_val = s0(index  )*(radius-r_cc_loc(n,index-1))/dr(n) &
-                                     + s0(index-1)*(r_cc_loc(n,index)-radius  )/dr(n)
-                      end if
-                   end if
 
-                else
-                   call bl_error('Error: interp_type_radial_bin_to_cart not defined')
-                end if
-
-                s0_carty(i,j,k) = s0_cart_val
              end do
           end do
-    end do
+       end do
 
-    do k = lo(3),hi(3)+1
+       do k = lo(3),hi(3)+1
           z = (dble(k)     )*dx(3) - center(3)
           do j = lo(2),hi(2)
              y = (dble(j)+HALF)*dx(2) - center(2)
@@ -488,39 +535,21 @@ contains
                 x = (dble(i)+HALF)*dx(1) - center(1)
                 radius = sqrt(x**2 + y**2 + z**2)
                 index  = int(radius / dr(n))
-                
-                if (interp_type_radial_bin_to_cart .eq. 1) then
 
-                   s0_cart_val = s0(index)
 
-                else if (interp_type_radial_bin_to_cart .eq. 2) then
 
-                   if (radius .ge. r_cc_loc(n,index)) then
-                      if (index .eq. nr_fine-1) then
-                         s0_cart_val = s0(index)
-                      else
-                         s0_cart_val = s0(index+1)*(radius-r_cc_loc(n,index)  )/dr(n) &
-                                     + s0(index  )*(r_cc_loc(n,index+1)-radius)/dr(n)
-                      endif
-                   else
-                      if (index .eq. 0) then
-                         s0_cart_val = s0(index)
-                      else
-                         s0_cart_val = s0(index  )*(radius-r_cc_loc(n,index-1))/dr(n) &
-                                     + s0(index-1)*(r_cc_loc(n,index)-radius  )/dr(n)
-                      end if
-                   end if
 
-                else
-                   call bl_error('Error: interp_type_radial_bin_to_cart not defined')
-                end if
-
-                s0_cartz(i,j,k) = s0_cart_val
              end do
           end do
-    end do
+       end do
 
-  end subroutine put_1d_array_on_edges_3d_sphr
+    else if (w0mac_interp_type .eq. 3) then
+
+    else
+       call bl_error('Error: fill_3d_data:w0mac_interp_type > 3')
+    end if
+
+  end subroutine put_w0_on_edges_3d_sphr
 
   subroutine make_3d_normal(normal,lo,hi,dx,ng)
 
