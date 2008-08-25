f4ab4c743a17a343e6ab21a67b1a29a794d8b1cf
almgren <almgren>
2008-08-25 17:35:04 +0000

    No longer need fine_mask -- no longer passed into ml_cc_applyop.

--- Source/macproject.f90
+++ Source/macproject.f90
@@ -1499,7 +1499,6 @@ contains
     real(dp_t) ::  xa(mla%dim),  xb(mla%dim)
     real(dp_t) :: pxa(mla%dim), pxb(mla%dim)
 
-    type(lmultifab), pointer     :: fine_mask(:) => Null()
     type(boxarray) :: bac
 
     type(bl_prof_timer), save :: bpt
@@ -1642,26 +1641,7 @@ contains
        do_diagnostics = 0
     end if
 
-    ! Copied in from ml_solve.f90:54 to create fine_mask
-    allocate(fine_mask(nlevs))
-    do n = nlevs, 1, -1
-       call lmultifab_build(fine_mask(n), mla%la(n), 1, 0)
-       call setval(fine_mask(n), val = .true., all = .true.)
-    end do
-    do n = nlevs-1, 1, -1
-       call copy(bac, get_boxarray(mla%la(n+1)))
-       call boxarray_coarsen(bac, ref_ratio(n,:))
-       call setval(fine_mask(n), .false., bac)
-       call destroy(bac)
-    end do
-
-    call ml_cc_applyop(mla, mgt, res, phi, fine_mask, ref_ratio)
-
-    ! Copied in from ml_solve.f90:85 to destroy fine_mask
-    do n = 1,nlevs
-       call lmultifab_destroy(fine_mask(n))
-    end do
-    deallocate(fine_mask)
+    call ml_cc_applyop(mla, mgt, res, phi, ref_ratio)
 
     if ( test == 3 ) then
        call sparse_destroy(sparse_object)
