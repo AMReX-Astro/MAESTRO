e8bf92cae531572c11a0f0e216b147d23858f390
ajnonaka <ajnonaka>
2008-09-04 00:27:56 +0000

    test2_adaptive now properly initializes the grids when you run with inputs_2d_2levels.
    regridding coding begins tomorrow

--- Source/initialize.f90
+++ Source/initialize.f90
@@ -228,12 +228,15 @@ contains
        
     end if
 
-    ! now that we have nr_fine and dr_fine we can
-    ! create nr, dr, r_cc_loc, r_edge_loc, numdisjointchunks, r_start_coord, r_end_coord
-    call init_geometry(nlevs,mla,boundingbox,diffboxarray)
+    ! now that we have nr_fine and dr_fine we can create nr, dr, r_cc_loc, r_edge_loc
+    call init_radial(nlevs,mba)
+
+    ! use boundingbox and diffboxarray to create numdisjointchunks, r_start_coord, 
+    ! and r_end_coord
+    call init_geometry(nlevs,boundingbox,diffboxarray)
 
     ! now that we have nr_fine we can allocate 1d arrays
-    call initialize_1d_arrays(div_coeff_old,div_coeff_new,gamma1bar,gamma1bar_hold, &
+    call initialize_1d_arrays(nlevs,div_coeff_old,div_coeff_new,gamma1bar,gamma1bar_hold, &
                               s0_init,rho0_old,rhoh0_old,rho0_new,rhoh0_new,p0_init, &
                               p0_old,p0_new,w0,etarho,etarho_cc,div_etarho,psi,tempbar, &
                               grav_cell)
@@ -392,12 +395,15 @@ contains
        
     end if
 
-    ! now that we have nr_fine and dr_fine we can
-    ! create nr, dr, r_cc_loc, r_edge_loc, numdisjointchunks, r_start_coord, r_end_coord
-    call init_geometry(nlevs,mla,boundingbox,diffboxarray)
+    ! now that we have nr_fine and dr_fine we can create nr, dr, r_cc_loc, r_edge_loc
+    call init_radial(nlevs,mba)
+
+    ! use boundingbox and diffboxarray to create numdisjointchunks, r_start_coord, 
+    ! and r_end_coord
+    call init_geometry(nlevs,boundingbox,diffboxarray)
 
     ! now that we have nr_fine we can allocate 1d arrays
-    call initialize_1d_arrays(div_coeff_old,div_coeff_new,gamma1bar,gamma1bar_hold, &
+    call initialize_1d_arrays(nlevs,div_coeff_old,div_coeff_new,gamma1bar,gamma1bar_hold, &
                               s0_init,rho0_old,rhoh0_old,rho0_new,rhoh0_new,p0_init, &
                               p0_old,p0_new,w0,etarho,etarho_cc,div_etarho,psi,tempbar, &
                               grav_cell)
@@ -411,17 +417,13 @@ contains
     call initscalardata(sold,s0_init,p0_init,dx,the_bc_tower%bc_tower_array,mla)
 
     if (perturb_model) then
-
        ! force rho0 to be the average density
        call average(mla,sold,rho0_old,dx,rho_comp)
-
     else
-
        ! s0_init already contains the average
        rho0_old = s0_init(:,:,rho_comp)
        call fill_ghost_base(rho0_old,.true.)
        call restrict_base(rho0_old,.true.)
-
     end if
 
     ! this will be overwritten if we are multilevel or if perturb_model = T
@@ -448,28 +450,51 @@ contains
 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
   subroutine initialize_with_adaptive_grids(mla,time,dt,pmask,dx,uold,sold,gpres,pres, &
-                                            dSdt,Source_old,rho_omegadot2,the_bc_tower)
+                                            dSdt,Source_old,rho_omegadot2,the_bc_tower, &
+                                            div_coeff_old,div_coeff_new,gamma1bar, &
+                                            gamma1bar_hold,s0_init,rho0_old,rhoh0_old, &
+                                            rho0_new,rhoh0_new,p0_init, &
+                                            p0_old,p0_new,w0,etarho,etarho_cc,div_etarho, &
+                                            psi,tempbar,grav_cell)
 
     use probin_module, only: n_cellx, n_celly, n_cellz, regrid_int, max_grid_size, &
          ref_ratio, max_levs
+    use init_module
+    use average_module
+    use restrict_base_module
+    use make_new_grids_module
 
-    type(ml_layout),intent(out)   :: mla
+    type(ml_layout),intent(out  ) :: mla
     real(dp_t)    , intent(inout) :: time,dt
     logical       , intent(in   ) :: pmask(:)
     real(dp_t)    , pointer       :: dx(:,:)
     type(multifab), pointer       :: uold(:),sold(:),gpres(:),pres(:),dSdt(:)
     type(multifab), pointer       :: Source_old(:),rho_omegadot2(:)
     type(bc_tower), intent(  out) :: the_bc_tower
+    real(dp_t)    , pointer       :: div_coeff_old(:,:),div_coeff_new(:,:),gamma1bar(:,:)
+    real(dp_t)    , pointer       :: gamma1bar_hold(:,:),s0_init(:,:,:),rho0_old(:,:)
+    real(dp_t)    , pointer       :: rhoh0_old(:,:),rho0_new(:,:),rhoh0_new(:,:),p0_init(:,:)
+    real(dp_t)    , pointer       :: p0_old(:,:),p0_new(:,:),w0(:,:),etarho(:,:)
+    real(dp_t)    , pointer       :: etarho_cc(:,:),div_etarho(:,:),psi(:,:),tempbar(:,:)
+    real(dp_t)    , pointer       :: grav_cell(:,:)
 
-    integer           :: buf_wid
-    type(layout)      :: la_array(max_levs)
-    type(box)         :: bxs
+    ! local
     type(ml_boxarray) :: mba
 
-    logical :: new_grid
-    integer :: lo(dm), hi(dm)
-    integer :: n, d
+    type(boxarray), allocatable :: validboxarr(:)
+    type(boxarray), allocatable :: diffboxarray(:)
 
+    type(box), allocatable :: boundingbox(:)
+
+    type(layout) :: la_array(max_levs)
+    type(box)    :: bxs
+
+    real(dp_t) :: lenx,leny,lenz,max_dist
+    integer    :: buf_wid,n,nl,d,i
+    integer    :: lo(dm), hi(dm)
+    logical    :: new_grid
+
+    ! set time and dt
     time = ZERO
     dt = 1.d20
 
@@ -491,6 +516,7 @@ contains
        mba%rr(n,:) = ref_ratio
     enddo
 
+    ! allocate states
     allocate(uold(max_levs),sold(max_levs),gpres(max_levs),pres(max_levs))
     allocate(dSdt(max_levs),Source_old(max_levs),rho_omegadot2(max_levs))
 
@@ -504,12 +530,195 @@ contains
     do n = 2, max_levs
        mba%pd(n) = refine(mba%pd(n-1),mba%rr((n-1),:))
     enddo
-    
+
+    ! initialize dx
     call initialize_dx(dx,mba,max_levs)
 
-    ! this may be modified later since we don't know how many levels we actually
-    ! need until we start initializing the data
-    nlevs = max_levs
+    ! now that we have dx we can initialize nr_fine and dr_fine
+    if (spherical .eq. 1) then
+
+       ! for spherical, we will now require that dr_fine = dx
+       dr_fine = dx(1,max_levs)
+
+       lenx = HALF * (prob_hi(1) - prob_lo(1))
+       leny = HALF * (prob_hi(2) - prob_lo(2))
+       lenz = HALF * (prob_hi(3) - prob_lo(3))
+
+       max_dist = sqrt(lenx**2 + leny**2 + lenz**2)
+       nr_fine = int(max_dist / dr_fine) + 1
+
+    else
+
+       nr_fine = extent(mba%pd(max_levs),dm)
+       dr_fine = (prob_hi(dm)-prob_lo(dm)) / dble(nr_fine)
+
+    end if
+
+    ! now that we have nr_fine and dr_fine we can create nr, dr, r_cc_loc, r_edge_loc
+    call init_radial(max_levs,mba)
+
+    ! now that we have nr_fine we can allocate 1d arrays
+    call initialize_1d_arrays(max_levs,div_coeff_old,div_coeff_new,gamma1bar, &
+                              gamma1bar_hold,s0_init,rho0_old,rhoh0_old,rho0_new, &
+                              rhoh0_new,p0_init,p0_old,p0_new,w0,etarho,etarho_cc, &
+                              div_etarho,psi,tempbar,grav_cell)
+
+    ! now that we have dr and nr we can fill initial state
+    do n=1,max_levs
+       call init_base_state(n,model_file,s0_init(n,:,:),p0_init(n,:),dx(n,:))
+    end do
+
+    ! Initialize bc's
+    call initialize_bc(the_bc_tower,max_levs,pmask)
+
+    ! Build the level 1 layout.
+    call layout_build_ba(la_array(1),mba%bas(1),mba%pd(1),pmask)
+
+    ! Build the level 1 data only
+    call multifab_build(sold(1), la_array(1), nscal, 3)
+
+    ! Define bc_tower at level 1.
+    call bc_tower_level_build(the_bc_tower,1,la_array(1))
+
+    nlevs = 1
+
+    if (max_levs > 1) then
+
+       ! Initialize the level 1 data only.
+       call initscalardata_on_level(1,sold(1),s0_init(1,:,:),p0_init(1,:), &
+                                    dx(1,:),the_bc_tower%bc_tower_array(1))
+
+       new_grid = .true.
+       nl = 1
+       
+       do while ( (nl .lt. max_levs) .and. (new_grid) )
+          
+          ! Do we need finer grids?
+          call make_new_grids(new_grid,la_array(nl),la_array(nl+1),sold(nl),dx(nl,1), &
+                              buf_wid,ref_ratio,nl,max_grid_size)
+          
+          if (new_grid) then
+              
+             call copy(mba%bas(nl+1),get_boxarray(la_array(nl+1)))
+             
+             ! Build the level nl+1 data only.
+             call multifab_build(sold(nl+1),la_array(nl+1),nscal,3)
+             
+             ! Define bc_tower at level nl+1.
+             call bc_tower_level_build(the_bc_tower,nl+1,la_array(nl+1))
+             
+             ! fills the physical region of each level with problem data
+             call initscalardata_on_level(nl+1,sold(nl+1),s0_init(nl+1,:,:), &
+                                          p0_init(nl+1,:),dx(nl+1,:), &
+                                          the_bc_tower%bc_tower_array(nl+1))
+
+             nlevs = nl+1
+             nl = nl + 1
+             
+          endif ! if (new_grid) 
+          
+       enddo
+       
+       do n = 1,nlevs
+          call destroy(sold(n))
+       end do
+
+       nlevs = nl
+
+       ! check for proper nesting
+       if (nlevs .ge. 3) then
+          call enforce_proper_nesting(mba,la_array,max_grid_size)
+       end if
+       
+    else
+
+       call destroy(sold(1))
+       
+    end if  ! end if (maxlev > 1)
+
+    call ml_layout_restricted_build(mla,mba,nlevs,pmask)
+    
+    nlevs = mla%nlevel
+    
+    do n = 1, nlevs
+       call destroy(la_array(n))
+    end do
+    
+    ! build states
+    do n = 1,nlevs
+       call multifab_build(         uold(n), mla%la(n),    dm, 3)
+       call multifab_build(         sold(n), mla%la(n), nscal, 3)
+       call multifab_build(        gpres(n), mla%la(n),    dm, 1)
+       call multifab_build(         pres(n), mla%la(n),     1, 1, nodal)
+       call multifab_build(         dSdt(n), mla%la(n),     1, 0)
+       call multifab_build(   Source_old(n), mla%la(n),     1, 1)
+       call multifab_build(rho_omegadot2(n), mla%la(n), nspec, 1)
+
+       call setval(         uold(n), ZERO, all=.true.)
+       call setval(         sold(n), ZERO, all=.true.)
+       call setval(        gpres(n), ZERO, all=.true.)
+       call setval(         pres(n), ZERO, all=.true.)
+       call setval(   Source_old(n), ZERO, all=.true.)
+       call setval(         dSdt(n), ZERO, all=.true.)
+       call setval(rho_omegadot2(n), ZERO, all=.true.)
+    end do
+
+    ! create a "bounding box" for each level
+    ! this the smallest possible box that fits every grid at a particular level
+    ! this even includes the empty spaces if there are gaps between grids
+    allocate(boundingbox(nlevs))
+    do n=1,nlevs
+       boundingbox(n) = get_box(sold(n),1)
+       do i=2, sold(n)%nboxes
+          boundingbox(n) = box_bbox(boundingbox(n),get_box(sold(n),i))
+       end do
+    end do
+
+    ! compute diffboxarray
+    ! each box in diffboxarray corresponds to an "empty space" between valid regions at 
+    ! each level, excluding the coarsest level.
+    ! I am going to use this to compute all of the intermediate r_start_coord and r_end_coord
+    allocate(validboxarr(nlevs))
+    allocate(diffboxarray(nlevs))
+    do n=1,nlevs
+       call boxarray_build_copy(validboxarr(n),get_boxarray(sold(n)))
+       call boxarray_boxarray_diff(diffboxarray(n),boundingbox(n),validboxarr(n))
+       call boxarray_simplify(diffboxarray(n))
+    end do
+
+    ! use boundingbox and diffboxarray to create numdisjointchunks, r_start_coord, 
+    ! and r_end_coord
+    call init_geometry(nlevs,boundingbox,diffboxarray)
+
+    call initveldata(uold,s0_init,p0_init,dx,the_bc_tower%bc_tower_array,mla)
+    call initscalardata(sold,s0_init,p0_init,dx,the_bc_tower%bc_tower_array,mla)
+
+    if (perturb_model) then
+       ! force rho0 to be the average density
+       call average(mla,sold,rho0_old,dx,rho_comp)
+    else
+       ! s0_init already contains the average
+       rho0_old = s0_init(:,:,rho_comp)
+       call fill_ghost_base(rho0_old,.true.)
+       call restrict_base(rho0_old,.true.)
+    end if
+
+    ! this will be overwritten if we are multilevel or if perturb_model = T
+    ! but we copy it anyway for the initial condition
+    p0_old = p0_init
+    call fill_ghost_base(p0_old,.true.)
+    call restrict_base(p0_old,.true.)
+
+    ! this will be overwritten if we are multilevel or if perturb_model = T
+    ! but we copy it anyway so s0_init can remain local
+    rhoh0_old = s0_init(:,:,rhoh_comp)
+    call fill_ghost_base(rhoh0_old,.true.)
+    call restrict_base(rhoh0_old,.true.)
+
+    do n=1,nlevs
+       call destroy(validboxarr(n))
+       call destroy(diffboxarray(n))
+    end do
 
     call destroy(mba)
 
@@ -517,7 +726,7 @@ contains
 
 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
-  subroutine initialize_bc(the_bc_tower,num_levs,pmask)
+   subroutine initialize_bc(the_bc_tower,num_levs,pmask)
 
     use bc_module
     use probin_module, only : bcx_lo, bcx_hi, bcy_lo, bcy_hi, bcz_lo, bcz_hi
@@ -580,36 +789,37 @@ contains
 
   end subroutine initialize_dx
 
-  subroutine initialize_1d_arrays(div_coeff_old,div_coeff_new,gamma1bar,gamma1bar_hold, &
-                                  s0_init,rho0_old,rhoh0_old,rho0_new,rhoh0_new,p0_init, &
-                                  p0_old,p0_new,w0,etarho,etarho_cc,div_etarho,psi,tempbar, &
-                                  grav_cell)
+  subroutine initialize_1d_arrays(num_levs,div_coeff_old,div_coeff_new,gamma1bar, &
+                                  gamma1bar_hold,s0_init,rho0_old,rhoh0_old,rho0_new, &
+                                  rhoh0_new,p0_init,p0_old,p0_new,w0,etarho,etarho_cc, &
+                                  div_etarho,psi,tempbar,grav_cell)
 
+    integer, intent(in)  :: num_levs    
     real(dp_t) , pointer :: div_coeff_old(:,:),div_coeff_new(:,:),gamma1bar(:,:)
     real(dp_t) , pointer :: gamma1bar_hold(:,:),s0_init(:,:,:),rho0_old(:,:)
     real(dp_t) , pointer :: rhoh0_old(:,:),rho0_new(:,:),rhoh0_new(:,:),p0_init(:,:)
     real(dp_t) , pointer :: p0_old(:,:),p0_new(:,:),w0(:,:),etarho(:,:),etarho_cc(:,:)
     real(dp_t) , pointer :: div_etarho(:,:),psi(:,:),tempbar(:,:),grav_cell(:,:)
 
-    allocate(div_coeff_old (nlevs,0:nr_fine-1))
-    allocate(div_coeff_new (nlevs,0:nr_fine-1))
-    allocate(gamma1bar     (nlevs,0:nr_fine-1))
-    allocate(gamma1bar_hold(nlevs,0:nr_fine-1))
-    allocate(s0_init       (nlevs,0:nr_fine-1,nscal))
-    allocate(rho0_old      (nlevs,0:nr_fine-1))
-    allocate(rhoh0_old     (nlevs,0:nr_fine-1))
-    allocate(rho0_new      (nlevs,0:nr_fine-1))
-    allocate(rhoh0_new     (nlevs,0:nr_fine-1))
-    allocate(p0_init       (nlevs,0:nr_fine-1))
-    allocate(p0_old        (nlevs,0:nr_fine-1))
-    allocate(p0_new        (nlevs,0:nr_fine-1))
-    allocate(w0            (nlevs,0:nr_fine))
-    allocate(etarho        (nlevs,0:nr_fine))
-    allocate(etarho_cc     (nlevs,0:nr_fine-1))
-    allocate(div_etarho    (nlevs,0:nr_fine-1))
-    allocate(psi           (nlevs,0:nr_fine-1))
-    allocate(tempbar       (nlevs,0:nr_fine-1))
-    allocate(grav_cell     (nlevs,0:nr_fine-1))
+    allocate(div_coeff_old (num_levs,0:nr_fine-1))
+    allocate(div_coeff_new (num_levs,0:nr_fine-1))
+    allocate(gamma1bar     (num_levs,0:nr_fine-1))
+    allocate(gamma1bar_hold(num_levs,0:nr_fine-1))
+    allocate(s0_init       (num_levs,0:nr_fine-1,nscal))
+    allocate(rho0_old      (num_levs,0:nr_fine-1))
+    allocate(rhoh0_old     (num_levs,0:nr_fine-1))
+    allocate(rho0_new      (num_levs,0:nr_fine-1))
+    allocate(rhoh0_new     (num_levs,0:nr_fine-1))
+    allocate(p0_init       (num_levs,0:nr_fine-1))
+    allocate(p0_old        (num_levs,0:nr_fine-1))
+    allocate(p0_new        (num_levs,0:nr_fine-1))
+    allocate(w0            (num_levs,0:nr_fine))
+    allocate(etarho        (num_levs,0:nr_fine))
+    allocate(etarho_cc     (num_levs,0:nr_fine-1))
+    allocate(div_etarho    (num_levs,0:nr_fine-1))
+    allocate(psi           (num_levs,0:nr_fine-1))
+    allocate(tempbar       (num_levs,0:nr_fine-1))
+    allocate(grav_cell     (num_levs,0:nr_fine-1))
 
     div_coeff_old = ZERO
     div_coeff_new = ZERO
