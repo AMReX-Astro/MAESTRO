37f380070ba4b2ddfeb790c012b168255e245fc1
ajnonaka <ajnonaka>
2008-09-10 17:07:21 +0000

    made the 3d version match the 2d

--- SCIENCE/xrb/tag_boxes.f90
+++ SCIENCE/xrb/tag_boxes.f90
@@ -143,7 +143,7 @@ contains
           do j = lo(2),lo(2)+ny-1
              do i = lo(1),lo(1)+nx-1
                 Xhe = he(i,j,k)/rho(i,j,k)
-                if (Xhe .gt. 1.d-1 .and. Xhe .lt. 0.99d0) then
+                if (Xhe .gt. 1.d-4 .and. Xhe .lt. 0.99d0) then
                    radialtag(k) = .true.
                 end if
              end do
