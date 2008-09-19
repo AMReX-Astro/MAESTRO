def70ab431f28a99fc54379fc6f6be5862195894
ajnonaka <ajnonaka>
2008-09-19 19:45:19 +0000

    rename predict_hprime to predict_hprime_then_rhohprime to be consistent with our current naming convention (and so we are crystal clear about what we're actually doing)

--- Source/pred_parameters.f90
+++ Source/pred_parameters.f90
@@ -2,10 +2,10 @@ module pred_parameters
 
    implicit none
 
-   integer, parameter :: predict_rhohprime        = 1
-   integer, parameter :: predict_h                = 2
-   integer, parameter :: predict_T_then_rhohprime = 3
-   integer, parameter :: predict_T_then_h         = 4
-   integer, parameter :: predict_hprime           = 5
+   integer, parameter :: predict_rhohprime             = 1
+   integer, parameter :: predict_h                     = 2
+   integer, parameter :: predict_T_then_rhohprime      = 3
+   integer, parameter :: predict_T_then_h              = 4
+   integer, parameter :: predict_hprime_then_rhohprime = 5
 
 end module pred_parameters
