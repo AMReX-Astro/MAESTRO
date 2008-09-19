3df3dcd032b6e4e2b91d93d4912b252afe5e42c0
ajnonaka <ajnonaka>
2008-09-19 19:37:04 +0000

    integer, parameter :: predict_hprime           = 5
    here we go

--- Source/pred_parameters.f90
+++ Source/pred_parameters.f90
@@ -6,5 +6,6 @@ module pred_parameters
    integer, parameter :: predict_h                = 2
    integer, parameter :: predict_T_then_rhohprime = 3
    integer, parameter :: predict_T_then_h         = 4
+   integer, parameter :: predict_hprime           = 5
 
 end module pred_parameters
