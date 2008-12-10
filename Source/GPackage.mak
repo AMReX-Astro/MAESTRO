46a0fc1695b9e4ea48231490837df287f9e0a5d2
ajnonaka <ajnonaka>
2008-12-10 22:58:42 +0000

    new ppm stuff

--- Source/GPackage.mak
+++ Source/GPackage.mak
@@ -55,6 +55,7 @@ f90sources += ml_solve.f90
 f90sources += modify_scal_force.f90
 f90sources += multifab_fill_ghost_cells.f90
 f90sources += multifab_physbc.f90
+f90sources += ppm.f90
 f90sources += pred_parameters.f90
 f90sources += probin.f90
 f90sources += proj_parameters.f90
---
+++ Source/ppm.f90
@@ -0,0 +1,92 @@
+module ppm_module
+
+  use bl_types
+
+  implicit none
+
+  private
+
+  public :: ppm_2d, ppm_fpu_2d, ppm_3d, ppm_fpu_3d
+
+contains
+
+  ! characteristics based on u
+  subroutine ppm_2d(s,ng_s,u,ng_u,slx,sly,lo,hi,bc)
+
+    use bc_module
+    use bl_constants_module
+
+    integer        , intent(in   ) :: lo(:),hi(:),ng_s,ng_u
+    real(kind=dp_t), intent(in   ) ::   s(lo(1)-ng_s:,lo(2)-ng_s:)
+    real(kind=dp_t), intent(in   ) ::   u(lo(1)-ng_u:,lo(2)-ng_u:,:)
+    real(kind=dp_t), intent(  out) :: slx(lo(1)-1   :,lo(2)-1   :,:) 
+    real(kind=dp_t), intent(  out) :: sly(lo(1)-1   :,lo(2)-1   :,:) 
+    integer        , intent(in   ) :: bc(:,:,:)
+
+    ! local
+
+
+
+  end subroutine ppm_2d
+
+  ! characteristics based on umac
+  subroutine ppm_fpu_2d(s,ng_s,umac,ng_u,slx,sly,lo,hi,bc)
+
+    use bc_module
+    use bl_constants_module
+
+    integer        , intent(in   ) :: lo(:),hi(:),ng_s,ng_u
+    real(kind=dp_t), intent(in   ) ::    s(lo(1)-ng_s:,lo(2)-ng_s:)
+    real(kind=dp_t), intent(in   ) :: umac(lo(1)-ng_u:,lo(2)-ng_u:,:)
+    real(kind=dp_t), intent(  out) ::  slx(lo(1)-1   :,lo(2)-1   :,:) 
+    real(kind=dp_t), intent(  out) ::  sly(lo(1)-1   :,lo(2)-1   :,:) 
+    integer        , intent(in   ) :: bc(:,:,:)
+
+    ! local
+
+
+
+  end subroutine ppm_fpu_2d
+
+  ! characteristics based on u
+  subroutine ppm_3d(s,ng_s,u,ng_u,slx,sly,lo,hi,bc)
+
+    use bc_module
+    use bl_constants_module
+
+    integer        , intent(in   ) :: lo(:),hi(:),ng_s,ng_u
+    real(kind=dp_t), intent(in   ) ::   s(lo(1)-ng_s:,lo(2)-ng_s:,hi(3)-ng_s:)
+    real(kind=dp_t), intent(in   ) ::   u(lo(1)-ng_u:,lo(2)-ng_u:,hi(3)-ng_u:,:)
+    real(kind=dp_t), intent(  out) :: slx(lo(1)-1   :,lo(2)-1   :,hi(3)-1   :,:) 
+    real(kind=dp_t), intent(  out) :: sly(lo(1)-1   :,lo(2)-1   :,hi(3)-1   :,:)  
+    real(kind=dp_t), intent(  out) :: slz(lo(1)-1   :,lo(2)-1   :,hi(3)-1   :,:)
+    integer        , intent(in   ) :: bc(:,:,:)
+
+    ! local
+
+
+
+  end subroutine ppm_3d
+
+  ! characteristics based on umac
+  subroutine ppm_fpu_3d(s,ng_s,umac,ng_u,slx,sly,lo,hi,bc)
+
+    use bc_module
+    use bl_constants_module
+
+    integer        , intent(in   ) :: lo(:),hi(:),ng_s,ng_u
+    real(kind=dp_t), intent(in   ) ::    s(lo(1)-ng_s:,lo(2)-ng_s:,hi(3)-ng_s:)
+    real(kind=dp_t), intent(in   ) :: umac(lo(1)-ng_u:,lo(2)-ng_u:,hi(3)-ng_u:,:)
+    real(kind=dp_t), intent(  out) ::  slx(lo(1)-1   :,lo(2)-1   :,hi(3)-1   :,:) 
+    real(kind=dp_t), intent(  out) ::  sly(lo(1)-1   :,lo(2)-1   :,hi(3)-1   :,:)  
+    real(kind=dp_t), intent(  out) ::  slz(lo(1)-1   :,lo(2)-1   :,hi(3)-1   :,:)
+    integer        , intent(in   ) :: bc(:,:,:)
+
+    ! local
+
+
+
+  end subroutine ppm_fpu_3d
+
+
+end module ppm_module
