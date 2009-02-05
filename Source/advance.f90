0568103899b8ab170a215ddda9d210622aac94f2
ajnonaka <ajnonaka>
2009-02-05 15:58:48 +0000

    remove "use react_base_module"

--- Source/advance.f90
+++ Source/advance.f90
@@ -35,7 +35,6 @@ contains
     use make_w0_module
     use advect_base_module
     use correct_base_module
-    use react_base_module
     use react_state_module
     use make_S_module
     use average_module
