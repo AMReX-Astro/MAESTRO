e44a4e94613c3ed049cf3fbbc45901cfcc0779c6
ajnonaka <ajnonaka>
2009-02-06 18:09:40 +0000

    simplified the correct_base interface

--- correct_base.f90
+++ correct_base.f90
@@ -10,55 +10,46 @@ module correct_base_module
 
 contains
 
-  subroutine correct_base(rho0_new,sprimebar)
+  subroutine correct_base(s0_new,sprimebar)
 
     use bl_prof_module
-    use geometry, only: spherical, nlevs_radial
+    use geometry, only: spherical
     use restrict_base_module
     use bl_error_module
 
-    real(kind=dp_t), intent(inout) :: rho0_new(:,0:)
+    real(kind=dp_t), intent(inout) :: s0_new(:,0:)
     real(kind=dp_t), intent(in   ) :: sprimebar(:,0:)
     
     ! local
-    integer :: n
-
     type(bl_prof_timer), save :: bpt
 
     call build(bpt, "correct_base")
     
-    do n=1,nlevs_radial
-       if (spherical .eq. 1) then
-          call correct_base_state_spherical(n,rho0_new(n,0:),sprimebar(n,0:))
-       else
-          call bl_error("correct_base not defined for plane-parallel")
-       end if
-    enddo
+    if (spherical .eq. 1) then
+       call correct_base_state_spherical(s0_new(1,:),sprimebar(1,:))
+    else
+       call bl_error("correct_base not defined for plane-parallel")
+    end if
 
-    call restrict_base(rho0_new,.true.)
-    call fill_ghost_base(rho0_new,.true.)
+    call restrict_base(s0_new,.true.)
+    call fill_ghost_base(s0_new,.true.)
 
     call destroy(bpt)
        
   end subroutine correct_base
 
-  subroutine correct_base_state_spherical(n,rho0_new,sprimebar)
+  subroutine correct_base_state_spherical(s0_new,sprimebar)
 
     use geometry, only: anelastic_cutoff_coord, r_cc_loc, r_edge_loc, dr
 
-    integer        , intent(in   ) :: n
-    real(kind=dp_t), intent(inout) :: rho0_new(0:)
+    real(kind=dp_t), intent(inout) :: s0_new(0:)
     real(kind=dp_t), intent(in   ) :: sprimebar(0:)
     
     ! Local variables
     integer :: r
-   
-!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-! UPDATE RHO0
-!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
-    do r=0,anelastic_cutoff_coord(n)-1
-       rho0_new(r) = rho0_new(r) + sprimebar(r)
+    do r=0,anelastic_cutoff_coord(1)-1
+       s0_new(r) = s0_new(r) + sprimebar(r)
     end do
     
   end subroutine correct_base_state_spherical
