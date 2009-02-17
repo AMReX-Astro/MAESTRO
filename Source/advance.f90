e26ead0b090938b648c0bf03ac0da2c07be2df90
ajnonaka <ajnonaka>
2009-02-17 22:21:37 +0000

    to update p0, advect_base_pres is now default rather than enforce_HSE.  I left that code in there for now, commented out, until we know more

--- Source/advance.f90
+++ Source/advance.f90
@@ -456,16 +456,20 @@ contains
     call make_grav_cell(grav_cell_new,rho0_new)
 
     if (evolve_base_state) then
-       ! set new p0 through HSE
-       p0_new = p0_old
-       call enforce_HSE(rho0_new,p0_new,grav_cell_new)
 
-       ! make psi
-       if (spherical .eq. 0) then
-          call make_psi_planar(etarho_cc,psi)
-       else
-          call make_psi_spherical(psi,w0,gamma1bar,p0_old,p0_new,Sbar)
-       end if
+       call advect_base_pres(w0,Sbar,p0_old,p0_new,gamma1bar,psi,etarho_cc,dx(:,1),dt)
+
+!       ! set new p0 through HSE
+!       p0_new = p0_old
+!       call enforce_HSE(rho0_new,p0_new,grav_cell_new)
+!
+!       ! make psi
+!       if (spherical .eq. 0) then
+!          call make_psi_planar(etarho_cc,psi)
+!       else
+!          call make_psi_spherical(psi,w0,gamma1bar,p0_old,p0_new,Sbar)
+!       end if
+
     else
        p0_new = p0_old
     end if
@@ -840,16 +844,20 @@ contains
     call make_grav_cell(grav_cell_nph,rho0_nph)
 
     if (evolve_base_state) then
-       ! set new p0 through HSE
-       p0_new = p0_old
-       call enforce_HSE(rho0_new,p0_new,grav_cell_new)
 
-       ! make psi
-       if (spherical .eq. 0) then
-          call make_psi_planar(etarho_cc,psi)
-       else
-          call make_psi_spherical(psi,w0,gamma1bar,p0_old,p0_new,Sbar)
-       end if
+       call advect_base_pres(w0,Sbar,p0_old,p0_new,gamma1bar,psi,etarho_cc,dx(:,1),dt)
+
+!       ! set new p0 through HSE
+!       p0_new = p0_old
+!       call enforce_HSE(rho0_new,p0_new,grav_cell_new)
+!
+!       ! make psi
+!       if (spherical .eq. 0) then
+!          call make_psi_planar(etarho_cc,psi)
+!       else
+!          call make_psi_spherical(psi,w0,gamma1bar,p0_old,p0_new,Sbar)
+!       end if
+
     else
        p0_new = p0_old
     end if
