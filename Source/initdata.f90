3836b78046f7757d7c19f69adb84ee92a4413a09
ajnonaka <ajnonaka>
2009-03-26 18:21:24 +0000

    wrote perturb_3d_sphr to add a hot temperature bubble to a full WD

--- Source/initdata.f90
+++ Source/initdata.f90
@@ -319,6 +319,32 @@ contains
        enddo
     enddo
 
+    if (perturb_model) then
+
+       ! add an optional perturbation
+       do k = lo(3), hi(3)
+          z = prob_lo(3) + (dble(k)+HALF) * dx(3)
+
+          do j = lo(2), hi(2)
+             y = prob_lo(2) + (dble(j)+HALF) * dx(2)
+
+             do i = lo(1), hi(1)
+                x = prob_lo(1) + (dble(i)+HALF) * dx(1)
+
+                call perturb_3d_sphr(x, y, z, p0_cart(i,j,k,1), s(i,j,k,:), &
+                                     dens_pert, rhoh_pert, rhoX_pert, temp_pert, trac_pert)
+
+                s(i,j,k,rho_comp) = dens_pert
+                s(i,j,k,rhoh_comp) = rhoh_pert
+                s(i,j,k,temp_comp) = temp_pert
+                s(i,j,k,spec_comp:spec_comp+nspec-1) = rhoX_pert(:)
+                s(i,j,k,trac_comp:trac_comp+ntrac-1) = trac_pert(:)
+             enddo
+          enddo
+       enddo
+
+    end if
+
     deallocate(p0_cart)
 
   end subroutine initscalardata_3d_sphr
@@ -554,6 +580,61 @@ contains
     trac_pert = ZERO
 
   end subroutine perturb_3d
+
+  subroutine perturb_3d_sphr(x, y, z, p0_background, s0_init, dens_pert, rhoh_pert, &
+                             rhoX_pert, temp_pert, trac_pert)
+
+    ! apply an optional perturbation to the initial temperature field
+    ! to see some bubbles
+    real(kind=dp_t), intent(in ) :: x, y, z
+    real(kind=dp_t), intent(in ) :: p0_background, s0_init(:)
+    real(kind=dp_t), intent(out) :: dens_pert, rhoh_pert, temp_pert
+    real(kind=dp_t), intent(out) :: rhoX_pert(:)
+    real(kind=dp_t), intent(out) :: trac_pert(:)
+
+    real(kind=dp_t) :: temp, t0
+    real(kind=dp_t) :: x0, y0, z0, r0
+
+    t0 = s0_init(temp_comp)
+
+    ! center of star is at 2.5d8
+    x0 = 2.5d8
+    y0 = 2.5d8
+    z0 = 3.0d8
+
+    ! Tanh bubbles
+    r0 = sqrt( (x-x0)**2 + (y-y0)**2 + (z-z0)**2 ) / 2.5e6
+    
+    ! This case works
+    temp = t0 * (ONE + TWO*(.150_dp_t * 0.5_dp_t * (1.0_dp_t + tanh((2.0-r0)))))
+
+    ! Use the EOS to make this temperature perturbation occur at constant 
+    ! pressure
+    temp_eos(1) = temp
+    p_eos(1) = p0_background
+    den_eos(1) = s0_init(rho_comp)
+    xn_eos(1,:) = s0_init(spec_comp:spec_comp+nspec-1)/s0_init(rho_comp)
+
+    call eos(eos_input_tp, den_eos, temp_eos, &
+             npts, nspec, &
+             xn_eos, &
+             p_eos, h_eos, e_eos, &
+             cv_eos, cp_eos, xne_eos, eta_eos, pele_eos, &
+             dpdt_eos, dpdr_eos, dedt_eos, dedr_eos, &
+             dpdX_eos, dhdX_eos, &
+             gam1_eos, cs_eos, s_eos, &
+             dsdt_eos, dsdr_eos, &
+             do_diag)
+
+    dens_pert = den_eos(1)
+    rhoh_pert = den_eos(1)*h_eos(1)
+    rhoX_pert = dens_pert*xn_eos(1,:)
+
+    temp_pert = temp
+
+    trac_pert = ZERO
+
+  end subroutine perturb_3d_sphr
 
   subroutine scalar_diags (istep,s,s0_init,p0_background,dx)
 
