48ae7a73b0b543d6f1f0bde30d657dea1ab08707
ajnonaka <ajnonaka>
2009-03-30 21:06:28 +0000

    clean up print statement

--- Source/make_w0.f90
+++ Source/make_w0.f90
@@ -68,16 +68,20 @@ contains
 
     call fill_ghost_base(w0_force,.true.)
 
-    do n=1,nlevs_radial
-       max_w0 = zero
-       do r=r_start_coord(n,1),r_end_coord(n,1)+1
-          max_w0 = max(max_w0, abs(w0(n,r)))
+    if (verbose .ge. 1) then
+       do n=1,nlevs_radial
+          max_w0 = zero
+          do r=r_start_coord(n,1),r_end_coord(n,1)+1
+             max_w0 = max(max_w0, abs(w0(n,r)))
+          end do
+          if (parallel_IOProcessor()) then
+             write(6,*) '... max CFL of w0: ',max_w0 * dt / dr(n)
+          end if
        end do
-       if (parallel_IOProcessor() .and. verbose .ge. 1) then
-          write(6,*) '... max CFL of w0: ',max_w0 * dt / dr(n)
+       if (parallel_IOProcessor()) then
           write(6,*) ''
        end if
-    end do
+    end if
 
     call destroy(bpt)
 
