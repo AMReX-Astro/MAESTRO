f73c45bbbf7d3e910b0946a47c23d5d03233819b
mzingale <mzingale>
2009-04-10 18:08:43 +0000

    also account for differing number of spaces between the parameter and the "="

--- docs/runtime_parameters/rp.sh
+++ docs/runtime_parameters/rp.sh
@@ -40,7 +40,7 @@ parameters=$(grep namelist probin.f90 | awk '{print $3}' | sort)
 
 for i in ${parameters}
 do
-    value=$(grep -m 1 "^[ ]*$i =" probin.f90 | cut -d'=' -f2 | cut -d"!" -f1)
+    value=$(grep -E -m 1 "^[ ]*$i +=" probin.f90 | cut -d'=' -f2 | cut -d"!" -f1)
 
     echo "\\verb= " $i "=" " &   & " `echo ${value} | sed 's/\_/\\\_/g'` "\\\\"
 done
