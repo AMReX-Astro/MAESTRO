f239c602ffb993fd02506925c3d0da640fd6d839
cmalone <cmalone>
2009-04-10 15:48:45 +0000

    remove comments from the default value column add "//" to the end of each line

--- docs/runtime_parameters/rp.sh
+++ docs/runtime_parameters/rp.sh
@@ -4,9 +4,8 @@ parameters=$(grep namelist probin.f90 | awk '{print $3}' | sort)
 
 for i in ${parameters}
 do
+    value=$(grep -m 1 "$i =" probin.f90 | cut -d'=' -f2 | cut -d"!" -f1)
 
-    value=$(grep -m 1 "$i =" probin.f90 | cut -d'=' -f2)
-
-    echo "\\verb= " $i "=" " &   & " ${value}
+    echo "\\verb= " $i "=" " &   & " ${value} "//"
 done
 
