8f45f9447680bf0d0152854a9fe24f8f6cd0c82a
ajnonaka <ajnonaka>
2009-06-18 21:34:21 +0000

    change the ghost cell fill to the fillpatch style (compute slope at coarse cell-center to maintain conservation in the fine cells).
    also filling two ghost cells instead of one.  No effect yet, but with ppm, it will need these.

--- Source/restrict_base.f90
+++ Source/restrict_base.f90
@@ -63,7 +63,8 @@ contains
     logical        , intent(in   ) :: is_cell_centered
 
     ! local
-    integer :: n,i
+    integer         :: n,i,r_crse
+    real(kind=dp_t) :: del,dpls,dmin,slim,slope
 
     type(bl_prof_timer), save :: bpt
 
@@ -71,39 +72,58 @@ contains
 
     if (is_cell_centered) then
 
+       ! compute limited slopes at the coarse level and set 2 fine ghost cells 
+       ! maintaining conservation
        do n=nlevs_radial,2,-1
           do i=1,numdisjointchunks(n)
              if (r_start_coord(n,i) .ne. 0) then
-                s0(n,r_start_coord(n,i)-1) = -(1.d0/5.d0)*s0(n,r_start_coord(n,i)+1) &
-                     + (2.d0/3.d0)*s0(n,r_start_coord(n,i)) &
-                     + (8.d0/15.d0)*s0(n-1,r_start_coord(n,i)/2-1)
+                r_crse = r_start_coord(n,i)/2-1
+                del = HALF*(s0(n-1,r_crse+1)-s0(n-1,r_crse-1))
+                dpls = TWO*(s0(n-1,r_crse+1)-s0(n-1,r_crse  ))
+                dmin = TWO*(s0(n-1,r_crse  )-s0(n-1,r_crse-1))
+                slim = min(abs(dpls),abs(dmin))
+                slim = merge(slim,ZERO,dpls*dmin.gt.ZERO)
+                slope=sign(ONE,del)*min(slim,abs(del))
+                s0(n,r_start_coord(n,i)-1) = s0(n-1,r_crse) + FOURTH*slope
+                s0(n,r_start_coord(n,i)-2) = s0(n-1,r_crse) - FOURTH*slope
              end if
              
              if (r_end_coord(n,i) .ne. nr(n)-1) then
-                s0(n,r_end_coord(n,i)+1) = -(1.d0/5.d0)*s0(n,r_end_coord(n,i)-1) &
-                     + (2.d0/3.d0)*s0(n,r_end_coord(n,i)) &
-                     + (8.d0/15.d0)*s0(n-1,(r_end_coord(n,i)+1)/2)
+                r_crse = (r_end_coord(n,i)+1)/2
+                del = HALF*(s0(n-1,r_crse+1)-s0(n-1,r_crse-1))
+                dpls = TWO*(s0(n-1,r_crse+1)-s0(n-1,r_crse  ))
+                dmin = TWO*(s0(n-1,r_crse  )-s0(n-1,r_crse-1))
+                slim = min(abs(dpls),abs(dmin))
+                slim = merge(slim,ZERO,dpls*dmin.gt.ZERO)
+                slope=sign(ONE,del)*min(slim,abs(del))
+                s0(n,r_end_coord(n,i)+1) = s0(n-1,r_crse) - FOURTH*slope
+                s0(n,r_end_coord(n,i)+2) = s0(n-1,r_crse) + FOURTH*slope
              end if
           end do
-
        end do
 
     else
 
        do n=nlevs_radial,2,-1
-
           do i=1,numdisjointchunks(n)
+
              if (r_start_coord(n,i) .ne. 0) then
+                ! quadratic interpolation from the three closest points
                 s0(n,r_start_coord(n,i)-1) = -THIRD*s0(n,r_start_coord(n,i)+1) &
                      + s0(n,r_start_coord(n,i)) + THIRD*s0(n-1,r_start_coord(n,i)/2-1)
+                ! copy the next ghost cell value directly in from the coarser level
+                s0(n,r_start_coord(n,i)-2) = s0(n-1,(r_start_coord(n,i)-2)/2) 
              end if
              
              if (r_end_coord(n,i)+1 .ne. nr(n)) then
+                ! quadratic interpolation from the three closest points
                 s0(n,r_end_coord(n,i)+2) = -THIRD*s0(n,r_end_coord(n,i)) &
                      + s0(n,r_end_coord(n,i)+1) + THIRD*s0(n-1,(r_end_coord(n,i)+2)/2)
+                ! copy the next ghost cell value directly in from the coarser level
+                s0(n,r_end_coord(n,i)+3) = s0(n-1,(r_end_coord(n,i)+3)/2)
              end if
-          end do
 
+          end do
        end do
 
     end if
