d23a411719f6519733384f752d04935cc2aaca09
mzingale <mzingale>
2009-07-01 19:00:08 +0000

    comments

--- Source/mkforce.f90
+++ Source/mkforce.f90
@@ -238,12 +238,12 @@ contains
              distance = sqrt(xx**2 + yy**2 + zz**2)
              cos_theta = normal(i,j,k,3)
 
-             ! omega x (omega x r )
+             ! omega x (omega x r ) = - omega^2 x e_x  - omega^2 y e_y
              centrifugal_term(1) = -omega * omega * distance * normal(i,j,k,1)
              centrifugal_term(2) = -omega * omega * distance * normal(i,j,k,2)
              centrifugal_term(3) = ZERO
 
-             ! 2 omega x u
+             ! 2 omega x U = - 2 omega v e_x  + 2 omega u e_y
              coriolis_term(1) = -TWO * omega * uold(i,j,k,2)
              coriolis_term(2) =  TWO * omega * uold(i,j,k,1)
              coriolis_term(3) = ZERO
