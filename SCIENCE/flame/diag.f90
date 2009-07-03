d0a18afd0d7befd616445d369a2b5535effa249f
mzingale <mzingale>
2009-07-03 14:28:37 +0000

    fix compilation

--- SCIENCE/flame/diag.f90
+++ SCIENCE/flame/diag.f90
@@ -98,7 +98,7 @@ contains
           select case (dm)
           case (2)
              if (n .eq. nlevs) then
-                call diag_2d(time,dt,dx(n,:), &
+                call diag_2d(n,time,dt,dx(n,:), &
                              sp(:,:,1,:),ng_s, &
                              rhnp(:,:,1,1),ng_rhn, &
                              rhep(:,:,1,1),ng_rhe, &
@@ -109,7 +109,7 @@ contains
                              lo,hi)
              else
                 mp => dataptr(mla%mask(n), i)
-                call diag_2d(time,dt,dx(n,:), &
+                call diag_2d(n,time,dt,dx(n,:), &
                              sp(:,:,1,:),ng_s, &
                              rhnp(:,:,1,1),ng_rhn, &
                              rhep(:,:,1,1),ng_rhe, &
@@ -149,7 +149,7 @@ contains
 
   end subroutine diag
 
-  subroutine diag_2d(time,dt,dx, &
+  subroutine diag_2d(n,time,dt,dx, &
                      s,ng_s, &
                      rho_Hnuc,ng_rhn, &
                      rho_Hext,ng_rhe, &
@@ -160,10 +160,11 @@ contains
                      mask)
 
     use variables, only: rho_comp, spec_comp, temp_comp, rhoh_comp
+    use bl_constants_module
     use network, only: nspec
     use probin_module, only: prob_lo
 
-    integer, intent(in) :: lo(:), hi(:), ng_s, ng_u, ng_rhn, ng_rhe
+    integer, intent(in) :: n, lo(:), hi(:), ng_s, ng_u, ng_rhn, ng_rhe
     real (kind=dp_t), intent(in   ) ::      s(lo(1)-ng_s:,lo(2)-ng_s:,:)
     real (kind=dp_t), intent(in   ) :: rho_Hnuc(lo(1)-ng_rhn:,lo(2)-ng_rhn:)
     real (kind=dp_t), intent(in   ) :: rho_Hext(lo(1)-ng_rhe:,lo(2)-ng_rhe:)
@@ -192,7 +193,7 @@ contains
 
           cell_valid = .true.
           if (present(mask)) then
-             if ( (.not. mask(i,j,k)) ) cell_valid = .false.
+             if ( (.not. mask(i,j)) ) cell_valid = .false.
           endif
 
           if (cell_valid) then
