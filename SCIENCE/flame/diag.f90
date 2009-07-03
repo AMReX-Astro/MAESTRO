2714fa475dc5a3cb3cdbfc7af31a46474cabaf86
mzingale <mzingale>
2009-07-03 14:25:49 +0000

    start of making this multilevel aware

--- SCIENCE/flame/diag.f90
+++ SCIENCE/flame/diag.f90
@@ -1,5 +1,4 @@
-! This is a general interface for doing runtime diagnostics on the state.
-! It is called at the end of advance
+! diagnostic routine for the flame propagation problem
 
 module diag_module
 
@@ -26,6 +25,9 @@ contains
     use bl_prof_module
     use geometry, only: dm, nlevs, spherical
     use bl_constants_module
+    use probin_module, only: prob_lo_x, prob_lo_y, prob_lo_z, &
+                             prob_hi_x, prob_hi_y, prob_hi_z, &
+                             job_name
 
     real(kind=dp_t), intent(in   ) :: dt,dx(:,:),time
     type(multifab) , intent(in   ) :: s(:)
@@ -44,16 +46,21 @@ contains
     type(ml_layout), intent(in   ) :: mla
     type(bc_tower) , intent(in   ) :: the_bc_tower
 
-
     ! Local
     real(kind=dp_t), pointer::  sp(:,:,:,:)
     real(kind=dp_t), pointer::  rhnp(:,:,:,:)
     real(kind=dp_t), pointer::  rhep(:,:,:,:)
     real(kind=dp_t), pointer::  up(:,:,:,:)
     real(kind=dp_t), pointer::  np(:,:,:,:)
-
-    integer :: lo(dm),hi(dm),ng_s,ng_u,ng_n,ng_rhn,ng_rhe
+    logical        , pointer::  mp(:,:,:,:)
+    
+    integer :: lo(dm),hi(dm)
+    integer :: ng_s,ng_u,ng_n,ng_rhn,ng_rhe
     integer :: i,n
+    integer :: un
+    logical :: lexist
+
+    logical, save :: firstCall = .true.
 
     type(bl_prof_timer), save :: bpt
 
@@ -65,7 +72,21 @@ contains
     ng_rhn = rho_Hnuc(1)%ng
     ng_rhe = rho_Hext(1)%ng
 
+    !=========================================================================
+    ! initialize
+    !=========================================================================
+
+
+    !=========================================================================
+    ! loop over the levels and compute the global quantities
+    !=========================================================================
     do n = 1, nlevs
+
+       ! initialize the local (processor's version) and level quantities to 0
+
+       !----------------------------------------------------------------------
+       ! loop over boxes in a given level
+       !----------------------------------------------------------------------
        do i = 1, s(n)%nboxes
           if ( multifab_remote(s(n), i) ) cycle
           sp => dataptr(s(n) , i)
@@ -76,43 +97,53 @@ contains
           hi =  upb(get_box(s(n), i))
           select case (dm)
           case (2)
-             call diag_2d(time,dt,dx(n,:), &
-                          sp(:,:,1,:),ng_s, &
-                          rhnp(:,:,1,1),ng_rhn, &
-                          rhep(:,:,1,1),ng_rhe, &
-                          rho0(n,:),rhoh0(n,:), &
-                          p0(n,:),tempbar(n,:),gamma1bar(n,:), &
-                          up(:,:,1,:),ng_u, &
-                          w0(n,:), &
-                          lo,hi)
-          case (3)
-             np => dataptr(normal(n) , i)
-             if (spherical .eq. 0) then
-                call diag_3d(time,dt,dx(n,:), &
-                             sp(:,:,:,:),ng_s, &
-                             rhnp(:,:,:,1),ng_rhn, &
-                             rhep(:,:,:,1),ng_rhe, &
+             if (n .eq. nlevs) then
+                call diag_2d(time,dt,dx(n,:), &
+                             sp(:,:,1,:),ng_s, &
+                             rhnp(:,:,1,1),ng_rhn, &
+                             rhep(:,:,1,1),ng_rhe, &
                              rho0(n,:),rhoh0(n,:), &
                              p0(n,:),tempbar(n,:),gamma1bar(n,:), &
-                             up(:,:,:,:),ng_u, &
+                             up(:,:,1,:),ng_u, &
                              w0(n,:), &
-                             np(:,:,:,:),ng_n, &
                              lo,hi)
              else
-                call diag_3d_sphr(time,dt,dx(n,:), &
-                                 sp(:,:,:,:),ng_s, &
-                                 rhnp(:,:,:,1),ng_rhn, &
-                                 rhep(:,:,:,1),ng_rhe, &
-                                 rho0(1,:),rhoh0(1,:), &
-                                 p0(1,:),tempbar(1,:),gamma1bar(1,:), &
-                                 up(:,:,:,:),ng_u, &
-                                 w0(1,:), &
-                                 np(:,:,:,:),ng_n, &
-                                 lo,hi)
-             end if
+                mp => dataptr(mla%mask(n), i)
+                call diag_2d(time,dt,dx(n,:), &
+                             sp(:,:,1,:),ng_s, &
+                             rhnp(:,:,1,1),ng_rhn, &
+                             rhep(:,:,1,1),ng_rhe, &
+                             rho0(n,:),rhoh0(n,:), &
+                             p0(n,:),tempbar(n,:),gamma1bar(n,:), &
+                             up(:,:,1,:),ng_u, &
+                             w0(n,:), &
+                             lo,hi, &
+                             mp(:,:,1,1))
+             endif
+          case (3)
+             call bl_error("ERROR: 3D diagnostics not implemented")
           end select
        end do
+
+       !----------------------------------------------------------------------
+       ! do the appropriate parallel reduction for the current level
+       !----------------------------------------------------------------------
+
+       ! NOTE: only the I/O Processor will have the correct reduced value
+
     end do
+
+    !=========================================================================
+    ! normalize
+    !=========================================================================
+
+
+    !=========================================================================
+    ! output
+    !=========================================================================
+
+
+
 
     call destroy(bpt)
 
@@ -125,10 +156,12 @@ contains
                      rho0,rhoh0,p0,tempbar,gamma1bar, &
                      u,ng_u, &
                      w0, &
-                     lo,hi)
+                     lo,hi, &
+                     mask)
 
     use variables, only: rho_comp, spec_comp, temp_comp, rhoh_comp
     use network, only: nspec
+    use probin_module, only: prob_lo
 
     integer, intent(in) :: lo(:), hi(:), ng_s, ng_u, ng_rhn, ng_rhe
     real (kind=dp_t), intent(in   ) ::      s(lo(1)-ng_s:,lo(2)-ng_s:,:)
@@ -139,115 +172,45 @@ contains
     real (kind=dp_t), intent(in   ) ::      u(lo(1)-ng_u:,lo(2)-ng_u:,:)
     real (kind=dp_t), intent(in   ) :: w0(0:)
     real (kind=dp_t), intent(in   ) :: time, dt, dx(:)
+    logical,          intent(in   ), optional :: mask(lo(1):,lo(2):)
 
     !     Local variables
     integer            :: i, j
+    real (kind=dp_t)   :: weight
+    logical            :: cell_valid
+    real (kind=dp_t)   :: x, y
+
+    ! weight is the factor by which the volume of a cell at the current level
+    ! relates to the volume of a cell at the coarsest level of refinement.
+    weight = 1.d0 / 4.d0**(n-1)
 
     do j = lo(2), hi(2)
+       y = prob_lo(2) + (dble(j) + HALF) * dx(2)
+       
        do i = lo(1), hi(1)
+          x = prob_lo(1) + (dble(i) + HALF) * dx(1)
 
-          ! do diagnostics here
-          !
-          ! access state variables as:
-          !   s(i,j,rho_comp)
-          !
-          ! access velocity components as:
-          !   u(i,j,1), u(i,j,2), u(i,j,3)
-          !
+          cell_valid = .true.
+          if (present(mask)) then
+             if ( (.not. mask(i,j,k)) ) cell_valid = .false.
+          endif
+
+          if (cell_valid) then
+
+             ! do diagnostics here
+             !
+             ! access state variables as:
+             !   s(i,j,rho_comp)
+             !
+             ! access velocity components as:
+             !   u(i,j,1), u(i,j,2), u(i,j,3)
+             !
+
+          endif
 
        enddo
     enddo
 
   end subroutine diag_2d
-
-  subroutine diag_3d(time,dt,dx, &
-                     s,ng_s, &
-                     rho_Hnuc,ng_rhn, &
-                     rho_Hext,ng_rhe, &
-                     rho0,rhoh0,p0,tempbar,gamma1bar, &
-                     u,ng_u,w0,normal,ng_n,lo,hi)
-
-    use variables, only: rho_comp, spec_comp, temp_comp, rhoh_comp
-    use network, only: nspec
-
-    integer, intent(in) :: lo(:), hi(:), ng_s, ng_u, ng_n, ng_rhn, ng_rhe
-    real (kind=dp_t), intent(in   ) ::      s(lo(1)-ng_s:,lo(2)-ng_s:,lo(3)-ng_s:,:)
-    real (kind=dp_t), intent(in   ) :: rho_Hnuc(lo(1)-ng_rhn:,lo(2)-ng_rhn:,lo(3)-ng_rhn:)
-    real (kind=dp_t), intent(in   ) :: rho_Hext(lo(1)-ng_rhe:,lo(2)-ng_rhe:,lo(3)-ng_rhe:)
-    real (kind=dp_t), intent(in   ) :: rho0(0:), rhoh0(0:), &
-                                         p0(0:),tempbar(0:),gamma1bar(0:)
-    real (kind=dp_t), intent(in   ) ::      u(lo(1)-ng_u:,lo(2)-ng_u:,lo(3)-ng_u:,:)
-    real (kind=dp_t), intent(in   ) :: w0(0:)
-    real (kind=dp_t), intent(in   ) :: normal(lo(1)-ng_n:,lo(2)-ng_n:,lo(3)-ng_n:,:)
-    real (kind=dp_t), intent(in   ) :: time, dt, dx(:)
-
-    !     Local variables
-    integer            :: i, j, k
-
-    do k = lo(3), hi(3)
-       do j = lo(2), hi(2)
-          do i = lo(1), hi(1)
-
-             ! do diagnostics here
-             !
-             ! access state variables as:
-             !   s(i,j,k,rho_comp)
-             !
-             ! access velocity components as:
-             !   u(i,j,k,1), u(i,j,k,2), u(i,j,k,3)
-             !
-             ! access normal vector as:
-             !   normal(i,j,k,1), normal(i,j,k,2), normal(i,j,k,3)
-
-          enddo
-       enddo
-    enddo
-
-  end subroutine diag_3d
-
-  subroutine diag_3d_sphr(time,dt,dx, &
-                          s,ng_s, &
-                          rho_Hnuc,ng_rhn, &
-                          rho_Hext,ng_rhe, &
-                          rho0,rhoh0,p0,tempbar,gamma1bar, &
-                          u,ng_u,w0,normal,ng_n,lo,hi)
-
-    use variables, only: rho_comp, spec_comp, temp_comp, rhoh_comp
-    use network, only: nspec
-
-    integer, intent(in) :: lo(:), hi(:), ng_s, ng_u, ng_n, ng_rhn, ng_rhe
-    real (kind=dp_t), intent(in   ) ::      s(lo(1)-ng_s:,lo(2)-ng_s:,lo(3)-ng_s:,:)
-    real (kind=dp_t), intent(in   ) :: rho_Hnuc(lo(1)-ng_rhn:,lo(2)-ng_rhn:,lo(3)-ng_rhn:)
-    real (kind=dp_t), intent(in   ) :: rho_Hext(lo(1)-ng_rhe:,lo(2)-ng_rhe:,lo(3)-ng_rhe:)
-    real (kind=dp_t), intent(in   ) :: rho0(0:), rhoh0(0:), &
-                                         p0(0:),tempbar(0:),gamma1bar(0:)
-    real (kind=dp_t), intent(in   ) ::      u(lo(1)-ng_u:,lo(2)-ng_u:,lo(3)-ng_u:,:)
-    real (kind=dp_t), intent(in   ) :: w0(0:)
-    real (kind=dp_t), intent(in   ) :: normal(lo(1)-ng_n:,lo(2)-ng_n:,lo(3)-ng_n:,:)
-    real (kind=dp_t), intent(in   ) :: time, dt, dx(:)
-
-    !     Local variables
-    integer            :: i, j, k
-
-    do k = lo(3), hi(3)
-       do j = lo(2), hi(2)
-          do i = lo(1), hi(1)
-
-             ! do diagnostics here
-             !
-             ! access state variables as:
-             !   s(i,j,k,rho_comp)
-             !
-             ! access velocity components as:
-             !   u(i,j,k,1), u(i,j,k,2), u(i,j,k,3)
-             !
-             ! access normal vector as:
-             !   normal(i,j,k,1), normal(i,j,k,2), normal(i,j,k,3)
-
-          enddo
-       enddo
-    enddo
-
-  end subroutine diag_3d_sphr
 
 end module diag_module
