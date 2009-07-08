133a2e37dc6a5c2be838765d943add94a4113c73
ajnonaka <ajnonaka>
2009-07-08 18:43:59 +0000

    properly wrote the inlet_bc module

--- TEST_PROBLEMS/test_smallscale/base_state.f90
+++ TEST_PROBLEMS/test_smallscale/base_state.f90
@@ -23,95 +23,37 @@ contains
     use inlet_bc_module
 
     integer,             intent(in   ) :: n
-    character(len=256),  intent(in   ) :: model_file ! I'm not using this anymore
+    character(len=256),  intent(in   ) :: model_file ! Not used
     real(kind=dp_t),     intent(inout) :: s0_init(0:,:)
     real(kind=dp_t),     intent(inout) :: p0_init(0:)
     real(kind=dp_t),     intent(in   ) :: dx(:)
 
     ! local
-    integer :: ndum,r,comp,i
-    real(dp_t) :: starting_rad
-
+    integer :: ndum,r,comp
     parameter (ndum = 30)
 
-    character(len=128) :: lamsolfile
-    real(kind=dp_t) :: state1d(ndum),Pamb,temporary
+    real(kind=dp_t) :: state1d(ndum),Pamb,starting_rad
     real(kind=dp_t) :: loloc,hiloc,flameloc
 
+    ! set bottom of domain
     starting_rad = prob_lo(dm)
 
-    lamsolfile = 'flame_4.e7_screen_left.out'
+    ! set flame location
+    flameloc = ONE
 
     ! first set the inflow boundary condition
-    call asin1d(lamsolfile, -.00125d0, 0.d0, state1d, ndum, .false.)
+    call set_inlet_bcs()
 
+    ! load in the background pressure
+    call asin1d('flame_4.e7_screen_left.out', -.00125d0, 0.d0, state1d, ndum, .false.)
     Pamb = state1d(18)
-    p_eos(1) = Pamb
 
-    den_eos(1) = state1d(3)
-    temp_eos(1) = state1d(9)
-    do comp=1,nspec
-       if(spec_names(comp) .eq. "carbon-12") then
-          xn_eos(1,comp) = state1d(21)
-       else if(spec_names(comp) .eq. "magnesium-24") then
-          xn_eos(1,comp) = state1d(22)
-       else if(spec_names(comp) .eq. "oxygen-16") then
-          xn_eos(1,comp) = state1d(23)
-       else
-          print*,"In initdata, spec_names(",comp,") invalid"
-       endif
-    enddo
-
-    ! given P, T, and X, compute rho
-    call eos(eos_input_tp, den_eos, temp_eos, &
-             npts, nspec, &
-             xn_eos, &
-             p_eos, h_eos, e_eos, & 
-             cv_eos, cp_eos, xne_eos, eta_eos, pele_eos, &
-             dpdt_eos, dpdr_eos, dedt_eos, dedr_eos, &
-             dpdX_eos, dhdX_eos, &
-             gam1_eos, cs_eos, s_eos, &
-             dsdt_eos, dsdr_eos, &
-             do_diag)
-
-    ! given rho, T, and X, compute h
-    call eos(eos_input_rt, den_eos, temp_eos, &
-             npts, nspec, &
-             xn_eos, &
-             p_eos, h_eos, e_eos, & 
-             cv_eos, cp_eos, xne_eos, eta_eos, pele_eos, &
-             dpdt_eos, dpdr_eos, dedt_eos, dedr_eos, &
-             dpdX_eos, dhdX_eos, &
-             gam1_eos, cs_eos, s_eos, &
-             dsdt_eos, dsdr_eos, &
-             do_diag)
-
-    INLET_VN = 0.0d0
-    INLET_VT = 0.0d0
-    INLET_RHO = den_eos(1)
-    INLET_RHOH = den_eos(1)*h_eos(1)
-
-    do comp=1,nspec
-       if(spec_names(comp) .eq. "carbon-12") then
-          INLET_RHOC12 = den_eos(1)*xn_eos(1,comp)
-       else if(spec_names(comp) .eq. "magnesium-24") then
-          INLET_RHOMG24 = den_eos(1)*xn_eos(1,comp)
-       else if(spec_names(comp) .eq. "oxygen-16") then
-          INLET_RHOO16 = den_eos(1)*xn_eos(1,comp)
-       endif
-    enddo
-    INLET_TEMP = temp_eos(1)
-    INLET_TRA = 0.0d0
-
-    ! Now do the interior cells
-    flameloc = ONE
-    
     do r=0,nr(n)-1
 
        loloc = starting_rad +  dble(r)     *dx(dm) - flameloc
        hiloc = starting_rad + (dble(r)+ONE)*dx(dm) - flameloc
 
-       call asin1d(lamsolfile, loloc, hiloc, state1d, ndum, .false.)
+       call asin1d('flame_4.e7_screen_left.out', loloc, hiloc, state1d, ndum, .false.)
 
        p_eos(1) = Pamb
        den_eos(1) = state1d(3)
--- TEST_PROBLEMS/test_smallscale/inlet_bc.f90
+++ TEST_PROBLEMS/test_smallscale/inlet_bc.f90
@@ -1,28 +1,103 @@
+! inlet_bc_module serves as a container to hold the inflow boundary 
+! condition information.
+!
+! These quantities are initialized through a call to set_inlet_bcs(),
+! which should be done on initialization and restart.
+
 module inlet_bc_module
 
   use bl_types
+  use bl_constants_module
   use bl_space
+  use network
 
   implicit none
 
-  real(dp_t), save    :: INLET_VN
-  real(dp_t), save    :: INLET_VT
-  real(dp_t), save    :: INLET_RHO
-  real(dp_t), save    :: INLET_RHOH
-  real(dp_t), save    :: INLET_RHOC12
-  real(dp_t), save    :: INLET_RHOO16
-  real(dp_t), save    :: INLET_RHOMG24
-  real(dp_t), save    :: INLET_TEMP
-  real(dp_t), save    :: INLET_TRA
+  real(dp_t), save :: INLET_VN
+  real(dp_t), save :: INLET_VT
+  real(dp_t), save :: INLET_RHO
+  real(dp_t), save :: INLET_RHOH
+  real(dp_t), save :: INLET_RHOC12
+  real(dp_t), save :: INLET_RHOO16
+  real(dp_t), save :: INLET_RHOMG24
+  real(dp_t), save :: INLET_TEMP
+  real(dp_t), save :: INLET_TRA
 
-  namelist /inlet_bc/ INLET_VN
-  namelist /inlet_bc/ INLET_VT
-  namelist /inlet_bc/ INLET_RHO
-  namelist /inlet_bc/ INLET_RHOH
-  namelist /inlet_bc/ INLET_RHOC12
-  namelist /inlet_bc/ INLET_RHOO16
-  namelist /inlet_bc/ INLET_RHOMG24
-  namelist /inlet_bc/ INLET_TEMP
-  namelist /inlet_bc/ INLET_TRA
+  logical, save :: inlet_bc_initialized = .false.
+
+contains
+
+  subroutine set_inlet_bcs()
+
+    use eos_module
+
+    ! local variables
+    integer :: ndum,comp
+    parameter (ndum=30)
+
+    real(kind=dp_t) :: state1d(ndum)
+
+    ! now reset inflow boundary conditions
+    call asin1d('flame_4.e7_screen_left.out', -.00125d0, 0.d0, state1d, ndum, .false.)
+
+    p_eos(1) = state1d(18)
+    den_eos(1) = state1d(3)
+    temp_eos(1) = state1d(9)
+    do comp=1,nspec
+       if(spec_names(comp) .eq. "carbon-12") then
+          xn_eos(1,comp) = state1d(21)
+       else if(spec_names(comp) .eq. "magnesium-24") then
+          xn_eos(1,comp) = state1d(22)
+       else if(spec_names(comp) .eq. "oxygen-16") then
+          xn_eos(1,comp) = state1d(23)
+       else
+          print*,"In initdata, spec_names(",comp,") invalid"
+       endif
+    enddo
+
+    ! given P, T, and X, compute rho
+    call eos(eos_input_tp, den_eos, temp_eos, &
+             npts, nspec, &
+             xn_eos, &
+             p_eos, h_eos, e_eos, & 
+             cv_eos, cp_eos, xne_eos, eta_eos, pele_eos, &
+             dpdt_eos, dpdr_eos, dedt_eos, dedr_eos, &
+             dpdX_eos, dhdX_eos, &
+             gam1_eos, cs_eos, s_eos, &
+             dsdt_eos, dsdr_eos, &
+             do_diag)
+
+    ! given rho, T, and X, compute h
+    call eos(eos_input_rt, den_eos, temp_eos, &
+             npts, nspec, &
+             xn_eos, &
+             p_eos, h_eos, e_eos, & 
+             cv_eos, cp_eos, xne_eos, eta_eos, pele_eos, &
+             dpdt_eos, dpdr_eos, dedt_eos, dedr_eos, &
+             dpdX_eos, dhdX_eos, &
+             gam1_eos, cs_eos, s_eos, &
+             dsdt_eos, dsdr_eos, &
+             do_diag)
+
+    INLET_VN = 0.0d0
+    INLET_VT = 0.0d0
+    INLET_RHO = den_eos(1)
+    INLET_RHOH = den_eos(1)*h_eos(1)
+
+    do comp=1,nspec
+       if(spec_names(comp) .eq. "carbon-12") then
+          INLET_RHOC12 = den_eos(1)*xn_eos(1,comp)
+       else if(spec_names(comp) .eq. "magnesium-24") then
+          INLET_RHOMG24 = den_eos(1)*xn_eos(1,comp)
+       else if(spec_names(comp) .eq. "oxygen-16") then
+          INLET_RHOO16 = den_eos(1)*xn_eos(1,comp)
+       endif
+    enddo
+    INLET_TEMP = temp_eos(1)
+    INLET_TRA = 0.0d0
+
+    inlet_bc_initialized = .true.
+
+  end subroutine set_inlet_bcs
 
 end module inlet_bc_module
