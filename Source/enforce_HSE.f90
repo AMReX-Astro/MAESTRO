0d3eedc31de01743c1a06da45f43b790826e486b
ajnonaka <ajnonaka>
2009-07-09 00:02:08 +0000

    if evolve_base_state=T, we now "erase" the history of any pressure that used to exist on the finer levels.  This makes looking at the 1D arrays in the checkpoint less confusing, as you only see p0 where it actually should exit.
    also added more comments

--- Source/enforce_HSE.f90
+++ Source/enforce_HSE.f90
@@ -25,16 +25,24 @@ contains
     integer         :: n,l,i,r
     real(kind=dp_t) :: temppres,temp,offset
     real(kind=dp_t) :: grav_edge(nlevs_radial,0:nr_fine-1)
+    real(kind=dp_t) ::    p0_old(nlevs_radial,0:nr_fine-1)
 
     call make_grav_edge(grav_edge,rho0)
-    
-    ! store the pressure at the top cell
-    temppres = p0(1,nr(1)-1)
 
+    ! create a copy of the input pressure to help us with initial conditions
+    p0_old = p0
+    
+    ! zero the old pressure so we don't leave a non-zero pressure in fine radial
+    ! regions that no longer have a corresponding full state
+    p0 = 0.d0
+    
     ! integrate all of level 1 first
-    ! we start at r=1, using the pressure at r=0 as a reference point,
-    ! i.e., we are numerically intergrating from the "bottom"
-    ! later, we will offset the entire pressure so we integrate from the "top"
+    ! use the old pressure at r=0 as a reference point
+    p0(1,0) = p0_old(1,0)
+
+    ! now integrate upwards from the bottom
+    ! later, we will offset the entire pressure so we have effectively 
+    ! integrated from the "top"
     do r=1,min(r_end_coord(1,1),base_cutoff_density_coord(1))
        p0(1,r) = p0(1,r-1) + (dr(1)/2.d0)*(rho0(1,r)+rho0(1,r-1))*grav_edge(1,r)
     end do
@@ -43,19 +51,15 @@ contains
     end do
 
     do n=2,nlevs_radial
-
-       if (spherical .eq. 1) then
-          call bl_error('enforce_HSE: should not have multiple base levels for spherical')
-       end if
-
        do i=1,numdisjointchunks(n)
 
-          ! use a special stencil for the first point
+          ! get pressure in the bottom cell of this disjointchunk
           if (r_start_coord(n,i) .eq. 0) then
-             ! do nothing - p0(n,r_start_coord(n,i) already contains the correct data
-
+             ! if we are at the bottom of the domain, use the old pressure as reference
+             p0(n,0) = p0_old(n,0)
           else if (r_start_coord(n,i) .le. base_cutoff_density_coord(n)) then
-             ! use coarse -> fine stencil in notes
+             ! we integrate upwards starting from the nearest coarse
+             ! cell at a lower physical height
              p0(n,r_start_coord(n,i)) = p0(n-1,r_start_coord(n,i)/2-1) &
                   + (3.d0*grav_cell(1,0)*dr(n)/4.d0)* &
                   (rho0(n-1,r_start_coord(n,i)/2-1)+rho0(n,r_start_coord(n,i)))
@@ -64,7 +68,7 @@ contains
              p0(n,r_start_coord(n,i)) = p0(n-1,r_start_coord(n,i)/2-1)
           end if
 
-          ! iterate normally over the rest          
+          ! integrate upwards as normal
           do r=r_start_coord(n,i)+1,min(r_end_coord(n,i),base_cutoff_density_coord(n))
              p0(n,r) = p0(n,r-1) + (dr(n)/2.d0)*(rho0(n,r)+rho0(n,r-1))*grav_cell(1,0)
           end do
@@ -72,10 +76,16 @@ contains
              p0(n,r) = p0(n,r-1)
           end do
 
-          ! use a special stencil to get the value of the coarse cell above
+          ! now we need to look at the first coarser cell above this disjoint chunk
+          ! and look at the mismatch between the integration performed over the
+          ! finer cells vs. the coarse cells.  Then we need to offset the coarse
+          ! cell above this point to sync up.
+          
+          ! first, compute the value of the pressure in the coarse cell above the
+          ! disjointchunk.
           if (r_end_coord(n,i) .eq. nr(n)-1) then
              ! do nothing - we are at the top of the domain
-
+             offset = 0.d0
           else if (r_end_coord(n,i) .le. base_cutoff_density_coord(n)) then
              ! use fine -> coarse stencil in notes
              temp = p0(n,r_end_coord(n,i)) + (3.d0*grav_cell(1,0)*dr(n)/4.d0)* &
@@ -103,7 +113,7 @@ contains
 
     ! now compare pressure in the last cell and offset to make sure we are !
     ! integrating "from the top"
-    offset = p0(1,nr(1)-1) - temppres
+    offset = p0(1,nr(1)-1) - p0_old(1,nr(1)-1)
 
     ! offset level 1
     p0(1,:) = p0(1,:) - offset
