5efaf818fd844b3bf5ad1945857b63e486bf017d
mzingale <mzingale>
2009-08-23 20:20:37 +0000

    store the output directory in the job_info file

--- Source/write_job_info.f90
+++ Source/write_job_info.f90
@@ -6,7 +6,8 @@ subroutine write_job_info(dirname)
   ! directory
 
   use parallel
-  use probin_module
+  use probin_module, only: job_name, probin
+  use bl_system_module, only: BL_CWD_SIZE, get_cwd 
 
   implicit none
   character (len=*)   :: dirname
@@ -18,10 +19,12 @@ subroutine write_job_info(dirname)
   character (len=256) :: out_name
   character (len=16) :: date, time
   integer, dimension(8) :: values
+  character (len=BL_CWD_SIZE) :: cwd
 
   call build_info(build_date, build_dir, build_machine)
   call date_and_time(date, time, VALUES=values)
-
+  call get_cwd(cwd)
+ 
   out_name = trim(dirname) // "/job_info"
 
 1000 format(79('-'))
@@ -39,6 +42,8 @@ subroutine write_job_info(dirname)
      write (99,1002) "number of processors: ", parallel_nprocs()
      write (99,1003) "output date:          ", values(1), values(2), values(3)
      write (99,1004) "output time:          ", values(5), values(6), values(7)
+     write (99,1001) "output dir:           ", trim(cwd)
+
      write (99,*) " "
      write (99,*) " "
 
