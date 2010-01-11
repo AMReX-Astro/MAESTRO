612a1cb2ca0d5b489583033c682c08b7b3a8e065
ajnonaka <ajnonaka>
2010-01-11 23:05:38 +0000

    cleaned up w0, psi, eta_cc and eta_ec init in restart_into_finer

--- Source/initialize.f90
+++ Source/initialize.f90
@@ -374,15 +374,15 @@ contains
           if (r .eq. 0) then
              psi      (1,0) = psi_temp      (1,0)
              etarho_cc(1,0) = etarho_cc_temp(1,0)
-          else if (r/2+1 .ge. nr_fine_old) then
-             psi(1,r) = psi_temp(1,nr_fine_old-1)
+          else if ((r+1)/2 .ge. nr_fine_old) then
+             psi      (1,r) = psi_temp      (1,nr_fine_old-1)
              etarho_cc(1,r) = etarho_cc_temp(1,nr_fine_old-1)
           else
              if (mod(r,2) .eq. 0) then
-                psi(1,r) = 0.75d0*psi_temp(1,r/2)+0.25d0*psi_temp(1,r/2-1)
-                etarho_cc(1,r) = 0.75d0*etarho_cc_temp(1,r/2)+0.25d0*etarho_cc_temp(1,r/2+1)
+                psi      (1,r) = 0.75d0*psi_temp      (1,r/2)+0.25d0*psi_temp      (1,r/2-1)
+                etarho_cc(1,r) = 0.75d0*etarho_cc_temp(1,r/2)+0.25d0*etarho_cc_temp(1,r/2-1)
              else
-                psi(1,r) = 0.75d0*psi_temp(1,r/2)+0.25d0*psi_temp(1,r/2+1)
+                psi      (1,r) = 0.75d0*psi_temp      (1,r/2)+0.25d0*psi_temp      (1,r/2+1)
                 etarho_cc(1,r) = 0.75d0*etarho_cc_temp(1,r/2)+0.25d0*etarho_cc_temp(1,r/2+1)
              end if
           end if
@@ -392,14 +392,14 @@ contains
        do r=0,nr_fine
           if (r .gt. 2*nr_fine_old) then
              etarho_ec(1,r) = etarho_ec_temp(1,nr_fine_old)
-             w0(1,r) = w0_temp(1,nr_fine_old)
+             w0       (1,r) = w0_temp       (1,nr_fine_old)
           else
              if (mod(r,2) .eq. 0) then
                 etarho_ec(1,r) = etarho_ec_temp(1,r/2)
-                w0(1,r) = w0_temp(1,r/2)
+                w0       (1,r) = w0_temp       (1,r/2)
              else
                 etarho_ec(1,r) = 0.5d0*etarho_ec_temp(1,r/2)+0.5d0*etarho_ec_temp(1,r/2+1)
-                w0(1,r) = 0.5d0*w0_temp(1,r/2)+0.5d0*w0_temp(1,r/2+1)
+                w0       (1,r) = 0.5d0*w0_temp       (1,r/2)+0.5d0*w0_temp       (1,r/2+1)
              end if
           end if
        end do
