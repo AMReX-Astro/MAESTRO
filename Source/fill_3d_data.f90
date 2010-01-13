f90dadbfccd5db04f3221befb0faa86dea8e9f4e
ajnonaka <ajnonaka>
2010-01-13 16:37:13 +0000

    fixed an indexing problem when restart_into_finer=T

--- Source/fill_3d_data.f90
+++ Source/fill_3d_data.f90
@@ -469,7 +469,7 @@ contains
                    ! we need to modify this if we're too close to the edge
                    if (index .eq. 0) then
                       index = 1
-                   else if (index .eq. nr_fine-1) then
+                   else if (index .ge. nr_fine-1) then
                       index = nr_fine-2
                    end if
 
