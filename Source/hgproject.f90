b32df03e5c6fd84da03856c9a09f47a1ee6cd32e
almgren <almgren>
2010-03-16 19:38:42 +0000

    No longer pass "verbose" to nodal_div.f90: subroutine divu

--- Source/hgproject.f90
+++ Source/hgproject.f90
@@ -1304,7 +1304,7 @@ contains
        call setval(rh(n),ZERO,all=.true.)
     end do
 
-    call divu(nlevs,mgt,unew,rh,mla%mba%rr,verbose,nodal)
+    call divu(nlevs,mgt,unew,rh,mla%mba%rr,nodal)
 
     ! Do rh = rh - divu_rhs (this routine preserves rh=0 on
     !  nodes which have bc_dirichlet = true.
@@ -1430,7 +1430,7 @@ contains
        call setval(rh(n),ZERO,all=.true.)
     end do
 
-    call divu(nlevs,mgt,unew,rh,mla%mba%rr,verbose,nodal)
+    call divu(nlevs,mgt,unew,rh,mla%mba%rr,nodal)
 
     ! Do rh = rh - divu_rhs (this routine preserves rh=0 on
     !  nodes which have bc_dirichlet = true.
