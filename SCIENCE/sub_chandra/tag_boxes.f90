0e1530582ebf31c3b15fa0ff522b29c828cad9c8
mzingale <mzingale>
2010-04-14 03:39:23 +0000

    first stab at a sub-Chandra ignition problem

---
+++ SCIENCE/sub_chandra/tag_boxes.f90
@@ -0,0 +1,93 @@
+module tag_boxes_module
+
+  use BoxLib
+  use omp_module
+  use f2kcli
+  use list_box_module
+  use boxarray_module
+  use ml_boxarray_module
+  use layout_module
+  use multifab_module
+  use box_util_module
+  use bl_IO_module
+  use cluster_module
+  use ml_layout_module
+
+  implicit none 
+
+contains
+
+  subroutine tag_boxes(mf,tagboxes,lev)
+
+    use variables, ONLY: rho_comp, spec_comp
+
+    use network, ONLY: network_species_index
+
+    type( multifab), intent(in   ) :: mf
+    type(lmultifab), intent(inout) :: tagboxes
+    integer        , intent(in   ) :: lev
+
+    real(kind = dp_t), pointer :: sp(:,:,:,:)
+    logical          , pointer :: tp(:,:,:,:)
+    integer           :: i, lo(mf%dim), ng
+
+    integer           :: ihe4
+
+    ihe4 = network_species_index("helium-4")
+
+    ng = mf%ng
+
+    do i = 1, mf%nboxes
+       if ( multifab_remote(mf, i) ) cycle
+       sp => dataptr(mf, i)
+       tp => dataptr(tagboxes, i)
+       lo =  lwb(get_box(tagboxes, i))
+
+       select case (mf%dim)
+       case (2)
+          call bl_error("ERROR: 2-d tag_boxes not implemented")
+       case  (3)
+          call tag_boxes_3d(tp(:,:,:,1),sp(:,:,:,rho_comp),sp(:,:,:,spec_comp-1+ihe4),lo,ng,lev)
+       end select
+    end do
+
+  end subroutine tag_boxes
+
+
+  subroutine tag_boxes_3d(tagbox,rho,rho_Xhe,lo,ng,lev)
+
+    use probin_module, ONLY: base_cutoff_density
+
+    integer          , intent(in   ) :: lo(:),ng
+    logical          , intent(  out) :: tagbox(lo(1):,lo(2):,lo(3):)
+    real(kind = dp_t), intent(in   ) :: rho(lo(1)-ng:,lo(2)-ng:,lo(3)-ng:)
+    real(kind = dp_t), intent(in   ) :: rho_Xhe(lo(1)-ng:,lo(2)-ng:,lo(3)-ng:)
+    integer, optional, intent(in   ) :: lev
+
+    real(kind = dp_t) :: Xhe
+
+    integer :: i,j,k,nx,ny,nz,llev
+
+    llev = 1; if (present(lev)) llev = lev
+    nx = size(tagbox,dim=1)
+    ny = size(tagbox,dim=2)
+    nz = size(tagbox,dim=3)
+
+    tagbox = .false.
+
+!$omp parallel do private(i,j,k)
+    do k = lo(3),lo(3)+nz-1
+       do j = lo(2),lo(2)+ny-1
+          do i = lo(1),lo(1)+nx-1
+             Xhe = rho_Xhe(i,j,k)/rho(i,j,k)
+             if (Xhe > 0.01_dp_t .and. rho(i,j,k) <= base_cutoff_density) then
+                tagbox(i,j,k) = .true.
+             end if
+          end do
+       enddo
+    end do
+!$omp end parallel do
+
+  end subroutine tag_boxes_3d
+
+end module tag_boxes_module
