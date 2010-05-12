5f957bf7de6e7f74c05b360d178a6a394e16ab3b
almgren <almgren>
2010-05-12 19:15:18 +0000

    This is the file that Chris fixed to test the eos_given_PS stuff...

--- Microphysics/EOS/gamma_law_general/test/testeos.f90
+++ Microphysics/EOS/gamma_law_general/test/testeos.f90
@@ -6,13 +6,17 @@ program testburn
 
   implicit none
 
-  real(kind=dp_t) :: dens, temp, hin, dt, hout
-  real(kind=dp_t), dimension(nspec) :: Xin, Xout, rho_omegadot
+  real(kind=dp_t) :: dens, temp, pres, entr
+  real(kind=dp_t), dimension(nspec) :: Xin
   
   integer :: ic12, io16, img24
 
+  logical :: do_diag
+
   call network_init()
   call eos_init(gamma_in=5.0d0/3.0d0)
+
+  do_diag = .false.
 
   ic12 = network_species_index("carbon-12")
   io16 = network_species_index("oxygen-16")
@@ -53,6 +57,12 @@ program testburn
   print *, 'dedT: ', dedt_eos(1), ' dedr: ', dedr_eos(1)
   print *, 'dpdX: ', dpdX_eos(1,:)
   print *, 'dhdX: ', dhdX_eos(1,:)
+
+  print *, ' '
+  print *, 'setting pres = ', p_eos(1)
+  pres = p_eos(1)
+  print *, 'setting entr = ', s_eos(1)
+  entr = s_eos(1)
 
   
   ! test eos_input_rh
@@ -157,5 +167,31 @@ program testburn
   print *, 'dhdX: ', dhdX_eos(1,:)
 
 
+  ! test eos_input_ps
+  p_eos(1) = pres
+  s_eos(1) = entr
+  call eos(eos_input_ps, den_eos, temp_eos, &
+           npts, &
+           xn_eos, &
+           p_eos, h_eos, e_eos, &
+           cv_eos, cp_eos, xne_eos, eta_eos, pele_eos, &
+           dpdt_eos, dpdr_eos, dedt_eos, dedr_eos, &
+           dpdX_eos, dhdX_eos, &
+           gam1_eos, cs_eos, s_eos, &
+           dsdt_eos, dsdr_eos, &
+           do_diag)
+
+  print *, ' '
+  print *, 'input: eos_input_ps'
+  print *, 'pres: ', pres, 'entr: ', entr
+  print *, 'dens: ', den_eos(1), ' temp: ', temp_eos(1)
+  print *, 'X: ', Xin
+  print *, 'pres_eos: ', p_eos(1),  'entr_eos: ', s_eos(1)
+  print *, 'h:    ', h_eos(1), ' ener: ', e_eos(1)
+  print *, 'c_v:  ', cv_eos(1), ' c_p : ', cp_eos(1)
+  print *, 'dpdT: ', dpdt_eos(1), ' dpdr: ', dpdr_eos(1)
+  print *, 'dedT: ', dedt_eos(1), ' dedr: ', dedr_eos(1)
+  print *, 'dpdX: ', dpdX_eos(1,:)
+  print *, 'dhdX: ', dhdX_eos(1,:)
 
 end program testburn
