a0337b98d697649c8db55124d4c007184e9dab01
gilet <gilet>
2010-06-02 23:24:57 +0000

    changes to add a 2nd prob_type to use analytic CNO heating

--- TEST_PROBLEMS/spherical_heat/make_heating.f90
+++ TEST_PROBLEMS/spherical_heat/make_heating.f90
@@ -86,8 +86,11 @@ contains
     
     use bl_constants_module
     use geometry, only: center
-    use variables, only: rho_comp
-    use probin_module, only: heating_time, heating_rad, heating_peak, heating_sigma
+    use variables, only: rho_comp, temp_comp, spec_comp
+    use probin_module, only: heating_time, heating_rad, heating_peak, &
+         heating_sigma, prob_type
+    use network
+    use bl_error_module
 
     integer, intent(in) :: lo(:), hi(:), ng_s, ng_h
     real(kind=dp_t), intent(inout) :: rho_Hext(lo(1)-ng_h:,lo(2)-ng_h:,lo(3)-ng_h:)
@@ -97,35 +100,77 @@ contains
     integer :: i, j, k
     real(kind=dp_t) :: x, y, z, r, fac
 
+    integer                        :: h1_comp
+    integer                        :: c12_comp
+    integer                        :: n14_comp
+    integer                        :: o16_comp
+    real(kind=dp_t)                :: rho, T_6_third, X_CNO, X_1, g14
+    real(kind=dp_t)                :: tmp1, tmp2, tmp3
+
+
     rho_Hext = 0.d0
 
-    if (time .lt. heating_time) then
+    if (prob_type .eq. 1) then
+       
+       if (time .lt. heating_time) then
 
-       if ( (time+dt) .gt. heating_time ) then
-          fac = (heating_time - time) / dt
-       else
-          fac = 1.d0
+          if ( (time+dt) .gt. heating_time ) then
+             fac = (heating_time - time) / dt
+          else
+             fac = 1.d0
+          end if
+
+          do k = lo(3), hi(3)
+             z = (dble(k) + HALF)*dx(3) - center(3)
+
+             do j = lo(2), hi(2)
+                y = (dble(j) + HALF)*dx(2) - center(2)
+
+                do i = lo(1), hi(1)
+                   x = (dble(i) + HALF)*dx(1) - center(1)
+
+                   r = sqrt(x**2 + y**2 + z**2)
+
+                   rho_Hext(i,j,k) = fac * s(i,j,k,rho_comp) * heating_peak &
+                        * exp(-(r-heating_rad)**2/heating_sigma)
+
+                enddo
+             enddo
+          enddo
+
        end if
 
+    elseif (prob_type .eq. 2) then
+
+       h1_comp = spec_comp - 1 + network_species_index("hydrogen-1")
+       c12_comp = spec_comp - 1 + network_species_index("carbon-12")
+       n14_comp = spec_comp - 1 + network_species_index("nitrogen-14")
+       o16_comp = spec_comp - 1 + network_species_index("oxygen-16")
+
        do k = lo(3), hi(3)
-          z = (dble(k) + HALF)*dx(3) - center(3)
-
           do j = lo(2), hi(2)
-             y = (dble(j) + HALF)*dx(2) - center(2)
-
              do i = lo(1), hi(1)
-                x = (dble(i) + HALF)*dx(1) - center(1)
-
-                r = sqrt(x**2 + y**2 + z**2)
-
-                rho_Hext(i,j,k) = fac * s(i,j,k,rho_comp) * heating_peak &
-                     * exp(-(r-heating_rad)**2/heating_sigma)
-
+                rho = s(i,j,k,rho_comp)
+                T_6_third = (s(i,j,k,temp_comp) / 1.0d6) ** THIRD
+                tmp1 = s(i,j,k,c12_comp)
+                tmp2 = s(i,j,k,n14_comp)
+                tmp3 = s(i,j,k,o16_comp)
+                X_CNO = (tmp1 + tmp2 + tmp3) / rho
+                X_1 = s(i,j,k,h1_comp) / rho
+                tmp1 =   2.7d-3 * T_6_third
+                tmp2 = -7.78d-3 * T_6_third**2
+                tmp3 = -1.49d-4 * T_6_third**3
+                g14 = 1.0_dp_t + tmp1 + tmp2 + tmp3
+                tmp1 = 8.67d27 * g14 * X_CNO * X_1 * rho / T_6_third**2
+                tmp2 = dexp(-1.5228d2 / T_6_third)
+                rho_Hext(i,j,k) = rho * tmp1 * tmp2
              enddo
           enddo
        enddo
 
-    end if
+    else
+       call bl_error("prob_type not yet supported.")       
+    endif
 
   end subroutine get_rho_Hext_3d
   
