5ef0b70116b56a006ab0e78c6d390a85260d69c3
almgren <almgren>
2010-07-19 23:36:07 +0000

    More cleanup.

--- Source/thermal_conduct.f90
+++ Source/thermal_conduct.f90
@@ -29,11 +29,10 @@ contains
     use multifab_physbc_module
     use multifab_fill_ghost_module
     use bndry_reg_module
-    use mac_multigrid_module
-    use mac_applyop_module, only : mac_applyop
-    use rhoh_vs_t_module
-    use make_explicit_thermal_module
-    use fill_3d_module       , only : put_1d_array_on_cart
+
+    use mac_multigrid_module , only : mac_multigrid
+    use mac_applyop_module   , only : mac_applyop
+    use fill_3d_module       , only : put_1d_array_on_cart, put_data_on_faces
     use ml_restriction_module, only : ml_cc_restriction_c
 
     use variables    , only : foextrap_comp, rho_comp, spec_comp, rhoh_comp, temp_comp
