55c0986fd172e1f4f1c4de4b3663c32e3d016fd9
almgren <almgren>
2010-07-22 00:23:26 +0000

    Split out hg_multigrid and put in option for calling hg_hypre.

--- Source/hgproject.f90
+++ Source/hgproject.f90
@@ -17,14 +17,17 @@ contains
                        div_coeff_3d,divu_rhs,eps_in)
 
     use bc_module
+    use bl_constants_module
     use bl_prof_module
     use proj_parameters
-    use nodal_divu_module
-    use ml_solve_module
-    use ml_restriction_module
-    use multifab_fill_ghost_module
-    use probin_module, only: verbose, mg_verbose, cg_verbose, hg_dense_stencil, nodal
-    use geometry, only: dm, nlevs, spherical
+    use multifab_fill_ghost_module , only : multifab_fill_ghost_cells
+    use ml_restriction_module      , only : ml_cc_restriction
+    use hg_multigrid_module        , only : hg_multigrid
+    use hg_hypre_module            , only : hg_hypre
+
+    use probin_module , only: verbose, mg_verbose, cg_verbose, hg_dense_stencil, nodal, &
+                              use_hypre
+    use geometry      , only: dm, nlevs, spherical
 
     integer        , intent(in   ) :: proj_type
     type(ml_layout), intent(inout) :: mla
@@ -120,11 +123,21 @@ contains
 !      call hg_1d_solver(mla,unew,rhohalf,phi,dx,the_bc_tower,divu_rhs)
 !   else if (present(eps_in)) then
     if (present(eps_in)) then
-       call hg_multigrid(mla,unew,rhohalf,phi,dx,the_bc_tower, &
-                         stencil_type,divu_rhs,eps_in)
+       if (use_hypre .eq. 1) then 
+          call hg_hypre(mla,unew,rhohalf,phi,dx,the_bc_tower, &
+                        stencil_type,divu_rhs,eps_in)
+       else
+          call hg_multigrid(mla,unew,rhohalf,phi,dx,the_bc_tower, &
+                            stencil_type,divu_rhs,eps_in)
+       endif
     else 
-       call hg_multigrid(mla,unew,rhohalf,phi,dx,the_bc_tower, &
-                         stencil_type,divu_rhs)
+       if (use_hypre .eq. 1) then 
+          call hg_hypre(mla,unew,rhohalf,phi,dx,the_bc_tower, &
+                        stencil_type,divu_rhs)
+       else
+          call hg_multigrid(mla,unew,rhohalf,phi,dx,the_bc_tower, &
+                            stencil_type,divu_rhs)
+       end if
     end if
 
     do n=1,nlevs
@@ -991,249 +1004,6 @@ contains
   end subroutine hgproject
 
   ! ******************************************************************************** !
-
-  subroutine hg_multigrid(mla,unew,rhohalf,phi,dx,the_bc_tower, &
-                          stencil_type,divu_rhs,eps_in)
-
-    use bl_prof_module
-    use bl_constants_module
-    use stencil_fill_module
-    use ml_solve_module
-    use nodal_divu_module
-    use probin_module, only : hg_bottom_solver, max_mg_bottom_nlevels, &
-                              verbose, mg_verbose, cg_verbose, nodal
-    use geometry, only: dm, nlevs
-    use variables, only: press_comp
-
-    type(ml_layout), intent(inout) :: mla
-    type(multifab ), intent(inout) :: unew(:)
-    type(multifab ), intent(in   ) :: rhohalf(:)
-    type(multifab ), intent(inout) :: phi(:)
-    real(dp_t)     , intent(in)    :: dx(:,:)
-    type(bc_tower ), intent(in   ) :: the_bc_tower
-    integer        , intent(in   ) :: stencil_type
-
-    type(multifab ), intent(in   ), optional :: divu_rhs(:)
-    real(dp_t)     , intent(in)   , optional :: eps_in 
-
-    ! Local variables
-    type(box     )  :: pd
-    type(  layout)  :: la
-
-    type(mg_tower) :: mgt(mla%nlevel)
-
-    type(multifab) :: rh(mla%nlevel)
-    type(multifab) :: one_sided_ss(2:mla%nlevel)
-
-    type(multifab), allocatable :: coeffs(:)
-
-    real(dp_t) :: bottom_solver_eps
-    real(dp_t) :: eps
-    real(dp_t) :: omega
-
-    integer :: i, ns
-    integer :: bottom_solver, bottom_max_iter
-    integer :: max_iter
-    integer :: min_width
-    integer :: max_nlevel
-    integer :: nu1, nu2, nub, gamma, cycle_type, smoother
-    integer :: n
-    integer :: max_nlevel_in
-    integer :: do_diagnostics
-
-    type(bl_prof_timer), save :: bpt
-
-    call build(bpt, "hg_multigrid")
-
-    !! Defaults:
-    max_nlevel        = mgt(nlevs)%max_nlevel
-    max_iter          = mgt(nlevs)%max_iter
-    eps               = mgt(nlevs)%eps
-    smoother          = mgt(nlevs)%smoother
-    nu1               = mgt(nlevs)%nu1
-    nu2               = mgt(nlevs)%nu2
-    nub               = mgt(nlevs)%nub
-    gamma             = mgt(nlevs)%gamma
-    omega             = mgt(nlevs)%omega
-    cycle_type        = mgt(nlevs)%cycle_type
-    bottom_solver     = mgt(nlevs)%bottom_solver
-    bottom_solver_eps = mgt(nlevs)%bottom_solver_eps
-    bottom_max_iter   = mgt(nlevs)%bottom_max_iter
-    min_width         = mgt(nlevs)%min_width
-
-    ! Note: put this here to minimize asymmetries - ASA
-    eps = 1.d-12
-    if (present(eps_in)) then
-       eps = eps_in
-    endif
-
-    if (parallel_IOProcessor()) print *, 'doing hgproject with tolerance, eps = ', eps
-
-    if ( hg_bottom_solver >= 0 ) then
-        if (hg_bottom_solver == 4 .and. nboxes(phi(1)) == 1) then
-           if (parallel_IOProcessor()) then
-              print *,'Dont use hg_bottom_solver == 4 with only one grid -- '
-              print *,'  Reverting to default bottom solver ',bottom_solver
-           end if
-        else if (hg_bottom_solver == 4 .and. max_mg_bottom_nlevels < 2) then
-           if (parallel_IOProcessor()) then
-              print *,'Dont use hg_bottom_solver == 4 with max_mg_bottom_nlevels < 2'
-              print *,'  Reverting to default bottom solver ',bottom_solver
-           end if
-        else
-           bottom_solver = hg_bottom_solver
-        end if
-    end if
-
-    min_width = 2
-
-    ! Note: put this here for robustness
-    max_iter = 100
-
-    if (stencil_type .eq. ST_DENSE) then
-       if (dm .eq. 3) then
-          i = mgt(nlevs)%nlevels
-          if ( (dx(nlevs,1) .eq. dx(nlevs,2)) .and. &
-               (dx(nlevs,1) .eq. dx(nlevs,3)) ) then
-             ns = 21
-          else
-             ns = 27
-          end if
-       else if (dm .eq. 2) then
-          ns = 9
-       else if (dm .eq. 1) then
-          ns = 3
-       end if
-    else
-       ns = 2*dm+1
-       do n = nlevs, 2, -1
-          la = mla%la(n)
-          call multifab_build(one_sided_ss(n), la, ns, 0, nodal)
-          call setval(one_sided_ss(n), ZERO,all=.true.)
-       end do
-    end if
-
-    do n = nlevs, 1, -1
-
-       if (dm == 1) then
-          max_nlevel_in = 1
-       else if (n == 1) then
-          max_nlevel_in = max_nlevel
-       else
-          if ( all(mla%mba%rr(n-1,:) == 2) ) then
-             max_nlevel_in = 1
-          else if ( all(mla%mba%rr(n-1,:) == 4) ) then
-             max_nlevel_in = 2
-          else 
-             call bl_error("HG_MULTIGRID: confused about ref_ratio")
-          end if
-       end if
-
-       pd = layout_get_pd(mla%la(n))
-
-       if (dm .eq. 1) then
-          max_iter = 200
-          omega = 1.33d0
-          nu1 = 200
-          nu2 = 200
-       end if
-
-       call mg_tower_build(mgt(n), mla%la(n), pd, &
-                       the_bc_tower%bc_tower_array(n)%ell_bc_level_array(0,:,:,press_comp), &
-                           dh = dx(n,:), &
-                           ns = ns, &
-                           smoother = smoother, &
-                           nu1 = nu1, &
-                           nu2 = nu2, &
-                           nub = nub, &
-                           gamma = gamma, &
-                           cycle_type = cycle_type, &
-                           omega = omega, &
-                           bottom_solver = bottom_solver, &
-                           bottom_max_iter = bottom_max_iter, &
-                           bottom_solver_eps = bottom_solver_eps, &
-                           max_iter = max_iter, &
-                           max_nlevel = max_nlevel_in, &
-                           max_bottom_nlevel = max_mg_bottom_nlevels, &
-                           min_width = min_width, &
-                           eps = eps, &
-                           verbose = mg_verbose, &
-                           cg_verbose = cg_verbose, &
-                           nodal = nodal)
-       
-    end do
-
-    !! Fill coefficient array
-    do n = nlevs,1,-1
-
-       allocate(coeffs(mgt(n)%nlevels))
-
-       la = mla%la(n)
-
-       call multifab_build(coeffs(mgt(n)%nlevels), la, 1, 1)
-       call setval(coeffs(mgt(n)%nlevels), 0.0_dp_t, 1, all=.true.)
-
-       call mkcoeffs(rhohalf(n),coeffs(mgt(n)%nlevels))
-       call multifab_fill_boundary(coeffs(mgt(n)%nlevels))
-
-       call stencil_fill_nodal_all_mglevels(mgt(n), coeffs, stencil_type)
-
-       if (stencil_type .eq. ST_CROSS .and. n .gt. 1) then
-          i = mgt(n)%nlevels
-          call stencil_fill_one_sided(one_sided_ss(n), coeffs(i), &
-                                      mgt(n)%dh(:,i), &
-                                      mgt(n)%mm(i), mgt(n)%face_type)
-       end if
-
-       call destroy(coeffs(mgt(n)%nlevels))
-       deallocate(coeffs)
-
-    end do
-
-    do n = 1, nlevs
-       call multifab_build(rh(n),mla%la(n),1,1,nodal)
-       call setval(rh(n),ZERO,all=.true.)
-    end do
-
-    call divu(nlevs,mgt,unew,rh,mla%mba%rr,nodal)
-
-    ! Do rh = rh - divu_rhs (this routine preserves rh=0 on
-    !  nodes which have bc_dirichlet = true.
-    if (present(divu_rhs)) &
-       call subtract_divu_from_rh(nlevs,mgt,rh,divu_rhs)
-
-    if ( mg_verbose >= 3 ) then
-       do_diagnostics = 1
-    else
-       do_diagnostics = 0
-    end if
-
-    if (present(eps_in)) then
-       call ml_nd_solve(mla,mgt,rh,phi,one_sided_ss,mla%mba%rr,do_diagnostics,eps_in=eps_in)
-    else
-       call ml_nd_solve(mla,mgt,rh,phi,one_sided_ss,mla%mba%rr,do_diagnostics)
-    end if
-
-    do n = nlevs,1,-1
-       call multifab_fill_boundary(phi(n))
-    end do
-
-    do n = 1, nlevs
-       call mg_tower_destroy(mgt(n))
-       call destroy(rh(n))
-    end do
-
-    if (stencil_type .ne. ST_DENSE) then
-       do n = nlevs, 2, -1
-          call destroy(one_sided_ss(n))
-       end do
-    end if
-
-    call destroy(bpt)
-
-  end subroutine hg_multigrid
-
-  !   ********************************************************************************* !
 
   subroutine hg_1d_solver(mla,unew,rhohalf,phi,dx,the_bc_tower,divu_rhs)
 
