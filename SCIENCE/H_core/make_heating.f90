38a70e0a3b5e2360e22353b439cfc6795ed49eb7
gilet <gilet>
2010-10-12 22:00:37 +0000

    fix to work with getting dm, nlevs from mla rather than geometry module

--- SCIENCE/H_core/make_heating.f90
+++ SCIENCE/H_core/make_heating.f90
@@ -20,7 +20,6 @@ contains
     use multifab_module
     use ml_layout_module
     use ml_restriction_module
-    use geometry, only: dm, nlevs
     use variables, only: foextrap_comp
 
     type(ml_layout), intent(in   ) :: mla
@@ -29,13 +28,15 @@ contains
     real(kind=dp_t), intent(in   ) :: dx(:,:),time,dt
 
     ! local
-    integer                  :: n,i,ng_s,ng_h
-    integer                  :: lo(dm),hi(dm)
+    integer                  :: n,i,ng_s,ng_h, nlevs
+    integer                  :: lo(mla%dim),hi(mla%dim)
     real(kind=dp_t), pointer :: sp(:,:,:,:)
     real(kind=dp_t), pointer :: hp(:,:,:,:)
     type(bl_prof_timer), save :: bpt
 
     call build(bpt, "get_rho_Hext")
+
+    nlevs = mla%nlevel
 
     ng_s = s(1)%ng
     ng_h = rho_Hext(1)%ng
@@ -48,7 +49,7 @@ contains
           hp => dataptr(rho_Hext(n) , i)
           lo =  lwb(get_box(s(n), i))
           hi =  upb(get_box(s(n), i))
-          select case (dm)
+          select case (mla%dim)
           case (1)
              call get_rho_Hext_1d(hp(:,1,1,1),ng_h,sp(:,1,1,:),ng_s,lo,hi,dx(n,:),time)
           case (2)
@@ -86,14 +87,50 @@ contains
   subroutine get_rho_Hext_2d(rho_Hext,ng_h,s,ng_s,lo,hi,dx,time)
 
     use bl_constants_module
+    use variables
+    use network
     
     integer, intent(in) :: lo(:), hi(:), ng_s, ng_h
     real(kind=dp_t), intent(inout) :: rho_Hext(lo(1)-ng_h:,lo(2)-ng_h:)
     real(kind=dp_t), intent(in   ) ::        s(lo(1)-ng_s:,lo(2)-ng_s:,:)
     real(kind=dp_t), intent(in   ) :: dx(:),time
 
+    integer                        :: i, j
+    integer                        :: h1_comp
+    integer                        :: c12_comp
+    integer                        :: n14_comp
+    integer                        :: o16_comp
+    real(kind=dp_t)                :: rho, T_6_third, X_CNO, X_1, g14
+    real(kind=dp_t)                :: tmp1, tmp2, tmp3
+
 !    call bl_error('2d external heating not written yet')
     rho_Hext = 0.0_dp_t
+
+!..............................................................................
+!     h1_comp = spec_comp - 1 + network_species_index("hydrogen-1")
+!     c12_comp = spec_comp - 1 + network_species_index("carbon-12")
+!     n14_comp = spec_comp - 1 + network_species_index("nitrogen-14")
+!     o16_comp = spec_comp - 1 + network_species_index("oxygen-16")
+
+!     do j = lo(2), hi(2)
+!        do i = lo(1), hi(1)
+!           rho = s(i,j,rho_comp)
+!           T_6_third = (s(i,j,temp_comp) / 1.0d6) ** THIRD
+!           tmp1 = s(i,j,c12_comp)
+!           tmp2 = s(i,j,n14_comp)
+!           tmp3 = s(i,j,o16_comp)
+!           X_CNO = (tmp1 + tmp2 + tmp3) / rho
+!           X_1 = s(i,j,h1_comp) / rho
+!           tmp1 =   2.7d-3 * T_6_third
+!           tmp2 = -7.78d-3 * T_6_third**2
+!           tmp3 = -1.49d-4 * T_6_third**3
+!           g14 = 1.0_dp_t + tmp1 + tmp2 + tmp3
+!           tmp1 = 8.67d27 * g14 * X_CNO * X_1 * rho / T_6_third**2
+!           tmp2 = dexp(-1.5228d2 / T_6_third)
+!           rho_Hext(i,j) = rho * tmp1 * tmp2
+!        enddo
+!     enddo
+!.............................................................................. 
 
   end subroutine get_rho_Hext_2d
   
