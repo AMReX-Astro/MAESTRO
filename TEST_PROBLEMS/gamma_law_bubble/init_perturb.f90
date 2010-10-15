0b501339b3868d9d252d9e66b42552c72bd94de9
mzingale <mzingale>
2010-10-15 13:54:07 +0000

    make the bubble size/position a parameter

--- TEST_PROBLEMS/gamma_law_bubble/init_perturb.f90
+++ TEST_PROBLEMS/gamma_law_bubble/init_perturb.f90
@@ -4,6 +4,7 @@ module init_perturb_module
   use variables
   use network, only: nspec
   use eos_module
+  use bl_error_module
 
   implicit none
 
@@ -15,7 +16,8 @@ contains
   subroutine perturb_2d(x, y, p0_init, s0_init, dens_pert, rhoh_pert, rhoX_pert, &
                         temp_pert, trac_pert)
 
-    use probin_module, only: pert_factor
+    use geometry, only: center
+    use probin_module, only: pert_factor, y_pert_center, pert_width
 
     ! apply an optional perturbation to the initial temperature field
     ! to see some bubbles
@@ -31,9 +33,9 @@ contains
 
     t0 = s0_init(temp_comp)
 
-    x1 = 1.0d0
-    y1 = 0.2d0
-    r1 = sqrt( (x-x1)**2 +(y-y1)**2 ) / (0.025d0)
+    x1 = center(1)
+    y1 = y_pert_center
+    r1 = sqrt( (x-x1)**2 +(y-y1)**2 ) / pert_width
     
     ! temperature perturbation -- pert_factor sets the amplitude
     ! of the perturbation (higher values means larger perturbation)
@@ -79,99 +81,20 @@ contains
     real(kind=dp_t), intent(out) :: rhoX_pert(:)
     real(kind=dp_t), intent(out) :: trac_pert(:)
 
-    real(kind=dp_t) :: temp, t0
-    real(kind=dp_t) :: x0, y0, z0, r0
-
-    t0 = s0_init(temp_comp)
-
-    x0 = 3.6d7
-    y0 = 3.6d7
-    z0 = 8.5d7
-
-    r0 = sqrt( (x-x0)**2 + (y-y0)**2 + (z-z0)**2 ) / 2.5d6
-
-    temp = t0 * (ONE + TWO * (0.15d0 * (1.d0 + tanh((2.d0-r0)))))
-
-    ! Use the EOS to make this temperature perturbation occur at constant 
-    ! pressure
-    temp_eos(1) = temp
-    p_eos(1) = p0_init
-    den_eos(1) = s0_init(rho_comp)
-    xn_eos(1,:) = s0_init(spec_comp:spec_comp+nspec-1)/s0_init(rho_comp)
-
-    call eos(eos_input_tp, den_eos, temp_eos, &
-             npts, &
-             xn_eos, &
-             p_eos, h_eos, e_eos, &
-             cv_eos, cp_eos, xne_eos, eta_eos, pele_eos, &
-             dpdt_eos, dpdr_eos, dedt_eos, dedr_eos, &
-             dpdX_eos, dhdX_eos, &
-             gam1_eos, cs_eos, s_eos, &
-             dsdt_eos, dsdr_eos, &
-             .false.)
-
-    dens_pert = den_eos(1)
-    rhoh_pert = den_eos(1)*h_eos(1)
-    rhoX_pert = dens_pert*xn_eos(1,:)
-
-    temp_pert = temp
-
-    trac_pert = ZERO
+    call bl_error("ERROR: perturb_3d not implemented")
 
   end subroutine perturb_3d
 
   subroutine perturb_3d_sphr(x, y, z, p0_init, s0_init, dens_pert, rhoh_pert, &
                              rhoX_pert, temp_pert, trac_pert)
 
-    ! apply an optional perturbation to the initial temperature field
-    ! to see some bubbles
     real(kind=dp_t), intent(in ) :: x, y, z
     real(kind=dp_t), intent(in ) :: p0_init, s0_init(:)
     real(kind=dp_t), intent(out) :: dens_pert, rhoh_pert, temp_pert
     real(kind=dp_t), intent(out) :: rhoX_pert(:)
     real(kind=dp_t), intent(out) :: trac_pert(:)
 
-    real(kind=dp_t) :: temp, t0
-    real(kind=dp_t) :: x0, y0, z0, r0
-
-    t0 = s0_init(temp_comp)
-
-    ! center of star is at 2.5d8
-    x0 = 2.5d8
-    y0 = 2.5d8
-    z0 = 3.0d8
-
-    ! Tanh bubbles
-    r0 = sqrt( (x-x0)**2 + (y-y0)**2 + (z-z0)**2 ) / 2.5d6
-    
-    ! This case works
-    temp = t0 * (ONE + TWO*(.150_dp_t * 0.5_dp_t * (1.0_dp_t + tanh((2.0_dp_t-r0)))))
-
-    ! Use the EOS to make this temperature perturbation occur at constant 
-    ! pressure
-    temp_eos(1) = temp
-    p_eos(1) = p0_init
-    den_eos(1) = s0_init(rho_comp)
-    xn_eos(1,:) = s0_init(spec_comp:spec_comp+nspec-1)/s0_init(rho_comp)
-
-    call eos(eos_input_tp, den_eos, temp_eos, &
-             npts, &
-             xn_eos, &
-             p_eos, h_eos, e_eos, &
-             cv_eos, cp_eos, xne_eos, eta_eos, pele_eos, &
-             dpdt_eos, dpdr_eos, dedt_eos, dedr_eos, &
-             dpdX_eos, dhdX_eos, &
-             gam1_eos, cs_eos, s_eos, &
-             dsdt_eos, dsdr_eos, &
-             .false.)
-
-    dens_pert = den_eos(1)
-    rhoh_pert = den_eos(1)*h_eos(1)
-    rhoX_pert = dens_pert*xn_eos(1,:)
-
-    temp_pert = temp
-
-    trac_pert = ZERO
+    call bl_error("ERROR: perturb_3d_sphr not implemented")
 
   end subroutine perturb_3d_sphr
 
