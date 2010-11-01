d98ade47a6b2f2e175c47392e4a0f0457259f67b
ajnonaka <ajnonaka>
2010-11-01 21:08:13 +0000

    cleanup

--- TEST_PROBLEMS/gamma_law_bubble/base_state.f90
+++ TEST_PROBLEMS/gamma_law_bubble/base_state.f90
@@ -43,6 +43,9 @@ contains
        call bl_error("ERROR: base_state.f90 is not valid for spherical")
     endif
 
+    ! for isentropic
+    ! an isentropic gas satisfies p ~ rho^gamma
+    const = pres_base/dens_base**gamma_const
 
     ! use the EOS to make the state consistent
     den_eos(1)  = dens_base
@@ -67,44 +70,36 @@ contains
        
        z = (dble(j)+HALF) * dr(1)
 
-       if (.not. do_isentropic) then
-          ! the density of an isothermal gamma-law atm is exponential
-          den_eos(1)  = dens_base * exp(-z/H)
-
-          s0_init(j, rho_comp) = den_eos(1)
-
-          ! compute the pressure by discretizing HSE
-          if (j.gt.0) then
-             p0_init(j) = p0_init(j-1) - &
-                  dr(1) * HALF * (s0_init(j,rho_comp) + s0_init(j-1,rho_comp)) * &
-                  abs(grav_const)
-             
-             p_eos(1) = p0_init(j)
-          end if
-
-       else 
-          ! assume isentropic
-          ! an isentropic gas satisfies p ~ rho^gamma
-          const = pres_base/dens_base**gamma_const
+       if (do_isentropic) then
 
           ! we can integrate HSE with p = K rho^gamma analytically
           den_eos(1) = dens_base*(grav_const*dens_base*(gamma_const - 1.0)*z/ &
                (gamma_const*pres_base) + 1.d0)**(1.d0/(gamma_const - 1.d0))
-          s0_init(j, rho_comp) = den_eos(1)
 
-          ! compute the pressure by discretizing HSE
-          if (j.gt.0) then
-             p0_init(j) = p0_init(j-1) - &
-                  dr(1) * HALF * (s0_init(j,rho_comp) + s0_init(j-1,rho_comp)) * &
-                  abs(grav_const)
+       else
+
+          ! the density of an isothermal gamma-law atm is exponential
+          den_eos(1)  = dens_base * exp(-z/H)
+
+       end if
+
+       s0_init(j, rho_comp) = den_eos(1)
+
+       ! compute the pressure by discretizing HSE
+       if (j.eq.0) then
+          p0_init(j) = p0_init(j) - &
+               dr(1) * HALF *s0_init(j,rho_comp) * &
+               abs(grav_const)
              
-             p_eos(1) = p0_init(j)
-          end if
-
-          print *, 'here'
-
-       endif
+          p_eos(1) = p0_init(j)
+       else if (j.gt.0) then
+          p0_init(j) = p0_init(j-1) - &
+               dr(1) * HALF * (s0_init(j,rho_comp) + s0_init(j-1,rho_comp)) * &
+               abs(grav_const)
           
+          p_eos(1) = p0_init(j)
+       end if
+       
        ! use the EOS to make the state consistent
 
        ! (rho,p) --> T, h
