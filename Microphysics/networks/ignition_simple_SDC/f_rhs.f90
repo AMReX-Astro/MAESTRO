ad64ee49eeb1559d4afead44fcc05b776d4a234f
mzingale <mzingale>
2011-06-03 22:50:33 +0000

    add support for enthalpy and use_tfromp = F.  Also remove some derivatives that were computed for the analytic Jacobian (which we no longer use)

--- Microphysics/networks/ignition_simple_SDC/f_rhs.f90
+++ Microphysics/networks/ignition_simple_SDC/f_rhs.f90
@@ -26,9 +26,10 @@ subroutine f_rhs(n, t, y, ydot, rpar, ipar)
 
   real(kind=dp_t) :: t
 
-  real(kind=dp_t) :: dens, temp, T9, T9a, dT9dt, dT9adt
+  real(kind=dp_t) :: dens, temp, T9, T9a
+  real(kind=dp_t) :: rhoh, rho_Hnuc
 
-  real(kind=dp_t) :: rate, dratedt
+  real(kind=dp_t) :: rate
   real(kind=dp_t) :: sc1212, dsc1212dt
   real(kind=dp_t) :: xc12tmp
 
@@ -38,9 +39,9 @@ subroutine f_rhs(n, t, y, ydot, rpar, ipar)
                        one_third = 1.0d0/ 3.0d0, &
                       two_thirds = 2.0d0/ 3.0d0
 
-  real(kind=dp_t) :: scratch, dscratchdt
+  real(kind=dp_t) :: scratch
   
-  real(kind=dp_t) :: a, b, dadt, dbdt
+  real(kind=dp_t) :: a, b
 
   integer, save :: ic12, io16, img24
 
@@ -61,58 +62,75 @@ subroutine f_rhs(n, t, y, ydot, rpar, ipar)
 
   X(1:nspec) = y(1:nspec)/dens
 
+  ! enthalpy
+  rhoh = y(nspec+1)
+
+  ! compute the temperature from the EOS
+  if (use_tfromp) then
+
+     ! get T from (rho, p, X)
+     den_eos(1) = dens
+     xn_eos(1,:) = X(1:nspec)
+     p_eos(1) = p0_pass
+
+     call eos(eos_input_rp, den_eos, temp_eos, &
+              npts, &
+              xn_eos, &
+              p_eos, h_eos, e_eos, &
+              cv_eos, cp_eos, xne_eos, eta_eos, pele_eos, &
+              dpdt_eos, dpdr_eos, dedt_eos, dedr_eos, &
+              dpdX_eos, dhdX_eos, &
+              gam1_eos, cs_eos, s_eos, &
+              dsdt_eos, dsdr_eos, &
+              .false., &
+              pt_index_eos)
+
+     temp = temp_eos(1)
+
+  else
+
+     ! get T from (rho, h, X)
+     den_eos(1) = dens
+     xn_eos(1,:) = X(1:nspec)
+     h_eos(1) = rhoh/dens
+
+     call eos(eos_input_rh, den_eos, temp_eos, &
+              npts, &
+              xn_eos, &
+              p_eos, h_eos, e_eos, &
+              cv_eos, cp_eos, xne_eos, eta_eos, pele_eos, &
+              dpdt_eos, dpdr_eos, dedt_eos, dedr_eos, &
+              dpdX_eos, dhdX_eos, &
+              gam1_eos, cs_eos, s_eos, &
+              dsdt_eos, dsdr_eos, &
+              .false., &
+              pt_index_eos)
+
+     temp = temp_eos(1)
+
+  endif
+
+
+  ! call the screening routine
   ! compute the molar fractions -- needed for the screening
   ymass(ic12) = X(1)/aion(ic12)
   ymass(io16) = X(2)/aion(io16)
   ymass(img24) = X(3)/aion(img24)
 
-  ! compute temp with EOS -- note, here we are assuming use_tfromp = T
-  den_eos(1) = dens
-  xn_eos(1,:) = X(1:nspec)
-  p_eos(1) = p0_pass
-
-  if (.not. use_tfromp) then
-     call bl_error("f_rhs.f90: use_tfromp = F not supported")
-  end if
-
-  call eos(eos_input_rp, den_eos, temp_eos, &
-           npts, &
-           xn_eos, &
-           p_eos, h_eos, e_eos, &
-           cv_eos, cp_eos, xne_eos, eta_eos, pele_eos, &
-           dpdt_eos, dpdr_eos, dedt_eos, dedr_eos, &
-           dpdX_eos, dhdX_eos, &
-           gam1_eos, cs_eos, s_eos, &
-           dsdt_eos, dsdr_eos, &
-           .false., &
-           pt_index_eos)
-
-  temp = temp_eos(1)
-
-  ! call the screening routine
   call screenz(temp,dens,6.0d0,6.0d0,12.0d0,12.0d0,ymass,aion,zion,nspec,     &
                sc1212, dsc1212dt)
 
   
   ! compute some often used temperature constants
   T9     = temp/1.d9
-  dT9dt  = ONE/1.d9
   T9a    = T9/(1.0d0 + 0.0396d0*T9)
-  dT9adt = (T9a / T9 - (T9a / (1.0d0 + 0.0396d0*T9)) * 0.0396d0) * dT9dt
 
   ! compute the CF88 rate
   scratch    = T9a**one_third
-  dscratchdt = one_third * T9a**(-2.0d0 * one_third) * dT9adt
 
   a       = 4.27d26*T9a**five_sixths*T9**(-1.5d0)
-  dadt    = five_sixths * (a/T9a) * dT9adt - 1.5d0 * (a/T9) * dT9dt
-
   b       = dexp(-84.165d0/scratch - 2.12d-3*T9*T9*T9)
-  dbdt    = (84.165d0 * dscratchdt/ scratch**2.0d0                            &
-             - 3.0d0 * 2.12d-3 * T9 * T9 * dT9dt) * b
-
   rate    = a *  b
-  dratedt = dadt * b + a * dbdt
 
   ! The change in number density of C12 is
   ! d(n12)/dt = - 2 * 1/2 (n12)**2 <sigma v>
@@ -146,17 +164,24 @@ subroutine f_rhs(n, t, y, ydot, rpar, ipar)
 
   ! changes in X due to reactions only
   xc12tmp = max(X(ic12),0.d0)
-  ydot(1) = -one_twelvth*dens*sc1212*rate*xc12tmp**2
-  ydot(2) = 0.d0
-  ydot(3) =  one_twelvth*dens*sc1212*rate*xc12tmp**2
+  ydot(ic12)  = -one_twelvth*dens*sc1212*rate*xc12tmp**2
+  ydot(io16)  = 0.d0
+  ydot(img24) =  one_twelvth*dens*sc1212*rate*xc12tmp**2
+
+  ! compute the enthalpy source from reactions (note: eventually, we
+  ! we will to add rho_Hext here too)
+  rho_Hnuc = 0.0d0
+  do n = 1, nspec
+     rho_Hnuc = rho_Hnuc - ebin(n)*dens*ydot(n)
+  enddo
 
   ! now make ydots refer to rhoX and include the sdc sources
-  ydot(1) = dens*ydot(1) + sdc_rhoX_pass(1)
-  ydot(2) = dens*ydot(2) + sdc_rhoX_pass(2)
-  ydot(3) = dens*ydot(3) + sdc_rhoX_pass(3)
+  ydot(ic12)  = dens*ydot(ic12)  + sdc_rhoX_pass(ic12)
+  ydot(io16)  = dens*ydot(io16)  + sdc_rhoX_pass(io16)
+  ydot(img24) = dens*ydot(img24) + sdc_rhoX_pass(img24)
 
-  ! WRONG
-  ydot(nspec+1) =  sdc_rhoh_pass
+  ! enthalpy
+  ydot(nspec+1) =  sdc_rhoh_pass + rho_Hnuc
 
   return
 
