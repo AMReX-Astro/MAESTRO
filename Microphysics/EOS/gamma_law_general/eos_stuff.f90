8fcc4b9323e27a5fb36d9e9898848f3a80f427be
mzingale <mzingale>
2012-02-02 02:22:49 +0000

    get this working with MAESTRO again -- remove npts.  All interfaces should be happy.

--- Microphysics/EOS/gamma_law_general/eos_stuff.f90
+++ Microphysics/EOS/gamma_law_general/eos_stuff.f90
@@ -36,34 +36,31 @@ module eos_module
 
   private
 
-  integer, parameter :: NP = 1
-  integer, parameter, public :: npts = 1
-
-  real(kind=dp_t), public :: xn_eos(NP,nspec)
-  real(kind=dp_t), public :: temp_eos(NP)
-  real(kind=dp_t), public :: den_eos(NP)
-  real(kind=dp_t), public :: abar_eos(NP)
-  real(kind=dp_t), public :: zbar_eos(NP)
-  real(kind=dp_t), public :: e_eos(NP)
-  real(kind=dp_t), public :: p_eos(NP)
-  real(kind=dp_t), public :: h_eos(NP)
-  real(kind=dp_t), public :: cv_eos(NP)
-  real(kind=dp_t), public :: cp_eos(NP)
-  real(kind=dp_t), public :: xne_eos(NP)
-  real(kind=dp_t), public :: eta_eos(NP)
-  real(kind=dp_t), public :: pele_eos(NP)
-  real(kind=dp_t), public :: dpdt_eos(NP)
-  real(kind=dp_t), public :: dpdr_eos(NP)
-  real(kind=dp_t), public :: dedr_eos(NP)
-  real(kind=dp_t), public :: dedt_eos(NP)
-  real(kind=dp_t), public :: gam1_eos(NP)
-  real(kind=dp_t), public ::   cs_eos(NP)
-  real(kind=dp_t), public ::    s_eos(NP)
-  real(kind=dp_t), public :: dsdt_eos(NP)
-  real(kind=dp_t), public :: dsdr_eos(NP)
-  real(kind=dp_t), public :: dpdX_eos(NP,nspec)
-  real(kind=dp_t), public :: dhdX_eos(NP,nspec)
-  real(kind=dp_t), public :: conduct_eos(NP)
+  real(kind=dp_t), public :: xn_eos(nspec)
+  real(kind=dp_t), public :: temp_eos
+  real(kind=dp_t), public :: den_eos
+  real(kind=dp_t), public :: abar_eos
+  real(kind=dp_t), public :: zbar_eos
+  real(kind=dp_t), public :: e_eos
+  real(kind=dp_t), public :: p_eos
+  real(kind=dp_t), public :: h_eos
+  real(kind=dp_t), public :: cv_eos
+  real(kind=dp_t), public :: cp_eos
+  real(kind=dp_t), public :: xne_eos
+  real(kind=dp_t), public :: eta_eos
+  real(kind=dp_t), public :: pele_eos
+  real(kind=dp_t), public :: dpdt_eos
+  real(kind=dp_t), public :: dpdr_eos
+  real(kind=dp_t), public :: dedr_eos
+  real(kind=dp_t), public :: dedt_eos
+  real(kind=dp_t), public :: gam1_eos
+  real(kind=dp_t), public ::   cs_eos
+  real(kind=dp_t), public ::    s_eos
+  real(kind=dp_t), public :: dsdt_eos
+  real(kind=dp_t), public :: dsdr_eos
+  real(kind=dp_t), public :: dpdX_eos(nspec)
+  real(kind=dp_t), public :: dhdX_eos(nspec)
+  real(kind=dp_t), public :: conduct_eos
 
   integer, public         :: pt_index_eos(MAX_SPACEDIM)
 
@@ -162,13 +159,12 @@ contains
 
      do_diag = .false.
 
-     temp_eos(1) = T
-      den_eos(1) = R
-        e_eos(1) = e
-      xn_eos(1,1:nspec) = X(1:nspec)
+     temp_eos = T
+      den_eos = R
+        e_eos = e
+      xn_eos(1:nspec) = X(1:nspec)
 
      call eos(eos_input_re, den_eos, temp_eos, &
-              npts, &
               xn_eos, &
               p_eos, h_eos, e_eos, &
               cv_eos, cp_eos, xne_eos, eta_eos, pele_eos, &
@@ -178,12 +174,12 @@ contains
               dsdt_eos, dsdr_eos, &
               do_diag)
 
-    G  = gam1_eos(1)
-    P  =    p_eos(1)
-    C  =   cs_eos(1)
-    T  = temp_eos(1)
-    dpdr_e = dpdr_eos(1) - dpdt_eos(1)*dedr_eos(1)/dedt_eos(1)
-    dpde = dpdt_eos(1) / dedt_eos(1)
+    G  = gam1_eos
+    P  =    p_eos
+    C  =   cs_eos
+    T  = temp_eos
+    dpdr_e = dpdr_eos - dpdt_eos*dedr_eos/dedt_eos
+    dpde = dpdt_eos / dedt_eos
 
   end subroutine eos_given_ReX
 
@@ -200,13 +196,12 @@ contains
 
      do_diag = .false.
 
-     temp_eos(1) = T
-      den_eos(1) = R
-        p_eos(1) = P
-      xn_eos(1,1:nspec) = X(1:nspec)
+     temp_eos = T
+      den_eos = R
+        p_eos = P
+      xn_eos(1:nspec) = X(1:nspec)
 
      call eos(eos_input_rp, den_eos, temp_eos, &
-              npts, &
               xn_eos, &
               p_eos, h_eos, e_eos, &
               cv_eos, cp_eos, xne_eos, eta_eos, pele_eos, &
@@ -216,8 +211,8 @@ contains
               dsdt_eos, dsdr_eos, &
               do_diag)
 
-    e  =    e_eos(1)
-    T  = temp_eos(1)
+    e  =    e_eos
+    T  = temp_eos
 
   end subroutine eos_e_given_RPX
 
@@ -235,13 +230,12 @@ contains
 
      do_diag = .false.
 
-     temp_eos(1) = T
-      den_eos(1) = R
-        e_eos(1) = e
-      xn_eos(1,1:nspec) = X(1:nspec)
+     temp_eos = T
+      den_eos = R
+        e_eos = e
+      xn_eos(1:nspec) = X(1:nspec)
 
      call eos(eos_input_re, den_eos, temp_eos, &
-              npts, &
               xn_eos, &
               p_eos, h_eos, e_eos, &
               cv_eos, cp_eos, xne_eos, eta_eos, pele_eos, &
@@ -251,7 +245,7 @@ contains
               dsdt_eos, dsdr_eos, &
               do_diag)
 
-    S  = s_eos(1)
+    S  = s_eos
 
   end subroutine eos_S_given_ReX
 
@@ -267,12 +261,11 @@ contains
 
      do_diag = .false.
 
-      den_eos(1) = R
-     temp_eos(1) = T
-      xn_eos(1,1:nspec) = X(1:nspec)
+      den_eos = R
+     temp_eos = T
+      xn_eos(1:nspec) = X(1:nspec)
 
      call eos(eos_input_rt, den_eos, temp_eos, &
-              npts, &
               xn_eos, &
               p_eos, h_eos, e_eos, &
               cv_eos, cp_eos, xne_eos, eta_eos, pele_eos, &
@@ -282,8 +275,8 @@ contains
               dsdt_eos, dsdr_eos, &
               do_diag)
 
-    P  =    p_eos(1)
-    e  =    e_eos(1)
+    P  =    p_eos
+    e  =    e_eos
 
   end subroutine eos_given_RTX
 
@@ -303,12 +296,11 @@ contains
 
     do_diag = .false.
 
-    den_eos(1) = R
-    temp_eos(1) = T
-    xn_eos(1,1:nspec) = X(1:nspec)
+    den_eos = R
+    temp_eos = T
+    xn_eos(1:nspec) = X(1:nspec)
 
     call eos(eos_input_rt, den_eos, temp_eos, &
-             npts, &
              xn_eos, &
              p_eos, h_eos, e_eos, &
              cv_eos, cp_eos, xne_eos, eta_eos, pele_eos, &
@@ -318,9 +310,9 @@ contains
              dsdt_eos, dsdr_eos, &
              do_diag)
 
-    P  =    p_eos(1)
-    e  =    e_eos(1)
-    dpdr =  dpdr_eos(1)
+    P  =    p_eos
+    e  =    e_eos
+    dpdr =  dpdr_eos
 
   end subroutine eos_dpdr_given_RTX
 
@@ -337,13 +329,12 @@ contains
      do_diag = .false.
 
      ! An initial guess of density needs to be given
-     den_eos(1) = R 
-     p_eos(1) = P
-     temp_eos(1) = T
-     xn_eos(1,1:nspec) = X(1:nspec)
+     den_eos = R 
+     p_eos = P
+     temp_eos = T
+     xn_eos(1:nspec) = X(1:nspec)
 
      call eos(eos_input_tp, den_eos, temp_eos, &
-              npts, &
               xn_eos, &
               p_eos, h_eos, e_eos, &
               cv_eos, cp_eos, xne_eos, eta_eos, pele_eos, &
@@ -353,8 +344,8 @@ contains
               dsdt_eos, dsdr_eos, &
               do_diag)
 
-    R  =    den_eos(1)
-    e  =    e_eos(1)
+    R  =    den_eos
+    e  =    e_eos
 
   end subroutine eos_given_TPX
 
@@ -370,12 +361,11 @@ contains
 
     do_diag = .false.
 
-    p_eos(1) = P
-    s_eos(1) = S
-    xn_eos(1,1:nspec) = X(1:nspec)
+    p_eos = P
+    s_eos = S
+    xn_eos(1:nspec) = X(1:nspec)
 
     call eos(eos_input_ps, den_eos, temp_eos, &
-             npts, &
              xn_eos, &
              p_eos, h_eos, e_eos, &
              cv_eos, cp_eos, xne_eos, eta_eos, pele_eos, &
@@ -385,9 +375,9 @@ contains
              dsdt_eos, dsdr_eos, &
              do_diag)
 
-    R = den_eos(1)
-    T = temp_eos(1)
-    e = e_eos(1)
+    R = den_eos
+    T = temp_eos
+    e = e_eos
 
   end subroutine eos_given_PSX
 
@@ -396,7 +386,6 @@ contains
   ! The main interface -- this is used directly by MAESTRO
   !---------------------------------------------------------------------------
   subroutine eos(input, dens, temp, &
-                 npoints, &
                  xmass, &
                  pres, enthalpy, eint, &
                  c_v, c_p, ne, eta, pele, &
@@ -412,7 +401,6 @@ contains
 
 ! dens     -- mass density (g/cc)
 ! temp     -- temperature (K)
-! npoints     -- the number of elements in input/output arrays
 ! xmass    -- the mass fractions of the individual isotopes
 ! pres     -- the pressure (dyn/cm**2)
 ! enthalpy -- the enthalpy (erg/g)
@@ -458,24 +446,23 @@ contains
 
     logical do_eos_diag
     integer, intent(in) :: input
-    integer, intent(in) :: npoints
 
-    real(kind=dp_t) :: dens(npoints), temp(npoints)
-    real(kind=dp_t) :: xmass(npoints,nspec)
-    real(kind=dp_t) :: pres(npoints), enthalpy(npoints), eint(npoints)
-    real(kind=dp_t) :: c_v(npoints), c_p(npoints)
-    real(kind=dp_t) :: ne(npoints), eta(npoints), pele(npoints)
-    real(kind=dp_t) :: dPdT(npoints), dPdR(npoints), dedT(npoints), dedR(npoints)
-    real(kind=dp_t) :: gam1(npoints), entropy(npoints), cs(npoints)
-    real(kind=dp_t) :: dPdX(npoints,nspec), dedX(npoints,nspec), dhdX(npoints,nspec)
-    real(kind=dp_t) :: dsdT(npoints), dsdR(npoints)
+    real(kind=dp_t) :: dens, temp
+    real(kind=dp_t) :: xmass(nspec)
+    real(kind=dp_t) :: pres, enthalpy, eint
+    real(kind=dp_t) :: c_v, c_p
+    real(kind=dp_t) :: ne, eta, pele
+    real(kind=dp_t) :: dPdT, dPdR, dedT, dedR
+    real(kind=dp_t) :: gam1, entropy, cs
+    real(kind=dp_t) :: dPdX(nspec), dedX(nspec), dhdX(nspec)
+    real(kind=dp_t) :: dsdT, dsdR
 
     integer, optional, intent(in   ) :: pt_index(:)
 
 
     ! local variables
-    real(kind=dp_t) :: ymass(npoints,nspec)    
-    real(kind=dp_t) :: mu(npoints)
+    real(kind=dp_t) :: ymass(nspec)    
+    real(kind=dp_t) :: mu
     real(kind=dp_t) :: dmudX, sum_y
 
     ! get the mass of a nucleon from Avogadro's number.
@@ -486,9 +473,6 @@ contains
     ! general sanity checks
     if (.not. initialized) call bl_error('EOS: not initialized')
       
-    if (npoints > NP) then
-       call bl_error('EOS: eos called with too large of a vector size')
-    endif
 
     !-------------------------------------------------------------------------
     ! compute mu -- the mean molecular weight
@@ -496,30 +480,26 @@ contains
     if (eos_assume_neutral) then
        ! assume completely neutral atoms
 
-       do k = 1, npoints
-          sum_y  = 0.d0
+       sum_y  = 0.d0
           
-          do n = 1, nspec
-             ymass(k,n) = xmass(k,n)/aion(n)
-             sum_y = sum_y + ymass(k,n)
-          enddo
-          
-          mu(k) = 1.d0/sum_y
+       do n = 1, nspec
+          ymass(n) = xmass(n)/aion(n)
+          sum_y = sum_y + ymass(n)
        enddo
+          
+       mu = 1.d0/sum_y
 
     else
        ! assume completely ionized species
 
-       do k = 1, npoints
-          sum_y  = 0.d0
+       sum_y  = 0.d0
           
-          do n = 1, nspec
-             ymass(k,n) = xmass(k,n)*(1.d0 + zion(n))/aion(n)
-             sum_y = sum_y + ymass(k,n)
-          enddo
-          
-          mu(k) = 1.d0/sum_y
+       do n = 1, nspec
+          ymass(n) = xmass(n)*(1.d0 + zion(n))/aion(n)
+          sum_y = sum_y + ymass(n)
        enddo
+          
+       mu = 1.d0/sum_y
 
     endif
 
@@ -530,137 +510,123 @@ contains
     if (input .EQ. eos_input_rh) then
 
        ! dens, enthalpy, and xmass are inputs
-       do k = 1, npoints
 
-          ! Solve for the temperature:
-          ! h = e + p/rho = (p/rho)*[1 + 1/(gamma-1)] = (p/rho)*gamma/(gamma-1)
-          temp(k) = (enthalpy(k)*mu(k)*m_nucleon/k_B)*(gamma_const - 1.0)/gamma_const
-       enddo
+       ! Solve for the temperature:
+       ! h = e + p/rho = (p/rho)*[1 + 1/(gamma-1)] = (p/rho)*gamma/(gamma-1)
+       temp = (enthalpy*mu*m_nucleon/k_B)*(gamma_const - 1.0)/gamma_const
 
 
     else if (input .EQ. eos_input_tp ) then
 
        ! temp, pres, and xmass are inputs
-       do k = 1, npoints
           
-          ! Solve for the density:
-          ! p = rho k T / (mu m_nucleon)
-          dens(k) = pres(k)*mu(k)*m_nucleon/(k_B*temp(k))
-       enddo
+       ! Solve for the density:
+       ! p = rho k T / (mu m_nucleon)
+       dens = pres*mu*m_nucleon/(k_B*temp)
 
 
     else if (input .EQ. eos_input_rp ) then
 
        ! dens, pres, and xmass are inputs
-       do k = 1, npoints
 
-          ! Solve for the temperature:
-          ! p = rho k T / (mu m_nucleon)
-          temp(k) = pres(k)*mu(k)*m_nucleon/(k_B*dens(k))
-       enddo
+       ! Solve for the temperature:
+       ! p = rho k T / (mu m_nucleon)
+       temp = pres*mu*m_nucleon/(k_B*dens)
 
 
     else if (input .EQ. eos_input_re) then
 
        ! dens, energy, and xmass are inputs
-       do k = 1, npoints
 
-          ! Solve for the temperature
-          ! e = k T / [(mu m_nucleon)*(gamma-1)]
-          temp(k) = eint(k)*mu(k)*m_nucleon*(gamma_const-1.0)/k_B
+       ! Solve for the temperature
+       ! e = k T / [(mu m_nucleon)*(gamma-1)]
+       temp = eint*mu*m_nucleon*(gamma_const-1.0)/k_B
 
-       enddo
 
     else if (input .EQ. eos_input_ps) then
        
        ! pressure and entropy are inputs
-       do k = 1, npoints
 
-          ! Solve for the temperature
-          ! Invert Sackur-Tetrode eqn (below) using 
-          ! rho = p mu m_nucleon / (k T)
-          temp(k) = pres(k)**(2.0_dp_t/5.0_dp_t) * &
-                    ( 2.0_dp_t*M_PI*hbar*hbar/(mu(k)*m_nucleon) )**(3.0_dp_t/5.0_dp_t) * &
-                    dexp(2.0_dp_t*mu(k)*m_nucleon*entropy(k)/(5.0_dp_t*k_B) - 1.0_dp_t) / &
-                    k_B
+       ! Solve for the temperature
+       ! Invert Sackur-Tetrode eqn (below) using 
+       ! rho = p mu m_nucleon / (k T)
+       temp = pres**(2.0_dp_t/5.0_dp_t) * &
+            ( 2.0_dp_t*M_PI*hbar*hbar/(mu*m_nucleon) )**(3.0_dp_t/5.0_dp_t) * &
+            dexp(2.0_dp_t*mu*m_nucleon*entropy/(5.0_dp_t*k_B) - 1.0_dp_t) / &
+            k_B
 
-          ! Solve for the density
-          ! rho = p mu m_nucleon / (k T)
-          dens(k) = pres(k)*mu(k)*m_nucleon/(k_B*temp(k))
+       ! Solve for the density
+       ! rho = p mu m_nucleon / (k T)
+       dens = pres*mu*m_nucleon/(k_B*temp)
 
-       enddo
-    
     endif
 
     !-------------------------------------------------------------------------
     ! now we have the density and temperature (and mass fractions /
     ! mu), regardless of the inputs.
     !-------------------------------------------------------------------------
-    do k = 1, npoints
 
-       ! compute the pressure simply from the ideal gas law, and the
-       ! specific internal energy using the gamma-law EOS relation
-       pres(k) = dens(k)*k_B*temp(k)/(mu(k)*m_nucleon)
-       eint(k) = pres(k)/(gamma_const - 1.0)/dens(k)
+    ! compute the pressure simply from the ideal gas law, and the
+    ! specific internal energy using the gamma-law EOS relation
+    pres = dens*k_B*temp/(mu*m_nucleon)
+    eint = pres/(gamma_const - 1.0)/dens
 
-       ! enthalpy is h = e + p/rho
-       enthalpy(k) = eint(k) + pres(k)/dens(k)
+    ! enthalpy is h = e + p/rho
+    enthalpy = eint + pres/dens
 
-       ! entropy (per gram) of an ideal monoatomic gas (the Sactur-Tetrode equation)
-       ! NOTE: this expression is only valid for gamma = 5/3.
-       entropy(k) = (k_B/(mu(k)*m_nucleon))*(2.5_dp_t + &
-            log( ( (mu(k)*m_nucleon)**2.5/dens(k) )*(k_B*temp(k))**1.5_dp_t / (2.0_dp_t*M_PI*hbar*hbar)**1.5_dp_t ) )
+    ! entropy (per gram) of an ideal monoatomic gas (the Sactur-Tetrode equation)
+    ! NOTE: this expression is only valid for gamma = 5/3.
+    entropy = (k_B/(mu*m_nucleon))*(2.5_dp_t + &
+         log( ( (mu*m_nucleon)**2.5/dens )*(k_B*temp)**1.5_dp_t / (2.0_dp_t*M_PI*hbar*hbar)**1.5_dp_t ) )
 
-       ! compute the thermodynamic derivatives and specific heats 
-       dPdT(k) = pres(k)/temp(k)
-       dPdR(k) = pres(k)/dens(k)
-       dedT(k) = eint(k)/temp(k)
-       dedR(k) = 0.d0
-       dsdT(k) = 0.d0
-       dsdR(k) = 0.d0
+    ! compute the thermodynamic derivatives and specific heats 
+    dPdT = pres/temp
+    dPdR = pres/dens
+    dedT = eint/temp
+    dedR = 0.d0
+    dsdT = 0.d0
+    dsdR = 0.d0
 
-       c_v(k) = dedT(k)
-       c_p(k) = gamma_const*c_v(k)
+    c_v = dedT
+    c_p = gamma_const*c_v
 
-       gam1(k) = gamma_const
+    gam1 = gamma_const
 
-       do n = 1, nspec
+    do n = 1, nspec
 
-          ! the species only come into p and e (and therefore h)
-          ! through mu, so first compute dmu/dX
-          !
-          ! NOTE: an extra, constant term appears in dmudx, which
-          ! results from writing mu = sum { X_k} / sum {X_k / A_k}
-          ! (for the neutral, analogous for the ionized).  The
-          ! numerator is simply 1, but we can differentiate
-          ! wrt it, giving the constant mu(k) term in dmudx.  Since
-          ! dPdX only appears in a sum over species creation rate 
-          ! (omegadot) and sum{omegadot} = 0, this term has no effect.
-          ! If is added simply for completeness.
+       ! the species only come into p and e (and therefore h)
+       ! through mu, so first compute dmu/dX
+       !
+       ! NOTE: an extra, constant term appears in dmudx, which
+       ! results from writing mu = sum { X_k} / sum {X_k / A_k}
+       ! (for the neutral, analogous for the ionized).  The
+       ! numerator is simply 1, but we can differentiate
+       ! wrt it, giving the constant mu(k) term in dmudx.  Since
+       ! dPdX only appears in a sum over species creation rate 
+       ! (omegadot) and sum{omegadot} = 0, this term has no effect.
+       ! If is added simply for completeness.
 
-          if (eos_assume_neutral) then
-             dmudX =  (mu(k)/aion(n))*(aion(n) - mu(k))
-          else
-             dmudX =  (mu(k)/aion(n))*(aion(n) - mu(k)*(1.0d0 + zion(n)))
-          endif
+       if (eos_assume_neutral) then
+          dmudX =  (mu/aion(n))*(aion(n) - mu)
+       else
+          dmudX =  (mu/aion(n))*(aion(n) - mu*(1.0d0 + zion(n)))
+       endif
 
-          dPdX(k,n) = -(pres(k)/mu(k))*dmudX
-          dedX(k,n) = -(eint(k)/mu(k))*dmudX
+       dPdX(n) = -(pres/mu)*dmudX
+       dedX(n) = -(eint/mu)*dmudX
           
-          ! dhdX is at constant pressure -- see paper III for details
-          dhdX(k,n) = dedX(k,n) + &
-               (pres(k)/dens(k)**2 - dedR(k))*dPdX(k,n)/dPdr(k)
-       enddo
-
-       ! electron-specific stuff (really for the degenerate EOS)
-       ne(k) = 0.d0
-       eta(k) = 0.d0
-       pele(k) = 0.d0
-
-       ! sound speed
-       cs(k) = sqrt(gamma_const*pres(k)/dens(k))
-
+       ! dhdX is at constant pressure -- see paper III for details
+       dhdX(n) = dedX(n) + &
+            (pres/dens**2 - dedR)*dPdX(n)/dPdr
     enddo
+
+    ! electron-specific stuff (really for the degenerate EOS)
+    ne = 0.d0
+    eta = 0.d0
+    pele = 0.d0
+
+    ! sound speed
+    cs = sqrt(gamma_const*pres/dens)
 
     return
   end subroutine eos
