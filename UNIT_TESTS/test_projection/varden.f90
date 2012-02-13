d9b49de5868b0ebcb2f292a24393804f60763f44
mzingale <mzingale>
2012-02-13 00:01:05 +0000

    full stub version that seems to initialize all of the necessary things

--- UNIT_TESTS/test_projection/varden.f90
+++ UNIT_TESTS/test_projection/varden.f90
@@ -9,8 +9,12 @@ subroutine varden()
   use ml_layout_module, only: ml_layout
   use ml_boxarray_module, only: ml_boxarray
   use runtime_init_module, only: runtime_init, runtime_close
-  use probin_module, only: test_set, pmask, max_levs
-  use geometry, only: spherical, init_spherical, init_center, destroy_geometry
+  use probin_module, only: test_set, pmask, max_levs, &
+       prob_lo, prob_hi, drdxfac
+  use geometry, only: spherical, init_spherical, &
+       init_center, destroy_geometry, &
+       dr_fine, nr_fine, &
+       init_cutoff, init_radial, init_multilevel
   use box_util_module, only: read_a_hgproj_grid
   use initialize_module, only: initialize_bc, initialize_dx
   use bl_IO_module
@@ -19,7 +23,7 @@ subroutine varden()
 
   implicit none
 
-  integer :: i, n, r, comp, nlevs, dm
+  integer :: i, n, nlevs, dm
   integer :: ng_s
 
   type(ml_boxarray) :: mba
@@ -31,6 +35,8 @@ subroutine varden()
 
   real(kind=dp_t), pointer :: dx(:,:)
 
+  real(kind=dp_t) :: lenx,leny,lenz,max_dist
+
   real(kind=dp_t) :: time
 
   real(dp_t), parameter :: SMALL = 1.d-13
@@ -38,121 +44,192 @@ subroutine varden()
   character (len=20), allocatable :: plot_names(:)
 
 
-
+  !---------------------------------------------------------------------------
   ! initialize some things
+  !---------------------------------------------------------------------------
+
+  ! this reads in the inputs file and sets the values of the parameters
+  ! in the probin_module
   call runtime_init()
-  call init_spherical()
+
+  ! set spherical in the geometry module
+  call init_spherical()     
+
+  ! set the center() array in geometry with the coordinates
+  ! of the center of the domain
   call init_center()
 
+  ! initialize the integer keys used for variable names
   call init_variables()
 
 
   time = ZERO
 
 
- ! we only use fixed grids
- if (test_set /= '') then
-    call read_a_hgproj_grid(mba, test_set)
-    call ml_layout_build(mla,mba,pmask)
- else
-    call bl_error('test_projection only implemented with fixed grids!')
- endif
+  !---------------------------------------------------------------------------
+  ! setup the grid
+  !---------------------------------------------------------------------------
 
- dm = mla%dim
- nlevs = mla%nlevel
-
-
- ! initialize dx
- call initialize_dx(dx,mba,nlevs)
+  ! we only use fixed grids
+  if (test_set /= '') then
+     call read_a_hgproj_grid(mba, test_set)
+     call ml_layout_build(mla,mba,pmask)
+  else
+     call bl_error('test_projection only implemented with fixed grids!')
+  endif
 
 
- ! sanity checks
- if (nlevs .ne. max_levs) then
-    call bl_error('varden.f90: nlevs .ne. max_levs not supported yet')
- end if
+  ! check for proper nesting
+  if (.not. ml_boxarray_properly_nested(mla%mba, 3, pmask)) then
+     call bl_error('fixed_grids not properly nested')
+  end if
 
- if (dm .ne. get_dim(mla%mba) .or. dm .ne. 2) then
-    call bl_error('dm_in not properly set in inputs file')
- endif
-
- if (spherical .eq. 1 ) then
-    call bl_error('spherical = 1 not supported')
- endif
-
- if (abs(dx(1,1) - dx(1,2)) > SMALL) then
-    call bl_error('zones must be square')
- endif
+  
+  dm = mla%dim
+  nlevs = mla%nlevel
 
 
-
- ! initialize boundary conditions
- call initialize_bc(the_bc_tower,nlevs,pmask)
- do n = 1,nlevs
-    call bc_tower_level_build(the_bc_tower,n,mla%la(n))
- end do
+  ! initialize dx
+  call initialize_dx(dx,mba,nlevs)
 
 
- ! allocate arrays
- allocate(uold(nlevs), unew(nlevs))
-
- ng_s = 4
-
- do n = 1, nlevs
-    call build(uold(n),  mla%la(n), dm, ng_s)
-    call setval(uold(n), ZERO, all=.true.)
-
-    call build(unew(n),  mla%la(n), dm, ng_s)
-    call setval(unew(n), ZERO, all=.true.)
- enddo
+  ! initialize boundary conditions
+  call initialize_bc(the_bc_tower,nlevs,pmask)
+  do n = 1,nlevs
+     call bc_tower_level_build(the_bc_tower,n,mla%la(n))
+  end do
 
 
- ! some stats
- if (parallel_IOProcessor()) then
-    print *, 'number of processors = ', parallel_nprocs()
-    print *, 'number of dimensions = ', dm
-    do n = 1, nlevs
-       print *, 'level: ', n
-       print *, '   number of boxes = ', uold(n)%nboxes
-       print *, '   maximum zones   = ', (extent(mla%mba%pd(n),i),i=1,dm)
-    end do
-    print *, ''
- end if
+  !---------------------------------------------------------------------------
+  ! sanity checks
+  !---------------------------------------------------------------------------
+  if (nlevs .ne. max_levs) then
+     call bl_error('varden.f90: nlevs .ne. max_levs not supported yet')
+  end if
+
+  if (dm .ne. get_dim(mla%mba) .or. dm .ne. 2) then
+     call bl_error('dm_in not properly set in inputs file')
+  endif
+  
+  if (spherical .eq. 1 ) then
+     call bl_error('spherical = 1 not supported')
+  endif
+  
+  if (abs(dx(1,1) - dx(1,2)) > SMALL) then
+     call bl_error('zones must be square')
+  endif
 
 
- ! plotnames
- allocate(plot_names(dm))
- plot_names(1) = "x-velocity"
- if (dm >= 2) plot_names(2) = "y-velocity"
- if (dm == 3) plot_names(3) = "z-velocity"
+  !---------------------------------------------------------------------------
+  ! allocate arrays
+  !---------------------------------------------------------------------------
+  allocate(uold(nlevs), unew(nlevs))
+
+  ng_s = 4
+
+  do n = 1, nlevs
+     call build(uold(n),  mla%la(n), dm, ng_s)
+     call setval(uold(n), ZERO, all=.true.)
+     
+     call build(unew(n),  mla%la(n), dm, ng_s)
+     call setval(unew(n), ZERO, all=.true.)
+  enddo
+  
+
+  ! some stats
+  if (parallel_IOProcessor()) then
+     print *, 'number of processors = ', parallel_nprocs()
+     print *, 'number of dimensions = ', dm
+     do n = 1, nlevs
+        print *, 'level: ', n
+        print *, '   number of boxes = ', uold(n)%nboxes
+        print *, '   maximum zones   = ', (extent(mla%mba%pd(n),i),i=1,dm)
+     end do
+     print *, ''
+  end if
 
 
- ! initialize velocity
+  !---------------------------------------------------------------------------
+  ! some multilevel base state initialization
+  !---------------------------------------------------------------------------
+
+  ! setup some base state coordinate information: nr_fine and dr_fine in
+  ! geometry
+  if (spherical .eq. 1) then
+
+     ! for spherical, we will now require that dr_fine = dx
+     dr_fine = dx(nlevs,1) / dble(drdxfac)
+
+     lenx = HALF * (prob_hi(1) - prob_lo(1))
+     leny = HALF * (prob_hi(2) - prob_lo(2))
+     lenz = HALF * (prob_hi(3) - prob_lo(3))
+     
+     max_dist = sqrt(lenx**2 + leny**2 + lenz**2)
+     nr_fine = int(max_dist / dr_fine) + 1
+
+  else
+
+     nr_fine = extent(mla%mba%pd(nlevs),dm)
+     dr_fine = (prob_hi(dm)-prob_lo(dm)) / dble(nr_fine)
+
+  end if
 
 
- ! fill ghostcells
+  ! create numdisjointchunks, r_start_coord, r_end_coord
+  call init_multilevel(uold)
 
 
- ! dump the initial data
- call fabio_ml_write(uold, mla%mba%rr(:,1), "u_init", &
-                     names=plot_names)
+  ! now that we have nr_fine and dr_fine we can create nr, dr,
+  ! r_cc_loc, r_edge_loc
+  call init_radial(nlevs,mba)
 
 
- ! clean-up
- do n = 1, nlevs
-    call destroy(uold(n))
-    call destroy(unew(n))
- enddo
+  ! allocate the cutoff coordinate arrays
+  call init_cutoff(nlevs)
 
- call destroy(mla)
- call destroy(mba)
 
- deallocate(uold, unew)
+  !---------------------------------------------------------------------------
+  ! initialize velocity field
+  !---------------------------------------------------------------------------
 
- call bc_tower_destroy(the_bc_tower)
 
- call runtime_close()
 
- call destroy_geometry()
+  ! fill ghostcells
+
+
+  !---------------------------------------------------------------------------
+  ! dump out the initial data
+  !---------------------------------------------------------------------------
+
+  ! plotnames
+  allocate(plot_names(dm))
+  plot_names(1) = "x-velocity"
+  if (dm >= 2) plot_names(2) = "y-velocity"
+  if (dm == 3) plot_names(3) = "z-velocity"
+
+  call fabio_ml_write(uold, mla%mba%rr(:,1), "u_init", names=plot_names)
+
+
+  !---------------------------------------------------------------------------
+  ! clean-up
+  !---------------------------------------------------------------------------
+
+  do n = 1, nlevs
+     call destroy(uold(n))
+     call destroy(unew(n))
+  enddo
+
+  call destroy(mla)
+  call destroy(mba)
+  
+  deallocate(uold, unew)
+  deallocate(plot_names)
+  
+  call bc_tower_destroy(the_bc_tower)
+  
+  call runtime_close()
+  
+  call destroy_geometry()
 
 end subroutine varden
 
