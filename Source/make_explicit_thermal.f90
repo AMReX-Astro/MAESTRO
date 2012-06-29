4fa2453de9a1b0accc99fbd80eea2a1e420fdd88
Michael Zingale <mzingale@mail.astro.sunysb.edu>
2012-06-28 21:09:43 -0400

    EOS interface change

--- Source/make_explicit_thermal.f90
+++ Source/make_explicit_thermal.f90
@@ -322,7 +322,8 @@ contains
                                     Xkcoeff,ng_X,pcoeff,ng_p)
 
     use variables, only: rho_comp, temp_comp, spec_comp
-    use eos_module
+    use eos_module, only: eos_input_rt, eos
+    use eos_type_module
     use conductivity_module
     use network, only: nspec
 
@@ -335,33 +336,28 @@ contains
     
     ! local
     integer :: i,comp    
-    
+    type (eos_t) :: eos_state
+    real (kind=dp_t) :: conductivity
+
+
     do i=lo(1)-1,hi(1)+1
           
-       den_eos = s(i,rho_comp)
-       temp_eos = s(i,temp_comp)
-       xn_eos(:) = s(i,spec_comp:spec_comp+nspec-1)/den_eos
+       eos_state%rho   = s(i,rho_comp)
+       eos_state%T     = s(i,temp_comp)
+       eos_state%xn(:) = s(i,spec_comp:spec_comp+nspec-1)/eos_state%rho
        
        ! dens, temp, and xmass are inputs
-       call conducteos(eos_input_rt, den_eos, temp_eos, &
-                       nspec, &
-                       xn_eos, &
-                       p_eos, h_eos, e_eos, & 
-                       cv_eos, cp_eos, xne_eos, eta_eos, pele_eos, &
-                       dpdt_eos, dpdr_eos, dedt_eos, dedr_eos, &
-                       dpdX_eos, dhdX_eos, &
-                       gam1_eos, cs_eos, s_eos, &
-                       dsdt_eos, dsdr_eos, &
-                       .false., conduct_eos)
+       call conducteos(eos_input_rt, eos_state, .false., conductivity)
        
-       Tcoeff(i) = -conduct_eos
-       hcoeff(i) = -conduct_eos/cp_eos
-       pcoeff(i) = (conduct_eos/cp_eos)* &
-            ((1.0d0/den_eos)* &
-            (1.0d0-p_eos/(den_eos*dpdr_eos))+dedr_eos/dpdr_eos)
+       Tcoeff(i) = -conductivity
+       hcoeff(i) = -conductivity/eos_state%cp
+       pcoeff(i) = (conductivity/eos_state%cp)* &
+            ((1.0d0/eos_state%rho)* &
+            (1.0d0-eos_state%p/(eos_state%rho*eos_state%dpdr))+ &
+             eos_state%dedr/eos_state%dpdr)
        
        do comp=1,nspec
-          Xkcoeff(i,comp) = (conduct_eos/cp_eos)*dhdX_eos(comp)
+          Xkcoeff(i,comp) = (conductivity/eos_state%cp)*eos_state%dhdX(comp)
        enddo
 
     enddo
@@ -379,7 +375,8 @@ contains
                                     Xkcoeff,ng_X,pcoeff,ng_p)
 
     use variables, only: rho_comp, temp_comp, spec_comp
-    use eos_module
+    use eos_module, only: eos_input_rt, eos
+    use eos_type_module
     use conductivity_module
     use network, only: nspec
 
@@ -392,34 +389,28 @@ contains
     
     ! local
     integer :: i,j,comp    
+    type (eos_t) :: eos_state
+    real (kind=dp_t) :: conductivity
     
     do j=lo(2)-1,hi(2)+1
        do i=lo(1)-1,hi(1)+1
           
-          den_eos = s(i,j,rho_comp)
-          temp_eos = s(i,j,temp_comp)
-          xn_eos(:) = s(i,j,spec_comp:spec_comp+nspec-1)/den_eos
+          eos_state%rho   = s(i,j,rho_comp)
+          eos_state%T     = s(i,j,temp_comp)
+          eos_state%xn(:) = s(i,j,spec_comp:spec_comp+nspec-1)/eos_state%rho
           
           ! dens, temp, and xmass are inputs
-          call conducteos(eos_input_rt, den_eos, temp_eos, &
-                          nspec, &
-                          xn_eos, &
-                          p_eos, h_eos, e_eos, & 
-                          cv_eos, cp_eos, xne_eos, eta_eos, pele_eos, &
-                          dpdt_eos, dpdr_eos, dedt_eos, dedr_eos, &
-                          dpdX_eos, dhdX_eos, &
-                          gam1_eos, cs_eos, s_eos, &
-                          dsdt_eos, dsdr_eos, &
-                          .false., conduct_eos)
+          call conducteos(eos_input_rt, eos_state, .false., conductivity)
           
-          Tcoeff(i,j) = -conduct_eos
-          hcoeff(i,j) = -conduct_eos/cp_eos
-          pcoeff(i,j) = (conduct_eos/cp_eos)* &
-               ((1.0d0/den_eos)* &
-               (1.0d0-p_eos/(den_eos*dpdr_eos))+dedr_eos/dpdr_eos)
+          Tcoeff(i,j) = -conductivity
+          hcoeff(i,j) = -conductivity/eos_state%cp
+          pcoeff(i,j) = (conductivity/eos_state%cp)* &
+               ((1.0d0/eos_state%rho)* &
+               (1.0d0-eos_state%p/(eos_state%rho*eos_state%dpdr))+ &
+                eos_state%dedr/eos_state%dpdr)
           
           do comp=1,nspec
-             Xkcoeff(i,j,comp) = (conduct_eos/cp_eos)*dhdX_eos(comp)
+             Xkcoeff(i,j,comp) = (conductivity/eos_state%cp)*eos_state%dhdX(comp)
           enddo
 
        enddo
@@ -438,7 +429,8 @@ contains
                                     Xkcoeff,ng_X,pcoeff,ng_p)
 
     use variables, only: rho_comp, temp_comp, spec_comp
-    use eos_module
+    use eos_module, only: eos_input_rt, eos
+    use eos_type_module
     use conductivity_module
     use network, only: nspec
     use fill_3d_module
@@ -452,36 +444,30 @@ contains
 
     ! local
     integer :: i,j,k,comp
+    type (eos_t) :: eos_state
+    real (kind=dp_t) :: conductivity
     
-    !$OMP PARALLEL DO PRIVATE(i,j,k,comp)
+    !$OMP PARALLEL DO PRIVATE(i,j,k,comp,eos_state,conductivity)
     do k=lo(3)-1,hi(3)+1
        do j=lo(2)-1,hi(2)+1
           do i=lo(1)-1,hi(1)+1
              
-             den_eos = s(i,j,k,rho_comp)
-             temp_eos = s(i,j,k,temp_comp)
-             xn_eos(:) = s(i,j,k,spec_comp:spec_comp+nspec-1)/den_eos
+             eos_state%rho   = s(i,j,k,rho_comp)
+             eos_state%T     = s(i,j,k,temp_comp)
+             eos_state%xn(:) = s(i,j,k,spec_comp:spec_comp+nspec-1)/eos_state%rho
              
              ! dens, temp, and xmass are inputs
-             call conducteos(eos_input_rt, den_eos, temp_eos, &
-                             nspec, &
-                             xn_eos, &
-                             p_eos, h_eos, e_eos, & 
-                             cv_eos, cp_eos, xne_eos, eta_eos, pele_eos, &
-                             dpdt_eos, dpdr_eos, dedt_eos, dedr_eos, &
-                             dpdX_eos, dhdX_eos, &
-                             gam1_eos, cs_eos, s_eos, &
-                             dsdt_eos, dsdr_eos, &
-                             .false., conduct_eos)
+             call conducteos(eos_input_rt, eos_state, .false., conductivity)
              
-             Tcoeff(i,j,k) = -conduct_eos
-             hcoeff(i,j,k) = -conduct_eos/cp_eos
-             pcoeff(i,j,k) = (conduct_eos/cp_eos)* &
-                  ((1.0d0/den_eos)* &
-                  (1.0d0-p_eos/(den_eos*dpdr_eos))+dedr_eos/dpdr_eos)
+             Tcoeff(i,j,k) = -conductivity
+             hcoeff(i,j,k) = -conductivity/eos_state%cp
+             pcoeff(i,j,k) = (conductivity/eos_state%cp)* &
+                  ((1.0d0/eos_state%rho)* &
+                  (1.0d0-eos_state%p/(eos_state%rho*eos_state%dpdr))+ &
+                   eos_state%dedr/eos_state%dpdr)
              
              do comp=1,nspec
-                Xkcoeff(i,j,k,comp) = (conduct_eos/cp_eos)*dhdX_eos(comp)
+                Xkcoeff(i,j,k,comp) = (conductivity/eos_state%cp)*eos_state%dhdX(comp)
              enddo
 
           enddo
