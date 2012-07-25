87f1a4e4d24aa65bf71510207872c1be86259067
Michael Zingale <zingale@bender.astro.sunysb.edu>
2012-07-25 14:58:08 -0400

    relax eps_hg to 1.d-11

--- SCIENCE/flame/mg_eps.f90
+++ SCIENCE/flame/mg_eps.f90
@@ -29,7 +29,7 @@ module mg_eps_module
 
 
   ! tolerances for the HG projection
-  real (kind=dp_t) :: eps_hg = 1.d-12
+  real (kind=dp_t) :: eps_hg = 1.d-11
   real (kind=dp_t) :: eps_hg_max = 1.d-10
 
   real (kind=dp_t) :: hg_level_factor = 10.d0
