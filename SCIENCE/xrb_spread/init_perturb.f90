db1b0c62af92add791365a5813b7729e6ca5b261
Michael Zingale <zingale@nan.astro.sunysb.edu>
2012-07-31 13:31:12 -0400

    update eos interface

--- SCIENCE/xrb_spread/init_perturb.f90
+++ SCIENCE/xrb_spread/init_perturb.f90
@@ -2,7 +2,8 @@ module init_perturb_module
 
   use variables
   use network, only: nspec
-  use eos_module
+  use eos_module, only: eos_input_tp, eos
+  use eos_type_module
   use bl_constants_module
 
   implicit none
@@ -31,6 +32,8 @@ contains
     real(kind=dp_t) :: temp,t0
     real(kind=dp_t) :: xmid, dist
 
+    type (eos_t) :: eos_state
+
     xmid = 0.5_dp_t * (prob_lo(1) + prob_hi(1))
 
     dist = sqrt((x - xmid)**2 + (y - xrb_pert_height)**2)
@@ -48,24 +51,16 @@ contains
           
     ! Use the EOS to make this temperature perturbation occur at
     ! constant pressure
-    temp_eos = temp
-    p_eos = p0_init
-    den_eos = s0_init(rho_comp)
-    xn_eos(:) = s0_init(spec_comp:spec_comp+nspec-1)/s0_init(rho_comp)
+    eos_state%T     = temp
+    eos_state%p     = p0_init
+    eos_state%rho   = s0_init(rho_comp)
+    eos_state%xn(:) = s0_init(spec_comp:spec_comp+nspec-1)/s0_init(rho_comp)
 
-    call eos(eos_input_tp, den_eos, temp_eos, &
-             xn_eos, &
-             p_eos, h_eos, e_eos, &
-             cv_eos, cp_eos, xne_eos, eta_eos, pele_eos, &
-             dpdt_eos, dpdr_eos, dedt_eos, dedr_eos, &
-             dpdX_eos, dhdX_eos, &
-             gam1_eos, cs_eos, s_eos, &
-             dsdt_eos, dsdr_eos, &
-             .false.)
+    call eos(eos_input_tp, eos_state, .false.)
 
-    dens_pert = den_eos
-    rhoh_pert = den_eos*h_eos
-    rhoX_pert = dens_pert*xn_eos(:)
+    dens_pert = eos_state%rho
+    rhoh_pert = eos_state%rho * eos_state%h
+    rhoX_pert = dens_pert*eos_state%xn(:)
 
     temp_pert = temp
 
@@ -106,6 +101,8 @@ contains
     real(kind=dp_t) :: temp, t0
     real(kind=dp_t) :: x0, y0, z0, r0
 
+    type (eos_t) :: eos_state
+
     t0 = s0_init(temp_comp)
 
     ! center of star is at 2.5d8
@@ -121,24 +118,16 @@ contains
 
     ! Use the EOS to make this temperature perturbation occur at constant 
     ! pressure
-    temp_eos = temp
-    p_eos = p0_init
-    den_eos = s0_init(rho_comp)
-    xn_eos(:) = s0_init(spec_comp:spec_comp+nspec-1)/s0_init(rho_comp)
+    eos_state%T     = temp
+    eos_state%p     = p0_init
+    eos_state%rho   = s0_init(rho_comp)
+    eos_state%xn(:) = s0_init(spec_comp:spec_comp+nspec-1)/s0_init(rho_comp)
 
-    call eos(eos_input_tp, den_eos, temp_eos, &
-             xn_eos, &
-             p_eos, h_eos, e_eos, &
-             cv_eos, cp_eos, xne_eos, eta_eos, pele_eos, &
-             dpdt_eos, dpdr_eos, dedt_eos, dedr_eos, &
-             dpdX_eos, dhdX_eos, &
-             gam1_eos, cs_eos, s_eos, &
-             dsdt_eos, dsdr_eos, &
-             .false.)
+    call eos(eos_input_tp, eos_state, .false.)
 
-    dens_pert = den_eos
-    rhoh_pert = den_eos*h_eos
-    rhoX_pert = dens_pert*xn_eos(:)
+    dens_pert = eos_state%rho
+    rhoh_pert = eos_state%rho * eos_state%h
+    rhoX_pert = dens_pert*eos_state%xn(:)
 
     temp_pert = temp
 
