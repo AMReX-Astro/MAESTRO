0f180af97d7894a7bd0e4805ea924b819b75ca6b
Michael Zingale <zingale@localhost.localdomain>
2012-08-31 11:09:26 -0400

    remove the old eos interface variables from the default eos module.  Now you need to explicitly include eos_old_interface to get them

--- Microphysics/EOS/GPackage.mak
+++ Microphysics/EOS/GPackage.mak
@@ -1,1 +1,2 @@
 f90sources += eos_type.f90
+f90sources += eos_old_type.f90
--- Microphysics/EOS/gamma_law_general/eos_stuff.f90
+++ Microphysics/EOS/gamma_law_general/eos_stuff.f90
@@ -37,41 +37,6 @@ module eos_module
 
   private
 
-  real(kind=dp_t), public :: xn_eos(nspec)
-  real(kind=dp_t), public :: temp_eos
-  real(kind=dp_t), public :: den_eos
-  real(kind=dp_t), public :: abar_eos
-  real(kind=dp_t), public :: zbar_eos
-  real(kind=dp_t), public :: e_eos
-  real(kind=dp_t), public :: p_eos
-  real(kind=dp_t), public :: h_eos
-  real(kind=dp_t), public :: cv_eos
-  real(kind=dp_t), public :: cp_eos
-  real(kind=dp_t), public :: xne_eos
-  real(kind=dp_t), public :: eta_eos
-  real(kind=dp_t), public :: pele_eos
-  real(kind=dp_t), public :: dpdt_eos
-  real(kind=dp_t), public :: dpdr_eos
-  real(kind=dp_t), public :: dedr_eos
-  real(kind=dp_t), public :: dedt_eos
-  real(kind=dp_t), public :: gam1_eos
-  real(kind=dp_t), public ::   cs_eos
-  real(kind=dp_t), public ::    s_eos
-  real(kind=dp_t), public :: dsdt_eos
-  real(kind=dp_t), public :: dsdr_eos
-  real(kind=dp_t), public :: dpdX_eos(nspec)
-  real(kind=dp_t), public :: dhdX_eos(nspec)
-  real(kind=dp_t), public :: conduct_eos
-
-  integer, public         :: pt_index_eos(MAX_SPACEDIM)
-
-  common /eos_common/ xn_eos,temp_eos,den_eos,abar_eos,zbar_eos,e_eos,p_eos,h_eos
-  common /eos_common/ cv_eos,cp_eos,xne_eos,eta_eos,pele_eos,dpdt_eos,dpdr_eos,dedr_eos
-  common /eos_common/ dedt_eos,gam1_eos,cs_eos,s_eos,dsdt_eos,dsdr_eos,dpdX_eos,dhdX_eos
-  common /eos_common/ conduct_eos,pt_index_eos
-  SAVE /eos_common/
-!$omp threadprivate(/eos_common/)
-
   integer, parameter, public :: eos_input_rt = 1   ! density, temperature are inputs
   integer, parameter, public :: eos_input_rh = 2   ! density, enthalpy are inputs
   integer, parameter, public :: eos_input_tp = 3   ! temperature, pressure are inputs
