93dab086f1744c7b3aee065586de1b98b2f5e0bc
ajnonaka <AJNonaka@lbl.gov>
2013-01-03 08:06:53 -0800

    sync this up with the VARDEN version of mac_applyop.  This will very soon be moved into BoxLib/Src/LinearSolvers/F_MG since the only two existing copies are identical.

--- Source/mac_applyop.f90
+++ Source/mac_applyop.f90
@@ -14,22 +14,22 @@ module mac_applyop_module
 
 contains
 
-  subroutine mac_applyop(mla,res,phi,alpha,beta,dx,the_bc_tower,bc_comp,stencil_order,ref_ratio)
+  subroutine mac_applyop(mla,res,phi,alpha,beta,dx, &
+                         the_bc_tower,bc_comp,stencil_order,ref_ratio)
 
-    use mg_module
+    use mg_module             , only: mg_tower, mg_tower_build, mg_tower_destroy
     use cc_stencil_fill_module, only: stencil_fill_cc
-    use cc_applyop_module, only: ml_cc_applyop
+    use cc_applyop_module     , only: ml_cc_applyop
     use stencil_types_module
-    use probin_module, only: cg_verbose, mg_verbose
 
-    type(ml_layout), intent(inout) :: mla
+    type(ml_layout), intent(in   ) :: mla
+    type(multifab) , intent(inout) :: res(:), phi(:)
+    type(multifab) , intent(in   ) :: alpha(:), beta(:,:)
+    real(dp_t)     , intent(in   ) :: dx(:,:)
+    type(bc_tower) , intent(in   ) :: the_bc_tower
+    integer        , intent(in   ) :: bc_comp
     integer        , intent(in   ) :: stencil_order
     integer        , intent(in   ) :: ref_ratio(:,:)
-    real(dp_t)     , intent(in)    :: dx(:,:)
-    type(bc_tower) , intent(in)    :: the_bc_tower
-    integer        , intent(in   ) :: bc_comp
-    type(multifab) , intent(in   ) :: alpha(:), beta(:,:)
-    type(multifab) , intent(inout) :: res(:), phi(:)
 
     type(layout  ) :: la
     type(box     ) :: pd
@@ -38,16 +38,10 @@ contains
     type(multifab) :: edge_coeffs(mla%dim)
 
     type(mg_tower)  :: mgt(mla%nlevel)
-    integer         :: ns,dm,nlevs
-    integer         :: stencil_type
+    integer         :: d, dm, nlevs
 
     ! MG solver defaults
-    integer    :: bottom_solver, bottom_max_iter
-    integer    :: max_iter
-    integer    :: min_width
-    integer    :: max_nlevel
-    integer    :: n, d, nu1, nu2, gamma, ncycle, smoother
-    real(dp_t) :: eps,abs_eps,omega,bottom_solver_eps
+    integer    :: n, ns
     real(dp_t) ::  xa(mla%dim),  xb(mla%dim)
     real(dp_t) :: pxa(mla%dim), pxb(mla%dim)
 
@@ -58,24 +52,7 @@ contains
     dm = mla%dim
     nlevs = mla%nlevel
 
-    max_nlevel        = mgt(nlevs)%max_nlevel
-    max_iter          = mgt(nlevs)%max_iter
-    eps               = mgt(nlevs)%eps
-    abs_eps           = mgt(nlevs)%abs_eps
-    smoother          = mgt(nlevs)%smoother
-    nu1               = mgt(nlevs)%nu1
-    nu2               = mgt(nlevs)%nu2
-    gamma             = mgt(nlevs)%gamma
-    omega             = mgt(nlevs)%omega
-    ncycle            = mgt(nlevs)%cycle_type
-    bottom_solver     = mgt(nlevs)%bottom_solver
-    bottom_solver_eps = mgt(nlevs)%bottom_solver_eps
-    bottom_max_iter   = mgt(nlevs)%bottom_max_iter
-    min_width         = mgt(nlevs)%min_width
-
     ns = 1 + dm*3
-
-    stencil_type = CC_CROSS_STENCIL
 
     do n = nlevs, 1, -1
 
@@ -83,25 +60,9 @@ contains
 
        call mg_tower_build(mgt(n), mla%la(n), pd, &
                            the_bc_tower%bc_tower_array(n)%ell_bc_level_array(0,:,:,bc_comp),&
-                           stencil_type, &
+                           stencil_type_in = CC_CROSS_STENCIL, &
                            dh = dx(n,:), &
                            ns = ns, &
-                           smoother = smoother, &
-                           nu1 = nu1, &
-                           nu2 = nu2, &
-                           gamma = gamma, &
-                           cycle_type = ncycle, &
-                           omega = omega, &
-                           bottom_solver = bottom_solver, &
-                           bottom_max_iter = bottom_max_iter, &
-                           bottom_solver_eps = bottom_solver_eps, &
-                           max_iter = max_iter, &
-                           max_nlevel = 1, &
-                           min_width = min_width, &
-                           eps = eps, &
-                           abs_eps = abs_eps, &
-                           verbose = mg_verbose, &
-                           cg_verbose = cg_verbose, &
                            nodal = nodal_flags(res(nlevs)))
 
     end do
@@ -134,7 +95,6 @@ contains
        call stencil_fill_cc(mgt(n)%ss(mgt(n)%nlevels), cell_coeffs, edge_coeffs, mgt(n)%dh(:,mgt(n)%nlevels), &
                             mgt(n)%mm(mgt(n)%nlevels), xa, xb, pxa, pxb, stencil_order, &
                             the_bc_tower%bc_tower_array(n)%ell_bc_level_array(0,:,:,bc_comp))
-
 
        call destroy(cell_coeffs)
 
