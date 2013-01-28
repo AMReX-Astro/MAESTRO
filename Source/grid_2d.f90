e72ce3e1ac5e7b3dede29b815c6e5cfc57298466
Michael Zingale <zingale@localhost.localdomain>
2013-01-28 09:10:42 -0500

    add optional y offset

--- Source/grid_2d.f90
+++ Source/grid_2d.f90
@@ -2,10 +2,11 @@ program grid_2d
 
   integer :: i, j
 
-  integer, parameter :: nx = 10
-  integer, parameter :: ny = 10
-  integer, parameter :: nzonesx = 1280
-  integer, parameter :: nzonesy = 1280
+  integer, parameter :: nx = 4
+  integer, parameter :: ny = 12
+  integer, parameter :: nzonesx = 128
+  integer, parameter :: nzonesy = 384
+  integer, parameter :: iyoff = 0
 
   integer :: ix, iy
   integer :: nlevs, ngrids
@@ -25,7 +26,7 @@ program grid_2d
      iy = 0
      do j = 1, ny
 
-        write (*,100) ix,iy,ix+nzonesx/nx-1,iy+nzonesy/ny-1,0,0
+        write (*,100) ix,iy+iyoff,ix+nzonesx/nx-1,iy+nzonesy/ny-1+iyoff,0,0
 
         iy = iy + nzonesy/ny
      enddo
