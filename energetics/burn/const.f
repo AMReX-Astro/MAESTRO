4c1a1423ffef467f50f203915eaf13b004ed92f1
Michael Zingale <zingale@inf.astro.sunysb.edu>
2013-10-31 11:40:00 -0400

    stan's isobaric burner for comparing the energetics

---
+++ energetics/burn/const.f
@@ -0,0 +1,13 @@
+      REAL*8 OneThird,TwoThird,OneSixths,FiveSixths
+
+      PARAMETER (
+     1     OneThird  =0.333333333333333333333333333333333333333D0,
+     2     TwoThird  =0.666666666666666666666666666666666666667D0,
+     3     OneSixths =0.166666666666666666666666666666666666667D0,
+     4     FiveSixths=0.833333333333333333333333333333333333333D0
+     5     )
+      
+      REAL*8 MinusOneThird
+      PARAMETER (
+     1     MinusOneThird=-OneThird
+     2     )
