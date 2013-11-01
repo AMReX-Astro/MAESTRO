54d879022e2f78fdea50e91dcbb61191c212474e
Chris Malone <malone@ucolick.org>
2013-11-01 11:46:17 -0700

    some simple scripts for evolution plots

---
+++ analysis/plotDiag.py
@@ -0,0 +1,38 @@
+import matplotlib.pyplot as plt
+import numpy
+import glob
+
+labelDict={'max{enuc}': r"$\epsilon_{\rm max}$ (erg g$^{-1}$ s$^{-1}$",
+           'max{T}': r"$T_{\rm max}$ (K)",
+           'max{Machno}': r"$M_{\rm max}$"}
+
+fns = glob.glob("xrb*diag.out")
+names = [fn.split('_')[1] for fn in fns]
+
+for ifn,fn in enumerate(fns):
+    print "working on %s" % fn
+    with open(fn) as fh:
+        for i,line in enumerate(fh):
+            if i == 5: # skip the header info and get the labels
+                cols = line.split()[1:]
+                indx = [i for i in range(len(cols)) if cols[i].find("loc") < 0]
+                cols = [c for c in cols if c.find("loc") < 0]
+
+    print "Found these columns: %s" % ' '.join(cols)
+
+    assert len(indx) == len(cols), \
+        "indx and cols don't match: %s\n%s" % (' '.join(indx), ' '.join(cols))
+    nplts = len(indx)
+    dat = numpy.loadtxt(fn,usecols=indx)
+    for iplt in range(1,nplts):
+        # only plot the quantities in labelDict
+        if labelDict.has_key(cols[iplt]):
+            plt.plot(dat[:,0],dat[:,iplt])
+            ax = plt.gca()
+            ax.set_xlabel(cols[0])
+            ax.set_ylabel(labelDict[cols[iplt]])
+            plt.savefig("%s_%s.png" % (names[ifn],
+                                       cols[iplt].replace('{','_').strip('}')))
+            plt.clf()
+            plt.cla()
+
---
+++ analysis/specMass.py
@@ -0,0 +1,58 @@
+import numpy
+import matplotlib
+matplotlib.rcParams['font.size'] =  18
+matplotlib.rcParams["lines.linewidth"] = 2
+matplotlib.rcParams["legend.columnspacing"] = 0.5
+matplotlib.rcParams["legend.labelspacing"] = 0.1
+matplotlib.rcParams["legend.fontsize"] = "medium"
+import matplotlib.pyplot as plt
+import subprocess
+import glob
+import StringIO
+import sys
+
+datFile = "masses.txt"
+lts = ['b-','g-','r-','c-','m-',
+       'b--','g--','r--','c--','m--']
+
+justPlot = False
+try:
+    justPlot = sys.argv[1] == '--just-plot'
+except:
+    pass
+
+if justPlot:
+    print "Just doing the plots"
+    masses = numpy.loadtxt(datFile)
+    labels = open(datFile,'r').readline().split()[1:]
+else:
+    fns = glob.glob("plt*")
+    fns.sort()
+
+    fmass = "./fspec_total_mass.Linux.gfortran.exe"
+    cmd = "%s %s" % (fmass, ' '.join(fns))
+
+    mass = subprocess.Popen(cmd,shell=True,
+                            stdout=subprocess.PIPE).communicate()[0]
+    labels = mass.split('\n')[0].split()[1:]
+
+    fh = StringIO.StringIO(mass)
+    masses = numpy.loadtxt(fh)
+    numpy.savetxt(datFile,masses,header=' '.join(labels))
+
+# normalize to starting value
+print 'Normalizing...'
+masses[:,1:] /= masses[0,1:]
+
+print "Plotting..."
+for i,curve in enumerate(labels[1:]):
+    plt.plot(masses[:,0],masses[:,i+1],lts[i],label=curve)
+
+ax = plt.gca()
+ax.semilogy()
+ax.set_xlabel(r"$t$ (s)")
+ax.set_ylabel(r"$M_X/M_{X,0}$")
+plt.legend(loc="upper center",ncol=4)
+
+plt.savefig("masses.png",bbox_inches='tight')
+plt.savefig("masses.pdf",bbox_inches='tight')
