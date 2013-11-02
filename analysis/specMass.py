5570550aa16c25fc6d9c7f2d81c8526428d01016
Chris Malone <malone@ucolick.org>
2013-11-02 14:30:32 -0700

    some new options to reuse previous data

--- analysis/specMass.py
+++ analysis/specMass.py
@@ -20,32 +20,50 @@ def fnCmp(fn1,fn2):
     return cmp(fn1,fn2)
 
 datFile = "masses.txt"
+fmass = "./fspec_total_mass.Linux.gfortran.exe"
+
 lts = ['b-','g-','r-','c-','m-',
        'b--','g--','r--','c--','m--']
 
 justPlot = False
-try:
-    justPlot = sys.argv[1] == '--just-plot'
-except:
-    pass
+append = False
+every = 1
+
+justPlot = '--just-plot' in sys.argv
+append = '--append' in sys.argv
+everyFlag = "--every" in sys.argv
+if everyFlag:
+    every = int(sys.argv[sys.argv.index("--every")+1])
+    print "Only summing every %s files" % every
 
 if justPlot:
     print "Just doing the plots"
     masses = numpy.loadtxt(datFile)
     labels = open(datFile,'r').readline().split()[1:]
+    # make sure this is the label list; might be second header line
+    if "time" not in labels:
+        labels = open(datFile,'r').readline().split()[1:]
+    assert "time" in labels, "Can't find headers!"
 else:
     fns = glob.glob("plt*")
     fns.sort(cmp=fnCmp)
+    fns = fns[::every]
+    print "Working these files:\n %s" % ' '.join(fns)
 
-    fmass = "./fspec_total_mass.Linux.gfortran.exe"
     cmd = "%s %s" % (fmass, ' '.join(fns))
 
     mass = subprocess.Popen(cmd,shell=True,
                             stdout=subprocess.PIPE).communicate()[0]
     labels = mass.split('\n')[0].split()[1:]
+    lastFile = "# last file used: %s\n" fns[-1]
+    labels = [lastFile,] + labels
 
     fh = StringIO.StringIO(mass)
     masses = numpy.loadtxt(fh)
+    if append:
+        print "Just appending the new values to the old %s file" % datFile
+        oldMasses = numpy.loadtxt(datFile)
+        masses = numpy.vstack((oldMasses,masses))
     numpy.savetxt(datFile,masses,header=' '.join(labels))
 
 # normalize to starting value
