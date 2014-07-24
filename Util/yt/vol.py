bdc2b0352339f3c5d08970723cfa276fa2833b83
Michael Zingale <michael.zingale@stonybrook.edu>
2014-07-23 22:45:35 -0400

    update the script for the latest version of yt -- in particular, we now get the handedness correct.

--- Util/yt/vol.py
+++ Util/yt/vol.py
@@ -17,6 +17,9 @@ def doit(plotfile, fname):
 
     ds = load(plotfile)
 
+    cm = "gist_rainbow"
+
+
     if fname == "vz":
         field = ('gas', 'velocity_z')
         use_log = False
@@ -26,10 +29,19 @@ def doit(plotfile, fname):
         
     elif fname == "magvel":
         field = ('gas', 'velocity_magnitude')
-        use_log = True
+        use_log = False
         
-        vals = [1.e5, 3.16e5, 1.e6, 3.16e6, 1.e7]
-        sigma = 0.1
+        vals = [1.e6, 2.e6, 4.e6, 8.e6, 1.6e7]
+        sigma = 2.e5
+
+    elif fname == "radvel":
+        field = ('boxlib', 'radial_velocity')
+        use_log = False
+        
+        vals = [-1.e7, -5.e6, -2.5e6, 2.5e6, 5.e6, 1.e7]
+        sigma = 2.e5
+        
+        cm = "coolwarm"
 
 
     dd = ds.h.all_data()
@@ -48,14 +60,13 @@ def doit(plotfile, fname):
     tf =  vr.ColorTransferFunction((mi, ma))
 
     # Set up the camera parameters: center, looking direction, width, resolution
-    c = (ds.domain_right_edge + ds.domain_left_edge)/2.0
-    L = np.array([-0.0, -1.0, -1.0])
-    W = 2.0*ds.domain_width
+    c = np.array([0.0, 0.0, 0.0])
+    L = np.array([1.0, 1.0, 1.0])
+    L = np.array([1.0, 1.0, 1.2])
+    W = 1.5*ds.domain_width
     N = 720
 
-    rot_vector=[0.0,0.0,1.0]
-
-    cm = "gist_rainbow"
+    north=[0.0,0.0,1.0]
 
     for v in vals:
         if (use_log):
@@ -64,40 +75,30 @@ def doit(plotfile, fname):
             tf.sample_colormap(v, sigma**2, colormap=cm) #, alpha=0.2)
 
 
-    # an attempt to get around the periodic assumption -- suggested by Nathan
-    #root_dds = .domain_width/pf.domain_dimensions
-    #half_w = pf.domain_width/2.# - root_dds
-    #half_w[2] -= root_dds[2]
-    #reg = pf.region(pf.domain_center, pf.domain_center-half_w, pf.domain_center+half_w)
-
-
     # alternate attempt
     ds.periodicity = (True, True, True)
 
     # Create a camera object
     cam = vr.Camera(c, L, W, N, transfer_function=tf, ds=ds, 
-                    no_ghost=False, #data_source=reg,
+                    no_ghost=False,
+                    north_vector=north,
                     fields = [field], log_fields = [use_log])
-
-    cam.rotate(2*np.pi/6., rot_vector=rot_vector)
 
     # make an image
     im = cam.snapshot()
 
-    # add an axes triad
-    cam.draw_coordinate_vectors(im)
 
-    # colorbar
-    cam.show_tf()
+    # add an axes triad -- note if we do this, we HAVE to do draw
+    # domain, otherwise the image is blank (likely a bug)
+    cam.draw_coordinate_vectors(im)
 
     # add the domain box to the image:
     nim = cam.draw_domain(im)
 
-    # increase the contrast
+    # increase the contrast -- for some reason, the enhance default
+    # to save_annotated doesn't do the trick (likely a bug)
     max_val = im[:,:,:3].std() * 4.0
     nim[:,:,:3] /= max_val
-
-    #nim.write_png("xrb_vol_test.png")
 
     f = pylab.figure()
 
@@ -108,7 +109,9 @@ def doit(plotfile, fname):
     
     # save annotated -- this added the transfer function values, 
     # but this messes up our image size defined above
-    cam.save_annotated("xrb_vol_{}.png".format(fname), nim, dpi=145, clear_fig=False)
+    cam.save_annotated("{}_{}.png".format(os.path.normpath(plotfile), fname), 
+                       nim, 
+                       dpi=145, clear_fig=False)
 
 
 
