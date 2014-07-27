ceb7556eecce2d6d62ad6369807e083df498f5aa
Michael Zingale <michael.zingale@stonybrook.edu>
2014-07-27 17:02:07 -0400

    this works with the latest yt 3.0 and does a better job with the orientation

--- analysis/yt/vol.py
+++ analysis/yt/vol.py
@@ -8,35 +8,37 @@ matplotlib.use('agg')
 
 import math
 import sys
-import os
+import pylab
 
 from yt.mods import *
-import yt.visualization.volume_rendering.api
-
+import yt.visualization.volume_rendering.api as vr
 
 def doit(plotfile, fname):
 
-    pf = load(plotfile)
+    ds = load(plotfile)
+
+    cm = "gist_rainbow"
+
 
     if fname == "vz":
         field = ('gas', 'velocity_z')
         use_log = False
-        
+
         vals = [-1.e7, -5.e6, 5.e6, 1.e7]
         sigma = 5.e5
+
+        cm = "coolwarm"
         
     elif fname == "magvel":
         field = ('gas', 'velocity_magnitude')
-        use_log = True
+        use_log = False
         
-        vals = [1.e5, 3.16e5, 1.e6, 3.16e6, 1.e7]
-        sigma = 0.1
+        vals = [1.e6, 2.e6, 4.e6, 8.e6, 1.6e7]
+        sigma = 2.e5
 
 
-    dd = pf.h.all_data()
 
-    pf.field_info[field].take_log = use_log
-
+    dd = ds.all_data()
 
     mi = min(vals)
     ma = max(vals)
@@ -46,17 +48,16 @@ def doit(plotfile, fname):
         
 
     # Instantiate the ColorTransferfunction.
-    tf =  yt.visualization.volume_rendering.api.ColorTransferFunction((mi, ma))
+    tf =  vr.ColorTransferFunction((mi, ma))
 
     # Set up the camera parameters: center, looking direction, width, resolution
-    c = (pf.domain_right_edge + pf.domain_left_edge)/2.0
-    L = np.array([-0.0, -1.0, -1.0])
-    W = 2.0*pf.domain_width
+    c = (ds.domain_right_edge + ds.domain_left_edge)/2.0
+    L = np.array([1.0, 1.0, 1.0])
+    L = np.array([1.0, 1.0, 1.2])
+    W = 2.0*ds.domain_width
     N = 720
 
-    rot_vector=[0.0,0.0,1.0]
-
-    cm = "gist_rainbow"
+    north=[0.0,0.0,1.0]
 
     for v in vals:
         if (use_log):
@@ -65,45 +66,43 @@ def doit(plotfile, fname):
             tf.sample_colormap(v, sigma**2, colormap=cm) #, alpha=0.2)
 
 
-    # an attempt to get around the periodic assumption -- suggested by Nathan
-    #root_dds = pf.domain_width/pf.domain_dimensions
-    #half_w = pf.domain_width/2.# - root_dds
-    #half_w[2] -= root_dds[2]
-    #reg = pf.region(pf.domain_center, pf.domain_center-half_w, pf.domain_center+half_w)
-
-
     # alternate attempt
-    pf.periodicity = (True, True, True)
+    ds.periodicity = (True, True, True)
 
     # Create a camera object
-    cam = pf.h.camera(c, L, W, N, tf, 
-                      no_ghost=False, #data_source=reg,
-                      fields = [field], log_fields = [use_log])
-
-    cam.rotate(2*np.pi/6, rot_vector=rot_vector)
+    cam = vr.Camera(c, L, W, N, transfer_function=tf, ds=ds, 
+                    no_ghost=False,
+                    north_vector=north,
+                    fields = [field], log_fields = [use_log])
 
     # make an image
     im = cam.snapshot()
 
-    # add an axes triad
-    cam.draw_coordinate_vectors(im)
 
-    # colorbar
-    cam.show_tf()
+    # add an axes triad -- note if we do this, we HAVE to do draw
+    # domain, otherwise the image is blank (likely a bug)
+    cam.draw_coordinate_vectors(im)
 
     # add the domain box to the image:
     nim = cam.draw_domain(im)
 
-    # increase the contrast
+    # increase the contrast -- for some reason, the enhance default
+    # to save_annotated doesn't do the trick (likely a bug)
     max_val = im[:,:,:3].std() * 4.0
     nim[:,:,:3] /= max_val
 
-    #nim.write_png("xrb_vol_{:04}.png".format(f))
+    f = pylab.figure()
 
+    pylab.text(0.2, 0.15, "{:.3g} s".format(float(ds.current_time.d)),
+               transform=f.transFigure, color="white")
+
+    cam._render_figure = f
+    
     # save annotated -- this added the transfer function values, 
     # but this messes up our image size defined above
-    cam.save_annotated("xrb_vol_{}_{}.png".format(fname, os.path.basename(plotfile)), nim, dpi=145,
-                       text="{:.3g} s".format(float(pf.current_time.d)), text_x=0.2, text_y=0.85)
+    cam.save_annotated("{}_{}.png".format(os.path.normpath(plotfile), fname), 
+                       nim, 
+                       dpi=145, clear_fig=False)
 
 
 
