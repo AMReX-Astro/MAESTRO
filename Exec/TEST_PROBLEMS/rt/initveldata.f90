f9987c71b76a62760ecc138e5b07a6bf90fbbbfc
Weiqun Zhang <weiqunzhang@lbl.gov>
2014-09-06 21:57:08 -0700

    fixed a mistakenly deleted actual argument to mulitfab_fill_ghost_cells

--- Exec/TEST_PROBLEMS/rt/initveldata.f90
+++ Exec/TEST_PROBLEMS/rt/initveldata.f90
@@ -78,7 +78,8 @@ contains
           ! fill level n ghost cells using interpolation from level n-1 data
           ! note that multifab_fill_boundary and multifab_physbc are called for
           ! both levels n-1 and n
-          call multifab_fill_ghost_cells(u(n),u(n-1),ng,mla%mba%rr(n-1,:))
+          call multifab_fill_ghost_cells(u(n),u(n-1),ng,mla%mba%rr(n-1,:), &
+                                         bc(n-1),bc(n),1,1,dm)
        enddo
        
     end if
