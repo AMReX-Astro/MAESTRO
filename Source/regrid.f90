642fc3aa2f78137cf45e1ba97275204a4dfacff5
Weiqun Zhang <weiqunzhang@lbl.gov>
2014-10-06 17:00:31 -0700

    does not change level 1 layout in regridding

--- Source/regrid.f90
+++ Source/regrid.f90
@@ -144,7 +144,8 @@ contains
     enddo
 
     ! Build the level 1 layout.
-    call layout_build_ba(la_array(1),mba%bas(1),mba%pd(1),pmask)
+    call layout_build_ba(la_array(1),mba%bas(1),mba%pd(1),pmask, &
+         mapping=LA_EXPLICIT, explicit_mapping=get_proc(mla%la(1)))
 
     ! This makes sure the boundary conditions are properly defined everywhere
     call bc_tower_level_build(the_bc_tower,1,la_array(1))
