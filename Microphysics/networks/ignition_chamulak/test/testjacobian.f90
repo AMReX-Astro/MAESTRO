bc3c9841109d4e12b1b5110d3fc13dad6a318900
Michael Zingale <michael.zingale@stonybrook.edu>
2015-01-26 16:51:00 -0500

    update testjacobian so it builds and uses the latest interfaces

--- Microphysics/networks/ignition_chamulak/test/testjacobian.f90
+++ Microphysics/networks/ignition_chamulak/test/testjacobian.f90
@@ -1,4 +1,4 @@
-! a simple code to check the analytic Jacobian via numerical 
+! a simple code to check the analytic Jacobian via numerical
 ! differencing
 
 program testjacobian
@@ -8,8 +8,9 @@ program testjacobian
   use bl_error_module
   use network
   use eos_module
+  use eos_type_module
   use burner_module
-  use burner_aux_module
+  use rpar_indices
 
   implicit none
 
@@ -21,7 +22,7 @@ program testjacobian
   real(kind=dp_t), dimension(nspec_advance+1,nspec_advance+1) :: pd
   real(kind=dp_t) :: enucdot
 
-  real(kind=dp_t) :: rpar
+  real(kind=dp_t), allocatable :: rpar(:)
   integer :: ipar
 
   integer :: ic12, io16, iash
@@ -30,6 +31,8 @@ program testjacobian
   real(kind=dp_t), parameter :: delta = 0.001d0
   real(kind=dp_t) :: num_jac
 
+  type (eos_t) :: eos_state
+  
   call network_init()
   call eos_init()
 
@@ -40,7 +43,7 @@ program testjacobian
   if (ic12 < 0 .or. io16 < 0 .or. iash < 0) then
      call bl_error("ERROR: species index not defined")
   endif
-  
+
   dens = 2.6e9_dp_t
   temp = 7.e8_dp_t
 
@@ -48,22 +51,11 @@ program testjacobian
   Xin(io16) = 0.5_dp_t
   Xin(iash) = 0.0_dp_t
 
+  eos_state%rho = dens
+  eos_state%T = temp
+  eos_state%xn(:) = Xin(:)
 
-  den_eos(1) = dens
-  temp_eos(1) = temp
-  xn_eos(1,:) = Xin(:)
-  
-  call eos(eos_input_rt, den_eos, temp_eos, &
-           npts, &
-           xn_eos, &
-           p_eos, h_eos, e_eos, &
-           cv_eos, cp_eos, xne_eos, eta_eos, pele_eos, &
-           dpdt_eos, dpdr_eos, dedt_eos, dedr_eos, &
-           dpdX_eos, dhdX_eos, &
-           gam1_eos, cs_eos, s_eos, &
-           dsdt_eos, dsdr_eos, &
-           .false.)
-
+  call eos(eos_input_rt, eos_state, .false.)
 
   print *, 'evaluating the RHS...'
 
@@ -72,11 +64,12 @@ program testjacobian
   y(nspec_advance+1) = temp
 
   ! set the burner_aux variables
-  dens_pass = dens
-  c_p_pass = cp_eos(1)
-  dhdx_pass(:) = dhdX_eos(1,:)
-  X_O16_pass = Xin(io16)
+  allocate(rpar(n_rpar_comps))
   
+  rpar(irp_dens) = dens
+  rpar(irp_cp) = eos_state%cp
+  rpar(irp_dhdX:irp_dhdX-1+nspec) = eos_state%dhdX(:)
+  rpar(irp_o16) = Xin(io16)
 
   call f_rhs(nspec_advance+1, ZERO, y, ydot, rpar, ipar)
 
@@ -91,12 +84,12 @@ program testjacobian
 
      yp(j) = (1.d0 + delta)*y(j)
      call f_rhs(nspec_advance+1, ZERO, yp, ydotp, rpar, ipar)
-     
+
      ym(j) = (1.d0 - delta)*y(j)
-     call f_rhs(nspec_advance+1, ZERO, ym, ydotm, rpar, ipar)        
+     call f_rhs(nspec_advance+1, ZERO, ym, ydotm, rpar, ipar)
 
      do i = 1, nspec_advance+1
-        
+
         num_jac = (ydotp(i) - ydotm(i))/(yp(j) - ym(j))
 
         write (*,999) i, j, num_jac, pd(i,j)
