c7f5b65b110a76d5c09e46c61bce3b9b354483e3
Chris Malone <chris.m.malone@gmail.com>
2015-10-19 17:13:21 -0400

    start to mirror Castro eos interface

--- Microphysics/EOS/eos_data.f90
+++ Microphysics/EOS/eos_data.f90
@@ -1,7 +1,5 @@
 module eos_data_module
 
-  use bl_types
-
   implicit none
 
   integer, parameter :: eos_input_rt = 1  ! rho, T are inputs
@@ -11,5 +9,62 @@ module eos_data_module
   integer, parameter :: eos_input_re = 5  ! rho, e are inputs
   integer, parameter :: eos_input_ps = 6  ! p, s are inputs
   integer, parameter :: eos_input_ph = 7  ! p, h are inputs
+  integer, parameter :: eos_input_th = 8  ! T, h are inputs
+
+  integer, parameter :: itemp = 1
+  integer, parameter :: idens = 2
+  integer, parameter :: iener = 3
+  integer, parameter :: ienth = 4
+  integer, parameter :: ientr = 5
+  integer, parameter :: ipres = 6
+
+  integer, parameter :: ierr_general         = 1
+  integer, parameter :: ierr_input           = 2
+  integer, parameter :: ierr_iter_conv       = 3
+  integer, parameter :: ierr_neg_e           = 4
+  integer, parameter :: ierr_neg_p           = 5
+  integer, parameter :: ierr_neg_h           = 6
+  integer, parameter :: ierr_neg_s           = 7
+  integer, parameter :: ierr_iter_var        = 8
+  integer, parameter :: ierr_init            = 9
+  integer, parameter :: ierr_init_xn         = 10
+  integer, parameter :: ierr_out_of_bounds   = 11
+  integer, parameter :: ierr_not_implemented = 12
+
+  ! Minimum and maximum temperature, density, and ye permitted by the EOS.
+
+  double precision :: mintemp = 1.d-200
+  double precision :: maxtemp = 1.d200
+  double precision :: mindens = 1.d-200
+  double precision :: maxdens = 1.d200
+  double precision :: minye   = 1.d-200
+  double precision :: maxye   = 1.d0
+
+  ! Smallest possible temperature and density permitted by the user.
+
+  double precision :: smallt = 1.d-200
+  double precision :: smalld = 1.d-200
+
+  logical :: initialized = .false.
+
+  public eos_get_small_temp, eos_get_small_dens
+
+contains
+
+  subroutine eos_get_small_temp(small_temp_out)
+ 
+    double precision, intent(out) :: small_temp_out
+ 
+    small_temp_out = smallt
+ 
+  end subroutine eos_get_small_temp
+ 
+  subroutine eos_get_small_dens(small_dens_out)
+ 
+    double precision, intent(out) :: small_dens_out
+ 
+    small_dens_out = smalld
+ 
+  end subroutine eos_get_small_dens
 
 end module eos_data_module
