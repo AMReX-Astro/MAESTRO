070b4b5dbc5ab2852294ee126946c046a7423154
Johann Higl <higljoha@mpa-garching.mpg.de>
2016-08-22 17:10:04 +0200

    improved tagging

--- Exec/SCIENCE/msconvect/tag_boxes.f90
+++ Exec/SCIENCE/msconvect/tag_boxes.f90
@@ -15,7 +15,8 @@ contains
   subroutine tag_boxes(mf,tagboxes,dx,lev,aux_tag_mf)
     
     use variables, only: rho_comp
-    
+    use probin_module, only: lo_dens_tag, hi_dens_tag, steep_tag
+                                 
     type( multifab)         , intent(in   ) :: mf
     type(lmultifab)         , intent(inout) :: tagboxes
     real(dp_t)              , intent(in   ) :: dx
@@ -31,18 +32,20 @@ contains
     integer           :: tlo(4), mflo(4)
     type(mfiter)      :: mfi
     type(box)         :: bx
-
+    real(dp_t)        :: lo_tag, hi_tag
    ! if (present(aux_tag_mf)) do nothing. Not implemented
     
     
     
-    !$omp parallel private(mfp,tp,i,lo,hi,mfi,bx,tlo,mflo)
+    !$omp parallel private(mfp,tp,i,lo,hi,lo_tag,hi_tag,mfi,bx,tlo,mflo)
     call mfiter_build(mfi,tagboxes,.true.)
     do while(next_tile(mfi,i))
        bx = get_tilebox(mfi)
        lo =  lwb(bx)
        hi =  upb(bx)
        
+       lo_tag = (1. + (steep_tag ** (lev-1.))) * lo_dens_tag
+       hi_tag = (1. - (steep_tag ** (lev-1.))) * hi_dens_tag
        
        mfp => dataptr(mf, i)
        tp  => dataptr(tagboxes, i)
@@ -55,7 +58,7 @@ contains
        case (2)
           call tag_boxes_2d(tp(:,:,1,1),tlo(1:2),mfp(:,:,1,rho_comp),mflo(1:2),lo,hi,dx,lev)
        case  (3)
-          call tag_boxes_3d(tp(:,:,:,1),tlo(1:3),mfp(:,:,:,rho_comp),mflo(1:3),lo,hi,dx,lev)
+          call tag_boxes_3d(tp(:,:,:,1),tlo(1:3),mfp(:,:,:,rho_comp),mflo(1:3),lo,hi,lo_tag,hi_tag,dx,lev)
        end select
     end do
     !$omp end parallel
@@ -108,16 +111,15 @@ contains
 
   end subroutine tag_boxes_2d
 
-  subroutine tag_boxes_3d(tagbox,tlo,mf,mflo,lo,hi,dx,lev)
+  subroutine tag_boxes_3d(tagbox,tlo,mf,mflo,lo,hi,lo_tag,hi_tag,dx,lev)
 
-    use probin_module, only: lo_dens_tag, hi_dens_tag, steep_tag
-                             
+
     integer          , intent(in   ) :: lo(3),hi(3),tlo(3),mflo(3)
     logical          , intent(inout) :: tagbox( tlo(1):, tlo(2):, tlo(3):)
     real(kind = dp_t), intent(in   ) ::     mf(mflo(1):,mflo(2):,mflo(3):)
     real(dp_t)       , intent(in   ) :: dx
     integer          , intent(in   ) :: lev
-    real(kind = dp_t) :: lo_tag, hi_tag
+    real(kind = dp_t), intent(in   ) :: lo_tag, hi_tag
 
     ! local variables
     integer :: i,j,k
@@ -125,35 +127,16 @@ contains
     ! initially say that we do not want to tag any cells for refinement
     tagbox(lo(1):hi(1),lo(2):hi(2),lo(3):hi(3)) = .false.
 
-    select case(lev)
-    case (1)
-      ! tag all boxes where the density is between lo_dens_tag  and hi_dens_tag
-	do k = lo(3),hi(3)
-	    do j = lo(2),hi(2)
-	      do i = lo(1),hi(1)
-		if (mf(i,j,k) .gt. lo_dens_tag .and. mf(i,j,k) .lt. hi_dens_tag) then
-		    tagbox(i,j,k) = .true.
-		  end if
-	      end do
-	    end do
+    do k = lo(3),hi(3)
+      do j = lo(2),hi(2)
+	do i = lo(1),hi(1)
+	  if (mf(i,j,k) .gt. lo_tag .and. mf(i,j,k) .lt. hi_tag) then
+	  tagbox(i,j,k) = .true.
+	  end if
 	end do
-    case default
-       ! tag all boxes where the density is between lo_dens_tag * ((1+steep_tag) ** lev) and hi_dens_tag * ((1-steep_tag) ** lev)
-      lo_tag = (1. + (steep_tag ** lev)) * lo_dens_tag
-      hi_tag = (1. - (steep_tag ** lev)) * hi_dens_tag
-      if (lo_tag .lt. hi_tag) then 
-	do k = lo(3),hi(3)
-	    do j = lo(2),hi(2)
-	      do i = lo(1),hi(1)
-		  if (mf(i,j,k) .gt. lo_tag .and. mf(i,j,k) .lt. hi_tag) then
-		    tagbox(i,j,k) = .true.
-		  end if
-	      end do
-	    enddo
-	end do
-      endif
-    end select
-
+      end do
+    end do
+    
   end subroutine tag_boxes_3d
 
 end module tag_boxes_module
