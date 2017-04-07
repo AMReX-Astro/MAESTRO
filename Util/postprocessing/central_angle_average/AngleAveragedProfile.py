a6fff6b294a59d230f0d70e2f30724ccd1369b1d
dwillcox <eugene.willcox@gmail.com>
2017-04-07 18:37:25 -0400

    Fixed some merge issues with the plotting scripts.

--- Util/postprocessing/central_angle_average/AngleAveragedProfile.py
+++ Util/postprocessing/central_angle_average/AngleAveragedProfile.py
@@ -51,7 +51,13 @@ class AngleAveragedProfile(object):
                     if '=' in ls:
                         k, v = ls[2:].split('=', 1)
                         k = k.strip()
-                        v = [float(vf) for vf in v.strip().split()]
+                        vv = []
+                        for vf in v.strip().split():
+                            try:
+                                vv.append(float(vf))
+                            except:
+                                vv.append(0.0)
+                        v = vv
                         if len(v) > 1:
                             v = np.array(v)
                         else:
@@ -83,7 +89,7 @@ class AngleAveragedProfile(object):
                 n += 1
                 yield float(10*n)
 
-    def plot_var(self, var, fmt='png', rup=None):
+    def plot_var(self, var, fmt='png', rup=None, fmin=None, fmax=None, sep_log=None):
         # Plot the variable corresponding to the data key var
         # Independent axis is radius r
         # Plots are log scale on the dependent axis
@@ -118,7 +124,7 @@ class AngleAveragedProfile(object):
             else:
                 lwb = None
         upb = np.log10(np.amax(np.absolute(self.data[var][:idxup])))
-        if (not lwb) or upb-lwb <= 1.0:
+        if ((not lwb) or upb-lwb <= 1.0) and not sep_log:
             # plot quantity on linear axis
             # plot linear scale magnitudes
             ax.plot(self.data['r'][:idxup], self.data[var][:idxup], color='green')
@@ -128,7 +134,9 @@ class AngleAveragedProfile(object):
             # plot negative points in red
             ax.plot(self.data['r'][:idxup][neg_idx], self.data[var][:idxup][neg_idx],
                     linestyle='None', marker='v', color='red', markersize=8, alpha=0.5)
-            ax.set_ylabel('$\mathrm{' + var.replace('_','\_') + '}$')                        
+            ax.set_ylabel('$\mathrm{' + var.replace('_','\_') + '}$')
+            if fmin and fmax:
+                ax.set_ylim((fmin, fmax))
         else:
             # plot quantity on log10 axis
             # plot log scale magnitudes
@@ -140,16 +148,21 @@ class AngleAveragedProfile(object):
             ax.plot(self.data['r'][:idxup][neg_idx], np.log10(np.absolute(self.data[var][:idxup][neg_idx])),
                     linestyle='None', marker='v', color='red', markersize=8, alpha=0.5)
             ax.set_ylabel('$\mathrm{Log_{10} \ | ' + var.replace('_','\_') + ' |}$')
+            if fmin:
+                lwb = np.log10(fmin)
+            if fmax:
+                upb = np.log10(fmax)
+            lwb = max(lwb, upb-10) # Span at most 10 decades
             upb = np.ceil(upb*10.0)/10.0
             lwb = np.floor(lwb*10.0)/10.0
-            yticks = None
-            for tspac in self.gen_tick_spacing():
-                nticks = int(np.floor((upb-lwb)/tspac) + 1)
-                eps = upb - (lwb + tspac*(nticks-2))
-                if nticks <= 10 and eps > 0.5*tspac:
-                    yticks = np.array([lwb + tspac*(j) for j in range(nticks-1)] + [upb])
-                    break
-            ax.set_yticks(yticks)
+            # yticks = None
+            # for tspac in self.gen_tick_spacing():
+            #     nticks = int(np.floor((upb-lwb)/tspac) + 1)
+            #     eps = upb - (lwb + tspac*(nticks-2))
+            #     if nticks <= 10 and eps > 0.5*tspac:
+            #         yticks = np.array([lwb + tspac*(j) for j in range(nticks-1)] + [upb])
+            #         break
+            # ax.set_yticks(yticks)
             ax.set_ylim((lwb, upb))
         # List the time above the plot
         tart = ax.text(1.0, 1.01, 'time = {}'.format(self.header['time']),
@@ -163,9 +176,22 @@ class AngleAveragedProfile(object):
             plt.savefig(outname, bbox_extra_artists=(tart,))
         plt.close(fig)
             
-    def plot_all_vars(self, fmt='png', rup=None):
+    def plot_all_vars(self, fmt='png', rup=None,
+                      field_mins={}, field_maxs={}, field_seps_type={}):
         # Plot all variables in the profile and save
         # fmt is the suffix passed to savefig
         for var in self.data.keys():
             if var != 'r':
-                self.plot_var(var, fmt, rup)
+                if var in field_mins:
+                    fmin = field_mins[var]
+                else:
+                    fmin = None
+                if var in field_maxs:
+                    fmax = field_maxs[var]
+                else:
+                    fmax = None
+                if var in field_seps_type:
+                    sep_log = field_seps_type[var]
+                else:
+                    sep_log = None
+                self.plot_var(var, fmt, rup, fmin, fmax, sep_log)
