87855cfa7b0cc7ecf1911d8cd70ffea9cfc5913a
dwillcox <eugene.willcox@gmail.com>
2017-11-22 17:28:59 -0500

    Fixed 2d tagging compilation.

--- Exec/SCIENCE/urca/wdconvect_urca-23/tag_boxes.f90
+++ Exec/SCIENCE/urca/wdconvect_urca-23/tag_boxes.f90
@@ -44,7 +44,7 @@ contains
 
        select case (get_dim(mf))
        case (2)
-          call tag_boxes_2d(tp(:,:,1,1),sp(:,:,1,rho_comp),lo,ng,lev)
+          call tag_boxes_2d(tp(:,:,1,1),sp(:,:,:,:),lo,ng,lev)
        case  (3)
           call tag_boxes_3d(tp(:,:,:,1),sp(:,:,:,:),lo,ng,lev)
        end select
@@ -62,15 +62,16 @@ contains
 
     integer          , intent(in   ) :: lo(:),ng
     logical          , intent(  out) :: tagbox(lo(1):,lo(2):)
-    real(kind = dp_t), intent(in   ) :: mf(lo(1)-ng:,lo(2)-ng:)
+    real(kind = dp_t), intent(in   ) :: mf(lo(1)-ng:,lo(2)-ng:,lo(3)-ng:,:)
     integer, optional, intent(in   ) :: lev
-    integer :: i,j,nx,ny,llev
+    integer :: i,j,k,nx,ny,llev
 
     real(kind = dp_t) :: ye, rhoye
 
     llev = 1; if (present(lev)) llev = lev
     nx = size(tagbox,dim=1)
     ny = size(tagbox,dim=2)
+    k  = lo(3)-ng
 
     tagbox = .false.
 
@@ -78,7 +79,7 @@ contains
     case (1)
        do j = lo(2),lo(2)+ny-1
           do i = lo(1),lo(1)+nx-1
-             if (mf(i,j) .gt. tag_density_1) then
+             if (mf(i,j,k,rho_comp) .gt. tag_density_1) then
                 tagbox(i,j) = .true.
              end if
           end do
@@ -86,8 +87,8 @@ contains
     case (2)
        do j = lo(2),lo(2)+ny-1
           do i = lo(1),lo(1)+nx-1
-             call get_electron_fraction(ye, mf(i, j, spec_comp:spec_comp + nspec - 1))
-             rhoye = mf(i, j, rho_comp) * ye
+             call get_electron_fraction(ye, mf(i, j, k, spec_comp:spec_comp + nspec - 1))
+             rhoye = mf(i, j, k, rho_comp) * ye
              if (rhoye .gt. tag_rhoye_lo_2) then
                 tagbox(i,j) = .true.
              end if
@@ -96,8 +97,8 @@ contains
     case (3)
        do j = lo(2),lo(2)+ny-1
           do i = lo(1),lo(1)+nx-1
-             call get_electron_fraction(ye, mf(i, j, spec_comp:spec_comp + nspec - 1))
-             rhoye = mf(i, j, rho_comp) * ye
+             call get_electron_fraction(ye, mf(i, j, k, spec_comp:spec_comp + nspec - 1))
+             rhoye = mf(i, j, k, rho_comp) * ye
              if (rhoye .gt. tag_rhoye_lo_3) then
                 tagbox(i,j) = .true.
              end if
