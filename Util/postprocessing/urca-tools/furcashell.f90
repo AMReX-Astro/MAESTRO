b1da253cff9659f0aedd10c9a57e8ae8986d2aaf
dwillcox <eugene.willcox@gmail.com>
2018-07-03 20:52:07 -0400

    Add average neutrino energy fields to furcashell output.

--- Util/postprocessing/urca-tools/furcashell.f90
+++ Util/postprocessing/urca-tools/furcashell.f90
@@ -1,7 +1,8 @@
 program furcashell
 
   use network
-  use table_rates, only: j_na23_ne23, j_ne23_na23
+  use table_rates, only: j_na23_ne23, j_ne23_na23, get_entries, &
+                         table_meta, jtab_rate, jtab_nuloss
   use actual_rhs_module, only: rate_eval_t, evaluate_rates, rhs_nuc, actual_rhs
   use physical_constants, only: N_AVO
   use sneut_module, only: sneut5
@@ -30,7 +31,7 @@ program furcashell
   character(len=256) :: fname
 
   ! local variables
-  integer :: uin, dim, nlevs, i, j, ii, jj, kk
+  integer :: uin, dim, nlevs, i, j, ii, jj, kk, itab
   type(plotfile) :: pf
   type(layout) :: la
   type(boxarray) :: ba
@@ -46,8 +47,9 @@ program furcashell
   type (eos_t)  :: eos_state
   type (rate_eval_t) :: rate_state
   double precision :: y_nuc(nspec), ydot_nuc(nspec), enuc, enucion, enucdiff
+  double precision :: table_entries(7) ! Hard-coded for Urca tabulated rates
 
-  integer, parameter :: size_rate_eval = 12
+  integer, parameter :: size_rate_eval = 14
 
   ! For sneut5
   double precision :: sneut, dsneutdt, dsneutdd, snuda, snudz
@@ -121,6 +123,8 @@ program furcashell
   plot_names(10) = "ionenuc"
   plot_names(11) = "sneut"
   plot_names(12) = "enucrdiff"
+  plot_names(13) = "enu_ecap23"
+  plot_names(14) = "enu_beta23"
 
   dens_comp = plotfile_var_index(pf,"density")
   if (use_tfromp) then
@@ -165,6 +169,7 @@ program furcashell
         !$OMP PARALLEL DO PRIVATE(kk, jj, ii, eos_state, burn_state, rate_state) &
         !$OMP PRIVATE(enucion, enuc, enucdiff, y_nuc, ydot_nuc) &
         !$OMP PRIVATE(sneut, dsneutdt, dsneutdd, snuda, snudz) &
+        !$OMP PRIVATE(itab, table_entries) &
         !$OMP SCHEDULE(DYNAMIC,1)
         do kk = lo(3), hi(3)
            do jj = lo(2), hi(2)
@@ -186,10 +191,10 @@ program furcashell
                  enuc = enucion
                  
                  ! Electron capture rate (A=23)
-                 r(ii,jj,kk,1) = rate_state % screened_rates(k_na23_ne23)
+                 r(ii,jj,kk,1) = rate_state % screened_rates(k_na23__ne23)
 
                  ! Beta decay rate (A=23)
-                 r(ii,jj,kk,2) = rate_state % screened_rates(k_ne23_na23)
+                 r(ii,jj,kk,2) = rate_state % screened_rates(k_ne23__na23)
 
                  ! Particle energy from electron capture (A=23) (erg/g/s)
                  r(ii,jj,kk,3) = rate_state % epart(j_na23_ne23) * y_nuc(jna23) * N_AVO
@@ -230,6 +235,18 @@ program furcashell
 
                  enucdiff = burn_state % ydot(net_ienuc) - enuc
                  r(ii,jj,kk,12) = abs(enucdiff/burn_state % ydot(net_ienuc))
+
+                 ! Average neutrino energy for electron capture (A=23)
+                 call get_entries(table_meta(j_na23_ne23), &
+                                  burn_state % rho * burn_state % y_e, &
+                                  burn_state % T, table_entries)
+                 r(ii,jj,kk,13) = table_entries(jtab_nuloss)/table_entries(jtab_rate)
+                 
+                 ! Average neutrino energy for beta decay (A=23)
+                 call get_entries(table_meta(j_ne23_na23), &
+                                  burn_state % rho * burn_state % y_e, &
+                                  burn_state % T, table_entries)
+                 r(ii,jj,kk,14) = table_entries(jtab_nuloss)/table_entries(jtab_rate)
 
               end do
            end do
